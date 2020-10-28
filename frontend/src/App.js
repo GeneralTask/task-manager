115e40e3f8609dacd855b63c9ac534c781cb7345
yohanstra <jreinstra@gmail.com>
2020-10-28 09:51:48 -0700

    Basic interactivity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {title: props.title, description: props.description};
+  }
+
+  handleDone() {
+    this.setState({title: "Oopsie done"});
+  }
+
+  handleNoTime() {
+    this.setState({title: "Oopsie no time"});
+  }
+
+  render() {
+    return (
+      <div className="Task">
+        <h2>{this.state.title}</h2>
+        <span>{this.state.description}</span><br/><br/>
+        <button onClick={() => this.handleDone()}>Done</button> <button onClick={() => this.handleNoTime()}>I don't have time</button>
+      </div>
+    );
+  }
+}
+
 function App() {
+  var title = "This is a task 2";
+  var description = "This is a description 3";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Task title={title} description={description}></Task>
     </div>
   );
 }
