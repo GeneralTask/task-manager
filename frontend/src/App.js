209406524ef479e752dd2e2dd888b8d405a701ed
Nolan Jimenez <nolan1299@gmail.com>
2021-02-21 23:00:29 -0800

    added enforced routes for landing page/task page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,57 +1,54 @@
-import React, { useState } from 'react';
-
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Cookies from 'js-cookie'
 
-function Task(props) {
-  const [title, setTitle] = useState(props.title);
-  const [description, setDescription] = useState(props.description);
-
-  function handleDone() {
-    setTitle("Oopsie done");
-    props.onDone();
-  }
-
-  function handleNoTime() {
-    setTitle("Oopsie no time");
-  }
+function App() {
+  useEffect(() => {
+    // Cookies.set('authToken', 'dummyToken')  // Remove this
+  }, []);
 
   return (
-    <div className="Task" style={{border: "1px solid black", width: "200px"}}>
-      <h2>{title}</h2>
-      <span>{description}</span><br/><br/>
-      <button onClick={() => handleDone()}>Done</button> <button onClick={() => handleNoTime()}>I don't have time</button>
+    <div className="App">
+      <Router>
+        <Switch>
+          <PrivateRoute path="/tasks" component={Tasks}/>
+          <Route path="/" component={Home}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
-function App() {
-  const initialTasks = [
-    {
-      id: 1,
-      title: "This is an important task",
-      description: "Do it now or else",
-    },
-    {
-      id: 2,
-      title: "This is less important task",
-      description: "Do it maybe",
-    }
-  ];
-  const [tasks, setTasks] = useState(initialTasks);
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    Cookies.get('authToken')
+      ? <Component {...props} />
+      : <Redirect to='/' />
+  )} />
+)
 
-  function handleDone(task_id) {
-    const newTasks = tasks.splice(task_id, 1)
-    setTasks(newTasks);
-    console.log(newTasks, task_id);
+function Home() {
+  if (Cookies.get('authToken')) {
+    return <Redirect to='/tasks' />
   }
-
   return (
-    <div className="App">
-      {tasks.map((task, task_index) =>
-        <Task title={task.title} description={task.description} onDone={() => handleDone(task_index)}></Task>
-      )}
+    <div id="home">
+      <h1>General Task</h1>
+      <h2>Welcome to the landing page!</h2>
+      <Link to="/tasks"> See Tasks </Link>
     </div>
   );
+}
+
+function Tasks() {
+  return <h2>Tasks</h2>;
 }
 
 export default App;
