7cece091b82f7122ef597888b6fb13873584034d
yohanstra <jreinstra@gmail.com>
2021-02-22 20:12:12 -0500

    First pass at terraform files

---
+++ infrastructure/kubernetes.tf
@@ -0,0 +1,126 @@
+# # Kubernetes provider
+# # https://learn.hashicorp.com/terraform/kubernetes/provision-eks-cluster#optional-configure-terraform-kubernetes-provider
+# # To learn how to schedule deployments and services using the provider, go here: ttps://learn.hashicorp.com/terraform/kubernetes/deploy-nginx-kubernetes.
+
+terraform {
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+
+  backend "remote" {
+    organization = "generaltask"
+
+    workspaces {
+      name = "task-manager"
+    }
+  }
+}
+
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+resource "kubernetes_deployment" "backend" {
+  metadata {
+    name = "backend"
+    labels = {
+      App = "Backend"
+    }
+  }
+
+  spec {
+    replicas = 10
+    selector {
+      match_labels = {
+        App = "Backend"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "Backend"
+        }
+      }
+      spec {
+        container {
+          image = "generaltask/task-manager:86d8026a"
+          name  = "task-manager"
+
+          port {
+            container_port = 8080
+          }
+        }
+
+        image_pull_secrets {
+          name = "docker-config"
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress" "backend_ingress" {
+  metadata {
+    name = "backend-ingress"
+
+    annotations = {
+      "ingress.kubernetes.io/rewrite-target" = "/"
+    }
+  }
+
+  spec {
+    backend {
+      service_name = "backend"
+      service_port = 443
+    }
+
+    rule {
+      host = "api.generaltask.io"
+
+      http {
+        path {
+          path = "/"
+
+          backend {
+            service_name = "backend"
+            service_port = 443
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "backend" {
+  metadata {
+    name = "backend"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.backend.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 443
+      target_port = 5000
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+resource "kubernetes_secret" "docker_config" {
+  metadata {
+    name = "docker-config"
+  }
+
+  data = {
+    ".dockerconfigjson" = var.docker_config
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+}
---
+++ infrastructure/security-groups.tf
@@ -0,0 +1,32 @@
+
+resource "aws_security_group" "backend_servers" {
+  name_prefix = "backend_servers"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+    ]
+  }
+}
+
+resource "aws_security_group" "all_worker_mgmt" {
+  name_prefix = "all_worker_management"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "10.0.0.0/8",
+      "172.16.0.0/12",
+      "192.168.0.0/16",
+    ]
+  }
+}
