726f646cf0202432687177b9663f29763e98582b
Scott Mai <scottmai@usc.edu>
2021-03-02 15:48:28 -0800

    removed unused settings accounts except jira

--- frontend/src/components/settings/Settings.js
+++ frontend/src/components/settings/Settings.js
@@ -11,13 +11,7 @@ function Settings(){
             </div>
             <div className="settings-body">
                 <h2>Accounts</h2>
-                {/* will need real links for most of these */}
-                <Account logo="images/google.svg" name="Google" link="https://google.com" />
                 <Account logo="images/jira.svg" name="Jira" link={JIRAURL} />
-                <Account logo="images/outlook-logo.png" name="Outlook" link="https://outlook.com" />
-                <Account logo="images/slack-logo-icon.png" name="Slack" link="https://slack.com" />
-                <Account logo="images/asana-logo.png" name="Asana" link="https://asana.com" />
-                <Account logo="images/zoom.svg" name="Zoom" link="https://zoom.us" />
             </div>
         </div>
     )
