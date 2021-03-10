8fbe64cacc83830e5cfd46867d5bf1cd67d8caca
yohanstra <jreinstra@gmail.com>
2021-03-09 23:37:49 -0800

    Fix instance type

--- infrastructure/eks-cluster.tf
+++ infrastructure/eks-cluster.tf
@@ -15,7 +15,7 @@ module "eks" {
   worker_groups = [
     {
       name                          = "backend-servers"
-      instance_type                 = "t4g.nano"
+      instance_type                 = "t2.micro"
       asg_desired_capacity          = 2
       asg_min_size                  = 2
       asg_max_size                  = 4
