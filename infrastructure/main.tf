c6f5dbc43206ca8b3ca2828fab76e784a67cdef9
John Reinstra <jreinstra@gmail.com>
2021-03-09 22:51:51 -0800

    Update TF files with fixes / refactoring (#19)
    * save
    
    * save
    
    * try again

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,10 +1,21 @@
 terraform {
+  required_version = ">= 0.12"
+  
   required_providers {
+    aws = {
+      version = "~> 3.25"
+    }
     kubernetes = {
       source = "hashicorp/kubernetes"
     }
     mongodbatlas = {
       source = "mongodb/mongodbatlas"
+    }
+    random = {
+      version = "~> 2.1"
+    }
+    local = {
+      version = "~> 1.2"
     }
   }
 
@@ -17,9 +28,21 @@ terraform {
   }
 }
 
+provider "aws" {
+  region  = var.region
+}
+
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
 }
