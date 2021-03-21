c0550cc03f5e82c2384f0a6ab368139582cf5584
Scott Mai <scottmai@usc.edu>
2021-03-20 19:53:25 -0700

    tasks are clickable

--- frontend/src/components/task/TaskHeader.js
+++ frontend/src/components/task/TaskHeader.js
@@ -4,10 +4,10 @@ import './Task.css'
 export default function TaskHeader(props){
     return(
         <div className="task-header" >
-            <div className="task-header-side">
+            <div className="task-header-side flex-expand">
                 <img className="domino" src="images/domino.svg" alt="" />
                 <img className="task-icon" src={props.icon_url} alt="icon"></img>
-                <div>{props.title}</div>
+                <div className="task-header-text">{props.title}</div>
             </div>
             <div className="task-header-side">
                 <div className="task-header-source">
