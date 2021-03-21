4eb601813f623db3dafe25a563e4bf76050e4a7e
yohanstra <jreinstra@gmail.com>
2021-03-20 19:10:29 -1000

    Increase instance size to t2 small and tag docker images with commit sha

--- infrastructure/eks-cluster.tf
+++ infrastructure/eks-cluster.tf
@@ -15,7 +15,7 @@ module "eks" {
   worker_groups = [
     {
       name                          = "backend-servers"
-      instance_type                 = "t2.micro"
+      instance_type                 = "t2.small"
       asg_desired_capacity          = 2
       asg_min_size                  = 2
       asg_max_size                  = 4
