f9f318f66fc4f73989a01133a4cbb6ec1f0bf482
Scott Mai <scottmai@usc.edu>
2021-03-20 20:12:38 -0700

    Frontend Deeplink Support (#35)
    * tasks are clickable

--- frontend/src/components/task/Task.js
+++ frontend/src/components/task/Task.js
@@ -4,12 +4,13 @@ import TaskHeader from './TaskHeader'
 
 export default function Task(props){
     return(
-        <div className="task-outer-div">
-            <TaskHeader 
+        <button className={"task-container " + (props.task.deeplink ? "deeplink" : "")}
+            onClick={() => { if (props.task.deeplink) { window.open(props.task.deeplink) } }} >
+            <TaskHeader
                 title={props.task.title} 
                 icon_url={props.task.logo_url} 
                 sender={props.task.sender} 
             />
-        </div>
+        </button>
     )
 }
