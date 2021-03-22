43867d454df75cfeac5f120a144e80e98cbe9486
yohanstra <jreinstra@gmail.com>
2021-03-21 17:59:41 -1000

    Fix newer privlink resource

--- infrastructure/mongo.tf
+++ infrastructure/mongo.tf
@@ -82,15 +82,15 @@ resource "mongodbatlas_privatelink_endpoint" "atlaspl" {
 
 resource "aws_vpc_endpoint" "ptfe_service" {
   vpc_id             = module.vpc.vpc_id
-  service_name       = mongodbatlas_private_endpoint.atlaspl.endpoint_service_name
+  service_name       = mongodbatlas_privatelink_endpoint.atlaspl.endpoint_service_name
   vpc_endpoint_type  = "Interface"
   subnet_ids         = [aws_subnet.primary-az1.id, aws_subnet.primary-az2.id]
   security_group_ids = [aws_security_group.all_worker_mgmt.id]
 }
 
 resource "mongodbatlas_privatelink_endpoint_service" "atlaseplink" {
-  project_id            = mongodbatlas_private_endpoint.atlaspl.project_id
-  private_link_id       = mongodbatlas_private_endpoint.atlaspl.private_link_id
+  project_id            = mongodbatlas_privatelink_endpoint.atlaspl.project_id
+  private_link_id       = mongodbatlas_privatelink_endpoint.atlaspl.private_link_id
   endpoint_service_id = aws_vpc_endpoint.ptfe_service.id
   provider_name = var.provider_name
 }
