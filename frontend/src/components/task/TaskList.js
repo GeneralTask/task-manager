33559d2c3cb86a1f5895472394621be9ab60210e
Nolan Jimenez <nolan1299@gmail.com>
2021-04-11 19:55:48 -0700

    showing all tasks in single container

--- frontend/src/components/task/TaskList.js
+++ frontend/src/components/task/TaskList.js
@@ -1,10 +1,66 @@
-import {React, useEffect} from 'react'
+import React, { useEffect} from 'react'
 import { connect, useSelector } from 'react-redux'
 import store from '../../redux/store'
 import {setTasks} from '../../redux/actions'
 import { TASKS_URL, REACT_APP_FRONTEND_BASE_URL, TASK_GROUP_SCHEDULED_TASK, TASK_GROUP_UNSCHEDULED_GROUP } from '../../constants'
 import Cookies from 'js-cookie';
 import {ScheduledTask, UnscheduledTaskGroup} from './TaskWrappers'
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+import { useState } from "react";
+//Remove Start
+const grid = 8;
+
+const initial = Array.from({ length: 10 }, (v, k) => k).map(k => {
+    const custom = {
+      id: `id-${k}`,
+      content: `Quote ${k}`
+    };
+  
+    return custom;
+  });
+
+
+
+function Quote({ quote, index }) {
+  return (
+    <Draggable draggableId={quote.id} index={index}>
+      {provided => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          {quote.content}
+        </div>
+      )}
+    </Draggable>
+  );
+}
+
+function TempTask({ task, index }) {
+    return (
+      <Draggable draggableId={task.id} index={index}>
+        {provided => (
+          <div
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            {task.title}
+          </div>
+        )}
+      </Draggable>
+    );
+  }
+
+const NewTaskList = React.memo(function NewTaskList({ tasks }) {
+    return tasks.map((task, index) => (
+      <TempTask task={task} index={index} key={task.id} />
+    ));
+  });
+  
+//Remove End
 
 function fetchTasks(){
     fetch(TASKS_URL, {
@@ -38,6 +94,9 @@ function TaskList(){
         fetchTasks();
     }, [])
 
+    const [state, setState] = useState({ quotes: initial });
+
+
     const tasks = useSelector(state => state.tasks);
     const task_groups = useSelector(state => state.task_groups);
     const id_to_index = new Map(tasks.map((task, index) => [task.id, index]));
@@ -54,6 +113,16 @@ function TaskList(){
             return <UnscheduledTaskGroup tasks={tasksSplice} key={index} time_duration={taskGroup.time_duration} />
         }
     }
+
+    function getGroupTasks(groupIndex) {
+        let group = task_groups[groupIndex]
+        return group.task_ids.map(taskId => tasks[id_to_index.get(taskId)]);
+    }
+
+    function onDragEnd(result) {
+        // TO-DO
+        console.log(tasks)
+    }
     
     return (
         <div>
@@ -62,7 +131,23 @@ function TaskList(){
             {tasks.length === 0  && 
                 <h2 className="spacer40">No Tasks :(</h2>
             }
-            { task_groups.map(renderTaskGroup) }
+            <DragDropContext>
+                { 
+                    task_groups.map((group, index) =>
+                        <div>
+                            <Droppable droppableId={`list-${index}`}>
+                                {provided => (
+                                    <div ref={provided.innerRef} {...provided.droppableProps}>
+                                        <NewTaskList tasks={getGroupTasks(index)} />
+                                        {provided.placeholder}
+                                    </div>
+                                )}
+                            </Droppable>
+                            End
+                        </div>
+                    )
+                }
+            </DragDropContext>
         </div>
     );
 }
