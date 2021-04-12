819f6d2d0bd45f9d43167b96adb4d8ee5b6be173
Scott Mai <scottmai@usc.edu>
2021-04-12 01:24:37 -0700

    put button back

--- frontend/src/components/task/Task.js
+++ frontend/src/components/task/Task.js
@@ -4,13 +4,13 @@ import TaskHeader from './TaskHeader'
 
 export default function Task(props){
     return(
-        <div className={"task-container " + (props.task.deeplink ? "deeplink" : "")}
+        <button className={"task-container " + (props.task.deeplink ? "deeplink" : "")}
             onClick={() => { if (props.task.deeplink) { window.open(props.task.deeplink) } }} >
             <TaskHeader
                 title={props.task.title} 
                 icon_url={props.task.logo_url} 
                 sender={props.task.sender} 
             />
-        </div>
+        </button>
     )
 }
