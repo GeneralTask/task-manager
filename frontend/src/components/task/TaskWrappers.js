a1150cd04584acb48756a7632e52d9e9b98efb05
Scott Mai <scottmai@usc.edu>
2021-04-13 11:49:50 -0700

    First task group time estimation (#59)

--- frontend/src/components/task/TaskWrappers.js
+++ frontend/src/components/task/TaskWrappers.js
@@ -1,4 +1,4 @@
-import { React } from 'react'
+import { React, useState, useEffect } from 'react'
 import Task from './Task'
 const moment = require('moment');
 
@@ -10,7 +10,7 @@ function ScheduledTask(props) {
                 <Task {...props} />
             </div>
             <div className="task-time-annotation">
-                {props.time_duration}
+                <TimeDuration time_duration={props.time_duration} next_time={props.next_time}/>
             </div>
         </div>
     )
@@ -27,27 +27,51 @@ function UnscheduledTaskGroup(props) {
             </div>
             <div className="task-time-annotation unscheduled-time-annotation-container">
                 <div className="unscheduled-spanbar"></div>
-                <div className="unscheduled-time-annotation">{props.time_duration}</div>
+                <div className="unscheduled-time-spacer"></div>
+                <TimeDuration time_duration={props.time_duration} next_time={props.next_time}/>
             </div>
         </div>
     )
 }
 
-function getTimeStr(datetime1, datetime2){
+function TimeDuration(props) {
+    let initialTimeStr = props.time_duration;
+    if(props.next_time){
+        initialTimeStr = getTimeStr(moment(), props.next_time);
+    }
+    const [timeStr, setTimeStr] = useState(initialTimeStr);
+    useEffect(() => {
+        let timer;
+        if(props.next_time){
+            timer = setInterval(()=>{
+                const time = getTimeStr(moment(), props.next_time);
+                setTimeStr(time);
+            }, 1000);
+        }
+        return () => {
+            if(timer) clearInterval();
+        }
+    }, [props.next_time])
+    return(
+        <div>{timeStr}</div>
+    )
+}
+
+// accepts two moment objects
+function getTimeStr(start, end){
     let timeStr = "";
-    if (datetime1 && datetime2) {
-        const start = moment(datetime1);
-        const end = moment(datetime2);
+    if (start && end) {
         const diff = moment.duration(end.diff(start));
-        const hours = diff.asHours();
-        const minutes = (hours % 1) * 60;
+        let hours = diff.asHours();
+        const minutes = Math.floor((hours % 1) * 60);
+        hours = Math.floor(hours);
         if (hours >= 1) {
-            const justHours = Math.floor(hours);
-            if (justHours > 1) {
-                timeStr += justHours + " hours ";
+            hours = Math.floor(hours);
+            if (hours > 1) {
+                timeStr += hours + " hours ";
             }
             else {
-                timeStr += justHours + " hour ";
+                timeStr += hours + " hour ";
             }
         }
         if (minutes > 0) {
@@ -57,6 +81,9 @@ function getTimeStr(datetime1, datetime2){
             else {
                 timeStr += minutes + " min ";
             }
+        }
+        if (hours === 0 && minutes < 1){
+            timeStr = "<1 min";
         }
     }
     return timeStr;
