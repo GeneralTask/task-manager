e0a684225f476ee685d518cbbfd761da7da4c166
Scott Mai <scottmai@usc.edu>
2021-04-17 11:06:04 -0700

    changed login link

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,7 +1,6 @@
-import { Link } from "react-router-dom";
-
 import TaskList from "./task/TaskList"
 import GLButton from "./login/GoogleLogin";
+import { LOGIN_URL } from '../constants'
 import Cookies from 'js-cookie'
 
 function LandingPage() {
@@ -12,7 +11,7 @@ function LandingPage() {
       <div id="home">
         <h1>General Task</h1>
         <h2>Welcome to the landing page!</h2>
-        <Link to="/login"><GLButton/></Link>
+        <a href={LOGIN_URL}><GLButton/></a>
       </div>
     )
 }
