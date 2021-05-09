8ba4ed6716eae301e80b78a9c6cca4149158cfcf
Scott Mai <scottmai702@gmail.com>
2021-05-09 18:38:07 -0400

    routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom"
 import Cookies from 'js-cookie'
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { LANDING_PATH, SETTINGS_PATH } from './constants'
+import { LANDING_PATH, SETTINGS_PATH, PRIVACY_PATH } from './constants'
 import {useEffect} from 'react'
 
 import Header from "./components/Header"
 import Settings from "./components/settings/Settings"
 import LandingPage from "./components/LandingPage"
+import PrivacyPolicy from "./components/PrivacyPolicy"
 
 function App() {
   useEffect(() => {
@@ -20,7 +21,8 @@ function App() {
       { Cookies.get('authToken') ? <Header/> : null }
         <Switch>
           {/* Settings page, only accessible if logged in */}
-          <PrivateRoute path={SETTINGS_PATH} component={Settings}/> 
+          <PrivateRoute path={SETTINGS_PATH} component={Settings}/>
+	  <Route path={PRIVACY_PATH} component={PrivacyPolicy}/>
 
           {/* MAKE SURE THIS IS THE LAST ROUTE */}
           {/* base url, shows landing page if not logged in, shows tasks page if logged in */}
---
+++ frontend/src/components/PrivacyPolicy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PrivacyPolicy(){
+    return(
+	<div>
+	    We promise not to steal your information.
+	</div>
+    );
+}
