58ccea085bd899107e391af43a4d15c140216909
Scott Mai <scottmai702@gmail.com>
2021-05-21 18:18:29 -0700

    one down, n to go

--- frontend/src/components/settings/Account.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-declare type Props = {
-    name: string;
-    logo: string;
-    link: string;
-};
-declare const Account: ({ name, logo, link }: Props) => JSX.Element;
-export default Account;
