4114075181032e5ccecf1592b0a88c239b0613ee
Jason Scharff <jasonscharff@gmail.com>
2021-05-22 23:50:17 -0700

    cleanup

--- backend/api/main_test.go
+++ backend/api/main_test.go
@@ -1,17 +1,19 @@
 package api
 
 import (
+	"github.com/GeneralTask/task-manager/backend/database"
+	"log"
 	"os"
 	"testing"
 )
 
 func TestMain(m *testing.M) {
-	//db, dbCleanup := database.GetDBConnection()
-	//defer dbCleanup()
-	//log.Println("Dropping test DB now.")
-	//err := db.Drop(context.TODO())
-	//if err != nil {
-	//	log.Fatalf("Failed to wipe test DB: %v", err)
-	//}
+	db, dbCleanup := database.GetDBConnection()
+	defer dbCleanup()
+	log.Println("Dropping test DB now.")
+	err := db.Drop(context.TODO())
+	if err != nil {
+		log.Fatalf("Failed to wipe test DB: %v", err)
+	}
 	os.Exit(m.Run())
 }
