119144af1720e8ae24a61eb08a1c8461349efb21
Scott Mai <scottmai702@gmail.com>
2021-05-23 14:41:04 -0700

    waitlist frontend (#140)

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 import TaskList from './task/TaskList'
 import GLButton from './login/GoogleLogin'
-import { LOGIN_URL } from '../constants'
-import {getAuthToken} from '../helpers/utils'
+import { LOGIN_URL, WAITLIST_URL } from '../constants'
+import {getAuthToken, getHeaders} from '../helpers/utils'
+import { match } from 'assert'
 
 const Logo = styled.div`
   font-weight: bold;
@@ -32,7 +33,7 @@ const WaitlistInput = styled.input`
   width: 250px;
   height: 100%;
   border: 1.5px solid black;
-  border-radius: 2px;
+  border-radius: 2px 0 0 2px;
   color: #969696;
   box-sizing: border-box;
   text-align: center;
@@ -41,19 +42,36 @@ const JoinWaitlistButton = styled.button`
   width: 200px;
   height: 100%;
   border: 1.5px solid black;
-  border-radius: 2px;
+  border-radius: 0 2px 2px 0;
   color: white;
   background-color: black;
 `
-const WaitlistInputs = styled.div`
+const WaitlistDiv = styled.div`
+  width: 450px;
+  margin: auto;
+  margin-bottom: 40px;
+`
+const WaitlistInputs = styled.form`
   height: 34px;
   text-align: center;
-  margin-bottom: 40px;
+`
+const WaitlistMessage = styled.div<{visibility: string, backgroundColor: string, color: string }>`
+  height: 34px;
+  background-color: red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  visibility: ${props => props.visibility};
+  background-color: ${props => props.backgroundColor};
+  border: 1.5px solid ${props => props.color};
+  border-top: none;
+  color: ${props => props.color};
 `
 const LoginWithGoogle = styled.a`
   border: 1px solid #cccccc;
   border-radius: 2px;
   margin: auto;
+  margin-bottom: 50px;
   display: flex;
   width: 200px;
   color: #969696;
@@ -62,6 +80,13 @@ const LoginWithGoogle = styled.a`
   align-items: center;
   padding: 4px;
 `
+
+enum WaitlistState {
+  NONE,
+  SUCCESS,
+  EXISTS,
+  ERROR,
+}
 
 const LandingPage: React.FC = () => {
   if (getAuthToken()) {
@@ -80,16 +105,7 @@ const LandingPage: React.FC = () => {
           <br />
           and prioritizes what matters most.
         </Subtitle>
-        <WaitlistInputs>
-          <WaitlistInput placeholder="Enter email address" />
-          <JoinWaitlistButton
-            onClick={() => {
-              alert('Waitlist is closed currently.')
-            }}
-          >
-            Join the Waitlist
-          </JoinWaitlistButton>
-        </WaitlistInputs>
+        <Waitlist/>
         <LoginWithGoogle href={LOGIN_URL} role="button">
           <GLButton />
           Sign in with Google
@@ -97,6 +113,75 @@ const LandingPage: React.FC = () => {
       </Container>
     </div>
   )
+}
+
+const Waitlist = () => {
+  const [waitlistState, setWaitlistState] = useState(WaitlistState.NONE)
+  const [email, setEmail] = useState('')
+
+  let messageVisibility = 'visible'
+  let messageText
+  let messageTextColor = 'black'
+  let messageBackgroundColor = 'white'
+  switch(waitlistState){
+    case WaitlistState.NONE:
+      messageVisibility = 'hidden'
+      messageText = ''
+      break
+    case WaitlistState.SUCCESS:
+      messageText = 'You\'ve been added to the waitlist!'
+      messageTextColor = '#4F8A10'
+      messageBackgroundColor = '#DFF2BF'
+      break
+    case WaitlistState.EXISTS:
+      messageText = 'This email already exists in the waitlist'
+      messageTextColor = '#00529B'
+      messageBackgroundColor = '#BDE5F8'
+      break
+    case WaitlistState.ERROR:
+      messageText = 'There was an error adding you to the waitlist'
+      messageTextColor = '#D8000C'
+      messageBackgroundColor = '#FFD2D2'
+      break
+  }
+
+  return (
+    <WaitlistDiv>
+      <WaitlistInputs onSubmit={(e) => {
+            e.preventDefault()
+            joinWaitlist(email, setWaitlistState)
+          }}>
+        <WaitlistInput placeholder="Enter email address" value={email} onChange={e => setEmail(e.target.value)} />
+        <JoinWaitlistButton role="submit">
+          Join the Waitlist
+        </JoinWaitlistButton>
+      </WaitlistInputs>
+
+      <WaitlistMessage visibility={messageVisibility} backgroundColor={messageBackgroundColor} color={messageTextColor}>
+        {messageText}
+      </WaitlistMessage>
+
+    </WaitlistDiv>
+  )
+}
+
+const joinWaitlist = async (email: string, setWaitlistState: (state: WaitlistState) => void) => {
+  const response: Response = await fetch(WAITLIST_URL, {
+    method: 'POST',
+    mode: 'cors',
+    headers: getHeaders(),
+    body: JSON.stringify({ email }),
+  })
+  if(response.ok){
+    setWaitlistState(WaitlistState.SUCCESS)
+  }
+  else if(response.status === 302){
+    setWaitlistState(WaitlistState.EXISTS)
+  }
+  else{
+    setWaitlistState(WaitlistState.ERROR)
+  }
+
 }
 
 export default LandingPage
