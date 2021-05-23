0bba866125a2d58a9984a71adc25a8014e6e3711
John Reinstra <jreinstra@gmail.com>
2021-05-22 17:47:55 -0700

    Add settings PATCH API (#126)
    * Add settings PATCH API
    
    * more testing
    
    * address comments

--- backend/api/settings_test.go
+++ backend/api/settings_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"bytes"
 	"context"
 	"io/ioutil"
 	"net/http"
@@ -61,6 +62,119 @@ func TestSettingsGet(t *testing.T) {
 	t.Run("Unauthorized", func(t *testing.T) {
 		router := GetRouter(&API{})
 		request, _ := http.NewRequest("GET", "/settings/", nil)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusUnauthorized, recorder.Code)
+	})
+}
+
+func TestSettingsModify(t *testing.T) {
+	db, dbCleanup := database.GetDBConnection()
+	defer dbCleanup()
+
+	t.Run("EmptyPayload", func(t *testing.T) {
+		authToken := login("approved@generaltask.io")
+		router := GetRouter(&API{})
+		request, _ := http.NewRequest("PATCH", "/settings/", nil)
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+		body, err := ioutil.ReadAll(recorder.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "{\"detail\":\"Parameters missing or malformatted.\"}", string(body))
+	})
+	t.Run("InvalidPayload", func(t *testing.T) {
+		authToken := login("approved@generaltask.io")
+		router := GetRouter(&API{})
+		request, _ := http.NewRequest(
+			"PATCH",
+			"/settings/",
+			bytes.NewBuffer([]byte(`["not", "a", "map"]`)),
+		)
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+		body, err := ioutil.ReadAll(recorder.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "{\"detail\":\"Parameters missing or malformatted.\"}", string(body))
+	})
+	t.Run("BadKey", func(t *testing.T) {
+		authToken := login("approved@generaltask.io")
+		router := GetRouter(&API{})
+		request, _ := http.NewRequest(
+			"PATCH",
+			"/settings/",
+			bytes.NewBuffer([]byte(`{"dogecoin": "tothemoon"}`)),
+		)
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+		body, err := ioutil.ReadAll(recorder.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "{\"detail\":\"Failed to update settings: invalid setting: dogecoin\"}", string(body))
+	})
+	t.Run("BadValue", func(t *testing.T) {
+		authToken := login("approved@generaltask.io")
+		router := GetRouter(&API{})
+		request, _ := http.NewRequest(
+			"PATCH",
+			"/settings/",
+			bytes.NewBuffer([]byte(`{"email_done_preference": "tothemoon"}`)),
+		)
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+		body, err := ioutil.ReadAll(recorder.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "{\"detail\":\"Failed to update settings: invalid value: tothemoon\"}", string(body))
+	})
+	t.Run("Success", func(t *testing.T) {
+		authToken := login("approved@generaltask.io")
+		router := GetRouter(&API{})
+		request, _ := http.NewRequest(
+			"PATCH",
+			"/settings/",
+			bytes.NewBuffer([]byte(`{"email_done_preference": "mark_as_read"}`)),
+		)
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusOK, recorder.Code)
+		body, err := ioutil.ReadAll(recorder.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "{}", string(body))
+
+		userID := getUserIDFromAuthToken(t, db, authToken)
+		assert.Equal(t, ChoiceKeyMarkAsRead, GetUserSetting(db, userID, SettingFieldEmailDonePreference))
+	})
+	t.Run("SuccessAlreadyExists", func(t *testing.T) {
+		authToken := login("approved@generaltask.io")
+		userID := getUserIDFromAuthToken(t, db, authToken)
+		UpdateUserSetting(db, userID, SettingFieldEmailDonePreference, ChoiceKeyMarkAsRead)
+
+		router := GetRouter(&API{})
+		request, _ := http.NewRequest(
+			"PATCH",
+			"/settings/",
+			bytes.NewBuffer([]byte(`{"email_done_preference": "archive"}`)),
+		)
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusOK, recorder.Code)
+		body, err := ioutil.ReadAll(recorder.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "{}", string(body))
+
+		assert.Equal(t, ChoiceKeyArchive, GetUserSetting(db, userID, SettingFieldEmailDonePreference))
+	})
+	t.Run("Unauthorized", func(t *testing.T) {
+		router := GetRouter(&API{})
+		request, _ := http.NewRequest("PATCH", "/settings/", nil)
 		recorder := httptest.NewRecorder()
 		router.ServeHTTP(recorder, request)
 		assert.Equal(t, http.StatusUnauthorized, recorder.Code)
