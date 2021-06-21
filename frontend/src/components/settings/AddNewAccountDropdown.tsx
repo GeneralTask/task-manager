30cf91b37b05204395dd8d447a4f89f9ffaa95b7
Scott Mai <scottmai@usc.edu>
2021-06-20 18:02:56 -0700

    close when clicked off

--- frontend/src/components/settings/AddNewAccountDropdown.tsx
+++ frontend/src/components/settings/AddNewAccountDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { CHEVRON_DOWN, JIRA_URL, LOGIN_URL, SUPPORTED_TYPES_URL } from '../../constants'
 import { makeAuthorizedRequest } from '../../helpers/utils'
 
@@ -16,7 +16,6 @@ const DropdownToggle = styled.div`
     border: 2.5px solid black;
     border-radius: 4px;
     padding: 4px 8px 4px 8px;
-    font-weight: 600;
     width: 100%;
     display: flex;
     justify-content: space-between;
@@ -61,71 +60,88 @@ interface DropdownProps {
 
 const AddNewAccountDropdown: React.FC = () => {
     const [supportedTypes, setSupportedTypes] = useState<SupportedType[]>([])
-    useEffect(()=>{
+    useEffect(() => {
         fetchSupportedTypes(setSupportedTypes)
     }, [])
     return (
-        <Dropdown supportedTypes={supportedTypes}/>
+        <Dropdown supportedTypes={supportedTypes} />
     )
 }
 
 const fetchSupportedTypes = async (
     setSupportedTypes: React.Dispatch<React.SetStateAction<SupportedType[]>>
 ) => {
-    try{
+    try {
         const response = await makeAuthorizedRequest({
             url: SUPPORTED_TYPES_URL,
             method: 'GET',
         })
-        if(response.ok){
+        if (response.ok) {
             setSupportedTypes(await response.json())
         }
-        else{
+        else {
             throw 'error fetching supported types'
         }
     }
     // dummy data in 
-    catch{
+    catch {
         setSupportedTypes([
             {
                 authorization_url: LOGIN_URL,
                 name: 'Google',
                 logo: '/images/google.svg'
-              },
-              {
+            },
+            {
                 authorization_url: JIRA_URL,
                 name: 'Jira',
                 logo: '/images/jira.svg'
-              }
+            }
         ])
     }
 }
 
-const Option = ({st}: OptionProps) => {
+const Option = ({ st }: OptionProps) => {
     return (
         <OptionContainer onClick={() => {
             window.open(
-              st.authorization_url,
-              st.name,
-              'height=640,width=960,toolbar=no,menubar=no,scrollbars=no,location=no,status=no'
+                st.authorization_url,
+                st.name,
+                'height=640,width=960,toolbar=no,menubar=no,scrollbars=no,location=no,status=no'
             )
-          }}>
-            <Logo src={st.logo}/>
+        }}>
+            <Logo src={st.logo} />
             <div>{st.name}</div>
         </OptionContainer>)
 }
 
-const Dropdown = ({supportedTypes}: DropdownProps) => {
+const Dropdown = ({ supportedTypes }: DropdownProps) => {
     const [isExpanded, setExpanded] = useState(false)
-    
+    const ref = useRef<HTMLDivElement>(null)
+
+    // so that the dropdown closes when clicked off from
+    const handleClick = (e: MouseEvent) => {
+        if (ref.current && e.target instanceof Element) {
+            if (!ref.current.contains(e.target)) {
+                setExpanded(false)
+            }
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClick)
+        return () => {
+            document.removeEventListener('mousedown', handleClick)
+        }
+    }, [])
+
     return (
-        <DropdownContainer>
-            <DropdownToggle onClick={()=>{setExpanded(!isExpanded)}}>
+        <DropdownContainer ref={ref}>
+            <DropdownToggle onClick={() => { setExpanded(!isExpanded) }}>
                 Add new account
-                <Chevron src={CHEVRON_DOWN}/>
+                <Chevron src={CHEVRON_DOWN} />
             </DropdownToggle>
-            {isExpanded && <Selector>
-                {supportedTypes.map(st => <Option st={st}/>)}
+            {isExpanded && <Selector onBlur={() => { setExpanded(false) }}>
+                {supportedTypes.map((st, i) => <Option st={st} key={i} />)}
             </Selector>}
         </DropdownContainer>
     )
