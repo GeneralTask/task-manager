7451c143bac7b3a3e4aa297b6ec3c518d0fbce2f
Scott Mai <scottmai@usc.edu>
2021-06-21 20:11:07 -0700

    added grab

--- frontend/src/components/task/TaskHeader.tsx
+++ frontend/src/components/task/TaskHeader.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Task.css'
-import {TASKS_URL} from '../../constants'
+import { TASKS_URL } from '../../constants'
 import store from '../../redux/store'
-import {removeTaskById, expandBody, retractBody} from '../../redux/actions'
-import {makeAuthorizedRequest} from '../../helpers/utils'
+import { removeTaskById, expandBody, retractBody } from '../../redux/actions'
+import { makeAuthorizedRequest } from '../../helpers/utils'
 import { useSelector } from 'react-redux'
 
 import styled from 'styled-components'
 import { RootState } from '../../redux/store'
 
-
-const Header = styled.div<{hover_effect: boolean}>`
+const Domino = styled.img`
+  height: 18px;
+  cursor: drag !important; 
+`
+const Header = styled.div<{ hover_effect: boolean }>`
   font-size: 20px;
   display: flex;
   flex-direction: row;
@@ -21,17 +24,16 @@ const Header = styled.div<{hover_effect: boolean}>`
   &:hover{
     background-color: ${props => props.hover_effect ? '#e3e3e3' : 'inherit'};
   }
+  ${Domino}:hover {
+    cursor: grab;
+  }
 `
-
 const HeaderSide = styled.div`
   text-align: left;
   flex: 1;
   display: flex;
   align-items: center;
   flex-direction: row;
-`
-const Domino = styled.img`
-  height: 18px;
 `
 const Icon = styled.img`
   max-width: 40px;
@@ -64,11 +66,11 @@ const DoneButton = styled.button`
 type UNKNOWN_PROVIDED_TYPE = any
 
 interface Props {
-  logo_url: string, 
-  title: string, 
-  sender: string | null, 
-  task_id: string, 
-  is_completable: boolean, 
+  logo_url: string,
+  title: string,
+  sender: string | null,
+  task_id: string,
+  is_completable: boolean,
   hover_effect: boolean,
   provided: UNKNOWN_PROVIDED_TYPE,
 }
@@ -80,13 +82,13 @@ const TaskHeader: React.FC<Props> = ({ logo_url, title, sender, task_id, is_comp
     onClick = () => {
       store.dispatch(expandBody(task_id))
     }
-  } else if(hover_effect && expanded_body === task_id) {
+  } else if (hover_effect && expanded_body === task_id) {
     onClick = () => {
       store.dispatch(retractBody())
     }
   }
-  else{
-    onClick = ()=>false
+  else {
+    onClick = () => false
   }
   return (
     <Header hover_effect={hover_effect} onClick={onClick}>
@@ -97,15 +99,15 @@ const TaskHeader: React.FC<Props> = ({ logo_url, title, sender, task_id, is_comp
       </HeaderSide>
       <Source>{sender}</Source>
       {is_completable ?
-      <DoneButton
-        onClick={(e) => {
-          e.stopPropagation()
-          done(task_id)
-        }}
-      >
-        Done
-      </DoneButton>
-      : null}
+        <DoneButton
+          onClick={(e) => {
+            e.stopPropagation()
+            done(task_id)
+          }}
+        >
+          Done
+        </DoneButton>
+        : null}
     </Header>
   )
 }
@@ -117,13 +119,13 @@ const done = async (task_id: string) => {
       url: TASKS_URL + task_id + '/',
       method: 'PATCH',
       body: JSON.stringify({ 'is_completed': true })
-  })
-    
+    })
+
     if (!response.ok) {
       throw new Error('PATCH /tasks api call failed')
-    } 
+    }
   } catch (e) {
-    console.log({e})
+    console.log({ e })
   }
 }
 
