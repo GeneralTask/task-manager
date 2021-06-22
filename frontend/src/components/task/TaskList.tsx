99f9803fb7f4db4c8fb9b78cdfc1e91c842a2cc7
Scott Mai <scottmai@usc.edu>
2021-06-21 19:13:16 -0700

    fixed time estimation

--- frontend/src/components/task/TaskList.tsx
+++ frontend/src/components/task/TaskList.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react'
 import { connect, useSelector } from 'react-redux'
 import store from '../../redux/store'
-import {setTasks, setTasksFetchStatus} from '../../redux/actions'
+import { setTasks, setTasksFetchStatus } from '../../redux/actions'
 import { FetchStatus } from '../../redux/enums'
 import { TASKS_URL, TASK_GROUP_SCHEDULED_TASK, TASK_GROUP_UNSCHEDULED_GROUP } from '../../constants'
-import {ScheduledTask, UnscheduledTaskGroup} from './TaskWrappers'
+import { ScheduledTask, UnscheduledTaskGroup } from './TaskWrappers'
 import TaskStatus from './TaskStatus'
 import { DragDropContext, Droppable, DropResult } from 'react-beautiful-dnd'
 import moment from 'moment'
 import styled from 'styled-components'
-import {makeAuthorizedRequest, resetOrderingIds} from '../../helpers/utils'
+import { makeAuthorizedRequest, resetOrderingIds } from '../../helpers/utils'
 import { TTaskGroup, TTask } from '../../helpers/types'
 import { RootState } from '../../redux/store'
 import _ from 'lodash'
@@ -21,23 +21,23 @@ const MyTasks = styled.h1`
 
 const fetchTasks = async () => {
     store.dispatch(setTasksFetchStatus(FetchStatus.LOADING))
-    try{
+    try {
         const response = await makeAuthorizedRequest({
             url: TASKS_URL,
             method: 'GET',
         })
-        if(!response.ok){
+        if (!response.ok) {
             throw new Error('/tasks api call failed')
         }
-        else{
+        else {
             const resj = await response.json()
             store.dispatch(setTasksFetchStatus(FetchStatus.SUCCESS))
             store.dispatch(setTasks(resj))
         }
     }
-    catch(e){
+    catch (e) {
         store.dispatch(setTasksFetchStatus(FetchStatus.ERROR))
-        console.log({e})
+        console.log({ e })
     }
 }
 
@@ -50,38 +50,41 @@ const TaskList: React.FC = () => {
         fetchTasks()
     }, [])
 
-    
+
     function renderTaskGroup(taskGroup: TTaskGroup, index: number) {
         let next_time = null
-        if(index === 0 && task_groups.length > 1){
+        // if currently in a task group, estimate time left until end of task group
+        // else display the duration of that task
+        if (index === 0 && task_groups.length > 1 && moment().isAfter(moment(taskGroup.datetime_start))) {
+            // if this is the first task
             next_time = moment(task_groups[1].datetime_start)
         }
-        if(taskGroup.type === TASK_GROUP_SCHEDULED_TASK){
-            if(taskGroup.tasks.length !== 0){
-                return <ScheduledTask task={taskGroup.tasks[0]} time_duration={taskGroup.time_duration} 
-                    next_time={!next_time ? null : next_time} datetime_start={taskGroup.datetime_start} index={task_counter++}/>
+        if (taskGroup.type === TASK_GROUP_SCHEDULED_TASK) {
+            if (taskGroup.tasks.length !== 0) {
+                return <ScheduledTask task={taskGroup.tasks[0]} time_duration={taskGroup.time_duration}
+                    next_time={!next_time ? null : next_time} datetime_start={taskGroup.datetime_start} index={task_counter++} />
             }
         }
-        else if(taskGroup.type === TASK_GROUP_UNSCHEDULED_GROUP){
-            return <UnscheduledTaskGroup tasks={taskGroup.tasks} time_duration={taskGroup.time_duration} 
-                next_time={!next_time ? null : next_time} index={task_counter++}/>
+        else if (taskGroup.type === TASK_GROUP_UNSCHEDULED_GROUP) {
+            return <UnscheduledTaskGroup tasks={taskGroup.tasks} time_duration={taskGroup.time_duration}
+                next_time={!next_time ? null : next_time} index={task_counter++} />
         }
     }
 
     function onDragEnd(result: DropResult) {
         const { destination, source } = result
-        
+
         if (!destination || !source || destination.index === source.index) return
-    
+
         const source_index: number = parseInt(source.droppableId.slice(-1))
         const destination_index: number = parseInt(destination.droppableId.slice(-1))
-    
+
         // makes deep copy to keep redux state intact
         const task_groups_copy: TTaskGroup[] = _.cloneDeep(task_groups)
         const source_group: TTaskGroup = task_groups_copy[source_index]
         const dest_group: TTaskGroup = task_groups_copy[destination_index]
         const source_task: TTask = source_group.tasks[source.index]
-    
+
         source_group.tasks.splice(source.index, 1)
         dest_group.tasks.splice(destination.index, 0, source_task)
 
@@ -99,13 +102,13 @@ const TaskList: React.FC = () => {
             })
         })
     }
-    
+
     return (
         <div>
             <MyTasks>My Tasks</MyTasks>
-            <TaskStatus/>
+            <TaskStatus />
             <DragDropContext onDragEnd={onDragEnd}>
-                { 
+                {
                     task_groups.map((group: TTaskGroup, index: number) =>
                         <div key={index}>
                             <Droppable droppableId={`list-${index}`} isDropDisabled={group.type === TASK_GROUP_SCHEDULED_TASK}>
@@ -127,5 +130,5 @@ const TaskList: React.FC = () => {
 
 
 export default connect(
-    (state: RootState) => ({task_groups: state.task_groups})
+    (state: RootState) => ({ task_groups: state.task_groups })
 )(TaskList)
