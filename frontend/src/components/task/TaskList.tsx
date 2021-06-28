6f29472952e924e2cc25caf002f03b14d180fcab
Scott Mai <scottmai@usc.edu>
2021-06-28 03:35:40 -0700

    ok now its good

--- frontend/src/components/task/TaskList.tsx
+++ frontend/src/components/task/TaskList.tsx
@@ -110,7 +110,7 @@ const TaskList: React.FC = () => {
                             <Droppable droppableId={`list-${index}`} isDropDisabled={group.type === TASK_GROUP_SCHEDULED_TASK}>
                                 {provided => {
                                     return <div ref={provided.innerRef} {...provided.droppableProps}>
-                                        {group.tasks.length && <TaskGroup taskGroup={group} />}
+                                        {group.tasks.length > 0 && <TaskGroup taskGroup={group} />}
                                         {provided.placeholder}
                                     </div>
                                 }}
