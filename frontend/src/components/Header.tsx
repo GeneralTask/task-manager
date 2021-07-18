1b5cc0c0e9bfbdbba4b188bb2423c60613592c35
johnhamilto <31417618+johnhamilto@users.noreply.github.com>
2021-07-17 23:49:35 -0700

    text dark (#198)

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,19 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { LANDING_PATH, SETTINGS_PATH } from '../constants'
 import { getAuthToken, logout } from '../helpers/utils'
+import { textBlack, textHover } from '../helpers/styles'
 import styled from 'styled-components'
 
 const Logo = styled.button`
   border: none;
   text-decoration: none;
   cursor: pointer;
-  color: black;
+  color: ${textBlack};
   font-weight: bold;
   font-size: 32px;
   margin-left: 20px;
   margin-top: 10px;
   padding: 20px;
   background-color: white;
+  &:hover {color: ${textHover}};
 `
 const HeaderDiv = styled.div` 
   width: 100%;
@@ -26,17 +28,19 @@ const Button = styled.button`
   margin: 0 12px 0 12px;
   border: none;
   text-decoration: none;
-  color: black;
+  color: ${textBlack};
   padding: 6px;
   text-align: center;
   cursor: pointer;
   font-size: 20px;
   background-color: white;
+  &:hover {color: ${textHover}};
 `
 const Logout = styled(Button)`
-  color: black;
+  color: ${textBlack};
   font-weight: 600;
   margin-right: 50px;
+  &:hover {color: ${textHover}};
 `
 
 const Header: React.FC = () => {
