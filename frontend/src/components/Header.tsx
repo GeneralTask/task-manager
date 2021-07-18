0901585b605a19d102e84052c3fce71cc2013322
johnhamilto <john.hamilton@wustl.edu>
2021-07-17 19:50:23 -0700

    change ui to not throw errs and make logo a button

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,12 +4,17 @@ import { LANDING_PATH, SETTINGS_PATH } from '../constants'
 import { getAuthToken, logout } from '../helpers/utils'
 import styled from 'styled-components'
 
-const Logo = styled.div`
+const Logo = styled.button`
+  border: none;
+  text-decoration: none;
+  cursor: pointer;
+  color: black;
   font-weight: bold;
   font-size: 32px;
   margin-left: 20px;
   margin-top: 10px;
   padding: 20px;
+  background-color: white;
 `
 const HeaderDiv = styled.div` 
   width: 100%;
@@ -21,6 +26,7 @@ const Button = styled.button`
   margin: 0 12px 0 12px;
   border: none;
   text-decoration: none;
+  color: black;
   padding: 6px;
   text-align: center;
   cursor: pointer;
@@ -28,6 +34,7 @@ const Button = styled.button`
   background-color: white;
 `
 const Logout = styled(Button)`
+  color: black;
   font-weight: 600;
   margin-right: 14px;
 `
@@ -36,7 +43,7 @@ const Header: React.FC = () => {
   return (
     <HeaderDiv>
       <div>
-        <Logo>General Task</Logo>
+        <Link to={LANDING_PATH}><Logo>General Task</Logo></Link>
       </div>
       <div>
         <Link to={LANDING_PATH}><Button>Tasks</Button></Link>
