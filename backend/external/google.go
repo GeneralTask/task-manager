c99aa59e4b7b71fdf8c4ab3d0b0d310633c754d5
yohanstra <jreinstra@gmail.com>
2021-07-26 20:25:17 -0700

    oops

---
+++ backend/external/google.go
@@ -0,0 +1,58 @@
+package external
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/GeneralTask/task-manager/backend/config"
+	"github.com/GeneralTask/task-manager/backend/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/oauth2"
+)
+
+type GoogleURLOverrides struct {
+	CalendarFetchURL *string
+	GmailModifyURL   *string
+	GmailReplyURL    *string
+}
+
+type GoogleService struct {
+	Config       OauthConfigWrapper
+	OverrideURLs GoogleURLOverrides
+}
+
+func GetGoogleConfig() OauthConfigWrapper {
+	googleConfig := &oauth2.Config{
+		ClientID:     config.GetConfigValue("GOOGLE_OAUTH_CLIENT_ID"),
+		ClientSecret: config.GetConfigValue("GOOGLE_OAUTH_CLIENT_SECRET"),
+		RedirectURL:  config.GetConfigValue("GOOGLE_OAUTH_REDIRECT_URL"),
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/gmail.modify https://www.googleapis.com/auth/calendar.events"},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://accounts.google.com/o/oauth2/auth",
+			TokenURL: "https://oauth2.googleapis.com/token",
+		},
+	}
+	return &OauthConfig{Config: googleConfig}
+}
+
+func GetGoogleHttpClient(externalAPITokenCollection *mongo.Collection, userID primitive.ObjectID, accountID string) *http.Client {
+	var googleToken database.ExternalAPIToken
+
+	if err := externalAPITokenCollection.FindOne(
+		context.TODO(),
+		bson.M{"$and": []bson.M{
+			{"user_id": userID},
+			{"source": "google"},
+			{"account_id": accountID},
+		}}).Decode(&googleToken); err != nil {
+		return nil
+	}
+
+	var token oauth2.Token
+	json.Unmarshal([]byte(googleToken.Token), &token)
+	config := GetGoogleConfig()
+	return config.Client(context.Background(), &token).(*http.Client)
+}
