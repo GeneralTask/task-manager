e19e58c2eacd9ed067dcc496b21675a7f36d9e16
yohanstra <jreinstra@gmail.com>
2021-08-28 19:07:11 -0700

    save

--- backend/external/config.go
+++ backend/external/config.go
@@ -14,3 +14,31 @@ func GetConfig() Config {
 		Atlassian: AtlassianConfig{OauthConfig: getAtlassianOauthConfig()},
 	}
 }
+
+/*
+
+Name to task source:
+- google
+- gmail
+- gcal
+- jira
+- slack
+
+ideally:
+Name to task service (used for external API keys)
+- google
+- atlassian
+- slack
+
+Name to task source (used for task db items and fetching)
+- gmail
+- gcal
+- jira
+- slack
+
+task service to task source (for fetching all tasks given external API keys)
+google -> [gmail, gcal]
+atlassian -> [jira]
+slack -> [slack]
+
+*/
