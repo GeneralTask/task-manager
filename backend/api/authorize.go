c1e5b428913709a799415f6c05ef1d47d791c523
yohanstra <jreinstra@gmail.com>
2021-09-19 16:59:15 -0400

    ready to test maybe

--- backend/api/authorize.go
+++ backend/api/authorize.go
@@ -8,6 +8,11 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type Oauth1RedirectParams struct {
+	Token    string `form:"oauth_token" binding:"required"`
+	Verifier string `form:"oauth_verifier" binding:"required"`
+}
+
 type Oauth2RedirectParams struct {
 	Code  string `form:"code" binding:"required"`
 	State string `form:"state" binding:"required"`
@@ -58,15 +63,21 @@ func (api *API) AuthorizeCallback(c *gin.Context) {
 	}
 	internalToken, err := getTokenFromCookie(c)
 	if err != nil {
+		Handle500(c)
 		return
 	}
 	callbackParams := external.CallbackParams{}
 	if taskServiceResult.Details.AuthType == external.AuthTypeOauth1 {
-		return
+		var redirectParams Oauth1RedirectParams
+		if c.ShouldBind(&redirectParams) != nil || redirectParams.Token == "" || redirectParams.Verifier == "" {
+			c.JSON(400, gin.H{"detail": "missing query params"})
+			return
+		}
+		callbackParams = external.CallbackParams{Oauth1Token: &redirectParams.Token, Oauth1Verifier: &redirectParams.Verifier}
 	} else if taskServiceResult.Details.AuthType == external.AuthTypeOauth2 {
 		var redirectParams Oauth2RedirectParams
 		if c.ShouldBind(&redirectParams) != nil || redirectParams.Code == "" || redirectParams.State == "" {
-			c.JSON(400, gin.H{"detail": "Missing query params"})
+			c.JSON(400, gin.H{"detail": "missing query params"})
 			return
 		}
 		stateTokenID, err := primitive.ObjectIDFromHex(redirectParams.State)
