5ad6e3baa8f9aef94c916482b96055fad78b2f28
johnhamilto <john.hamilton@wustl.edu>
2021-09-18 21:21:13 -0400

    beginnings of trello integrations

---
+++ backend/external/trello.go
@@ -0,0 +1,28 @@
+package external
+
+type TrelloSource struct {
+	Atlassian AtlassianService
+}
+
+type TrelloBoard struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Desc string `json:"desc"`
+	// Closed bool   `json:"closed"`
+	URL string `json:"url"`
+}
+
+type TrelloCard struct {
+	ID        string   `json:"id"`
+	Desc      string   `json:"desc"`
+	Due       string   `json:"due"` // "date"
+	BoardID   string   `json:"idBoard"`
+	ListID    string   `json:"idList"`
+	MembersID []string `json:"idMembers"`
+	Name      string   `json:"name"`
+	URL       string   `json:"url"`
+}
+
+// func (Trello TrelloSource) getAPIBaseURL(siteConfiguration database.AtlassianSiteConfiguration) string {
+// 	return "https://api.trello.com/1/" + siteConfiguration.CloudID
+// }
---
+++ backend/external/trello_oath1.go
@@ -0,0 +1,83 @@
+package external
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+
+	"github.com/GeneralTask/task-manager/backend/config"
+	"github.com/GeneralTask/task-manager/backend/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/oauth2"
+)
+
+type TrelloService struct {
+	Config OauthConfigWrapper
+}
+
+func getTrelloConfig() *OauthConfig {
+	return &OauthConfig{Config: &oauth2.Config{
+		ClientID:     config.GetConfigValue("TRELLO_OAUTH_CLIENT_ID"),
+		ClientSecret: config.GetConfigValue("TRELLO_OAUTH_CLIENT_SECRET"),
+		RedirectURL:  "https://api.generaltask.io/authorize/trello/callback",
+		Scopes:       []string{
+			// TODO
+		},
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  "https://trello.com/1/OAuthAuthorizeToken",
+			TokenURL: "https://trello.com/1/OAuthGetAccessToken",
+		},
+	}}
+}
+
+func (Trello TrelloService) GetLinkURL(stateTokenID primitive.ObjectID, userID primitive.ObjectID) (*string, error) {
+	authURL := Trello.Config.AuthCodeURL(stateTokenID.Hex(), oauth2.AccessTypeOffline, oauth2.ApprovalForce)
+	return &authURL, nil
+}
+
+func (Trello TrelloService) GetSignupURL(stateTokenID primitive.ObjectID, forcePrompt bool) (*string, error) {
+	return nil, errors.New("trello does not support signup")
+}
+
+func (Trello TrelloService) HandleLinkCallback(code string, userID primitive.ObjectID) error {
+	db, dbCleanup, err := database.GetDBConnection()
+	if err != nil {
+		return errors.New("internal server error")
+	}
+	defer dbCleanup()
+
+	token, err := Trello.Config.Exchange(context.Background(), code)
+	if err != nil {
+		log.Printf("failed to fetch token from trello: %v", err)
+		return errors.New("internal server error")
+	}
+
+	tokenString, err := json.Marshal(&token)
+	if err != nil {
+		log.Printf("error parsing token: %v", err)
+		return errors.New("internal server error")
+	}
+
+	externalAPITokenCollection := db.Collection("external_api_tokens")
+	_, err = externalAPITokenCollection.UpdateOne(
+		context.TODO(),
+		bson.M{"$and": []bson.M{{"user_id": userID}, {"service_id": TASK_SERVICE_ID_TRELLO}}},
+		bson.M{"$set": &database.ExternalAPIToken{
+			UserID:    userID,
+			ServiceID: TASK_SERVICE_ID_TRELLO,
+			Token:     string(tokenString)}},
+		options.Update().SetUpsert(true),
+	)
+	if err != nil {
+		log.Printf("error saving token: %v", err)
+		return errors.New("internal server error")
+	}
+	return nil
+}
+
+func (Trello TrelloService) HandleSignupCallback(code string) (primitive.ObjectID, *string, error) {
+	return primitive.NilObjectID, nil, errors.New("trello does not support signup")
+}
