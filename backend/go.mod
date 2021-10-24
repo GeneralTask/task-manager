ab2a242435806a81414d54ca07eab7d65a21af64
yohanstra <jreinstra@gmail.com>
2021-10-24 17:37:56 -0600

    save

--- backend/go.mod
+++ backend/go.mod
@@ -7,13 +7,12 @@ require (
 	github.com/dghubble/oauth1 v0.7.0
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/assert/v2 v2.0.1
-	github.com/golang-migrate/migrate/v4 v4.15.0
+	github.com/golang-migrate/migrate/v4 v4.14.1
 	github.com/google/uuid v1.2.0
 	github.com/joho/godotenv v1.3.0
 	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.6.1
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
-	github.com/xdg/stringprep v0.0.0-20180714160509-73f8eece6fdc // indirect
 	go.mongodb.org/mongo-driver v1.7.0
 	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914
 	google.golang.org/api v0.51.0
--- backend/main.go
+++ backend/main.go
@@ -6,21 +6,29 @@ import (
 	"github.com/GeneralTask/task-manager/backend/api"
 	"github.com/GeneralTask/task-manager/backend/config"
 	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/golang-migrate/migrate/v4/database/mongodb"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func main() {
-	f := file.File{}
-	fmt.Println(f)
-	m, err := migrate.New("file://migrations", config.GetConfigValue("MONGO_URI"))
-	if err != nil {
-		fmt.Printf("error setting up migrations: %v", err)
-		return
-	}
-	err = m.Up()
+	err := runMigrations()
 	if err != nil {
 		fmt.Printf("error running migrations: %v", err)
 		return
 	}
 	api.GetRouter(api.GetAPI()).Run()
+}
+
+func runMigrations() error {
+	fullDBURL := fmt.Sprintf("%s/%s?authSource=admin", config.GetConfigValue("MONGO_URI"), config.GetConfigValue("DB_NAME"))
+	m, err := migrate.New("file://migrations", fullDBURL)
+	if err != nil {
+		return err
+	}
+	err = m.Up()
+	if err.Error() == "no change" {
+		// we'll consider no change to be a successful migration run
+		return nil
+	}
+	return err
 }
