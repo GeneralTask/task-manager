e156b141a4f76e0abe73202c3b79f43f40c01372
yohanstra <jreinstra@gmail.com>
2021-10-24 17:13:42 -0600

    savE'

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/GeneralTask/task-manager/backend/api"
+	"github.com/GeneralTask/task-manager/backend/config"
 	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func main() {
-	m, err := migrate.New("", "")
+	f := file.File{}
+	fmt.Println(f)
+	m, err := migrate.New("file://migrations", config.GetConfigValue("MONGO_URI"))
+	if err != nil {
+		fmt.Printf("error setting up migrations: %v", err)
+		return
+	}
+	err = m.Up()
+	if err != nil {
+		fmt.Printf("error running migrations: %v", err)
+		return
+	}
 	api.GetRouter(api.GetAPI()).Run()
 }
