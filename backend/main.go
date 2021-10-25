31167c9306eb6791363186d2c2536ce9585d928b
yohanstra <jreinstra@gmail.com>
2021-10-24 18:03:08 -0600

    save

--- backend/main.go
+++ backend/main.go
@@ -4,31 +4,14 @@ import (
 	"fmt"
 
 	"github.com/GeneralTask/task-manager/backend/api"
-	"github.com/GeneralTask/task-manager/backend/config"
-	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/mongodb"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/GeneralTask/task-manager/backend/migrations"
 )
 
 func main() {
-	err := runMigrations()
+	err := migrations.RunMigrations()
 	if err != nil {
 		fmt.Printf("error running migrations: %v", err)
 		return
 	}
 	api.GetRouter(api.GetAPI()).Run()
-}
-
-func runMigrations() error {
-	fullDBURL := fmt.Sprintf("%s/%s?authSource=admin", config.GetConfigValue("MONGO_URI"), config.GetConfigValue("DB_NAME"))
-	m, err := migrate.New("file://migrations", fullDBURL)
-	if err != nil {
-		return err
-	}
-	err = m.Up()
-	if err.Error() == "no change" {
-		// we'll consider no change to be a successful migration run
-		return nil
-	}
-	return err
 }
