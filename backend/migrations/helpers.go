7bef7787341cd419be9b254439656ef032339cb5
yohanstra <jreinstra@gmail.com>
2021-10-24 18:27:34 -0600

    add tests

---
+++ backend/migrations/helpers.go
@@ -0,0 +1,33 @@
+package migrations
+
+import (
+	"fmt"
+
+	"github.com/GeneralTask/task-manager/backend/config"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/mongodb"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func RunMigrations(relativePath string) error {
+	migrate, err := getMigrate(relativePath)
+	if err != nil {
+		return err
+	}
+	err = migrate.Up()
+	if err != nil && err.Error() == "no change" {
+		// we consider a no op to be a successful migration run
+		return nil
+	}
+	return err
+}
+
+func getMigrate(relativePath string) (*migrate.Migrate, error) {
+	filePath := fmt.Sprintf("file://%s", relativePath)
+	fullDBURL := fmt.Sprintf("%s/%s?authSource=admin", config.GetConfigValue("MONGO_URI"), config.GetConfigValue("DB_NAME"))
+	migrate, err := migrate.New(filePath, fullDBURL)
+	if err != nil {
+		return nil, err
+	}
+	return migrate, nil
+}
