fc0f9a05c0e38faa08a20b8271677f8cf0ae5f21
yohanstra <jreinstra@gmail.com>
2021-11-02 14:31:15 -0700

    Add general task service and source

---
+++ backend/external/gt_service.go
@@ -0,0 +1,25 @@
+package external
+
+import (
+	"errors"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type GeneralTaskService struct{}
+
+func (GeneralTask GeneralTaskService) GetLinkURL(stateTokenID primitive.ObjectID, userID primitive.ObjectID) (*string, error) {
+	return nil, errors.New("general task service does not support linking")
+}
+
+func (GeneralTask GeneralTaskService) GetSignupURL(stateTokenID primitive.ObjectID, forcePrompt bool) (*string, error) {
+	return nil, errors.New("general task service does not support signup")
+}
+
+func (GeneralTask GeneralTaskService) HandleLinkCallback(params CallbackParams, userID primitive.ObjectID) error {
+	return errors.New("general task service does not support linking")
+}
+
+func (GeneralTask GeneralTaskService) HandleSignupCallback(params CallbackParams) (primitive.ObjectID, *string, error) {
+	return primitive.NilObjectID, nil, errors.New("general task service does not support signup")
+}
