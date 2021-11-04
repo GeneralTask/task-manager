3a592e7dafb6a9374cd530bdbf83228a347858ec
John Reinstra <jreinstra@gmail.com>
2021-11-03 20:31:47 -0400

    Make task create endpoint conform to new spec (#313)
    * Make task create endpoint conform to new spec
    
    * just need tests
    
    * more tests
    
    * finish tests
    
    * nits

---
+++ backend/api/task_create.go
@@ -0,0 +1,87 @@
+package api
+
+import (
+	"context"
+	"time"
+
+	"github.com/GeneralTask/task-manager/backend/constants"
+	"github.com/GeneralTask/task-manager/backend/database"
+	"github.com/GeneralTask/task-manager/backend/external"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type TaskCreateParams struct {
+	AccountID    string     `json:"account_id"`
+	Title        string     `json:"title" binding:"required"`
+	Body         string     `json:"body"`
+	DueDate      *time.Time `json:"due_date"`
+	TimeDuration *int       `json:"time_duration"`
+}
+
+func (api *API) TaskCreate(c *gin.Context) {
+	parentCtx := c.Request.Context()
+	sourceID := c.Param("source_id")
+	taskSourceResult, err := api.ExternalConfig.GetTaskSourceResult(sourceID)
+	if err != nil || !taskSourceResult.Details.IsCreatable {
+		Handle404(c)
+		return
+	}
+
+	var taskCreateParams TaskCreateParams
+	err = c.BindJSON(&taskCreateParams)
+	if err != nil {
+		c.JSON(400, gin.H{"detail": "Invalid or missing parameter."})
+		return
+	}
+
+	userIDRaw, _ := c.Get("user")
+	userID := userIDRaw.(primitive.ObjectID)
+
+	if sourceID != external.TASK_SOURCE_ID_GT_TASK {
+		db, dbCleanup, err := database.GetDBConnection()
+		if err != nil {
+			Handle500(c)
+			return
+		}
+		defer dbCleanup()
+
+		externalAPICollection := database.GetExternalTokenCollection(db)
+		dbCtx, cancel := context.WithTimeout(parentCtx, constants.DatabaseTimeout)
+		defer cancel()
+		count, err := externalAPICollection.CountDocuments(
+			dbCtx,
+			bson.M{"$and": []bson.M{
+				{"account_id": taskCreateParams.AccountID},
+				{"source_id": sourceID},
+				{"user_id": userID},
+			}},
+		)
+		if err != nil || count <= 0 {
+			c.JSON(404, gin.H{"detail": "Account ID not found"})
+			return
+		}
+	} else {
+		// default is currently the only acceptable accountID for general task task source
+		taskCreateParams.AccountID = external.GeneralTaskDefaultAccountID
+	}
+
+	var timeAllocation *int64
+	if taskCreateParams.TimeDuration != nil {
+		timeAllocationTemp := (time.Duration(*taskCreateParams.TimeDuration) * time.Second).Nanoseconds()
+		timeAllocation = &timeAllocationTemp
+	}
+	taskCreationObject := external.TaskCreationObject{
+		Title:          taskCreateParams.Title,
+		Body:           taskCreateParams.Body,
+		DueDate:        taskCreateParams.DueDate,
+		TimeAllocation: timeAllocation,
+	}
+	err = taskSourceResult.Source.CreateNewTask(userID, taskCreateParams.AccountID, taskCreationObject)
+	if err != nil {
+		c.JSON(503, gin.H{"detail": "Failed to create task"})
+		return
+	}
+	c.JSON(200, gin.H{})
+}
---
+++ backend/api/task_create_test.go
@@ -0,0 +1,110 @@
+package api
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/GeneralTask/task-manager/backend/database"
+	"github.com/GeneralTask/task-manager/backend/external"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateTask(t *testing.T) {
+	db, dbCleanup, err := database.GetDBConnection()
+	assert.NoError(t, err)
+	defer dbCleanup()
+
+	authToken := login("approved@generaltask.io", "")
+	router := GetRouter(GetAPI())
+
+	t.Run("BadSourceID", func(t *testing.T) {
+		request, _ := http.NewRequest(
+			"POST",
+			"/tasks/create/dogecoin/",
+			nil)
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusNotFound, recorder.Code)
+		body, err := ioutil.ReadAll(recorder.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "{\"detail\":\"not found\"}", string(body))
+	})
+	t.Run("UnsupportedSourceID", func(t *testing.T) {
+		request, _ := http.NewRequest(
+			"POST",
+			"/tasks/create/gmail/",
+			nil)
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusNotFound, recorder.Code)
+		body, err := ioutil.ReadAll(recorder.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "{\"detail\":\"not found\"}", string(body))
+	})
+	t.Run("MissingTitle", func(t *testing.T) {
+		request, _ := http.NewRequest(
+			"POST",
+			"/tasks/create/gt_task/",
+			nil)
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+		body, err := ioutil.ReadAll(recorder.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "{\"detail\":\"Invalid or missing parameter.\"}", string(body))
+	})
+	t.Run("WrongAccountID", func(t *testing.T) {
+		// this currently isn't possible because only GT tasks are supported, but we should add this when it's possible
+	})
+	t.Run("SuccessTitleOnly", func(t *testing.T) {
+		authToken := login("create_task_success_title_only@generaltask.io", "")
+		userID := getUserIDFromAuthToken(t, db, authToken)
+
+		request, _ := http.NewRequest(
+			"POST",
+			"/tasks/create/gt_task/",
+			bytes.NewBuffer([]byte(`{"title": "buy more dogecoin"}`)))
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusOK, recorder.Code)
+
+		tasks, err := database.GetActiveTasks(db, userID)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(*tasks))
+		task := (*tasks)[0]
+		assert.Equal(t, "buy more dogecoin", task.Title)
+		assert.Equal(t, "", task.Body)
+		assert.Equal(t, external.GeneralTaskDefaultAccountID, task.SourceAccountID)
+		// 1 hour is the default
+		assert.Equal(t, int64(3600000000000), task.TimeAllocation)
+	})
+	t.Run("Success", func(t *testing.T) {
+		authToken := login("create_task_success@generaltask.io", "")
+		userID := getUserIDFromAuthToken(t, db, authToken)
+
+		request, _ := http.NewRequest(
+			"POST",
+			"/tasks/create/gt_task/",
+			bytes.NewBuffer([]byte(`{"title": "buy more dogecoin", "body": "seriously!", "due_date": "2020-12-09T16:09:53+00:00", "time_duration": 300}`)))
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusOK, recorder.Code)
+
+		tasks, err := database.GetActiveTasks(db, userID)
+		assert.NoError(t, err)
+		assert.Equal(t, 1, len(*tasks))
+		task := (*tasks)[0]
+		assert.Equal(t, "buy more dogecoin", task.Title)
+		assert.Equal(t, "seriously!", task.Body)
+		assert.Equal(t, int64(300000000000), task.TimeAllocation)
+		assert.Equal(t, external.GeneralTaskDefaultAccountID, task.SourceAccountID)
+	})
+}
