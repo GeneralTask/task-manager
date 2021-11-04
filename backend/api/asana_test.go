eb765901057ab73850247dd269103e249b01dd57
yohanstra <jreinstra@gmail.com>
2021-11-04 11:20:58 -0700

    save

--- backend/api/asana_test.go
+++ backend/api/asana_test.go
@@ -11,6 +11,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const DefaultTokenPayload string = `{"access_token":"sample-access-token", "data": {"email": "moon@dogecoin.tesla"}}`
+
 func TestLinkAsana(t *testing.T) {
 	t.Run("CookieMissing", func(t *testing.T) {
 		TestAuthorizeCookieMissing(t, GetAPI(), "/link/asana/")
@@ -20,7 +22,7 @@ func TestLinkAsana(t *testing.T) {
 	})
 	t.Run("Success", func(t *testing.T) {
 		TestAuthorizeSuccess(t, GetAPI(), "/link/asana/", func(stateToken string) string {
-			return "<a href=\"https://slack.com/oauth/authorize?access_type=offline&amp;client_id=" + config.GetConfigValue("ASANA_OAUTH_CLIENT_ID") + "&amp;prompt=consent&amp;redirect_uri=https%3A%2F%2Fapi.generaltask.io%2Flink%2Fslack%2Fcallback&amp;response_type=code&amp;scope=channels%3Ahistory+channels%3Aread+im%3Aread+mpim%3Ahistory+im%3Ahistory+groups%3Ahistory+groups%3Aread+mpim%3Awrite+im%3Awrite+channels%3Awrite+groups%3Awrite+chat%3Awrite%3Auser&amp;state=" + stateToken + "\">Found</a>.\n\n"
+			return "<a href=\"https://app.asana.com/-/oauth_authorize?access_type=offline&amp;client_id=" + config.GetConfigValue("ASANA_OAUTH_CLIENT_ID") + "&amp;prompt=consent&amp;redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Flink%2Fasana%2Fcallback%2F&amp;response_type=code&amp;state=" + stateToken + "\">Found</a>.\n\n"
 		})
 	})
 }
@@ -45,25 +47,37 @@ func TestLinkAsanaCallback(t *testing.T) {
 		TestAuthorizeCallbackInvalidStateToken(t, GetAPI(), "/link/asana/callback/")
 	})
 	t.Run("UnsuccessfulResponse", func(t *testing.T) {
-		server := getTokenServerForAsana(t, http.StatusUnauthorized)
+		server := getTokenServerForAsana(t, http.StatusUnauthorized, DefaultTokenPayload)
+		api := GetAPI()
+		(api.ExternalConfig.Asana.(*external.OauthConfig)).Config.Endpoint.TokenURL = server.URL
+		TestAuthorizeCallbackUnsuccessfulResponse(t, api, "/link/asana/callback/")
+	})
+	t.Run("MissingData", func(t *testing.T) {
+		server := getTokenServerForAsana(t, http.StatusOK, `{"access_token":"sample-access-token"}`)
+		api := GetAPI()
+		(api.ExternalConfig.Asana.(*external.OauthConfig)).Config.Endpoint.TokenURL = server.URL
+		TestAuthorizeCallbackUnsuccessfulResponse(t, api, "/link/asana/callback/")
+	})
+	t.Run("MissingEmail", func(t *testing.T) {
+		server := getTokenServerForAsana(t, http.StatusOK, `{"access_token":"sample-access-token", "data": {}}`)
 		api := GetAPI()
 		(api.ExternalConfig.Asana.(*external.OauthConfig)).Config.Endpoint.TokenURL = server.URL
 		TestAuthorizeCallbackUnsuccessfulResponse(t, api, "/link/asana/callback/")
 	})
 	t.Run("Success", func(t *testing.T) {
-		server := getTokenServerForAsana(t, http.StatusOK)
+		server := getTokenServerForAsana(t, http.StatusOK, DefaultTokenPayload)
 		api := GetAPI()
 		(api.ExternalConfig.Asana.(*external.OauthConfig)).Config.Endpoint.TokenURL = server.URL
 		TestAuthorizeCallbackSuccessfulResponse(t, api, "/link/asana/callback/", external.TASK_SERVICE_ID_ASANA)
 	})
 }
 
-func getTokenServerForAsana(t *testing.T, statusCode int) *httptest.Server {
+func getTokenServerForAsana(t *testing.T, statusCode int, body string) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		_, err := ioutil.ReadAll(r.Body)
 		assert.NoError(t, err)
 		w.Header().Add("Content-Type", "application/json")
 		w.WriteHeader(statusCode)
-		w.Write([]byte(`{"access_token":"sample-access-token"}`))
+		w.Write([]byte(body))
 	}))
 }
