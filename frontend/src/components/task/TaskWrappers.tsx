91fc41ceea987aa1b8c4d76d928a9fcd672e4d90
Scott Mai <scottmai@usc.edu>
2021-11-08 12:37:20 -0800

    Condensed time annotations in mobile view

--- frontend/src/components/task/TaskWrappers.tsx
+++ frontend/src/components/task/TaskWrappers.tsx
@@ -1,4 +1,5 @@
 import { DateTime, Duration } from 'luxon'
+import { DeviceSize, useDeviceSize } from '../../helpers/utils'
 import React, { useEffect, useState } from 'react'
 import { TEXT_GRAY, device } from '../../helpers/styles'
 import { TTask, TTaskGroup } from '../../helpers/types'
@@ -6,6 +7,28 @@ import { TTask, TTaskGroup } from '../../helpers/types'
 import Task from './Task'
 import humanizeDuration from 'humanize-duration'
 import styled from 'styled-components'
+
+const short_en_expanded = {
+  y: (c: number | undefined) => 'year' + (c === 1 ? '' : 's'),
+  mo: (c: number | undefined) => 'month' + (c === 1 ? '' : 's'),
+  w: (c: number | undefined) => 'week' + (c === 1 ? '' : 's'),
+  d: (c: number | undefined) => 'day' + (c === 1 ? '' : 's'),
+  h: (c: number | undefined) => 'hour' + (c === 1 ? '' : 's'),
+  m: (c: number | undefined) => 'min' + (c === 1 ? '' : 's'),
+  s: (c: number | undefined) => 'second' + (c === 1 ? '' : 's'),
+  ms: (c: number | undefined) => 'millisecond' + (c === 1 ? '' : 's'),
+}
+
+const short_en_condensed = {
+  y: () => 'yr',
+  mo: () => 'mo',
+  w: () => 'wk',
+  d: () => 'd',
+  h: () => 'hr',
+  m: () => 'm',
+  s: () => 's',
+  ms: () => 'ms',
+}
 
 const TaskGroup = styled.div`
   display: flex;
@@ -114,14 +137,14 @@ export function useCountdown(datetimeStart: string | null): string | null {
   if (datetimeStart == null) {
     return null
   }
+  const isMobile = useDeviceSize() === DeviceSize.MOBILE
 
   const start = parseDateTime(datetimeStart)
 
-
-  const [time, setTime] = useState<string | null>(getLiveTimeStr(start))
+  const [time, setTime] = useState<string | null>(getLiveTimeStr(start, isMobile))
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime(getLiveTimeStr(start))
+      setTime(getLiveTimeStr(start, isMobile))
     }, 1000)
 
     return () => {
@@ -143,6 +166,8 @@ export function useTimeDuration(
   datetime_start: string | null,
   alwaysShowTimeRemaining = false,
 ): string | null {
+  const isMobile = useDeviceSize() === DeviceSize.MOBILE
+
   const duration = Duration.fromMillis(time_duration * 1000)
   const start = parseDateTime(datetime_start)
   const end = start.plus(duration)
@@ -152,48 +177,40 @@ export function useTimeDuration(
   let initialTimeStr = ''
   if (hasStarted && !hasEnded) {
     // this will update every second
-    initialTimeStr = getLiveTimeStr(end)
+    initialTimeStr = getLiveTimeStr(end, isMobile)
   } else {
     // will show the full duration of the task ( 1 hour )
-    initialTimeStr = getTimeStringFromDuration(duration)
+    initialTimeStr = getTimeStringFromDuration(duration, isMobile)
   }
 
   const [timeStr, setTimeStr] = useState(initialTimeStr)
 
   useEffect(() => {
+    setTimeStr(getLiveTimeStr(end, isMobile))
     const interval = setInterval(() => {
       if ((hasStarted && !hasEnded) || alwaysShowTimeRemaining) {
-        setTimeStr(getLiveTimeStr(end))
+        setTimeStr(getLiveTimeStr(end, isMobile))
       } else {
-        setTimeStr(getTimeStringFromDuration(duration))
+        setTimeStr(getTimeStringFromDuration(duration, isMobile))
       }
     }, 1000)
 
     return () => {
       clearInterval(interval)
     }
-  }, [])
+  }, [isMobile])
   return timeStr
 }
 
-const getLiveTimeStr = (dtEnd: DateTime): string => {
-  return getTimeStringFromDuration(dtEnd.diffNow())
+const getLiveTimeStr = (dtEnd: DateTime, condensed=false): string => {
+  return getTimeStringFromDuration(dtEnd.diffNow(), condensed)
 }
 
-const getTimeStringFromDuration = (dur: Duration): string => {
+const getTimeStringFromDuration = (dur: Duration, condensed=false): string => {
   const shortEnglishHumanizer = humanizeDuration.humanizer({
     language: 'short_en',
     languages: {
-      short_en: {
-        y: (c) => 'year' + (c === 1 ? '' : 's'),
-        mo: (c) => 'month' + (c === 1 ? '' : 's'),
-        w: (c) => 'week' + (c === 1 ? '' : 's'),
-        d: (c) => 'day' + (c === 1 ? '' : 's'),
-        h: (c) => 'hour' + (c === 1 ? '' : 's'),
-        m: (c) => 'min' + (c === 1 ? '' : 's'),
-        s: (c) => 'second' + (c === 1 ? '' : 's'),
-        ms: (c) => 'millisecond' + (c === 1 ? '' : 's'),
-      },
+      short_en: condensed ? short_en_condensed : short_en_expanded,
     },
   })
   return shortEnglishHumanizer(dur.toMillis(), { units: ['d', 'h', 'm'], largest: 2, delimiter: ' ', round: true, language: 'short_en', })
