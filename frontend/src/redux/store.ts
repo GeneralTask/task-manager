4452bb0cedb79ce96485566c18e00dca8355e513
Scott Mai <scottmai@usc.edu>
2021-12-08 13:54:27 -0800

    remove commented code

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,17 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import settingsReducer from './settingsSlice'
-import tasksReducer from './tasksPageSlice'
-
-// declare global {
-//     interface Window {
-//         __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-//     }
-// }
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+import tasksPageReducer from './tasksPageSlice'
 
 export const store = configureStore({
     reducer: {
-        tasks_page: tasksReducer,
+        tasks_page: tasksPageReducer,
         settings_page: settingsReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
