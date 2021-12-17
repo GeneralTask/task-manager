5f7b64f93155bffedecfd4e22849fecff646f040
Scott Mai <scottmai702@gmail.com>
2021-12-17 07:56:22 -0800

    changed tasks page background to off-white color matching mockup

--- frontend/src/components/task/TasksPage.tsx
+++ frontend/src/components/task/TasksPage.tsx
@@ -10,7 +10,7 @@ import { useFetchSettings } from '../settings/Preferences'
 import { useFetchTasks } from '../../helpers/utils'
 import Navbar from '../Navbar'
 import { NavbarPages } from '../../helpers/types'
-import { BACKGROUND_WHITE } from '../../helpers/styles'
+import { BACKGROUND_WHITE, TASKS_BACKROUND } from '../../helpers/styles'
 import CalendarSidebar from '../calendar/CalendarSidebar'
 
 const TasksPageContainer = styled.div`
@@ -23,6 +23,7 @@ const TasksContentContainer = styled.div`
     overflow: scroll;
     padding-top: 50px;
     flex-direction: column;
+    background-color: ${TASKS_BACKROUND};
 `
 const Header = styled.div`
     display: flex;
