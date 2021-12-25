f213e0b654c97d08bc836d8f34692acab654ecfe
Scott Mai <scottmai702@gmail.com>
2021-12-24 16:21:55 -0800

    removed log

--- frontend/src/components/task/TaskCreate.tsx
+++ frontend/src/components/task/TaskCreate.tsx
@@ -27,8 +27,6 @@ export default function TaskCreate(): JSX.Element {
 
     const fetchTasks = useFetchTasks()
 
-    console.log({ showCreateTaskForm })
-
     return <>
         {showCreateTaskForm && <>
             <styles.OuterContainer>
