aa4117decc9ebf3be34f0c793ea164c2eb62456b
Nolan Jimenez <nolan1299@gmail.com>
2022-01-07 13:26:16 -0800

    lint + npm fixed grumble grumble

--- frontend/public/notarize.js
+++ frontend/public/notarize.js
@@ -1,18 +1,19 @@
-require('dotenv').config();
-const { notarize } = require('electron-notarize');
+/* eslint @typescript-eslint/no-var-requires: "off" */
+require('dotenv').config()
+const { notarize } = require('electron-notarize')
 
 exports.default = async function notarizing(context) {
-    const { electronPlatformName, appOutDir } = context;
+    const { electronPlatformName, appOutDir } = context
     if (electronPlatformName !== 'darwin') {
-        return;
+        return
     }
 
-    const appName = context.packager.appInfo.productFilename;
+    const appName = context.packager.appInfo.productFilename
 
     return await notarize({
         appBundleId: 'com.electron.generaltask',
         appPath: `${appOutDir}/${appName}.app`,
         appleId: process.env.APPLEID,
         appleIdPassword: process.env.APPIDPWD,
-    });
-};
+    })
+}
