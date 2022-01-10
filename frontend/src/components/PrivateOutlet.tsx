e78c778e184934287e0c4b2280c5cf9c2e5ad39f
Nolan Jimenez <nolan1299@gmail.com>
2022-01-10 12:23:20 -0800

    switched to react router v6 in preparation for moving routes to navbar

---
+++ frontend/src/components/PrivateOutlet.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Outlet, Navigate } from 'react-router-dom'
+import { getAuthToken } from '../helpers/utils'
+
+function PrivateOutlet(): JSX.Element {
+    const authToken = getAuthToken()
+    return authToken ? <Outlet /> : <Navigate to='/' />
+}
+
+export default PrivateOutlet
