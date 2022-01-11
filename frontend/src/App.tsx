9c4b34082143e689dcfcc0c9cb886a1f08771203
Nolan Jimenez <nolan1299@gmail.com>
2022-01-10 17:03:13 -0800

    switched to react router v6 in preparation for moving routes to navbar (#466)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,8 @@
 import './App.css'
 import 'react-toastify/dist/ReactToastify.css'
-
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { LANDING_PATH, PRIVACY_PATH, SETTINGS_PATH, TOAST_DURATION } from './constants'
-import { ToastContainer, Zoom, toast } from 'react-toastify'
-
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import LandingPage from './components/LandingPage'
-import PrivacyPolicy from './components/PrivacyPolicy'
-import { Provider } from 'react-redux'
 import React from 'react'
-import Settings from './components/settings/Settings'
-import { getAuthToken } from './helpers/utils'
-import store from './redux/store'
+import { Outlet } from 'react-router-dom'
 
-const App: React.FC = () => {
-  return (
-    <Provider store={store}>
-      <ToastContainer draggable={false} transition={Zoom} autoClose={TOAST_DURATION} position={toast.POSITION.BOTTOM_RIGHT} />
-      <DndProvider backend={HTML5Backend}>
-        <BrowserRouter>
-          <Switch>
-            {/* Settings page, only accessible if logged in */}
-            {getAuthToken() ? <Route path={SETTINGS_PATH} component={Settings} /> : null}
-
-            <Route path={PRIVACY_PATH} component={PrivacyPolicy} />
-
-            {/* MAKE SURE THIS IS THE LAST ROUTE */}
-            {/* base url, shows landing page if not logged in, shows tasks page if logged in */}
-            <Route path={LANDING_PATH} component={LandingPage} />
-          </Switch>
-        </BrowserRouter>
-      </DndProvider>
-    </Provider>
-  )
-}
+const App: React.FC = () => <Outlet />
 
 export default App
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,34 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ToastContainer, Zoom, toast } from 'react-toastify'
+import LandingPage from './components/LandingPage'
+import { TOAST_DURATION } from './constants'
+import store from './redux/store'
+import Settings from './components/settings/Settings'
+import PrivateOutlet from './components/PrivateOutlet'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ToastContainer draggable={false} transition={Zoom} autoClose={TOAST_DURATION} position={toast.POSITION.BOTTOM_RIGHT} />
+      <DndProvider backend={HTML5Backend}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<LandingPage />} />
+              <Route path="settings" element={<PrivateOutlet />} >
+                <Route index element={<Settings />} />
+              </Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </DndProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
