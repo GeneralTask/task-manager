c7e984b7b433445a5a26072e9e4d48945f3c8fa3
Nolan Jimenez <nolan1299@gmail.com>
2022-01-14 14:57:43 -0800

    all events that are within 20minutes are shown in a stack (#490)
    * all events that are within 20minutes are shown in a stack, rather than
    just the soonest one
    
    * can't inglish

--- frontend/src/components/alert/EventAlert.tsx
+++ frontend/src/components/alert/EventAlert.tsx
@@ -10,6 +10,7 @@ const EventAlertContainer = styled.div`
     flex-direction: column;
     background-image: linear-gradient(to bottom right, ${TASKS_BACKGROUND_GRADIENT}, ${TASKS_BACKROUND} 90%);
     min-width: 600px;
+    padding-top: 10px;
 `
 const EventAlertContentContainer = styled.div`
     display: flex;
@@ -18,9 +19,9 @@ const EventAlertContentContainer = styled.div`
     width: 500px;
     height: 40px;
     background-color: white;
-    margin: 20px auto;
+    margin: 10px auto;
     border-radius: 10px;
-    padding: 5px 5px 5px 10px;
+    padding: 5px 10px;
     font-size: 13px;
     line-height: 16px;
     font-style: normal;
@@ -47,30 +48,31 @@ interface EventAlertProps {
 
 export default function EventAlert({ children }: EventAlertProps): JSX.Element {
     const eventList = useAppSelector((state) => state.tasks_page.events.event_list)
-    const nextSoonEvent = eventList.find((event) => {
+    const soonEvents = eventList.filter((event) => {
         const eventDate = new Date(event.datetime_start)
         const minutesUntilEvent = Math.ceil((eventDate.getTime() - new Date().getTime()) / 1000 / 60)
         return minutesUntilEvent >= 0 && minutesUntilEvent < 20
     })
-
-    let eventAlertElement = null
-    if (nextSoonEvent) {
-        const tempDate = new Date(nextSoonEvent.datetime_start)
-        eventAlertElement = (
-            <EventAlertContentContainer id='event-alert'>
-                <EventAlertHeader>
-                    Your event&nbsp;
-                    <EventAlertEventTitle>{nextSoonEvent.title}</EventAlertEventTitle>
-                    &nbsp;in {Math.ceil((tempDate.getTime() - new Date().getTime()) / 1000 / 60)} minutes.
-                </EventAlertHeader>
-                {nextSoonEvent.conference_call && <JoinConferenceButton conferenceCall={nextSoonEvent.conference_call} />}
-            </EventAlertContentContainer>
-        )
+    const eventAlertElements: JSX.Element[] = []
+    if (soonEvents.length > 0) {
+        for (const event of soonEvents) {
+            const tempDate = new Date(event.datetime_start)
+            eventAlertElements.push(
+                <EventAlertContentContainer id='event-alert'>
+                    <EventAlertHeader>
+                        Your event&nbsp;
+                        <EventAlertEventTitle>{event.title}</EventAlertEventTitle>
+                        &nbsp;starts in {Math.ceil((tempDate.getTime() - new Date().getTime()) / 1000 / 60)} minutes.
+                    </EventAlertHeader>
+                    {event.conference_call && <JoinConferenceButton conferenceCall={event.conference_call} />}
+                </EventAlertContentContainer>
+            )
+        }
     }
 
     return (
         <EventAlertContainer>
-            {eventAlertElement}
+            {eventAlertElements}
             {children}
         </EventAlertContainer>
     )
