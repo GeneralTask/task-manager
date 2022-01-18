a28e7c1a65c234ba49477d3965aeb659119cbf85
Scott Mai <scottmai702@gmail.com>
2022-01-18 02:26:35 -0800

    reverted isostring

--- frontend/src/components/calendar/CalendarEvents.tsx
+++ frontend/src/components/calendar/CalendarEvents.tsx
@@ -74,24 +74,6 @@ function EventBody({ event }: EventBodyProps): JSX.Element {
     )
 }
 
-function getISOString(date: Date): string {
-    const tzo = -date.getTimezoneOffset()
-    const dif = tzo >= 0 ? '+' : '-'
-    const pad = (num: number) => {
-        const norm = Math.floor(Math.abs(num))
-        return (norm < 10 ? '0' : '') + norm
-    }
-
-    return date.getFullYear() +
-        '-' + pad(date.getMonth() + 1) +
-        '-' + pad(date.getDate()) +
-        'T' + pad(date.getHours()) +
-        ':' + pad(date.getMinutes()) +
-        ':' + pad(date.getSeconds()) +
-        dif + pad(tzo / 60) +
-        ':' + pad(tzo % 60)
-}
-
 function useFetchEvents(): (start: Date, end: Date) => Promise<void> {
     const dispatch = useAppDispatch()
     const fetchEvents = useCallback(async (start: Date, end: Date) => {
@@ -100,8 +82,8 @@ function useFetchEvents(): (start: Date, end: Date) => Promise<void> {
                 url: EVENTS_URL,
                 method: 'GET',
                 params: {
-                    datetime_start: getISOString(start),
-                    datetime_end: getISOString(end),
+                    datetime_start: start.toISOString(),
+                    datetime_end: end.toISOString(),
                 },
                 abortID: AbortID.EVENTS,
             })
--- frontend/src/helpers/utils.ts
+++ frontend/src/helpers/utils.ts
@@ -188,21 +188,3 @@ export function dateIsToday(date: Date): boolean {
         date.getFullYear() === today.getFullYear()
     )
 }
-
-export function getISOString(date: Date): string {
-    const tzo = -date.getTimezoneOffset()
-    const dif = tzo >= 0 ? '+' : '-'
-    const pad = (num: number) => {
-        const norm = Math.floor(Math.abs(num))
-        return (norm < 10 ? '0' : '') + norm
-    }
-
-    return date.getFullYear() +
-        '-' + pad(date.getMonth() + 1) +
-        '-' + pad(date.getDate()) +
-        'T' + pad(date.getHours()) +
-        ':' + pad(date.getMinutes()) +
-        ':' + pad(date.getSeconds()) +
-        dif + pad(tzo / 60) +
-        ':' + pad(tzo % 60)
-}
