029a6cf881a703e3b1c2d97e29199398ec1d551d
Nolan Jimenez <nolan1299@gmail.com>
2022-01-20 14:11:57 -0800

    style fix for overflow event name + reformatted join meeting button to match figma

--- frontend/src/components/task/JoinConferenceButton.tsx
+++ frontend/src/components/task/JoinConferenceButton.tsx
@@ -1,35 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Margin } from '../../helpers/styles'
 import { TConferenceCall } from '../../helpers/types'
-import GTButton from '../common/GTButton'
 
+const JointButtonContainer = styled.div`
+    background: #3F3F46;
+    padding: 6px 15px;
+    border-radius: 10px;
+    margin-left: 10px;
+`
 const Logo = styled.img`
     max-height: 16px;
-    margin-right: 6px;
 `
-const JoinConference = styled.div`
+const JoinConferenceHeader = styled.div`
     display: flex;
     align-items: center;
-    font-size: 16px;
+    font-size: 13px;
+    justify-content: space-between;
+    color: white;
+    gap: 3px;
 `
 const Link = styled.a`
     text-decoration: none;
+    margin-left: auto;
 `
 
 interface Props {
     conferenceCall: TConferenceCall
 }
-
 function JoinConferenceButton({ conferenceCall }: Props): JSX.Element {
     return (
         <Link href={conferenceCall.url} target="_blank" onClick={(e) => e.stopPropagation()}>
-            <GTButton theme="black" margin={Margin.ml10}>
-                <JoinConference>
+            <JointButtonContainer>
+                <JoinConferenceHeader>
+                    <div>Join</div>
                     <Logo src={conferenceCall.logo} />
-                    Join Meeting
-                </JoinConference>
-            </GTButton>
+                </JoinConferenceHeader>
+            </JointButtonContainer>
         </Link>
     )
 }
