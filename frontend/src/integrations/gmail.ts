dd30098848f6ec0303e4261e1bce85b0c29bb08e
Jack Hamilton <john.hamilton@wustl.edu>
2022-01-25 14:30:26 -0800

    confusion is me

--- frontend/src/integrations/gmail.ts
+++ frontend/src/integrations/gmail.ts
@@ -6,5 +6,28 @@ interface EmailContents {
     body: string
 }
 
-export const getEmails = (emailInput) => {
+export const signIn = () => {
+
+}
+
+export const getEmails = (emailInput: string) => {
     let result = []
+}
+
+export const initGmailClient = (apiKey: string, clientID: string) => {
+
+    const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest']
+    const SCOPES = 'https://mail.google.com'
+
+    const gapi = window.gapi
+
+    gapi.client.init({
+        apiKey: apiKey,
+        clientId: clientID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES
+    })
+
+    gapi.client.load('gmail', 'v1')
+
+}
