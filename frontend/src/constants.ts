d59c8b509a1256eeeb10d22cd25555995f998efb
Jack Hamilton <john.hamilton@wustl.edu>
2022-01-25 09:22:10 -0800

    yes

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -48,3 +48,6 @@ export const TASKS_FETCH_INTERVAL = 30
 
 export const GT_TASK_SOURCE_ID = 'gt_task'
 export const NOW = 'Now'
+
+//Integrations
+export const GOOGLE_URL = 'https://accounts.google.com/o/oauth2/auth'
