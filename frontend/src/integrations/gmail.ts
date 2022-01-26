ce937ef2621089c9db08a7e3199212daa0cccbae
Jack Hamilton <john.hamilton@wustl.edu>
2022-01-25 16:37:44 -0800

    initial tests were a failure, proceeding with caution

--- frontend/src/integrations/gmail.ts
+++ frontend/src/integrations/gmail.ts
@@ -7,27 +7,35 @@ interface EmailContents {
 }
 
 export const signIn = () => {
-
+    initGmailClient('dummy-value', '')
+    getEmails()
 }
 
-export const getEmails = (emailInput: string) => {
-    let result = []
+export const getEmails = () => {
+    // let result = []
+
+    gapi.client.request({
+        'path': 'https://people.googleapis.com/v1/people/me?requestMask.includeField=person.names',
+    }).then(function (response) {
+        console.log(response.result)
+    }, function (reason) {
+        console.log('Error: ', reason.result.error.message)
+    })
 }
 
-export const initGmailClient = (apiKey: string, clientID: string) => {
+export const initGmailClient = (apiKey: string, clientID: string): Promise<void> => {
+
+    const GOOGLE_OAUTH_CLIENT_ID = '786163085684-uvopl20u17kp4p2vd951odnm6f89f2f6.apps.googleusercontent.com'
 
     const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest']
     const SCOPES = 'https://mail.google.com'
 
     const gapi = window.gapi
 
-    gapi.client.init({
+    return gapi.client.init({
         apiKey: apiKey,
-        clientId: clientID,
+        clientId: GOOGLE_OAUTH_CLIENT_ID,
         discoveryDocs: DISCOVERY_DOCS,
         scope: SCOPES
     })
-
-    gapi.client.load('gmail', 'v1')
-
 }
