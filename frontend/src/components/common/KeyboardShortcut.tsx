495afad8d14da68808e04c5b6755dfa69c32701b
Jack Hamilton <john.hamilton@wustl.edu>
2022-01-28 12:02:22 -0800

    update styles to use constants

--- frontend/src/components/common/KeyboardShortcut.tsx
+++ frontend/src/components/common/KeyboardShortcut.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
+import { BACKGROUND_WHITE, SHADOW_KEYBOARD_SHORTCUT, TEXT_GRAY } from '../../helpers/styles'
 
 const KeyboardShortcutContainer = styled.div`
-height: 24px;
-width: 24px;
-display: flex;
-flex-shrink: 0;
-background: rgba(255, 255, 255, 0.8);
-box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06);
-margin-right: 12px;
-border-radius: 5px;
+    height: 24px;
+    width: 24px;
+    display: flex;
+    flex-shrink: 0;
+    background: ${BACKGROUND_WHITE};
+    box-shadow: ${SHADOW_KEYBOARD_SHORTCUT};
+    margin-right: 12px;
+    border-radius: 5px;
 
-font-family: Switzer-Variable;
-font-style: normal;
-font-weight: 500;
-font-size: 12px;
-line-height: 16px;
-color: #A1A1AA;
-display: flex;
-align-items: center;
-justify-content: center;
-text-align: center;
+    font-family: Switzer-Variable;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 16px;
+    color: ${TEXT_GRAY};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
 `
 
 interface KeyboardShortcutProps {
