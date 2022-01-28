199c8ba775d79ac1b68a13023d09767dbe9d42f5
Jack Hamilton <john.hamilton@wustl.edu>
2022-01-28 15:52:05 -0800

    remove unused funcs and imports

--- frontend/src/components/messages/Message.tsx
+++ frontend/src/components/messages/Message.tsx
@@ -6,8 +6,8 @@ import { useFetchMessages } from './MessagesPage'
 import { collapseBody, expandBody, removeMessageByID } from '../../redux/messagesPageSlice'
 import { logEvent, makeAuthorizedRequest } from '../../helpers/utils'
 import { LogEvents } from '../../helpers/enums'
-import { ButtonIcon, ButtonRight, ButtonRightContainer, DoneButton, HeaderLeft, HeaderRight, TaskHeaderContainer, Title } from '../task/TaskHeader-style'
-import { DONE_BUTTON, EXPAND_ICON, MESSAGES_MODIFY_URL } from '../../constants'
+import { ButtonIcon, ButtonRight, ButtonRightContainer, HeaderLeft, HeaderRight, TaskHeaderContainer, Title } from '../task/TaskHeader-style'
+import { EXPAND_ICON, MESSAGES_MODIFY_URL } from '../../constants'
 import { Action } from '@reduxjs/toolkit'
 import MessageBody from './MessageBody'
 import { DateTime } from 'luxon'
@@ -36,11 +36,6 @@ const MessageHeader: React.FC<MessageHeaderProps> = (props: MessageHeaderProps) 
             }
         }
     }, [hoverEffectEnabled, props.isExpanded])
-
-    const onDoneButtonClick = useCallback(() => {
-        done(props.message.id, dispatch, fetchMessages)
-        logEvent(LogEvents.MESSAGE_MARK_AS_DONE)
-    }, [])
 
     return (
         <TaskHeaderContainer hoverEffect={hoverEffectEnabled} showButtons={props.isExpanded} onClick={onClick}>
@@ -71,24 +66,6 @@ const MessageHeader: React.FC<MessageHeaderProps> = (props: MessageHeaderProps) 
             </HeaderRight >
         </TaskHeaderContainer >
     )
-}
-
-const done = async (id: string, dispatch: Dispatch<Action<string>>, fetchMessages: () => void) => {
-    try {
-        dispatch(removeMessageByID(id))
-        const response = await makeAuthorizedRequest({
-            url: MESSAGES_MODIFY_URL + id + '/',
-            method: 'PATCH',
-            body: JSON.stringify({ is_completed: true }),
-        })
-
-        if (!response.ok) {
-            throw new Error('PATCH /messages/modify Mark as Done failed: ' + response.text())
-        }
-        fetchMessages()
-    } catch (e) {
-        console.log({ e })
-    }
 }
 
 const read = async (id: string, fetchMessages: () => void) => {
