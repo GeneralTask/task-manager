e584a26531206f58e612934b35506ef3bea0defb
Jack Hamilton <john.hamilton@wustl.edu>
2022-02-03 12:55:13 -0800

    label things

---
+++ frontend/src/components/task/header/options/Label.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { TASKS_MODIFY_URL, LABEL_ICON } from '../../../../constants'
+import { TTask } from '../../../../helpers/types'
+import { sectionDropReorder, makeAuthorizedRequest } from '../../../../helpers/utils'
+import { useAppDispatch, useAppSelector } from '../../../../redux/hooks'
+import { hideLabelSelector, setTasks } from '../../../../redux/tasksPageSlice'
+import { useFetchTasks } from '../../TasksPage'
+
+import { LabelContainer, LabelHeader, LabelIcon, LabelOption } from './Label-style'
+
+interface LabelProps {
+    task: TTask
+}
+
+export default function Label({ task }: LabelProps): JSX.Element {
+    const dispatch = useAppDispatch()
+    const fetchTasks = useFetchTasks()
+
+    const { taskSections } = useAppSelector((state) => ({
+        taskSections: state.tasks_page.tasks.task_sections,
+    }))
+
+    // const currentTaskSection = taskSections.find((s) => s.tasks.includes(task))
+    const currentTaskSectionIndex = taskSections.findIndex((s) => s.tasks.includes(task))
+
+    return (
+        <LabelContainer onClick={e => e.stopPropagation()} >
+            <LabelHeader>Edit Label</LabelHeader>
+            {
+                taskSections.map((newSection, newSectionIndex) => {
+                    return (
+                        <LabelOption key={newSectionIndex} onClick={
+                            (e) => {
+                                e.stopPropagation()
+                                if (newSectionIndex === currentTaskSectionIndex) {
+                                    dispatch(hideLabelSelector())
+                                    return
+                                }
+                                const newTaskSections = sectionDropReorder(taskSections, newSectionIndex, {
+                                    // task: 0,
+                                    task: task.id_ordering - 1,
+                                    section: currentTaskSectionIndex,
+                                })
+                                dispatch(setTasks(newTaskSections))
+                                dispatch(hideLabelSelector())
+
+                                const patchBody = JSON.stringify({
+                                    id_task_section: newSection.id,
+                                    id_ordering: newTaskSections[newSectionIndex].tasks[0].id_ordering,
+                                })
+
+                                makeAuthorizedRequest({
+                                    url: TASKS_MODIFY_URL + task.id + '/',
+                                    method: 'PATCH',
+                                    body: patchBody,
+                                })
+                                    .then(fetchTasks)
+                                    .catch((error) => {
+                                        throw new Error('PATCH /tasks/ failed' + error)
+                                    })
+                            }
+                        }>
+                            <LabelIcon src={LABEL_ICON} />
+                            {newSection.name}
+                        </LabelOption>
+                    )
+                })
+            }
+        </LabelContainer>
+    )
+}
