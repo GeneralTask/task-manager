b8c2d51184f139c4b11c1e46e8e5f16e63af2857
yohanstra <jreinstra@gmail.com>
2022-02-07 20:47:13 -0800

    save

--- backend/constants/starter_tasks.go
+++ backend/constants/starter_tasks.go
@@ -1,7 +1,7 @@
 package constants
 
 var StarterTasks = []string{
-	"Welcome to General Task! Here are some tasks to get you started.",
-	"Try creating a task above for something you need to do!",
-	"Link your email and task manager accounts in the settings page!",
+	"🎉 Welcome! Here are some tasks to get you started.",
+	"Try creating a task above! 👆",
+	"👈 Link your email and task accounts in settings!",
 }
