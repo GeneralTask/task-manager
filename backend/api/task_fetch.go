0d5a9306ad1ccd0810569621d8205f04cf9b79cf
alfaindia <97246822+alfaindia@users.noreply.github.com>
2022-02-14 15:26:46 -0600

    Add tasks/fetch and tasks/v3 endpoints to fetch externally to db and load directly from db (#623)
    * Add tasks/fetch/ endpoint
    
    * Add tasks/fetch/ and tasks/v3 endpoint loading from db
    
    * Add tasks/fetch/ and tasks/v3 endpoint loading from db
    
    Co-authored-by: wooglin <alfaindiagt@protonmail.com>

---
+++ backend/api/task_fetch.go
@@ -0,0 +1,67 @@
+package api
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/GeneralTask/task-manager/backend/constants"
+
+	"github.com/GeneralTask/task-manager/backend/database"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (api *API) TasksFetch(c *gin.Context) {
+	parentCtx := c.Request.Context()
+	db, dbCleanup, err := database.GetDBConnection()
+	if err != nil {
+		Handle500(c)
+		return
+	}
+
+	defer dbCleanup()
+	userID, _ := c.Get("user")
+	var userObject database.User
+	userCollection := database.GetUserCollection(db)
+	dbCtx, cancel := context.WithTimeout(parentCtx, constants.DatabaseTimeout)
+	defer cancel()
+	err = userCollection.FindOne(dbCtx, bson.M{"_id": userID}).Decode(&userObject)
+
+	if err != nil {
+		log.Printf("failed to find user: %v", err)
+		Handle500(c)
+		return
+	}
+
+	currentTasks, err := database.GetActiveTasks(db, userID.(primitive.ObjectID))
+	if err != nil {
+		Handle500(c)
+		return
+	}
+
+	fetchedTasks, err := api.fetchTasks(parentCtx, db, userID)
+	if err != nil {
+		Handle500(c)
+		return
+	}
+	dbCtx, cancel = context.WithTimeout(parentCtx, constants.DatabaseTimeout)
+	defer cancel()
+	_, err = userCollection.UpdateOne(
+		dbCtx,
+		bson.M{"_id": userID},
+		bson.M{"$set": bson.M{"last_refreshed": primitive.NewDateTimeFromTime(time.Now())}},
+	)
+	if err != nil {
+		log.Printf("failed to update user last_refreshed: %v", err)
+	}
+
+	err = adjustForCompletedTasks(db, currentTasks, fetchedTasks)
+	if err != nil {
+		Handle500(c)
+		return
+	}
+
+	c.JSON(200, gin.H{})
+}
