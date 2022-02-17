8539931cd6664c53c80a6af14698f3c990340f6e
Nolan Jimenez <nolan1299@gmail.com>
2022-02-17 11:20:26 -0800

    added redux

--- frontend-new/App.tsx
+++ frontend-new/App.tsx
@@ -5,7 +5,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import LandingScreen from './src/screens/LandingScreen'
 import { isAuthenticated } from './src/utils/auth'
 import TasksScreen from './src/screens/TasksScreen'
-
+import { Provider } from 'react-redux'
+import store from './src/redux/store'
 
 const linking = {
   prefixes: ['https://generaltask.com', 'generaltask://'],
@@ -19,23 +20,25 @@ const linking = {
 const App = () => {
   const signedIn = isAuthenticated()
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <NavigationContainer linking={linking}>
-        <Stack.Navigator initialRouteName='Landing' screenOptions={{ headerShown: false }} >
-          {
-            signedIn ? (
-              <>
-                <Stack.Screen name="Landing" component={TasksScreen} />
-              </>
-            ) : (
-              <>
-                <Stack.Screen name="Landing" component={LandingScreen} />
-              </>
-            )
-          }
-        </Stack.Navigator>
-      </NavigationContainer >
-    </SafeAreaView >
+    <Provider store={store}>
+      <SafeAreaView style={styles.safeArea}>
+        <NavigationContainer linking={linking}>
+          <Stack.Navigator initialRouteName='Landing' screenOptions={{ headerShown: false }} >
+            {
+              signedIn ? (
+                <>
+                  <Stack.Screen name="Landing" component={TasksScreen} />
+                </>
+              ) : (
+                <>
+                  <Stack.Screen name="Landing" component={LandingScreen} />
+                </>
+              )
+            }
+          </Stack.Navigator>
+        </NavigationContainer >
+      </SafeAreaView >
+    </Provider>
 
   )
 }
