74514e60b0f41edc13bfb5999c52f95dda125b68
Nolan Jimenez <nolan1299@gmail.com>
2022-02-19 10:53:52 -0800

    added auth flow for web and mobile (#632)
    * added auth flow for web and mobile
    
    * environment variables

--- frontend-new/src/screens/TasksScreen.tsx
+++ frontend-new/src/screens/TasksScreen.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { View, Text, StyleSheet, Pressable } from 'react-native'
+import { View, Text, StyleSheet, Pressable, Platform } from 'react-native'
+import { useAppDispatch } from '../redux/hooks'
 import { Screens, Flex } from '../styles'
 import { authSignOut } from '../utils/auth'
 
 
 const TasksScreen = () => {
+    const dispatch = useAppDispatch()
     return (
         <View style={styles.container}>
             <Text>Authorized Tasks Screen</Text>
-            <Pressable style={styles.signOut} onPress={() => authSignOut()}>
+            <Pressable style={styles.signOut} onPress={() => authSignOut(dispatch)}>
                 <Text>Sign Out</Text>
             </Pressable>
         </View>
@@ -21,7 +23,12 @@ const styles = StyleSheet.create({
         ...Flex.column,
     },
     signOut: {
-        width: '100px',
+        ...Platform.select({
+            ios: {},
+            default: {
+                width: '100px',
+            }
+        }),
         backgroundColor: 'red'
     }
 })
