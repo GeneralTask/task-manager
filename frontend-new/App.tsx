bc742afde3ea7c1ac7720bfb43d0f1778d8bfeaf
Nolan Jimenez <nolan1299@gmail.com>
2022-02-22 10:52:28 -0800

    typo

--- frontend-new/App.tsx
+++ frontend-new/App.tsx
@@ -1,4 +1,4 @@
-import React, from 'react'
+import React from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
