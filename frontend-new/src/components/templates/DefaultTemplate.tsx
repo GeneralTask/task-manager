537970e4fc20d009ca1f8612e92e5d58e0e4314b
Nolan Jimenez <nolan1299@gmail.com>
2022-03-07 18:58:14 -0800

    lint

--- frontend-new/src/components/templates/DefaultTemplate.tsx
+++ frontend-new/src/components/templates/DefaultTemplate.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Platform, StyleSheet, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Flex } from '../../styles'
 import CalendarSidebar from '../calendar/CalendarSidebar'
 import Navbar from '../views/NavigationView'
