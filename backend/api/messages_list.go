fdfe0a1f9e7dad5cbbf733ef5ad431cdf8316442
alfaindia <97246822+alfaindia@users.noreply.github.com>
2022-03-08 17:19:14 -0600

    Remove messages/v1 and tasks/v1 & tasks/v2 endpoints (#696)
    Co-authored-by: wooglin <alfaindiagt@protonmail.com>

--- backend/api/messages_list.go
+++ backend/api/messages_list.go
@@ -1,20 +1,13 @@
 package api
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"sort"
 	"time"
 
-	"github.com/GeneralTask/task-manager/backend/config"
-	"github.com/GeneralTask/task-manager/backend/constants"
-	"github.com/GeneralTask/task-manager/backend/external"
 	"github.com/GeneralTask/task-manager/backend/settings"
 
 	"github.com/GeneralTask/task-manager/backend/database"
-	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -37,146 +30,6 @@ type message struct {
 	SentAt     string             `json:"sent_at"`
 	IsUnread   bool               `json:"is_unread"`
 	Source     messageSource      `json:"source"`
-}
-
-func (api *API) MessagesList(c *gin.Context) {
-	parentCtx := c.Request.Context()
-	db, dbCleanup, err := database.GetDBConnection()
-	if err != nil {
-		Handle500(c)
-		return
-	}
-
-	defer dbCleanup()
-	externalAPITokenCollection := database.GetExternalTokenCollection(db)
-	userID, _ := c.Get("user")
-	var userObject database.User
-	userCollection := database.GetUserCollection(db)
-	dbCtx, cancel := context.WithTimeout(parentCtx, constants.DatabaseTimeout)
-	defer cancel()
-	err = userCollection.FindOne(dbCtx, bson.M{"_id": userID}).Decode(&userObject)
-	if err != nil {
-		log.Printf("failed to find user: %v", err)
-		Handle500(c)
-		return
-	}
-
-	currentEmails, err := database.GetEmails(db, userID.(primitive.ObjectID), true, database.Pagination{})
-	if err != nil {
-		Handle500(c)
-		return
-	}
-
-	var tokens []database.ExternalAPIToken
-	dbCtx, cancel = context.WithTimeout(parentCtx, constants.DatabaseTimeout)
-	defer cancel()
-	cursor, err := externalAPITokenCollection.Find(
-		dbCtx,
-		bson.M{"user_id": userID},
-	)
-	if err != nil {
-		log.Printf("failed to fetch api tokens: %v", err)
-		Handle500(c)
-		return
-	}
-	dbCtx, cancel = context.WithTimeout(parentCtx, constants.DatabaseTimeout)
-	defer cancel()
-	err = cursor.All(dbCtx, &tokens)
-	if err != nil {
-		log.Printf("failed to iterate through api tokens: %v", err)
-		Handle500(c)
-		return
-	}
-
-	emailChannels := []chan external.EmailResult{}
-	tokensInUse := []*database.ExternalAPIToken{}
-	// Loop through linked accounts and fetch relevant items
-	for index, token := range tokens {
-		taskServiceResult, err := api.ExternalConfig.GetTaskServiceResult(token.ServiceID)
-		if err != nil {
-			log.Printf("error loading task service: %v", err)
-			Handle500(c)
-			return
-		}
-		for _, taskSource := range taskServiceResult.Sources {
-			var emails = make(chan external.EmailResult)
-			go taskSource.GetEmails(userID.(primitive.ObjectID), token.AccountID, emails)
-			emailChannels = append(emailChannels, emails)
-			// need to use tokens array here to ensure pointer isn't same memory address each time
-			tokensInUse = append(tokensInUse, &tokens[index])
-		}
-	}
-
-	fetchedEmails := []*database.Item{}
-	badTokens := []*database.ExternalAPIToken{}
-	for index, emailChannel := range emailChannels {
-		emailResult := <-emailChannel
-		if emailResult.Error != nil {
-			if emailResult.IsBadToken {
-				badTokens = append(badTokens, tokensInUse[index])
-			}
-			continue
-		}
-		fetchedEmails = append(fetchedEmails, emailResult.Emails...)
-	}
-
-	for index := range fetchedEmails {
-		fetchedEmails[index].TaskBase.Body = "<base target=\"_blank\">" + fetchedEmails[index].TaskBase.Body
-	}
-
-	err = markReadMessagesInDB(api, db, currentEmails, &fetchedEmails)
-	if err != nil {
-		Handle500(c)
-		return
-	}
-
-	orderedMessages, err := api.orderMessages(
-		db,
-		fetchedEmails,
-		userID.(primitive.ObjectID),
-	)
-	if err != nil {
-		Handle500(c)
-		return
-	}
-	for _, token := range badTokens {
-		// For now, marking as GT_TASK source to show visual distinction from emails
-		taskSourceResult, err := api.ExternalConfig.GetTaskSourceResult(external.TASK_SOURCE_ID_GT_TASK)
-		if err != nil {
-			log.Printf("error loading task service: %v", err)
-			Handle500(c)
-			return
-		}
-		body := (`<!DOCTYPE html><html lang="en"><head></head><body>Please un-link and re-link your email account ` +
-			`in the settings page to continue seeing messages from this account. If this is your primary account, ` +
-			`you will need to visit the following link to reauthorize: ` +
-			`<a href="%slogin/?force_prompt=true">Click here</a><br><br><i>Note: once we are verified by Google, this ` +
-			`issue will happen less often!</i></body></html>`)
-		body = fmt.Sprintf(body, config.GetConfigValue("SERVER_URL"))
-		if err != nil {
-			log.Printf("failed to convert plain text to HTML: %v", err)
-			continue
-		}
-		orderedMessages = append([]*message{
-			{
-				ID:       primitive.NilObjectID,
-				Title:    fmt.Sprintf("%s needs to be re-authorized!", token.AccountID),
-				Deeplink: "",
-				Body:     body,
-				Sender:   "General Task",
-				SentAt:   time.Now().Format(time.RFC3339),
-				IsUnread: false,
-				Source: messageSource{
-					AccountId:     token.AccountID,
-					Name:          taskSourceResult.Details.Name,
-					Logo:          taskSourceResult.Details.Logo,
-					IsCompletable: taskSourceResult.Details.IsCreatable,
-					IsReplyable:   taskSourceResult.Details.IsReplyable,
-				},
-			},
-		}, orderedMessages...)
-	}
-	c.JSON(200, orderedMessages)
 }
 
 func (api *API) orderMessages(
