360f9233269d215f6a82f55f13515e69bf61326e
Nolan Jimenez <nolan1299@gmail.com>
2022-03-08 08:59:24 -0800

    empty calendar component for iOS (#693)
    * created empty component for ios so calendar style-components import doesn't cause the app to crash
    
    * lint

--- frontend-new/src/components/templates/DefaultTemplate.tsx
+++ frontend-new/src/components/templates/DefaultTemplate.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Platform, StyleSheet, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { Flex } from '../../styles'
 import CalendarSidebar from '../calendar/CalendarSidebar'
 import Navbar from '../views/NavigationView'
@@ -12,7 +12,7 @@ const DefaultTemplate = ({ children }: DefaultTemplateProps) => {
         <View style={styles.container}>
             <Navbar />
             {children}
-            {Platform.OS === 'web' && <CalendarSidebar />}
+            <CalendarSidebar />
         </View>
     )
 }
