826e9f8f6fc5dc16c34c62ffe026b972fe625dfd
Jack Hamilton <john.hamilton@wustl.edu>
2022-03-09 12:30:31 -0800

    fix event overflow

--- frontend-new/src/components/calendar/EventBody.tsx
+++ frontend-new/src/components/calendar/EventBody.tsx
@@ -1,15 +1,9 @@
-import {
-    EventBodyStyle,
-    EventInfoContainer,
-    EventInfo,
-    EventTitle,
-    EventTime,
-    EventFillContinues,
-    EventFill,
-    CELL_HEIGHT,
-} from './CalendarEvents-styles'
+import { DateTime } from 'luxon'
 import React from 'react'
 import { TEvent } from '../../utils/types'
+import {
+    CELL_HEIGHT, EventBodyStyle, EventFill, EventInfo, EventInfoContainer, EventTime, EventTitle
+} from './CalendarEvents-styles'
 
 const LONG_EVENT_THRESHOLD = 45 // minutes
 const MINIMUM_BODY_HEIGHT = 15 // minutes
@@ -18,48 +12,42 @@ interface EventBodyProps {
     event: TEvent
     collisionGroupSize: number
     leftOffset: number
+    date: DateTime
 }
-function EventBody({ event, collisionGroupSize, leftOffset }: EventBodyProps): JSX.Element {
-    const startTime = new Date(event.datetime_start)
-    const endTime = new Date(event.datetime_end)
-    const timeDurationMinutes = (endTime.getTime() - startTime.getTime()) / 1000 / 60
-    const timeDurationHours = timeDurationMinutes / 60
+function EventBody(props: EventBodyProps): JSX.Element {
+    const startTime = DateTime.fromISO(props.event.datetime_start)
+    const endTime = DateTime.fromISO(props.event.datetime_end)
+    const timeDurationMinutes = endTime.diff(startTime).toMillis() / 1000 / 60
+    const startedBeforeToday = startTime <= props.date.startOf('day')
+    const endedAfterToday = endTime >= props.date.endOf('day')
 
-    const rollsOverMidnight = endTime.getDay() !== startTime.getDay()
-    const eventBodyHeight = Math.max(
-        MINIMUM_BODY_HEIGHT,
-        rollsOverMidnight
-            ? ((new Date(startTime).setHours(24, 0, 0, 0) - startTime.getTime()) / 1000 / 3600) * CELL_HEIGHT
-            : timeDurationHours * CELL_HEIGHT
-    )
+    const top = startedBeforeToday ? 0 : CELL_HEIGHT * startTime.diff(props.date.startOf('day'), 'hours').hours
+    const bottom = endedAfterToday ? CELL_HEIGHT * 24 : CELL_HEIGHT * endTime.diff(props.date.startOf('day'), 'hours').hours
+    const eventBodyHeight = Math.max(bottom - top, MINIMUM_BODY_HEIGHT)
 
-    const startTimeHours = startTime.getHours() - 1
-    const startTimeMinutes = startTime.getMinutes()
-    const topOffset = (60 * startTimeHours + startTimeMinutes) * (CELL_HEIGHT / 60)
-
-    const MMHH = { hour: 'numeric', minute: 'numeric', hour12: true } as const
-    const startTimeString = startTime.toLocaleString('en-US', MMHH).replace(/AM|PM/, '')
-    const endTimeString = endTime.toLocaleString('en-US', MMHH)
+    const startTimeString = startTime.toFormat('h:mm') // ex: 3:00
+    const endTimeString = endTime.toFormat('h:mm a') // ex: 3:00 PM
 
     const isLongEvent = timeDurationMinutes >= LONG_EVENT_THRESHOLD
-    const eventHasEnded = endTime.getTime() < Date.now()
+    const eventHasEnded = endTime.toMillis() < DateTime.now().toMillis()
 
     return (
         <EventBodyStyle
-            key={event.id}
-            squishFactor={collisionGroupSize}
-            leftOffset={leftOffset}
-            topOffset={topOffset}
+            key={props.event.id}
+            squishFactor={props.collisionGroupSize}
+            leftOffset={props.leftOffset}
+            topOffset={top}
             eventBodyHeight={eventBodyHeight}
             eventHasEnded={eventHasEnded}
         >
             <EventInfoContainer>
                 <EventInfo isLongEvent={isLongEvent}>
-                    <EventTitle isLongEvent={isLongEvent}>{event.title || '(no title)'}</EventTitle>
+                    <EventTitle isLongEvent={isLongEvent}>{props.event.title || '(no title)'}</EventTitle>
                     <EventTime>{`${startTimeString} - ${endTimeString}`}</EventTime>
                 </EventInfo>
             </EventInfoContainer>
-            {rollsOverMidnight ? <EventFillContinues /> : <EventFill />}
+            {/* {rollsOverMidnight ? <EventFillContinues /> : <EventFill />} */}
+            <EventFill squareStart={startedBeforeToday} squareEnd={endedAfterToday} />
         </EventBodyStyle>
     )
 }
