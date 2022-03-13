dd554da44fe1622c236d8877ec84a53d8a99b611
Scott Mai <scottmai@usc.edu>
2022-03-13 01:17:07 -0800

    Optimistic rendering xD

--- frontend-new/src/utils/api.ts
+++ frontend-new/src/utils/api.ts
@@ -1,3 +1,4 @@
+import { TTask } from './types'
 import getEnvVars from '../environment'
 
 const { REACT_APP_FRONTEND_BASE_URL } = getEnvVars()
@@ -10,6 +11,24 @@ export const getHeaders = (): Record<string, string> => {
             'Authorization,Access-Control-Allow-Origin,Access-Control-Allow-Headers,Access-Control-Allow-Methods,Timezone-Offset',
         'Access-Control-Allow-Methods': 'POST,OPTIONS,GET,PATCH,DELETE',
         'Timezone-Offset': date.getTimezoneOffset().toString(),
+    }
+}
+
+export function resetOrderingIds(tasks: TTask[]) {
+    for (let i = 1; i < tasks.length; i++) {
+        tasks[i].id_ordering = i
+    }
+}
+
+// https://github.com/sindresorhus/array-move/blob/main/index.js
+export function arrayMoveInPlace<T>(array: Array<T>, fromIndex: number, toIndex: number) {
+    const startIndex = fromIndex < 0 ? array.length + fromIndex : fromIndex;
+
+    if (startIndex >= 0 && startIndex < array.length) {
+        const endIndex = toIndex < 0 ? array.length + toIndex : toIndex;
+
+        const [item] = array.splice(fromIndex, 1);
+        array.splice(endIndex, 0, item);
     }
 }
 
