a81589950a5fe27089c0371768eda8af6906716a
Nolan Jimenez <nolan1299@gmail.com>
2022-03-17 11:30:10 -0700

    removed redirect logic, should be in future PR

--- frontend-new/src/screens/TasksScreen.tsx
+++ frontend-new/src/screens/TasksScreen.tsx
@@ -7,17 +7,12 @@ import Messages from '../components/views/MessagesView'
 import TaskBottomSheet from '../components/views/TaskBottomSheetView'
 import TaskSection from '../components/views/TaskSectionView'
 import Settings from '../components/views/SettingsView'
-import { Navigate, useLocation } from '../services/routing'
-import { useQuery } from 'react-query'
-import { fetchUserInfo } from '../services/queryUtils'
-import Loading from '../components/atoms/Loading'
+import { useLocation } from '../services/routing'
 
 const TasksScreen = () => {
     const [sheetTaskId, setSheetTaskId] = useState('')
     const sheetRef = React.useRef<BottomSheet>(null)
     const location = useLocation()
-
-    const { isLoading, data } = useQuery('user_info', fetchUserInfo)
 
     const currentPage = (() => {
         switch (location.pathname.split('/')[1]) {
@@ -32,12 +27,6 @@ const TasksScreen = () => {
         }
     })()
 
-    if (!isLoading && !data.agreed_to_terms) {
-        return <Navigate to="/tos-summary"></Navigate>
-    }
-    if (isLoading) {
-        return <Loading />
-    }
     return (
         <>
             <DefaultTemplate>
