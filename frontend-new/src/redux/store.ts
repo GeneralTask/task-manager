8aebf898d769125060922c135715b33e33afd48d
Nolan Jimenez <nolan1299@gmail.com>
2022-03-17 19:01:07 -0700

    removed unused redux values (#741)
    * removed unused redux values
    
    * fixed

--- frontend-new/src/redux/store.ts
+++ frontend-new/src/redux/store.ts
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userDataReducer from './userDataSlice'
 import tasksPageReducer from './tasksPageSlice'
-import messagesPageReducer from './messagesPageSlice'
 import { generalTaskApi } from '../services/generalTaskApi'
 
 export const store = configureStore({
     reducer: {
         [generalTaskApi.reducerPath]: generalTaskApi.reducer,
         tasks_page: tasksPageReducer,
-        messages_page: messagesPageReducer,
         user_data: userDataReducer,
     },
     devTools: true,
