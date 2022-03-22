4f47cb07abc0726ccdf0c7b64411b3bdd8b83efd
Nolan Jimenez <nolan1299@gmail.com>
2022-03-22 12:45:28 -0700

    stuff

--- frontend-new/src/components/atoms/TaskHTMLBody.tsx
+++ frontend-new/src/components/atoms/TaskHTMLBody.tsx
@@ -1,5 +1,6 @@
 import React, { forwardRef } from 'react'
 import styled from 'styled-components'
+import { ContentState, convertFromHTML, convertToRaw, Editor, EditorState } from 'draft-js'
 
 const ContentEditableDiv = styled.div`
     white-space: pre-line;
@@ -13,7 +14,20 @@ interface TaskHTMLBodyProps {
 }
 
 const ContentEditable = forwardRef<HTMLDivElement, TaskHTMLBodyProps>(({ html, onBlur }: TaskHTMLBodyProps, ref) => {
-    return <ContentEditableDiv ref={ref} onBlur={onBlur} contentEditable dangerouslySetInnerHTML={{ __html: html }} />
+    const [editorState, setEditorState] = React.useState(() => {
+        const html2 = html.replace(/(?:\r\n|\r|\n)/g, '<br>')
+        const blocksFromHTML = convertFromHTML(html2)
+        const state = ContentState.createFromBlockArray(blocksFromHTML.contentBlocks, blocksFromHTML.entityMap)
+        console.log(JSON.stringify(html2))
+        console.log(convertToRaw(state))
+        return EditorState.createWithContent(state)
+    })
+
+    const onChange = (state: EditorState) => {
+        setEditorState(state)
+    }
+
+    return <Editor editorState={editorState} onChange={onChange} />
 })
 
 export default ContentEditable
