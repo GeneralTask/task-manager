4814f9f79e7984eecb0eb1f403c7b3b5cddbe305
wooglin <alfaindiagt@protonmail.com>
2022-03-23 21:10:06 -0500

    refactor

--- backend/api/message_compose_test.go
+++ backend/api/message_compose_test.go
@@ -16,7 +16,7 @@ import (
 	"google.golang.org/api/gmail/v1"
 )
 
-func TestComposeReplyToEmail(t *testing.T) {
+func TestComposeEmail(t *testing.T) {
 	parentCtx := context.Background()
 	db, dbCleanup, err := database.GetDBConnection()
 	assert.NoError(t, err)
@@ -178,7 +178,7 @@ func TestComposeReplyToEmail(t *testing.T) {
 		assert.Equal(t, http.StatusNotFound, recorder.Code)
 	})
 
-	t.Run("SuccessNoReplyTo", func(t *testing.T) {
+	t.Run("SuccessReply", func(t *testing.T) {
 		var headers = []*gmail.MessagePartHeader{
 			{
 				Name:  "Subject",
@@ -203,39 +203,30 @@ func TestComposeReplyToEmail(t *testing.T) {
 		testSuccessfulComposeWithServer(t, emailID, authToken, "test reply", toStr, "[]", "[]", server)
 	})
 
-	// t.Run("SuccessReplyToAndExistingSubjectRe", func(t *testing.T) {
-	// 	var headers = []*gmail.MessagePartHeader{
-	// 		{
-	// 			Name:  "Subject",
-	// 			Value: "Re: Sample subject",
-	// 		},
-	// 		{
-	// 			Name:  "From",
-	// 			Value: "Sample sender <sample@generaltask.com>",
-	// 		},
-	// 		{
-	// 			Name:  "Reply-To",
-	// 			Value: "Reply address <reply@generaltask.com>",
-	// 		},
-	// 		{
-	// 			Name:  "Message-ID",
-	// 			Value: "<id2@gt.io>",
-	// 		},
-	// 		{
-	// 			Name:  "References",
-	// 			Value: "<id1@gt.io>",
-	// 		},
-	// 	}
+	t.Run("SuccessEmail", func(t *testing.T) {
+		// var headers = []*gmail.MessagePartHeader{
+		// 	// {
+		// 	// 	Name:  "Subject",
+		// 	// 	Value: "Sample subject",
+		// 	// },
+		// 	// {
+		// 	// 	Name:  "From",
+		// 	// 	Value: "Sample sender <sample@generaltask.com>",
+		// 	// },
+		// 	// {
+		// 	// 	Name:  "Message-ID",
+		// 	// 	Value: "<id1@gt.io>",
+		// 	// },
+		// }
 
-	// 	server := getReplyServer(t,
-	// 		"sample_message_id",
-	// 		"sample_thread_id",
-	// 		headers,
-	// 		"To: Reply address <reply@generaltask.com>\r\nCc: \r\nBcc: \r\nFrom: General Tasker <approved@generaltask.com>\nSubject: Re: Sample subject\nIn-Reply-To: <id2@gt.io>\nReferences: <id1@gt.io> <id2@gt.io>\nMIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\ntest reply")
-
-	// 	// testSuccessfulReplyWithServer(t, emailID, authToken, "test reply", server)
-	// 	testSuccessfulComposeWithServer(t, emailID, authToken, "test reply", server)
-	// })
+		// server := getReplyServer(t,
+		// 	"sample_message_id",
+		// 	"sample_thread_id",
+		// 	headers,
+		// 	"To: Sample sender <sample@generaltask.com>\r\nCc: \r\nBcc: \r\nFrom: General Tasker <approved@generaltask.com>\nSubject: Re: Sample subject\nIn-Reply-To: <id1@gt.io>\nReferences: <id1@gt.io>\nMIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\ntest reply")
+		// toStr := `[{"name": "Sample sender", "email": "sample@generaltask.com"}]`
+		// testSuccessfulComposeWithServer(t, emailID, authToken, "test reply", toStr, "[]", "[]", server)
+	})
 }
 
 func testSuccessfulComposeWithServer(t *testing.T,
@@ -250,11 +241,16 @@ func testSuccessfulComposeWithServer(t *testing.T,
 	api.ExternalConfig.GoogleOverrideURLs.GmailReplyURL = &server.URL
 	router := GetRouter(api)
 
+	messageIDStr := ""
+	if len(emailID) > 0 {
+		messageIDStr = `"message_id": "`+emailID+`",`
+	}
+
 	request, _ := http.NewRequest(
 		"POST",
 		"/messages/compose/",
 		bytes.NewBuffer([]byte(`{
-			"message_id": "`+emailID+`",
+			` + messageIDStr + `
 			"body": "`+body+`",
 			"recipients": {
 				"to": `+ toStr +`,
@@ -264,19 +260,6 @@ func testSuccessfulComposeWithServer(t *testing.T,
 			"source_id": "gmail",
 			"source_account_id": "approved@generaltask.com"
 		}`)))
-
-	// "/messages/compose/",
-	// bytes.NewBuffer([]byte(`{
-	// 	"body": "`+body+`",
-	// 	"message_id": "` + emailID + `",
-	// 	"recipients": {
-	// 		"to": [{"name": "Mike Yo22", "email": "gng.vike13@gmail.com"}, {"name": "Mark Smith", "email": "gng.vike13+to_2@gmail.com"}, {"name": "", "email": "gng.vike13+to_3@gmail.com"}],
-	// 		"cc": [{"name": "Jon Bravo", "email": "gng.vike13+cc_1@gmail.com"}],
-	// 		"bcc": [{"name": "Stan Lee", "email": "gng.vike13+bcc_1@gmail.com"}]
-	// 	},
-	// 	"source_id": "gmail",
-	// 	"source_account_id": "maz@generaltask.com"
-	// }`)))
 
 	request.Header.Add("Authorization", "Bearer "+authToken)
 	recorder := httptest.NewRecorder()
