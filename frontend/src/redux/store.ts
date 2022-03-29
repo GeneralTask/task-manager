7994ea2f1fa571c69afeb61ff2e3aeeb4722ef5d
Nolan Jimenez <nolan1299@gmail.com>
2022-03-29 16:11:53 -0700

    remove rtk query middleware

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,17 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userDataReducer from './userDataSlice'
 import tasksPageReducer from './tasksPageSlice'
-import { generalTaskApi } from '../services/generalTaskApi'
 
 export const store = configureStore({
     reducer: {
-        [generalTaskApi.reducerPath]: generalTaskApi.reducer,
         tasks_page: tasksPageReducer,
         user_data: userDataReducer,
     },
     devTools: true,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(generalTaskApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
