2cd9c70f5ae1c06afe7c5b2a418e742133435590
Jack Hamilton <31417618+johnhamilto@users.noreply.github.com>
2022-03-29 14:17:30 -0700

    Email details (#785)
    * initial email details
    
    * body container fixed
    
    * re-add calendar
    
    * sanitize html
    
    * separate details
    
    * add dependencies
    
    * make template >:^(
    
    * dont rebuild tooltips when we dont need to
    
    * accept everyone and div

--- frontend/src/components/views/MessagesView.tsx
+++ frontend/src/components/views/MessagesView.tsx
@@ -1,16 +1,31 @@
-import React, { useCallback, useEffect, useRef } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { Platform, ScrollView, StyleSheet, View } from 'react-native'
+import { useParams } from 'react-router-dom'
 import { useFetchMessages, useGetInfiniteMessages } from '../../services/api-query-hooks'
 import { Colors, Flex, Screens, Spacing } from '../../styles'
 import { TMessage } from '../../utils/types'
 import Loading from '../atoms/Loading'
 import TaskTemplate from '../atoms/TaskTemplate'
+import MessageDetails from '../details/MessageDetails'
 import { SectionHeader } from '../molecules/Header'
 import Message from '../molecules/Message'
 
 const Messages = () => {
     const { refetch: refetchMessages } = useFetchMessages()
     const { data, isLoading, isFetching, fetchNextPage } = useGetInfiniteMessages()
+
+    const params = useParams()
+    const [selectedMessage, setSelectedMessage] = useState<TMessage | undefined>(undefined)
+    useEffect(() => {
+        setSelectedMessage(undefined)
+        data?.pages.forEach((page) => {
+            page.forEach((message: TMessage) => {
+                if (message.id === params.message) {
+                    setSelectedMessage(message)
+                }
+            })
+        })
+    }, [params, data])
 
     const observer = useRef<IntersectionObserver>()
     const lastElementRef = useCallback(
@@ -36,28 +51,31 @@ const Messages = () => {
     }, [])
 
     return (
-        <ScrollView>
-            <View style={styles.messagesContent}>
-                <SectionHeader sectionName="Messages" allowRefresh={true} refetch={refetchMessages} />
-                {data?.pages.map((page, index) => {
-                    return page?.map((message: TMessage, msgIndex: number) => {
-                        if (data.pages.length === index + 1 && page.length === msgIndex + 1) {
+        <>
+            <ScrollView>
+                <View style={styles.messagesContent}>
+                    <SectionHeader sectionName="Messages" allowRefresh={true} refetch={refetchMessages} />
+                    {data?.pages.map((page, index) => {
+                        return page?.map((message: TMessage, msgIndex: number) => {
+                            if (data.pages.length === index + 1 && page.length === msgIndex + 1) {
+                                return (
+                                    <TaskTemplate ref={lastElementRef} style={styles.shell} key={message.id}>
+                                        <Message message={message} setSheetTaskId={() => null} />
+                                    </TaskTemplate>
+                                )
+                            }
                             return (
-                                <TaskTemplate ref={lastElementRef} style={styles.shell} key={message.id}>
+                                <TaskTemplate style={styles.shell} key={message.id}>
                                     <Message message={message} setSheetTaskId={() => null} />
                                 </TaskTemplate>
                             )
-                        }
-                        return (
-                            <TaskTemplate style={styles.shell} key={message.id}>
-                                <Message message={message} setSheetTaskId={() => null} />
-                            </TaskTemplate>
-                        )
-                    })
-                })}
-                {(isLoading || isFetching) && <Loading />}
-            </View>
-        </ScrollView>
+                        })
+                    })}
+                    {(isLoading || isFetching) && <Loading />}
+                </View>
+            </ScrollView>
+            {selectedMessage && <MessageDetails message={selectedMessage} />}
+        </>
     )
 }
 
