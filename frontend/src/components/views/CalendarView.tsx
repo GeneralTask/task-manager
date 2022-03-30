937c23166c7230471ee141cde9fdb3b9440a3a5c
Jack Hamilton <john.hamilton@wustl.edu>
2022-03-30 14:48:27 -0700

    initial calendar refactor

--- frontend/src/components/views/CalendarView.tsx
+++ frontend/src/components/views/CalendarView.tsx
@@ -1,32 +1,77 @@
 import { DateTime } from 'luxon'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Colors } from '../../styles'
 import CalendarEvents from '../calendar/CalendarEvents'
 import CalendarHeader from '../calendar/CalendarHeader'
 
-const CalendarViewContainer = styled.div`
+const CalendarSidebarViewContainer = styled.div`
     min-width: 300px;
     height: 100vh;
+    background-color: ${Colors.gray._100};
+    border-left: 1px solid ${Colors.gray._200};
+    display: flex;
+    flex-direction: column;
+`
+
+const CalendarFullViewContainer = styled.div`
+    flex: 1;
     background-color: ${Colors.gray._100};
     display: flex;
     flex-direction: column;
 `
 
+const WeekContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    overflow-y: scroll;
+`
+const CalendarDayHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    height: 40px;
+    font-size: 16px;
+    font-weight: 600;
+    color: ${Colors.gray._800};
+    border-bottom: 1px solid ${Colors.gray._800};
+`
+
 const CalendarView = () => {
     const [date, setDate] = useState<DateTime>(DateTime.now())
-    const [selectedDateIsToday, setSelectedDateIsToday] = useState<boolean>(true)
 
-    // keep track of when the selected date is supposed to be today
-    useEffect(() => {
-        setSelectedDateIsToday(date.day === DateTime.now().day)
-    }, [date])
-
+    // return (
+    //     <CalendarFullViewContainer>
+    //         <CalendarHeader date={date} setDate={setDate} />
+    //         <div style={{ justifyContent: 'space-around', flexDirection: 'row', display: 'flex' }}>
+    //             {Array(7)
+    //                 .fill(0)
+    //                 .map((_, i) => {
+    //                     return <CalendarDayHeader key={i}>{date.plus({ days: i }).toFormat('ccc dd')}</CalendarDayHeader>
+    //                 })}
+    //         </div>
+    //         <WeekContainer>
+    //             {Array(7)
+    //                 .fill(0)
+    //                 .map((_, i) => {
+    //                     return (
+    //                         <CalendarEvents
+    //                             key={i}
+    //                             date={date.plus({ days: i })}
+    //                             isToday={date.plus({ days: i }).day === DateTime.now().day}
+    //                         />
+    //                     )
+    //                 })}
+    //         </WeekContainer>
+    //     </CalendarFullViewContainer>
+    // )
     return (
-        <CalendarViewContainer>
+        <CalendarSidebarViewContainer>
             <CalendarHeader date={date} setDate={setDate} />
-            <CalendarEvents date={date} isToday={selectedDateIsToday} />
-        </CalendarViewContainer>
+            <CalendarEvents date={date} numDays={1} />
+        </CalendarSidebarViewContainer>
     )
 }
 
