262b28e02fb183746381a6dec52365031652b3db
Scott Mai <scottmai@usc.edu>
2022-03-30 12:36:07 -0700

    Fix useInterval in TimeIndicator

--- frontend/src/components/calendar/TimeIndicator.tsx
+++ frontend/src/components/calendar/TimeIndicator.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 import { CELL_HEIGHT } from './CalendarEvents-styles'
 import { Colors } from '../../styles'
@@ -20,7 +20,7 @@ const TimeIndicatorContainer = styled.div<TimeIndicatorContainerProps>`
 
 export function TimeIndicator(): JSX.Element {
     const [time, setTime] = useState(DateTime.now())
-    useInterval(() => setTime(DateTime.now()), TIME_INDICATOR_INTERVAL)
+    useInterval(useCallback(() => setTime(DateTime.now()), []), TIME_INDICATOR_INTERVAL)
 
     const topOffset = (60 * time.hour + time.minute) * (CELL_HEIGHT / 60)
     return <TimeIndicatorContainer topOffset={topOffset} />
