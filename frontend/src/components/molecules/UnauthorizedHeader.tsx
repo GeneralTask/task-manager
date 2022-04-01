d358a209b5f1f4aa2fe3e54f1b7f124bf72545f9
Nolan Jimenez <nolan1299@gmail.com>
2022-03-31 17:36:02 -0700

    change Views to divs in unauthorized header

--- frontend/src/components/molecules/UnauthorizedHeader.tsx
+++ frontend/src/components/molecules/UnauthorizedHeader.tsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Pressable } from 'react-native'
 import { Colors } from '../../styles'
-import styled from 'styled-components/native'
+import styled from 'styled-components'
 import { Spacing } from '../../styles'
 import { Icon } from '../atoms/Icon'
 import { Divider } from '../atoms/SectionDivider'
 import { useNavigate } from 'react-router-dom'
 
-const Header = styled.View`
+const Header = styled.div`
     width: 100%;
     position: sticky;
     top: 0;
     background-color: ${Colors.white};
     z-index: 1;
 `
-const IconContainer = styled.View`
+const IconContainer = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
