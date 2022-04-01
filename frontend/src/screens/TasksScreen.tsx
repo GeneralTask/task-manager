f38176c8de7310f897ea54256e8ae1de7b3e7d44
Nolan Jimenez <nolan1299@gmail.com>
2022-04-01 14:22:30 -0700

    defaulttemplate + taskscreen only using reactjs elements

--- frontend/src/screens/TasksScreen.tsx
+++ frontend/src/screens/TasksScreen.tsx
@@ -1,22 +1,16 @@
 import { Navigate, useLocation, useParams } from 'react-router-dom'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useGetTasks, useGetUserInfo } from '../services/api-query-hooks'
-
-import BottomSheet from 'reanimated-bottom-sheet'
 import CalendarView from '../components/views/CalendarView'
 import DefaultTemplate from '../components/templates/DefaultTemplate'
 import Loading from '../components/atoms/Loading'
 import Messages from '../components/views/MessagesView'
-import { Platform } from 'react-native'
 import Settings from '../components/views/SettingsView'
-import TaskBottomSheet from '../components/views/TaskBottomSheetView'
 import TaskSection from '../components/views/TaskSectionView'
 import { setSelectedItemId } from '../redux/tasksPageSlice'
 import { useAppDispatch } from '../redux/hooks'
 
 const TasksScreen = () => {
-    const [sheetTaskId, setSheetTaskId] = useState('')
-    const sheetRef = useRef<BottomSheet>(null)
     const location = useLocation()
     const dispatch = useAppDispatch()
     const params = useParams()
@@ -47,17 +41,12 @@ const TasksScreen = () => {
     if (!isTaskSectionsLoading && !userInfo.agreed_to_terms) return <Navigate to="/tos-summary" />
 
     return (
-        <>
-            <DefaultTemplate>
-                <>
-                    {currentPage}
-                    <CalendarView />
-                </>
-            </DefaultTemplate>
-            {Platform.OS === 'ios' && (
-                <TaskBottomSheet sheetTaskId={sheetTaskId} setSheetTaskId={setSheetTaskId} ref={sheetRef} />
-            )}
-        </>
+        <DefaultTemplate>
+            <>
+                {currentPage}
+                <CalendarView />
+            </>
+        </DefaultTemplate>
     )
 }
 
