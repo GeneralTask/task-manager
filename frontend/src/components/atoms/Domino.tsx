03fd966f8e2baac9098415923043891abe125bb6
Scott Mai <scottmai@usc.edu>
2022-04-01 12:07:00 -0700

    merge domino

--- frontend/src/components/atoms/Domino.tsx
+++ frontend/src/components/atoms/Domino.tsx
@@ -1,20 +1,12 @@
-import { Flex, Spacing } from '../../styles'
-import { StyleSheet, View } from 'react-native'
+import { Platform, StyleSheet, View } from 'react-native'
 
+import { Flex } from '../../styles'
 import React from 'react'
 import { gray } from '../../styles/colors'
-import styled from 'styled-components'
-
-const DominoOuterContainer = styled.div`
-    cursor: grab;
-    height: ${Spacing.margin.medium}px;
-    padding-left: ${Spacing.padding.xSmall}px;
-    padding-right: 12px;
-`
 
 const Domino = React.forwardRef<HTMLDivElement>((_, ref) => {
     return (
-        <DominoOuterContainer ref={ref}>
+        <div style={styles.DominoOuterContainer} ref={ref}>
             <View style={styles.DominoInnerContainer}>
                 {Array(6)
                     .fill(0)
@@ -22,11 +14,21 @@ const Domino = React.forwardRef<HTMLDivElement>((_, ref) => {
                         <View style={styles.DominoDot} key={index} />
                     ))}
             </View>
-        </DominoOuterContainer>
+        </div>
     )
 })
 
 const styles = StyleSheet.create({
+    DominoOuterContainer: {
+        ...Platform.select({
+            web: {
+                cursor: 'grab',
+            },
+        }),
+        height: 16,
+        paddingLeft: 4,
+        paddingRight: 12,
+    },
     DominoInnerContainer: {
         height: '100%',
         width: 10,
