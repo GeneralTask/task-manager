04b8b846c366bae9fa23073c3ca44b2fcce8c6c9
Scott Mai <scottmai@usc.edu>
2022-04-01 12:11:50 -0700

    Fix domino

--- frontend/src/components/atoms/Domino.tsx
+++ frontend/src/components/atoms/Domino.tsx
@@ -1,12 +1,20 @@
-import { Platform, StyleSheet, View } from 'react-native'
+import { Flex, Spacing } from '../../styles'
+import { StyleSheet, View } from 'react-native'
 
-import { Flex } from '../../styles'
 import React from 'react'
 import { gray } from '../../styles/colors'
+import styled from 'styled-components'
+
+const DominoOuterContainer = styled.div`
+    cursor: grab;
+    height: ${Spacing.margin.medium}px;
+    padding-left: ${Spacing.padding.xSmall}px;
+    padding-right: 12px;
+`
 
 const Domino = React.forwardRef<HTMLDivElement>((_, ref) => {
     return (
-        <div style={styles.DominoOuterContainer} ref={ref}>
+        <DominoOuterContainer ref={ref}>
             <View style={styles.DominoInnerContainer}>
                 {Array(6)
                     .fill(0)
@@ -14,21 +22,11 @@ const Domino = React.forwardRef<HTMLDivElement>((_, ref) => {
                         <View style={styles.DominoDot} key={index} />
                     ))}
             </View>
-        </div>
+        </DominoOuterContainer>
     )
 })
 
 const styles = StyleSheet.create({
-    DominoOuterContainer: {
-        ...Platform.select({
-            web: {
-                cursor: 'grab',
-            },
-        }),
-        height: 16,
-        paddingLeft: 4,
-        paddingRight: 12,
-    },
     DominoInnerContainer: {
         height: '100%',
         width: 10,
