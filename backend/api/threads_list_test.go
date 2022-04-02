6cdb639e5117c654c92d659f387fa9df18d1e6d1
wooglin <alfaindiagt@protonmail.com>
2022-04-02 15:04:41 -0500

    succeeded tests

--- backend/api/threads_list_test.go
+++ backend/api/threads_list_test.go
@@ -38,8 +38,8 @@ func TestThreadList(t *testing.T) {
 			ThreadID:      "sample_gmail_thread_id",
 			LastUpdatedAt: createTimestamp("2020-04-20"),
 			Emails: []database.Email{
-				{EmailID: "sample_email_1_1", SentAt: createTimestamp("2018-04-20")},
-				{EmailID: "sample_email_1_2", SentAt: createTimestamp("2020-04-20")},
+				{EmailID: "sample_email_1_1", SentAt: createTimestamp("2018-04-20"), IsUnread: false},
+				{EmailID: "sample_email_1_2", SentAt: createTimestamp("2020-04-20"), IsUnread: false},
 			},
 		},
 		TaskType: database.TaskType{IsThread: true},
@@ -55,7 +55,7 @@ func TestThreadList(t *testing.T) {
 			ThreadID:      "sample_gmail_thread_id2",
 			LastUpdatedAt: createTimestamp("2019-04-20"),
 			Emails: []database.Email{
-				{EmailID: "sample_email_2", SentAt: createTimestamp("2019-04-20")},
+				{EmailID: "sample_email_2", SentAt: createTimestamp("2019-04-20"), IsUnread: true},
 			},
 		},
 		TaskType: database.TaskType{IsThread: true},
@@ -71,7 +71,7 @@ func TestThreadList(t *testing.T) {
 			ThreadID:      "sample_gmail_thread_id3",
 			LastUpdatedAt: createTimestamp("2021-04-20"),
 			Emails: []database.Email{
-				{EmailID: "sample_email_3", SentAt: createTimestamp("2021-04-20")},
+				{EmailID: "sample_email_3", SentAt: createTimestamp("2021-04-20"), IsUnread: false},
 			},
 		},
 		TaskType: database.TaskType{IsThread: true},
@@ -119,7 +119,7 @@ func TestThreadList(t *testing.T) {
 		body, err := ioutil.ReadAll(recorder.Body)
 		assert.NoError(t, err)
 		assert.Equal(t,
-			fmt.Sprintf("[{\"id\":\"%s\",\"deeplink\":\"\",\"is_task\":false,\"source\":{\"account_id\":\"%s\",\"name\":\"Gmail\",\"logo\":\"/images/gmail.svg\",\"logo_v2\":\"gmail\",\"is_replyable\":true},\"emails\":[{\"smtp_id\":\"\",\"subject\":\"\",\"body\":\"\",\"sent_at\":\"2021-04-19T19:00:00-05:00\",\"is_unread\":false,\"sender\":{\"name\":\"\",\"email\":\"\",\"reply_to\":\"\"},\"recipients\":{\"to\":[],\"cc\":[],\"bcc\":[]}}]},{\"id\":\"%s\",\"deeplink\":\"\",\"is_task\":false,\"source\":{\"account_id\":\"%s\",\"name\":\"Gmail\",\"logo\":\"/images/gmail.svg\",\"logo_v2\":\"gmail\",\"is_replyable\":true},\"emails\":[{\"smtp_id\":\"\",\"subject\":\"\",\"body\":\"\",\"sent_at\":\"2018-04-19T19:00:00-05:00\",\"is_unread\":false,\"sender\":{\"name\":\"\",\"email\":\"\",\"reply_to\":\"\"},\"recipients\":{\"to\":[],\"cc\":[],\"bcc\":[]}},{\"smtp_id\":\"\",\"subject\":\"\",\"body\":\"\",\"sent_at\":\"2020-04-19T19:00:00-05:00\",\"is_unread\":false,\"sender\":{\"name\":\"\",\"email\":\"\",\"reply_to\":\"\"},\"recipients\":{\"to\":[],\"cc\":[],\"bcc\":[]}}]},{\"id\":\"%s\",\"deeplink\":\"\",\"is_task\":false,\"source\":{\"account_id\":\"prefix_%s\",\"name\":\"Gmail\",\"logo\":\"/images/gmail.svg\",\"logo_v2\":\"gmail\",\"is_replyable\":true},\"emails\":[{\"smtp_id\":\"\",\"subject\":\"\",\"body\":\"\",\"sent_at\":\"2019-04-19T19:00:00-05:00\",\"is_unread\":false,\"sender\":{\"name\":\"\",\"email\":\"\",\"reply_to\":\"\"},\"recipients\":{\"to\":[],\"cc\":[],\"bcc\":[]}}]}]",
+			fmt.Sprintf("[{\"id\":\"%s\",\"deeplink\":\"\",\"is_task\":false,\"source\":{\"account_id\":\"%s\",\"name\":\"Gmail\",\"logo\":\"/images/gmail.svg\",\"logo_v2\":\"gmail\",\"is_replyable\":true},\"emails\":[{\"smtp_id\":\"\",\"subject\":\"\",\"body\":\"\",\"sent_at\":\"2021-04-19T19:00:00-05:00\",\"is_unread\":false,\"sender\":{\"name\":\"\",\"email\":\"\",\"reply_to\":\"\"},\"recipients\":{\"to\":[],\"cc\":[],\"bcc\":[]}}]},{\"id\":\"%s\",\"deeplink\":\"\",\"is_task\":false,\"source\":{\"account_id\":\"%s\",\"name\":\"Gmail\",\"logo\":\"/images/gmail.svg\",\"logo_v2\":\"gmail\",\"is_replyable\":true},\"emails\":[{\"smtp_id\":\"\",\"subject\":\"\",\"body\":\"\",\"sent_at\":\"2018-04-19T19:00:00-05:00\",\"is_unread\":false,\"sender\":{\"name\":\"\",\"email\":\"\",\"reply_to\":\"\"},\"recipients\":{\"to\":[],\"cc\":[],\"bcc\":[]}},{\"smtp_id\":\"\",\"subject\":\"\",\"body\":\"\",\"sent_at\":\"2020-04-19T19:00:00-05:00\",\"is_unread\":false,\"sender\":{\"name\":\"\",\"email\":\"\",\"reply_to\":\"\"},\"recipients\":{\"to\":[],\"cc\":[],\"bcc\":[]}}]},{\"id\":\"%s\",\"deeplink\":\"\",\"is_task\":false,\"source\":{\"account_id\":\"prefix_%s\",\"name\":\"Gmail\",\"logo\":\"/images/gmail.svg\",\"logo_v2\":\"gmail\",\"is_replyable\":true},\"emails\":[{\"smtp_id\":\"\",\"subject\":\"\",\"body\":\"\",\"sent_at\":\"2019-04-19T19:00:00-05:00\",\"is_unread\":true,\"sender\":{\"name\":\"\",\"email\":\"\",\"reply_to\":\"\"},\"recipients\":{\"to\":[],\"cc\":[],\"bcc\":[]}}]}]",
 				threadIDHex3, testEmail, threadIDHex1, testEmail, threadIDHex2, testEmail),
 			string(body))
 	})
@@ -141,6 +141,47 @@ func TestThreadList(t *testing.T) {
 		assert.Equal(t,
 			fmt.Sprintf("[{\"id\":\"%s\",\"deeplink\":\"\",\"is_task\":false,\"source\":{\"account_id\":\"%s\",\"name\":\"Gmail\",\"logo\":\"/images/gmail.svg\",\"logo_v2\":\"gmail\",\"is_replyable\":true},\"emails\":[{\"smtp_id\":\"\",\"subject\":\"\",\"body\":\"\",\"sent_at\":\"2018-04-19T19:00:00-05:00\",\"is_unread\":false,\"sender\":{\"name\":\"\",\"email\":\"\",\"reply_to\":\"\"},\"recipients\":{\"to\":[],\"cc\":[],\"bcc\":[]}},{\"smtp_id\":\"\",\"subject\":\"\",\"body\":\"\",\"sent_at\":\"2020-04-19T19:00:00-05:00\",\"is_unread\":false,\"sender\":{\"name\":\"\",\"email\":\"\",\"reply_to\":\"\"},\"recipients\":{\"to\":[],\"cc\":[],\"bcc\":[]}}]}]",
 				threadIDHex1, testEmail),
+			string(body))
+	})
+	t.Run("SuccessOnlyUnread", func(t *testing.T) {
+		params := []byte(`{
+			"only_unread": true
+		}`)
+		request, _ := http.NewRequest(
+			"GET",
+			"/threads/",
+			bytes.NewBuffer(params))
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusOK, recorder.Code)
+		body, err := ioutil.ReadAll(recorder.Body)
+		assert.NoError(t, err)
+		assert.Equal(t,
+			fmt.Sprintf("[{\"id\":\"%s\",\"deeplink\":\"\",\"is_task\":false,\"source\":{\"account_id\":\"prefix_%s\",\"name\":\"Gmail\",\"logo\":\"/images/gmail.svg\",\"logo_v2\":\"gmail\",\"is_replyable\":true},\"emails\":[{\"smtp_id\":\"\",\"subject\":\"\",\"body\":\"\",\"sent_at\":\"2019-04-19T19:00:00-05:00\",\"is_unread\":true,\"sender\":{\"name\":\"\",\"email\":\"\",\"reply_to\":\"\"},\"recipients\":{\"to\":[],\"cc\":[],\"bcc\":[]}}]}]",
+				threadIDHex2, testEmail),
+			string(body))
+	})
+	t.Run("SuccessSingleAccount", func(t *testing.T) {
+		params := []byte(fmt.Sprintf(`{
+			"account": {
+				 "source_id": "gmail",
+				 "source_account_id": "prefix_%s"
+			}			
+		}`, testEmail))
+		request, _ := http.NewRequest(
+			"GET",
+			"/threads/",
+			bytes.NewBuffer(params))
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusOK, recorder.Code)
+		body, err := ioutil.ReadAll(recorder.Body)
+		assert.NoError(t, err)
+		assert.Equal(t,
+			fmt.Sprintf("[{\"id\":\"%s\",\"deeplink\":\"\",\"is_task\":false,\"source\":{\"account_id\":\"prefix_%s\",\"name\":\"Gmail\",\"logo\":\"/images/gmail.svg\",\"logo_v2\":\"gmail\",\"is_replyable\":true},\"emails\":[{\"smtp_id\":\"\",\"subject\":\"\",\"body\":\"\",\"sent_at\":\"2019-04-19T19:00:00-05:00\",\"is_unread\":true,\"sender\":{\"name\":\"\",\"email\":\"\",\"reply_to\":\"\"},\"recipients\":{\"to\":[],\"cc\":[],\"bcc\":[]}}]}]",
+				threadIDHex2, testEmail),
 			string(body))
 	})
 }
