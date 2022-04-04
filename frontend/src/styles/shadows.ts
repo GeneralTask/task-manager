734446927daa4c251f223db054090cd6d652c09f
Scott Mai <scottmai@usc.edu>
2022-04-04 11:49:49 -0700

    use figma shadows

--- frontend/src/styles/shadows.ts
+++ frontend/src/styles/shadows.ts
@@ -2,4 +2,6 @@ export const xSmall = '0px 1px 2px rgba(0, 0, 0, 0.07)'
 export const small = '0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)'
 export const medium = '0px 2px 5px rgba(0, 0, 0, 0.06), 0px 4px 6px -1px rgba(0, 0, 0, 0.1)'
 export const large = '0px 4px 6px -2px rgba(0, 0, 0, 0.06), 0px 10px 15px -3px rgba(0, 0, 0, 0.1)'
-export const xLarge = '2px 10px 10px -5px rgba(0, 0, 0, 0.06), 0px 20px 25px -5px rgba(0, 0, 0, 0.1)'
+export const xLarge = '0px 10px 10px -5px rgba(0, 0, 0, 0.06), 0px 20px 25px -5px rgba(0, 0, 0, 0.1)'
+export const xxLarge = '0px 4px 6px -2px rgba(0, 0, 0, 0.06), 0px 25px 50px -12px rgba(0, 0, 0, 0.25)'
+
