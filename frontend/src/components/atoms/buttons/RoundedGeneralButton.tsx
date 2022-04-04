16d841e914e8d11eebf8657ee3507df7edd86a60
Scott Mai <scottmai@usc.edu>
2022-04-04 13:28:39 -0700

    Convert RoundedGeneralButton to reactjs (#841)

--- frontend/src/components/atoms/buttons/RoundedGeneralButton.tsx
+++ frontend/src/components/atoms/buttons/RoundedGeneralButton.tsx
@@ -1,32 +1,25 @@
-import { Border, Colors, Spacing, Typography } from '../../../styles'
+import { Border, Colors, Shadows, Spacing, Typography } from '../../../styles'
 
 import React from 'react'
-import styled from 'styled-components/native'
+import styled from 'styled-components'
 
-const PressableStyled = styled.Pressable<{ hasBorder: boolean }>`
+const RoundedButton = styled.button<{ hasBorder: boolean, textStyle: 'light' | 'dark', wrapText?: boolean }>`
     border-radius: ${Border.radius.large};
-    ${(props) =>
-        props.hasBorder &&
-        `
-        border: 1px solid ${Colors.gray._200};
-        box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px;
-    `}
-`
-const RoundedView = styled.View<{ color: string }>`
+    box-shadow: ${Shadows.medium};
+    border: ${(props) => props.hasBorder ? `1px solid ${Colors.gray._200}` : 'none'};
     background-color: ${(props) => props.color};
     padding: ${Spacing.padding._8}px;
     text-align: center;
     border-radius: ${Border.radius.large};
     height: 100%;
     width: 100%;
-`
-const ModalText = styled.Text<{ textStyle: 'light' | 'dark', wrapText?: boolean }>`
     color: ${(props) => (props.textStyle === 'light' ? Colors.white : Colors.black)};
     font-weight: ${Typography.weight._600.fontWeight};
     font-size: ${Typography.xSmall.fontSize}px;
     white-space: ${(props) => (props.wrapText ? 'normal' : 'nowrap')};
     overflow: hidden;
     text-overflow: ellipsis;
+    cursor: pointer;
 `
 
 interface RoundedGeneralButtonProps {
@@ -41,11 +34,16 @@ interface RoundedGeneralButtonProps {
 const RoundedGeneralButton = (props: RoundedGeneralButtonProps) => {
     const color = props.disabled ? Colors.gray._400 : props.color || Colors.white
     return (
-        <PressableStyled disabled={!!props.disabled} onPress={props.onPress} hasBorder={!!props.hasBorder}>
-            <RoundedView color={color}>
-                <ModalText textStyle={props.textStyle || 'light'} wrapText={props.wrapText}>{props.value}</ModalText>
-            </RoundedView>
-        </PressableStyled >
+        <RoundedButton
+            disabled={!!props.disabled}
+            onClick={props.onPress}
+            hasBorder={!!props.hasBorder}
+            color={color}
+            textStyle={props.textStyle || 'light'}
+            wrapText={props.wrapText}
+        >
+            {props.value}
+        </RoundedButton >
     )
 }
 
