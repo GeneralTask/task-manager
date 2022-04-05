758caf132256e12b3054a801a232eede1e8c1ca5
Nolan Jimenez <nolan1299@gmail.com>
2022-04-05 12:08:00 -0700

    remove native style from JoinMeetingButton (#860)
    * remove native style from JoinMeetingButton
    
    * NoStyleAnchor as its own atom
    
    * forgot to add NoStyleAnchor.tsx

--- frontend/src/components/atoms/buttons/JointMeetingButton.tsx
+++ frontend/src/components/atoms/buttons/JointMeetingButton.tsx
@@ -1,12 +1,12 @@
 import { Border, Colors } from '../../../styles'
-import { Linking, Platform } from 'react-native'
-
 import { Icon } from '../Icon'
 import React from 'react'
 import { TConferenceCall } from '../../../utils/types'
-import styled from 'styled-components/native'
+import styled from 'styled-components'
+import NoStyleButton from './NoStyleButton'
+import NoStyleAnchor from '../NoStyleAnchor'
 
-const JoinMeetingButtonContainer = styled.Pressable`
+const JoinMeetingButtonContainer = styled(NoStyleButton)`
     height: 30px;
     width: 65px;
     display: flex;
@@ -16,32 +16,23 @@ const JoinMeetingButtonContainer = styled.Pressable`
     background-color: ${Colors.gray._700};
     border-radius: ${Border.radius.regular};
 `
-const ButtonText = styled.Text`
+const ButtonText = styled.span`
     color: ${Colors.white};
     margin-right: 5px;
+    
 `
 
 interface JoinMeetingButtonProps {
     conferenceCall: TConferenceCall
 }
-const JoinMeetingButton = ({ conferenceCall }: JoinMeetingButtonProps) => {
-    if (Platform.OS === 'web') {
-        return (
-            <a href={conferenceCall.url} target="_blank" rel="noreferrer">
-                <JoinMeetingButtonContainer>
-                    <ButtonText>Join</ButtonText>
-                    <Icon size="xSmall" uri={conferenceCall.logo} />
-                </JoinMeetingButtonContainer>
-            </a>
-        )
-    } else {
-        return (
-            <JoinMeetingButtonContainer onPress={() => Linking.openURL(conferenceCall.url)}>
-                <ButtonText>Join</ButtonText>
-                <Icon size="xSmall" uri={conferenceCall.logo} />
-            </JoinMeetingButtonContainer>
-        )
-    }
-}
+const JoinMeetingButton = ({ conferenceCall }: JoinMeetingButtonProps) => (
+    <NoStyleAnchor href={conferenceCall.url} target="_blank" rel="noreferrer">
+        <JoinMeetingButtonContainer>
+            <ButtonText>Join</ButtonText>
+            <Icon size="xSmall" uri={conferenceCall.logo} />
+        </JoinMeetingButtonContainer>
+    </NoStyleAnchor>
+)
+
 
 export default JoinMeetingButton
