186c22b47d1e7424f86e38272e628c3a267f4156
Nolan Jimenez <nolan1299@gmail.com>
2022-04-06 10:26:53 -0700

    remove a lot of react native dependencies. Keeping react-native and react-native-web which I think are necessary for the expo build

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -4,7 +4,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin',
       'babel-plugin-styled-components',
     ],
   }
