dbf479795340e7774f3163c236f5d2bf7e532309
Nolan Jimenez <nolan1299@gmail.com>
2022-04-08 14:33:01 -0700

    missed cookies lol

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,7 @@
 import Cookies from 'js-cookie'
 import axios from 'axios'
 import getEnvVars from '../environment'
+import { AUTHORIZATION_COOKE } from '../constants'
 
 const { REACT_APP_FRONTEND_BASE_URL, REACT_APP_API_BASE_URL } = getEnvVars()
 
@@ -11,7 +12,7 @@ const apiClient = axios.create({
 apiClient.interceptors.request.use(
     config => {
         config.headers = {
-            Authorization: `Bearer ${Cookies.get('authToken')}`,
+            Authorization: `Bearer ${Cookies.get(AUTHORIZATION_COOKE)}`,
             'Access-Control-Allow-Origin': REACT_APP_FRONTEND_BASE_URL,
             'Access-Control-Allow-Headers':
                 'Authorization,Access-Control-Allow-Origin,Access-Control-Allow-Headers,Access-Control-Allow-Methods,Timezone-Offset',
@@ -28,7 +29,7 @@ apiClient.interceptors.response.use(response => {
 }, error => {
     if (error.response.status === 401) {
         axios.defaults.headers.common['Authorization'] = ''
-        Cookies.remove('authToken')
+        Cookies.remove(AUTHORIZATION_COOKE)
         window.location.href = REACT_APP_FRONTEND_BASE_URL
     }
     return error
