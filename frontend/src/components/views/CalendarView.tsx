1c64eaa1004ac2faf5804560d0108dfa7efe664f
Nolan Jimenez <nolan1299@gmail.com>
2022-04-09 00:00:37 -0700

    sidebar calendar changes to next day at midnight

--- frontend/src/components/views/CalendarView.tsx
+++ frontend/src/components/views/CalendarView.tsx
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
 import React, { useState } from 'react'
 import { useAppSelector } from '../../redux/hooks'
+import { useInterval } from '../../utils/hooks'
 import CalendarEvents from '../calendar/CalendarEvents'
 import { CalendarContainer } from '../calendar/CalendarEvents-styles'
 import CalendarHeader from '../calendar/CalendarHeader'
@@ -8,6 +9,7 @@ import CalendarHeader from '../calendar/CalendarHeader'
 const CalendarView = () => {
     const [date, setDate] = useState<DateTime>(DateTime.now())
     const expandedCalendar = useAppSelector((state) => state.tasks_page.expanded_calendar)
+    useInterval(() => setDate(DateTime.now()), 1, false)
 
     return (
         <CalendarContainer expanded={expandedCalendar}>
