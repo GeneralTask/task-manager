a328430c4fe1726ca6d4ed99cc16701b4761f38b
Nolan Jimenez <nolan1299@gmail.com>
2022-04-08 17:38:38 -0700

    reset calendar every 20min when idle

--- frontend/src/components/views/CalendarView.tsx
+++ frontend/src/components/views/CalendarView.tsx
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
 import React, { useState } from 'react'
+import { useIdleTimer } from 'react-idle-timer'
+
 import { useAppSelector } from '../../redux/hooks'
 import { useInterval } from '../../utils/hooks'
 import CalendarEvents from '../calendar/CalendarEvents'
@@ -7,10 +9,12 @@ import { CalendarContainer } from '../calendar/CalendarEvents-styles'
 import CalendarHeader from '../calendar/CalendarHeader'
 
 const CalendarView = () => {
+    const timeoutTimer = useIdleTimer({}) // default timeout is 20 minutes
     const [date, setDate] = useState<DateTime>(DateTime.now())
     const expandedCalendar = useAppSelector((state) => state.tasks_page.expanded_calendar)
-    useInterval(() => setDate(DateTime.now()), 1, false)
-
+    useInterval(() => {
+        if (timeoutTimer.isIdle()) setDate(DateTime.now())
+    }, 1, false)
     return (
         <CalendarContainer expanded={expandedCalendar}>
             <CalendarHeader date={date} setDate={setDate} />
