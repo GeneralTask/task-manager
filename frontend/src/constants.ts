321b7eccc22dce1c707548745595f9cb61266a15
Jack Hamilton <31417618+johnhamilto@users.noreply.github.com>
2022-04-08 19:23:41 -0700

    Don't let the user click on a section in the navbar until its loaded (#923)
    * don't let the user click on section until its loaded
    
    * add constant for task section default id

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -10,6 +10,7 @@ export const ScreenDimensions = {
 
 // API Constants
 export const MESSAGES_PER_PAGE = 50
+export const TASK_SECTION_DEFAULT_ID = '-1'
 export const AUTHORIZATION_COOKE = 'authToken'
 
 // Time constants (in seconds)
