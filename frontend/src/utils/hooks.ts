0f9f4bfe87859ff3e21c5f6d2c240760b17fbc57
Jack Hamilton <john.hamilton@wustl.edu>
2022-04-11 15:55:38 -0700

    change to single quotes

--- frontend/src/utils/hooks.ts
+++ frontend/src/utils/hooks.ts
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect } from 'react'
 
 // duration in seconds
 export function useInterval(func: () => void, seconds: number, callFuncImmediately = true): void {
