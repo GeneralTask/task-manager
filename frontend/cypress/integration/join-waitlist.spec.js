c9baf5c1ebdef3680c3a4f757e11a00c664cc8c8
Nolan Jimenez <nolan1299@gmail.com>
2022-04-11 11:29:02 -0700

    stuff

--- frontend/cypress/integration/join-waitlist.spec.js
+++ frontend/cypress/integration/join-waitlist.spec.js
@@ -3,9 +3,9 @@ describe('joint waitlist from landing page', () => {
     beforeEach(() => {
         cy.visit('localhost:3000/')
     })
-    it('landing page header is present', () => {
-        const tmp = cy.get('div')
-        cy.log(tmp)
-        cy.log('this is a test')
+    it('enter email in email input field', () => {
+        cy.get('input').type('test@generaltask.com')
+        cy.get('button').contains('Join the Waitlist').click()
+
     })
 })
