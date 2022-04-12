78dd338320dc73e9554790d0272c94684f0187f9
Nolan Jimenez <nolan1299@gmail.com>
2022-04-11 19:31:11 -0700

    actual comments wow

--- frontend/cypress/integration/join-waitlist.ts
+++ frontend/cypress/integration/join-waitlist.ts
@@ -1,16 +1,19 @@
 describe('join waitlist from landing page', () => {
     it('visit unauthorized landing page', () => {
-
         cy.visit('/')
     })
     it('submit valid non-duplicate email in join waitlist form', () => {
+        // Intercept waitlist requests
+        cy.intercept('POST', '/waitlist/', { statusCode: 201 }).as('waitlistPost')
 
-        const url = "/waitlist/"
-        cy.intercept('POST', url, { statusCode: 201 }).as('waitlist')
-
+        // Enter email and click Join the Waitlist button
         cy.get('input').type('join_waitlist_test@generaltask.com')
         cy.get('button').contains('Join the Waitlist').click()
-        cy.wait('@waitlist')
+
+        // Wait for request to complete
+        cy.wait('@waitlistPost')
+
+        // Check if success field shows up
         cy.findByTestId('response-container').should('be.visible')
         cy.findByTestId('response-container').invoke('text').then((text) => {
             expect(text).to.equal('Success!')
