49fd49e9b6b624eb90fa667e9c83ecce974f3f39
Jack Hamilton <john.hamilton@wustl.edu>
2022-04-13 13:44:02 -0700

    header is there

--- frontend/src/components/details/ThreadDetails.tsx
+++ frontend/src/components/details/ThreadDetails.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Colors, Spacing, Typography } from '../../styles'
+import { logos } from '../../styles/images'
 import { TEmailThread } from '../../utils/types'
+import { Icon } from '../atoms/Icon'
 import EmailTemplate from './EmailTemplate'
 
 const FlexColumnContainer = styled.div`
@@ -8,12 +11,48 @@ const FlexColumnContainer = styled.div`
     flex-direction: column;
     overflow-y: auto;
 `
+const HeaderContainer = styled.div`
+    flex: 1;
+    display: flex;
+    height: 100px;
+    padding: ${Spacing.padding._16}px;
+    align-items: center;
+`
+const HeaderTitleContainer = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+`
+const Title = styled.span`
+    margin-left: ${Spacing.margin._8}px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    font-family: Switzer-Variable;
+    font-size: ${Typography.small.fontSize};
+    color: ${Colors.gray._600};
+`
+const SubTitle = styled(Title)`
+    font-size: ${Typography.xSmall.fontSize};
+    color: ${Colors.gray._400};
+`
 interface ThreadDetailsProps {
     thread: TEmailThread | undefined
 }
 const ThreadDetails = ({ thread }: ThreadDetailsProps) => {
+
+    const title = `${thread?.emails[0]?.subject} (${thread?.emails.length})`
+    const people = Array.from(new Set(thread?.emails.map((email) => (email.recipients.to)).flat().map((recipient) => (recipient.email))))
+
     return (
         <FlexColumnContainer>
+            <HeaderContainer>
+                <Icon size={'medium'} source={logos.gmail} />
+                <HeaderTitleContainer>
+                    <Title>{title}</Title>
+                    <SubTitle>{`To: ${people.join(', ')}`}</SubTitle>
+                </HeaderTitleContainer>
+            </HeaderContainer>
             {
                 thread && thread.emails.map((email, index) => (
                     <EmailTemplate
