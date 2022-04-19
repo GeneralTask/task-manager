5d171138a7e6af2d3040f06c366832587525e2e6
Scott Mai <scottmai@usc.edu>
2022-04-19 11:45:35 -0700

    Revert "Add Fast Refresh (#982)"
    This reverts commit 0e6ec1c242d8f53c2e13e47bdf121d2d92a4cd04.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -4,9 +4,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const createStyledComponentsTransformer = require('typescript-plugin-styled-components').default;
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const ReactRefreshTypeScript = require('react-refresh-typescript');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 const styledComponentsTransformer = createStyledComponentsTransformer();
 const host = process.env.HOST || 'localhost';
@@ -23,13 +20,7 @@ module.exports = {
                 use: {
                     loader: 'ts-loader',
                     options: {
-                        getCustomTransformers: () => ({
-                            before: [
-                                styledComponentsTransformer,
-                                process.env.NODE_ENV === 'development' && ReactRefreshTypeScript(),
-                            ].filter(Boolean)
-                        }),
-                        transpileOnly: true,
+                        getCustomTransformers: () => ({ before: [styledComponentsTransformer] })
                     }
                 },
                 exclude: /node_modules/,
@@ -75,11 +66,6 @@ module.exports = {
             patterns: [
                 { from: 'public', to: '' }
             ]
-        }),
+        })
     ]
-        // dev plugins
-        .concat(process.env.NODE_ENV !== 'development' ? [] : [
-            new ReactRefreshWebpackPlugin({ overlay: false }),
-            new ForkTsCheckerWebpackPlugin(),
-        ])
 };
