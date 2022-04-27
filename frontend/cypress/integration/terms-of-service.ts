35833465df61ecbb420255a6695c0f841e24bf40
Nolan Jimenez <nolan1299@gmail.com>
2022-04-27 14:32:28 -0700

    make pathname for redirect test more specific

--- frontend/cypress/integration/terms-of-service.ts
+++ frontend/cypress/integration/terms-of-service.ts
@@ -22,6 +22,6 @@ describe('new user is redirected to terms of service page', () => {
     it('clicking submit button and accepting TOS redirects to landing page', () => {
         cy.findByTestId('terms-check-button').click()
         cy.findByTestId('terms-submit-button').click()
-        cy.location('pathname', { timeout: 60000 }).should('equal', '/')
+        cy.location('pathname', { timeout: 60000 }).should('include', '/tasks')
     })
 })
