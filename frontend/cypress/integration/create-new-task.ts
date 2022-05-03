179e8f5ca494ad2ea94b8e433ab6972741b5f3ba
Nolan Jimenez <nolan1299@gmail.com>
2022-05-03 16:35:02 -0700

    cleanup some language in the tests

--- frontend/cypress/integration/create-new-task.ts
+++ frontend/cypress/integration/create-new-task.ts
@@ -1,4 +1,4 @@
-describe('User can create a new task', () => {
+describe('user can create a new General Task task', () => {
     before('login the user', () => {
         cy.login()
         cy.visit('/')
@@ -10,11 +10,11 @@ describe('User can create a new task', () => {
     it('user visits initial task section', () => {
         cy.visit('/tasks/000000000000000000000001')
     })
-    it('add text to create new task field', () => {
+    it('add text to "create new task" field', () => {
         cy.findByPlaceholderText('Add new task').type('New task')
         cy.findByPlaceholderText('Add new task').should('have.value', 'New task')
     })
-    it('clicking enter in task field should create new task', () => {
+    it('clicking "enter" in the input field should create a new task with title "New task"', () => {
         cy.intercept('POST', '/tasks/create/gt_task/').as('createTask')
         cy.findByPlaceholderText('Add new task').type('{enter}')
         cy.wait('@createTask')
