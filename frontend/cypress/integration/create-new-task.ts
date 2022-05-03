d218ec25049a4fda00babe19358b7972db1c0401
Nolan Jimenez <nolan1299@gmail.com>
2022-05-03 16:26:45 -0700

    add create new task single test

---
+++ frontend/cypress/integration/create-new-task.ts
@@ -0,0 +1,24 @@
+describe('User can create a new task', () => {
+    before('login the user', () => {
+        cy.login()
+        cy.visit('/')
+        cy.acceptTermsOfService()
+    })
+    beforeEach(() => {
+        Cypress.Cookies.preserveOnce('authToken')
+    })
+    it('user visits initial task section', () => {
+        cy.visit('/tasks/000000000000000000000001')
+    })
+    it('add text to create new task field', () => {
+        cy.findByPlaceholderText('Add new task').type('New task')
+        cy.findByPlaceholderText('Add new task').should('have.value', 'New task')
+    })
+    it('clicking enter in task field should create new task', () => {
+        cy.intercept('POST', '/tasks/create/gt_task/').as('createTask')
+        cy.findByPlaceholderText('Add new task').type('{enter}')
+        cy.wait('@createTask')
+        cy.findAllByTestId('list-item').should('have.length', 4)
+        cy.findAllByTestId('list-item').first().should('contain', 'New task')
+    })
+})
