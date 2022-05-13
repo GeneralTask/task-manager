edea3dda2cc4e081e0829d0d85c6d60207baef70
Nolan Jimenez <nolan1299@gmail.com>
2022-05-13 14:47:19 -0700

    update failing tests

--- frontend/cypress/integration/logout.spec.ts
+++ frontend/cypress/integration/logout.spec.ts
@@ -7,6 +7,8 @@ describe('user successfuly logs out after being authenticated', () => {
     it('clicking "Sign Out" button removes authToken cookie and redirects user to unauthorized landing page', () => {
         cy.findByText('Sign Out').click()
         cy.url().should('equal', `${Cypress.config().baseUrl}/`)
-        cy.getCookie('authToken').should('not.exist')
+        cy.getCookie('authToken').then((cookie) => {
+            expect(cookie).to.be.null
+        })
     })
 })
