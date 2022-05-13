e24c2214d76cf4508b4a8877fa6f67a3bb305455
Nolan Jimenez <nolan1299@gmail.com>
2022-05-13 16:15:30 -0700

    check

--- frontend/cypress/integration/logout.spec.ts
+++ frontend/cypress/integration/logout.spec.ts
@@ -5,8 +5,14 @@ describe('user successfuly logs out after being authenticated', () => {
         cy.acceptTermsOfService()
     })
     it('clicking "Sign Out" button removes authToken cookie and redirects user to unauthorized landing page', () => {
+        cy.getCookie('authToken').then((cookie) => {
+            cy.log(cookie.name)
+        })
         cy.findByText('Sign Out').click()
         cy.url().should('equal', `${Cypress.config().baseUrl}/`)
+        cy.getCookie('authToken').then((cookie) => {
+            cy.log(cookie.name)
+        })
         cy.getCookie('authToken').should('be.null')
     })
 })
