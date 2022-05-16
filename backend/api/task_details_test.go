26897f3f18f4425a11a8a6cdfca43643aa064993
wooglin <alfaindiagt@protonmail.com>
2022-05-16 01:02:07 -0500

    Add test case in task_details_test for task created from email

--- backend/api/task_details_test.go
+++ backend/api/task_details_test.go
@@ -1,8 +1,13 @@
 package api
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"github.com/GeneralTask/task-manager/backend/constants"
+	"github.com/GeneralTask/task-manager/backend/testutils"
+	"github.com/rs/zerolog/log"
+	"go.mongodb.org/mongo-driver/bson"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -10,7 +15,6 @@ import (
 
 	"github.com/GeneralTask/task-manager/backend/database"
 	"github.com/GeneralTask/task-manager/backend/external"
-	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -19,8 +23,10 @@ func TestTaskDetail(t *testing.T) {
 	db, dbCleanup, err := database.GetDBConnection()
 	assert.NoError(t, err)
 	defer dbCleanup()
+	dbCtx, cancel := context.WithTimeout(context.Background(), constants.DatabaseTimeout)
+	defer cancel()
 
-	testEmail := fmt.Sprintf("%s@generaltask.com", uuid.New().String()[:4])
+	testEmail := createRandomGTEmail()
 	authToken := login(testEmail, "General Tasker")
 	userID := getUserIDFromAuthToken(t, db, authToken)
 	notUserID := primitive.NewObjectID()
@@ -42,19 +48,56 @@ func TestTaskDetail(t *testing.T) {
 		},
 		TaskType: database.TaskType{IsTask: true},
 	})
+
+	firstEmailID := primitive.NewObjectID()
+	threadIDHex := insertTestItem(t, userID, database.Item{
+		TaskBase: database.TaskBase{
+			UserID:     userID,
+			IDExternal: "sample_gmail_thread_id",
+			SourceID:   external.TASK_SOURCE_ID_GMAIL,
+		},
+		EmailThread: database.EmailThread{
+			ThreadID:      "sample_gmail_thread_id",
+			LastUpdatedAt: 0,
+			Emails: []database.Email{
+				{
+					MessageID:    firstEmailID,
+					SMTPID:       "sample_smtp_1",
+					EmailID:      "sample_gmail_thread_id",
+					Subject:      "test subject 1",
+					Body:         "test body 1",
+					SenderDomain: "gmail",
+					SenderEmail:  "test@generaltask.com",
+					SenderName:   "test",
+					ReplyTo:      "test-reply@generaltask.com",
+					IsUnread:     true,
+					Recipients: database.Recipients{
+						To:  []database.Recipient{{Name: "p1", Email: "p1@gmail.com"}},
+						Cc:  []database.Recipient{{Name: "p2", Email: "p2@gmail.com"}},
+						Bcc: []database.Recipient{{Name: "p3", Email: "p3@gmail.com"}},
+					},
+					SentAt: *testutils.CreateDateTime("2019-04-20"),
+				},
+				{
+					SMTPID:       "sample_smtp_1",
+					EmailID:      "sample_gmail_thread_id",
+					Subject:      "test subject 2",
+					Body:         "test body 2",
+					SenderDomain: "gmail",
+					SenderEmail:  "test@generaltask.com",
+					SenderName:   "test",
+					IsUnread:     true,
+					SentAt:       *testutils.CreateDateTime("2018-04-20"),
+				},
+			},
+		},
+		TaskType: database.TaskType{IsThread: true},
+	})
+	threadID, _ := primitive.ObjectIDFromHex(threadIDHex)
+
 	router := GetRouter(GetAPI())
 
-	t.Run("Unauthorized", func(t *testing.T) {
-		router := GetRouter(GetAPI())
-		request, _ := http.NewRequest(
-			"GET",
-			fmt.Sprintf("/tasks/detail/%s/", jiraTaskIDHex),
-			nil)
-
-		recorder := httptest.NewRecorder()
-		router.ServeHTTP(recorder, request)
-		assert.Equal(t, http.StatusUnauthorized, recorder.Code)
-	})
+	UnauthorizedTest(t, "GET", fmt.Sprintf("/tasks/detail/%s/", jiraTaskIDHex), nil)
 	t.Run("InvalidTaskID", func(t *testing.T) {
 		request, _ := http.NewRequest(
 			"GET",
@@ -94,6 +137,27 @@ func TestTaskDetail(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t,
 			fmt.Sprintf(`{"id":"%s","id_ordering":0,"source":{"name":"Jira","logo":"/images/jira.svg","logo_v2":"jira","is_completable":true,"is_replyable":false},"deeplink":"","title":"","body":"","sender":"","due_date":"","time_allocated":0,"sent_at":"1970-01-01T00:00:00Z","is_done":true}`, jiraTaskIDHex),
+			string(body))
+	})
+	t.Run("SuccessTaskFromEmail", func(t *testing.T) {
+		ServeRequest(t, authToken, "POST", "/create_task_from_thread/"+threadIDHex+"/",
+			bytes.NewBuffer([]byte(`{
+				"title": "sample title",
+				"body": "sample body",
+				"email_id": "`+firstEmailID.Hex()+`"
+			}`)), http.StatusOK)
+		var task database.Item
+		err = database.GetTaskCollection(db).FindOne(dbCtx, bson.M{"title": "sample title", "user_id": userID}).Decode(&task)
+		assert.True(t, task.IsTask)
+		assert.Equal(t, threadID, *task.LinkedMessage.ThreadID)
+		assert.Equal(t, firstEmailID, *task.LinkedMessage.EmailID)
+		log.Printf("jerdyy %+v", task)
+
+		body := ServeRequest(t, authToken, "GET", fmt.Sprintf("/tasks/detail/%s/", task.ID.Hex()),
+			nil, http.StatusOK)
+		assert.Equal(t,
+			fmt.Sprintf(`{"id":"%s","id_ordering":0,"source":{"name":"Gmail","logo":"/images/gmail.svg","logo_v2":"gmail","is_completable":true,"is_replyable":true},"deeplink":"","title":"sample title","body":"sample body","sender":"","due_date":"","time_allocated":0,"sent_at":"1970-01-01T00:00:00Z","is_done":false,"linked_email_thread":{"thread_id":"sample_gmail_thread_id","last_updated_at":"1969-12-31T18:00:00-06:00","emails":[{"message_id":"%s","smtp_id":"sample_smtp_1","thread_id":"","email_id":"sample_gmail_thread_id","subject":"test subject 1","body":"test body 1","sender_domain":"gmail","sender_email":"test@generaltask.com","sender_name":"test","reply_to":"test-reply@generaltask.com","is_unread":true,"recipients":{"to":[{"name":"p1","email":"p1@gmail.com"}],"cc":[{"name":"p2","email":"p2@gmail.com"}],"bcc":[{"name":"p3","email":"p3@gmail.com"}]},"sent_at":"2019-04-19T19:00:00-05:00"},{"message_id":"000000000000000000000000","smtp_id":"sample_smtp_1","thread_id":"","email_id":"sample_gmail_thread_id","subject":"test subject 2","body":"test body 2","sender_domain":"gmail","sender_email":"test@generaltask.com","sender_name":"test","reply_to":"","is_unread":true,"recipients":{"to":null,"cc":null,"bcc":null},"sent_at":"2018-04-19T19:00:00-05:00"}]},"linked_email_id":"%s"}`,
+				task.ID.Hex(), firstEmailID.Hex(), firstEmailID.Hex()),
 			string(body))
 	})
 }
