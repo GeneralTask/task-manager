594ca44c70dc99b191106347e26c49e59dab5012
Nolan Jimenez <nolan1299@gmail.com>
2022-05-16 15:11:21 -0700

    add optimistic logic to useCreateTaskFromThread

--- frontend/src/services/api-query-hooks.ts
+++ frontend/src/services/api-query-hooks.ts
@@ -138,8 +138,34 @@ const createTask = async (data: TCreateTaskData) => {
 export const useCreateTaskFromThread = () => {
     const queryClient = useQueryClient()
     return useMutation((data: TCreateTaskFromThreadData) => createTaskFromThread(data), {
-        onMutate: async () => {
-            queryClient.invalidateQueries('tasks')
+        onMutate: async (data: TCreateTaskFromThreadData) => {
+            queryClient.cancelQueries('tasks')
+            const sections: TTaskSection[] | undefined = queryClient.getQueryData('tasks')
+            if (!sections) return
+            sections[0].tasks = [
+                {
+                    id: '0',
+                    id_ordering: 0,
+                    title: data.title,
+                    body: data.body,
+                    deeplink: '',
+                    sent_at: '',
+                    time_allocated: 0,
+                    due_date: '',
+                    source: {
+                        name: 'General Task',
+                        logo: '',
+                        logo_v2: 'generaltask',
+                        is_completable: false,
+                        is_replyable: false,
+                    },
+                    sender: '',
+                    is_done: false,
+                    recipients: {} as TRecipients,
+                },
+                ...sections[0].tasks
+            ]
+            queryClient.setQueryData('tasks', () => sections)
         }
     })
 }
