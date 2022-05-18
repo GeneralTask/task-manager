4ec2008949e05791b648a45b95245a9e2a961046
Nolan Jimenez <nolan1299@gmail.com>
2022-05-17 18:33:49 -0700

    progress

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -32,9 +32,8 @@ module.exports = {
                 use: "babel-loader",
             },
             {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-                exclude: /node_modules/,
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
             },
         ],
     },
