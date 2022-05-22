6c6af156bf1da93d837edb886d94653a8047f4d4
Scott Mai <scottmai@usc.edu>
2022-05-21 18:52:43 -0700

    props

--- frontend/src/components/details/EmailCompose/MultiEmailInput.tsx
+++ frontend/src/components/details/EmailCompose/MultiEmailInput.tsx
@@ -15,71 +15,78 @@ interface MultiEmailInputProps {
     title?: string
     updateRecipients: (recipients: TRecipient[]) => void
 }
-const MultiEmailInput = forwardRef<HTMLInputElement, MultiEmailInputProps>((props, ref) => {
-    const [text, setText] = useState('')
-    const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-        setText(e.target.value)
-    }, [])
+const MultiEmailInput = forwardRef<HTMLInputElement, MultiEmailInputProps>(
+    ({ recipients, title, updateRecipients }, ref) => {
+        const [text, setText] = useState('')
+        const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+            setText(e.target.value)
+        }, [])
 
-    const addRecipientIfValid = useCallback(
-        (emailText: string) => {
-            emailText = emailText.trim()
-            console.log({ emailText })
-            if (emailText.length < 0) return
-            if (isValidEmail(emailText) && !props.recipients.some((r) => r.email === emailText)) {
-                const newRecipient = {
-                    name: '',
-                    email: emailText,
+        const addRecipientIfValid = useCallback(
+            (emailText: string) => {
+                emailText = emailText.trim()
+                console.log({ emailText })
+                if (emailText.length < 0) return
+                if (isValidEmail(emailText) && !recipients.some((r) => r.email === emailText)) {
+                    const newRecipient = {
+                        name: '',
+                        email: emailText,
+                    }
+                    updateRecipients([...recipients, newRecipient])
+                    setText('')
                 }
-                props.updateRecipients([...props.recipients, newRecipient])
-                setText('')
-            }
-        },
-        [props.recipients, props.updateRecipients]
-    )
+            },
+            [recipients, updateRecipients]
+        )
 
-    const deleteRecipient = useCallback(
-        (email: string) => {
-            props.updateRecipients(props.recipients.filter((r) => r.email !== email))
-        },
-        [props.recipients, props.updateRecipients]
-    )
+        const deleteRecipient = useCallback(
+            (email: string) => {
+                updateRecipients(recipients.filter((r) => r.email !== email))
+            },
+            [recipients, updateRecipients]
+        )
 
-    const handleKeyDown = useCallback(
-        (e: React.KeyboardEvent<HTMLInputElement>, emailText: string) => {
-            if (ADD_RECIPIENT_KEYBOARD_SHORTCUTS.includes(e.key)) {
-                e.preventDefault()
-                addRecipientIfValid(emailText)
-            } else if (DELETE_RECIPIENT_KEYBOARD_SHORTCUTS === e.key) {
-                if (emailText.trim().length === 0) {
+        const handleKeyDown = useCallback(
+            (e: React.KeyboardEvent<HTMLInputElement>, emailText: string) => {
+                if (ADD_RECIPIENT_KEYBOARD_SHORTCUTS.includes(e.key)) {
                     e.preventDefault()
-                    deleteRecipient(props.recipients[props.recipients.length - 1].email)
+                    addRecipientIfValid(emailText)
+                } else if (DELETE_RECIPIENT_KEYBOARD_SHORTCUTS === e.key) {
+                    if (emailText.trim().length === 0) {
+                        e.preventDefault()
+                        deleteRecipient(recipients[recipients.length - 1].email)
+                    }
                 }
-            }
-            e.stopPropagation()
-        },
-        [addRecipientIfValid, deleteRecipient]
-    )
+                e.stopPropagation()
+            },
+            [addRecipientIfValid, deleteRecipient]
+        )
 
-    return (
-        <EmailFieldContainer>
-            <span>{props.title}</span>
-            {props.recipients.map(({ email }) => (
-                <EmailTag key={email}>
-                    {email}
-                    <NoStyleButton
-                        data-tag-handle
-                        onClick={() => {
-                            deleteRecipient(email)
-                        }}
-                    >
-                        <Icon size="xSmall" source={icons.x} />
-                    </NoStyleButton>
-                </EmailTag>
-            ))}
-            <EmailFieldInput ref={ref} value={text} onChange={handleChange} onKeyDown={(e) => handleKeyDown(e, text)} />
-        </EmailFieldContainer>
-    )
-})
+        return (
+            <EmailFieldContainer>
+                <span>{title}</span>
+                {recipients.map(({ email }) => (
+                    <EmailTag key={email}>
+                        {email}
+                        <NoStyleButton
+                            data-tag-handle
+                            onClick={() => {
+                                deleteRecipient(email)
+                            }}
+                        >
+                            <Icon size="xSmall" source={icons.x} />
+                        </NoStyleButton>
+                    </EmailTag>
+                ))}
+                <EmailFieldInput
+                    ref={ref}
+                    value={text}
+                    onChange={handleChange}
+                    onKeyDown={(e) => handleKeyDown(e, text)}
+                />
+            </EmailFieldContainer>
+        )
+    }
+)
 
 export default MultiEmailInput
