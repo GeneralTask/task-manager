beff0621b00567f6fe146bf9897ddd97055ce788
Jack Hamilton <31417618+johnhamilto@users.noreply.github.com>
2022-05-24 18:00:32 -0700

    Show if thread and message is unread (#1144)
    * show if thread and message is unread
    
    * nits
    
    * this better?
    
    * some
    
    * remove comment
    
    * constant

--- frontend/src/styles/images.ts
+++ frontend/src/styles/images.ts
@@ -15,6 +15,7 @@ export const logos: { [key: string]: string } = {
 export const icons: { [key: string]: string } = {
     arrows_in: '/images/arrows_in.png',
     arrows_out: '/images/arrows_out.png',
+    dot: '/images/dot.svg',
     calendar_blank: '/images/calendar_blank.png',
     caret_down: '/images/caret_down.svg',
     caret_left: '/images/caret_left.png',
