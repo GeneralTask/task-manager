4a653f9b4f62fc6b23c073ae6e4a6baef959070f
Jack Hamilton <john.hamilton@wustl.edu>
2022-05-25 17:06:13 -0700

    v1

--- frontend/src/components/details/TaskDetails.tsx
+++ frontend/src/components/details/TaskDetails.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react'
+import React, { Fragment, useEffect, useLayoutEffect, useState } from 'react'
 import ActionOption from '../molecules/ActionOption'
 import { Icon } from '../atoms/Icon'
 import { DETAILS_SYNC_TIMEOUT, KEYBOARD_SHORTCUTS } from '../../constants'
 import ReactTooltip from 'react-tooltip'
-import { TTask } from '../../utils/types'
+import { TEmailComposeState, TTask } from '../../utils/types'
 import { logos } from '../../styles/images'
 import { useModifyTask } from '../../services/api-query-hooks'
 import RoundedGeneralButton from '../atoms/buttons/RoundedGeneralButton'
@@ -13,6 +13,10 @@ import { SubtitleSmall } from '../atoms/subtitle/Subtitle'
 import { useCallback, useRef } from 'react'
 import Spinner from '../atoms/Spinner'
 import { useNavigate, useParams } from 'react-router-dom'
+import PreviousMessages from './email/PreviousMessages'
+import EmailContainer from './email/EmailContainer'
+import EmailCompose from './email/compose/EmailCompose'
+import EmailMainActions from './email/compose/MainActions'
 
 const DetailsViewContainer = styled.div`
     flex: 1;
@@ -29,8 +33,8 @@ const DetailsTopContainer = styled.div`
     align-items: center;
     height: 50px;
 `
-const BodyTextArea = styled.textarea`
-    flex: 1;
+const BodyTextArea = styled.textarea<{ isFullHeight: boolean }>`
+    ${({ isFullHeight }) => isFullHeight && `flex: 1;`}
     display: block;
     background-color: inherit;
     border: 1px solid transparent;
@@ -67,6 +71,11 @@ const MarginLeftAuto = styled.div`
 const MarginRight8 = styled.div`
     margin-right: ${Spacing.margin._8};
 `
+const EmailThreadsContainer = styled.div`
+    flex: 1;
+    overflow-y: auto;
+    min-width: 0;
+`
 
 const SYNC_MESSAGES = {
     SYNCING: 'Syncing...',
@@ -83,6 +92,18 @@ const TaskDetails = ({ task }: TaskDetailsProps) => {
     const [isEditing, setIsEditing] = useState(false)
     const [labelEditorShown, setLabelEditorShown] = useState(false)
     const [syncIndicatorText, setSyncIndicatorText] = useState(SYNC_MESSAGES.COMPLETE)
+    const [isCollapsed, setIsCollapsed] = useState(true)
+    const [composeState, setComposeState] = useState<TEmailComposeState>({
+        emailComposeType: null,
+        emailId: null,
+    })
+    const thread = task.linked_email_thread?.email_thread
+    useLayoutEffect(() => {
+        setComposeState({
+            emailComposeType: null,
+            emailId: null,
+        })
+    }, [thread?.id])
 
     const titleRef = useRef<HTMLTextAreaElement>(null)
     const bodyRef = useRef<HTMLTextAreaElement>(null)
@@ -203,17 +224,78 @@ const TaskDetails = ({ task }: TaskDetailsProps) => {
             {task.isOptimistic ? (
                 <Spinner />
             ) : (
-                <BodyTextArea
-                    ref={bodyRef}
-                    data-testid="task-body-input"
-                    placeholder="Add task details"
-                    value={bodyInput}
-                    onChange={(e) => {
-                        setBodyInput(e.target.value)
-                        onEdit(task.id, titleRef.current?.value || '', bodyRef.current?.value || '')
-                    }}
-                    onKeyDown={(e) => e.stopPropagation()}
-                />
+                <>
+                    <BodyTextArea
+                        ref={bodyRef}
+                        data-testid="task-body-input"
+                        placeholder="Add task details"
+                        isFullHeight={!thread}
+                        value={bodyInput}
+                        onChange={(e) => {
+                            setBodyInput(e.target.value)
+                            onEdit(task.id, titleRef.current?.value || '', bodyRef.current?.value || '')
+                        }}
+                        onKeyDown={(e) => e.stopPropagation()}
+                    />
+                    {thread && (
+                        <>
+                            <EmailThreadsContainer>
+                                {isCollapsed && thread.emails.length > 1 ? (
+                                    <>
+                                        <PreviousMessages
+                                            numMessages={thread.emails.length - 1}
+                                            onClick={() => setIsCollapsed(false)}
+                                        />
+                                        <EmailContainer
+                                            email={thread.emails[thread.emails.length - 1]}
+                                            isLastThread={true}
+                                            setThreadComposeState={setComposeState}
+                                            sourceAccountId={thread.source.account_id}
+                                        />
+                                    </>
+                                ) : (
+                                    thread.emails.map((email, index) => (
+                                        <Fragment key={email.message_id}>
+                                            <EmailContainer
+                                                email={email}
+                                                isLastThread={index === thread.emails.length - 1}
+                                                setThreadComposeState={setComposeState}
+                                                sourceAccountId={thread.source.account_id}
+                                            />
+                                            {composeState.emailId === email.message_id &&
+                                                index !== thread.emails.length - 1 &&
+                                                composeState.emailComposeType != null && (
+                                                    <EmailCompose
+                                                        email={thread.emails[thread?.emails.length - 1]}
+                                                        composeType={composeState.emailComposeType}
+                                                        sourceAccountId={thread.source.account_id}
+                                                        isPending={!!composeState.isPending}
+                                                        setThreadComposeState={setComposeState}
+                                                    />
+                                                )}
+                                        </Fragment>
+                                    ))
+                                )}
+                            </EmailThreadsContainer>
+                            {composeState.emailId === thread.emails[thread.emails.length - 1].message_id &&
+                                composeState.emailComposeType != null && (
+                                    <EmailCompose
+                                        email={thread.emails[thread.emails.length - 1]}
+                                        composeType={composeState.emailComposeType}
+                                        sourceAccountId={thread.source.account_id}
+                                        isPending={!!composeState.isPending}
+                                        setThreadComposeState={setComposeState}
+                                    />
+                                )}
+                            {composeState.emailComposeType === null && (
+                                <EmailMainActions
+                                    email={thread.emails[thread.emails.length - 1]}
+                                    setThreadComposeState={setComposeState}
+                                />
+                            )}
+                        </>
+                    )}
+                </>
             )}
         </DetailsViewContainer>
     )
