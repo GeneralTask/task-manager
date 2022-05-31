09b982691544b20c1ceb183dd5c90e981fcbc764
Scott Mai <scottmai@usc.edu>
2022-05-31 10:42:02 -0700

    inherit

--- frontend/src/components/atoms/TextArea.tsx
+++ frontend/src/components/atoms/TextArea.tsx
@@ -18,7 +18,7 @@ const TextAreaStyled = styled.textarea`
     font-weight: normal;
     font-size: ${Typography.xSmall.fontSize};
     background-color: inherit;
-    font-family: 'Switzer-Variable';
+    font-family: inherit;
 `
 
 interface TextAreaProps {
