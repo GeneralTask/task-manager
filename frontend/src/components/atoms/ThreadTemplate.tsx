95b96326d962e29ae6c9cec8becf4ecdab815598
yohanstra <jreinstra@gmail.com>
2022-05-31 14:24:56 -0700

    Make email list slightly more compact

--- frontend/src/components/atoms/ThreadTemplate.tsx
+++ frontend/src/components/atoms/ThreadTemplate.tsx
@@ -6,7 +6,7 @@ import { TASK_DEFAULT_LINE_HEIGHT } from '../../styles/dimensions'
 const TemplateContainer = styled.div`
     width: 100%;
     position: relative;
-    height: ${TASK_DEFAULT_LINE_HEIGHT * 5}px;
+    height: ${TASK_DEFAULT_LINE_HEIGHT * 4}px;
     border-radius: ${Border.radius.large};
     padding: 1px 0;
 `
