7ddd0de1c2e5e85d16f5e911c12fa5418bd86f39
Nolan Jimenez <nolan1299@gmail.com>
2022-06-01 14:10:34 -0700

    add constants for dimensions

--- frontend/src/components/templates/DefaultTemplate.tsx
+++ frontend/src/components/templates/DefaultTemplate.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import ReactTooltip from 'react-tooltip'
-import { Colors, Dimensions } from '../../styles'
+import { Colors } from '../../styles'
 import NavigationView from '../views/NavigationView'
 import '../../styles/tooltip.css'
 import styled from 'styled-components'
 import CalendarView from '../views/CalendarView'
+import { NAVIGATION_BAR_WIDTH, COLLAPSED_CALENDAR_WIDTH, WINDOW_MIN_WIDTH } from '../../styles/dimensions'
 
 const DefaultTemplateContainer = styled.div`
     display: grid;
-    grid-template-columns: 230px minmax(300px, auto) minmax(${Dimensions.COLLAPSED_CALENDAR_WIDTH}, max-content);
+    grid-template-columns: ${NAVIGATION_BAR_WIDTH}px minmax(300px, auto) minmax(
+            ${COLLAPSED_CALENDAR_WIDTH}px,
+            max-content
+        );
     grid-auto-flow: column;
     grid-template-rows: 1fr;
     height: 100vh;
     background-color: ${Colors.gray._50};
     position: relative;
-    min-width: 800px;
+    min-width: ${WINDOW_MIN_WIDTH}px;
 `
 
 const TasksandDetails = styled.div`
