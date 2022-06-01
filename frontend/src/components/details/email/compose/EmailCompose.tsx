3f7622d784f05a0eb56913b9ce7e3704ada24b92
Scott Mai <scottmai@usc.edu>
2022-06-01 10:43:53 -0700

    comment

--- frontend/src/components/details/email/compose/EmailCompose.tsx
+++ frontend/src/components/details/email/compose/EmailCompose.tsx
@@ -88,6 +88,7 @@ const EmailCompose = (props: EmailComposeProps) => {
     const startSendEmail = useCallback(
         (recipients: TRecipients, subject: string, body: string) => {
             const timeout = setTimeout(() => {
+                // insert breaks to ensure email is properly formatted on other clients
                 sendEmail(recipients, subject, body.replace(/\n/g, '<br/>'))
                 props.setThreadComposeState({
                     emailComposeType: null,
