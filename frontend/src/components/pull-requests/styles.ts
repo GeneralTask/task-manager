736fb7c114ae3374d2a6da0e97879d35726b5f05
Scott Mai <scottmai702@gmail.com>
2022-06-03 13:21:22 -0700

    units in constant

--- frontend/src/components/pull-requests/styles.ts
+++ frontend/src/components/pull-requests/styles.ts
@@ -4,18 +4,18 @@ import NoStyleAnchor from "../atoms/NoStyleAnchor"
 import styled from "styled-components"
 
 export const ColumnWidths = {
-    title: '35',
-    status: '25',
-    author: '20',
-    branch: '15',
-    link: '5',
+    title: '35%',
+    status: '25%',
+    author: '20%',
+    branch: '15%',
+    link: '5%',
 }
 
 export const Column = styled.div<{ type: keyof typeof ColumnWidths }>`
     display: flex;
     flex-direction: column;
     justify-content: center;
-    width: ${props => ColumnWidths[props.type]}%;
+    width: ${props => ColumnWidths[props.type]};
     padding: ${Spacing.padding._8}px;
     overflow: hidden;
     white-space: nowrap;
