fc7281f6a260d32b07e852c9048f40ee31aa9aec
Scott Mai <scottmai702@gmail.com>
2022-06-03 11:58:53 -0700

    Make iconSize include units and simplify logic

--- frontend/src/components/atoms/Icon.tsx
+++ frontend/src/components/atoms/Icon.tsx
@@ -1,12 +1,12 @@
 import { Dimensions, Images } from '../../styles'
 
-/* eslint @typescript-eslint/no-var-requires: "off" */
 import React from 'react'
 import styled from 'styled-components'
+import { TIconSize } from '../../styles/dimensions'
 
-const IconContainer = styled.div<{ width: number; height: number }>`
-    width: ${(props) => props.width}px;
-    height: ${(props) => props.height}px;
+const IconContainer = styled.div<{ width: string; height: string }>`
+    width: ${(props) => props.width};
+    height: ${(props) => props.height};
     display: flex;
     align-items: center;
     justify-content: center;
@@ -18,27 +18,14 @@ const ImageContainer = styled.img`
 `
 
 interface IconProps {
-    size: 'xxSmall' | 'xSmall' | 'small' | 'medium' | 'large'
+    size: TIconSize
     uri?: string
     source?: string
 }
 export const Icon = (props: IconProps) => {
     const image = props.uri != undefined ? props.uri : props.source ? props.source : Images.logos.generaltask
 
-    const dimension = (() => {
-        switch (props.size) {
-            case 'xxSmall':
-                return Dimensions.iconSize.xxSmall
-            case 'xSmall':
-                return Dimensions.iconSize.xSmall
-            case 'small':
-                return Dimensions.iconSize.small
-            case 'medium':
-                return Dimensions.iconSize.medium
-            case 'large':
-                return Dimensions.iconSize.large
-        }
-    })()
+    const dimension = Dimensions.iconSize[props.size]
 
     return (
         <IconContainer width={dimension} height={dimension}>
