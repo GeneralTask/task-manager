2c1a33eb755a34b39829007adf2f6d0164f2a87d
Scott Mai <scottmai@usc.edu>
2022-06-08 23:41:11 -0700

    copy

--- frontend/src/components/pull-requests/BranchName.tsx
+++ frontend/src/components/pull-requests/BranchName.tsx
@@ -2,13 +2,16 @@ import { Border, Colors, Spacing } from '../../styles'
 
 import { Icon } from '../atoms/Icon'
 import React from 'react'
+import TooltipWrapper from '../atoms/TooltipWrapper'
 import { TruncatedText } from './styles'
 import { icons } from '../../styles/images'
 import styled from 'styled-components'
+import toast from '../../utils/toast'
 
 const BranchNameContainer = styled.div`
     display: flex;
     align-items: center;
+    color: ${Colors.purple._1};
     border: 0.5px solid ${Colors.gray._200};
     border-radius: ${Border.radius.regular};
     padding: ${Spacing.padding._4} ${Spacing.padding._4};
@@ -19,13 +22,27 @@ interface BranchNameProps {
     name: string
 }
 const BranchName = ({ name }: BranchNameProps) => {
+    const handleClick = async () => {
+        await navigator.clipboard.writeText(name)
+        toast(
+            {
+                message: 'Branch copied to clipboard',
+            },
+            {
+                autoClose: 2000,
+                pauseOnFocusLoss: false,
+            }
+        )
+    }
     return (
-        <BranchNameContainer>
-            <TruncatedText>{name}</TruncatedText>
-            <div>
-                <Icon size="small" source={icons.copy} />
-            </div>
-        </BranchNameContainer>
+        <TooltipWrapper dataTip={name} tooltipId="tooltip">
+            <BranchNameContainer onClick={handleClick}>
+                <TruncatedText>{name}</TruncatedText>
+                <div>
+                    <Icon size="xSmall" source={icons.copy} />
+                </div>
+            </BranchNameContainer>
+        </TooltipWrapper>
     )
 }
 
