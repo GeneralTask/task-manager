5e2e0ce07c2b109d778c422f1bae04f8b311d710
Jack Hamilton <john.hamilton@wustl.edu>
2022-06-13 15:20:24 -0700

    remove the god function

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -63,15 +63,3 @@ export const emptyFunction = () => void 0
 // https://stackoverflow.com/a/46181/12679075
 export const isValidEmail = (email: string): boolean =>
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email.toLowerCase())
-
-// Inspired by https://stackoverflow.com/questions/5069464/replace-multiple-strings-at-once
-export function replaceBulk(str: string, findReplaceMap: { [key: string]: string }) {
-    const regexArr: string[] = []
-    for (const find of Object.keys(findReplaceMap)) {
-        regexArr.push(find.replace(/([-[\]{}()*+?.\\^$|#,])/g, '\\$1'))
-    }
-    const regex = regexArr.join('|')
-    return str.replace(new RegExp(regex, 'g'), function (matched) {
-        return findReplaceMap[matched]
-    })
-}
