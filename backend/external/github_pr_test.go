6a888f09910ae4c36c9a1d1367c2043b516420f5
Nolan Jimenez <nolan1299@gmail.com>
2022-06-14 10:50:07 -0700

    add TestUserIsOwner tests

--- backend/external/github_pr_test.go
+++ backend/external/github_pr_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/GeneralTask/task-manager/backend/database"
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/google/go-github/v45/github"
 )
 
 func TestLoadGithubPullRequests(t *testing.T) {
@@ -24,5 +25,38 @@ func TestMarkGithubPRTaskAsDone(t *testing.T) {
 		isCompleted := true
 		err := gmailTask.ModifyTask(userID, "sample_account@email.com", "6942069420", &database.TaskChangeableFields{IsCompleted: &isCompleted})
 		assert.NoError(t, err)
+	})
+}
+
+func TestUserIsOwner(t *testing.T) {
+	githubUserId1 := int64(1)
+	githubUserId2 := int64(2)
+	testGithubUser1 := github.User{
+		ID: &githubUserId1,
+	}
+	testGithubUser2 := github.User{
+		ID: &githubUserId2,
+	}
+	testGithubUser3 := github.User{
+		ID: nil,
+	}
+	pullRequestUser1 := github.PullRequest{
+		User:&testGithubUser1,
+	}
+	pullRequestUser3 := github.PullRequest{
+		User:&testGithubUser3,
+	}
+
+	t.Run("UserIsOwner", func(t *testing.T) {
+		assert.True(t, userIsOwner(&testGithubUser1, &pullRequestUser1))
+	})
+	t.Run("UserIsNotOwner", func(t *testing.T) {
+		assert.False(t, userIsOwner(&testGithubUser2, &pullRequestUser1))
+	})
+	t.Run("UserIdIsNil", func(t *testing.T) {
+		assert.False(t, userIsOwner(&testGithubUser3, &pullRequestUser1))
+	})
+	t.Run("PullRequestUserIdIsNil", func(t *testing.T) {
+		assert.False(t, userIsOwner(&testGithubUser1, &pullRequestUser3))
 	})
 }
