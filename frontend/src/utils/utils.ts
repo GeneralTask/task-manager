f4cc35de2a0add0b3a3e8607abd3a9e8f530c67a
Jack Hamilton <john.hamilton@wustl.edu>
2022-06-14 15:48:23 -0700

    FINE

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -64,7 +64,7 @@ export const emptyFunction = () => void 0
 export const isValidEmail = (email: string): boolean =>
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email.toLowerCase())
 
-export const countWithOverflow = (count: number, max = 99): string => {
+export const countWithOverflow = (count: number, max = 99) => {
     if (count > max) {
         return `${max}+`
     }
