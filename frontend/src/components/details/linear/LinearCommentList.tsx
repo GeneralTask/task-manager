eea866268ea92e6a9f8dbea03a58e0042cab0625
Jack Hamilton <john.hamilton@wustl.edu>
2022-06-17 12:30:59 -0700

    max height and scroll to bottom

--- frontend/src/components/details/linear/LinearCommentList.tsx
+++ frontend/src/components/details/linear/LinearCommentList.tsx
@@ -1,25 +1,46 @@
-import React from 'react'
+import React, { Ref, useEffect, useRef } from 'react'
 import { TLinearComment } from '../../../utils/types'
 import LinearComment from './LinearComment'
 import styled from 'styled-components'
-import { Spacing } from '../../../styles'
+import { Colors, Spacing } from '../../../styles'
 
 const CommentListContainer = styled.div`
     display: flex;
     flex-direction: column;
+    max-height: 400px;
+    overflow-y: auto;
     gap: ${Spacing.margin._8};
+`
+
+const DividerView = styled.div`
+    height: 1px;
+    background-color: ${Colors.gray._300};
+    margin: ${Spacing.margin._8};
 `
 
 interface LinearCommentListProps {
     comments: TLinearComment[]
 }
 
-const LinearCommentList = ({ comments }: LinearCommentListProps) => (
-    <CommentListContainer>
-        {comments.map((comment) => (
-            <LinearComment key={comment.created_at} comment={comment} />
-        ))}
-    </CommentListContainer>
-)
+const LinearCommentList = ({ comments }: LinearCommentListProps) => {
+    const bottomRef: Ref<HTMLDivElement> = useRef(null)
+
+    useEffect(() => bottomRef.current?.scrollIntoView(), [])
+
+    return (
+        <>
+            <DividerView />
+            <CommentListContainer>
+                {comments
+                    .slice()
+                    .reverse()
+                    .map((comment) => (
+                        <LinearComment key={comment.created_at} comment={comment} />
+                    ))}
+                <div ref={bottomRef} />
+            </CommentListContainer>
+        </>
+    )
+}
 
 export default LinearCommentList
