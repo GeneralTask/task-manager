953395e5221f26135172c33c1fa8e0280455d24d
Ahmed AlHaffar <alhaffar.ahmed@gmail.com>
2022-06-20 11:24:59 +0300

    Updating kube-config files for uswest1

--- kubernetes/manifests/prod/helm-installation.sh
+++ kubernetes/manifests/prod/helm-installation.sh
@@ -14,11 +14,11 @@ kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller/
 echo "Install the helm chart if using IAM roles for service accounts. "
 helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
  -n kube-system \
- --set clusterName=prd-gtsk-uswest2-backend \
+ --set clusterName=prd-gtsk-uswest1-backend \
  --set serviceAccount.create=false \
  --set serviceAccount.name=aws-load-balancer-controller \
- --set vpcId=vpc-029c38c0210a0f3dc \
- --set replicaCount=1
+ --set vpcId=vpc-0f4a35f3131c1aa49 \
+ --set replicaCount=1 \
 
 
 #================#
