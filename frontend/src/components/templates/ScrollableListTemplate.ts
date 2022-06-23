ab834a33b64827ccc82ad618b40a845fb6de308a
Scott Mai <scottmai702@gmail.com>
2022-06-23 13:16:31 -0700

    inherit

--- frontend/src/components/templates/ScrollableListTemplate.ts
+++ frontend/src/components/templates/ScrollableListTemplate.ts
@@ -4,7 +4,7 @@ const ScrollableListTemplate = styled.div<{ backgroundColor?: string }>`
     padding: 40px 10px 100px;
     overflow-y: auto;
     flex: 1 0;
-    background-color: ${props => props.backgroundColor || "white"};
+    background-color: ${props => props.backgroundColor || 'inherit'};
 `
 
 export default ScrollableListTemplate
