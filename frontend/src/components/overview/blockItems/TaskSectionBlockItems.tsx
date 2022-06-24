fdf7a8cc8f47e359f33f18d93585f8bdadb23441
Scott Mai <scottmai702@gmail.com>
2022-06-24 11:34:47 -0700

    sectionId

--- frontend/src/components/overview/blockItems/TaskSectionBlockItems.tsx
+++ frontend/src/components/overview/blockItems/TaskSectionBlockItems.tsx
@@ -11,25 +11,23 @@ const TaskSectionBlockItems = ({ block }: { block: TOverviewBlock }) => {
     // TODO: either change Task to make this optional or add better support for scrolling. Unused for now.
     const scrollingRef = useRef<HTMLDivElement>(null)
 
-    if (!sectionId) {
-        return null
-    }
     return (
         <div ref={scrollingRef}>
-            {block.view_items.map((item, index) => (
-                <TaskDropContainer key={item.id} task={item} taskIndex={index} sectionId={sectionId}>
-                    <Task
-                        key={item.id}
-                        task={item}
-                        dragDisabled={true}
-                        index={index}
-                        sectionId={sectionId}
-                        sectionScrollingRef={scrollingRef}
-                        isSelected={overviewItem === item.id}
-                        link={`/overview/${item.id}`}
-                    />
-                </TaskDropContainer>
-            ))}
+            {sectionId &&
+                block.view_items.map((item, index) => (
+                    <TaskDropContainer key={item.id} task={item} taskIndex={index} sectionId={sectionId}>
+                        <Task
+                            key={item.id}
+                            task={item}
+                            dragDisabled={true}
+                            index={index}
+                            sectionId={sectionId}
+                            sectionScrollingRef={scrollingRef}
+                            isSelected={overviewItem === item.id}
+                            link={`/overview/${item.id}`}
+                        />
+                    </TaskDropContainer>
+                ))}
         </div>
     )
 }
