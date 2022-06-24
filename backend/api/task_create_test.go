c23c4d310879b84a3feb9fab804d4a6c9f13d222
Julian <julian@sweetrain.com>
2022-06-24 16:04:24 -0400

    added test

--- backend/api/task_create_test.go
+++ backend/api/task_create_test.go
@@ -127,3 +127,23 @@ func TestCreateTask(t *testing.T) {
 		assert.Equal(t, fmt.Sprintf("{\"task_id\":\"%s\"}", task.ID.Hex()), string(body))
 	})
 }
+
+func TestSlackTaskCreate(t *testing.T) {
+	router := GetRouter(GetAPI())
+
+	t.Run("BadSourceID", func(t *testing.T) {
+		request, _ := http.NewRequest(
+			"POST",
+			"/tasks/create_external/slack/",
+			bytes.NewBuffer([]byte(`{"payload": []}`)))
+
+		request.Header.Add("X-Slack-Request-Timestamp", "invalid timestamp")
+		request.Header.Add("X-Slack-Signature", "invalid signature")
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+		body, err := ioutil.ReadAll(recorder.Body)
+		assert.NoError(t, err)
+		assert.Equal(t, "{\"detail\":\"signing secret invalid.\"}", string(body))
+	})
+}
