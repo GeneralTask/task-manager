d422a6bd37a84922b8eaef9997ad268c931ea669
Julian <julian@sweetrain.com>
2022-06-29 12:17:24 -0700

    update comment

--- backend/external/gmail.go
+++ backend/external/gmail.go
@@ -94,6 +94,7 @@ func (gmailSource GmailSource) GetEmails(userID primitive.ObjectID, accountID st
 		historiesRequiringUpdate, recentHistoryID, err = getAllRecentGmailHistory(gmailService, latestHistoryID)
 		if err != nil {
 			// retry request with full refresh in history fetching fails
+			// this will happen if the history is too old and too many threads will be returned
 			if strings.Contains(err.Error(), "Error 404") {
 				log.Error().Err(err).Msg("failed to fetch history")
 				gmailSource.GetEmails(userID, accountID, latestHistoryID, result, true)
