41cd41b15d65b2bb6708040af14564589daf13a3
Julian <julian@sweetrain.com>
2022-06-29 12:04:53 -0700

    Add 404 check on gmail history fetching

--- backend/external/gmail.go
+++ backend/external/gmail.go
@@ -93,6 +93,12 @@ func (gmailSource GmailSource) GetEmails(userID primitive.ObjectID, accountID st
 
 		historiesRequiringUpdate, recentHistoryID, err = getAllRecentGmailHistory(gmailService, latestHistoryID)
 		if err != nil {
+			// retry request with full refresh in history fetching fails
+			if strings.Contains(err.Error(), "Error 404") {
+				log.Error().Err(err).Msg("failed to fetch history")
+				gmailSource.GetEmails(userID, accountID, latestHistoryID, result, true)
+				return
+			}
 			log.Error().Err(err).Msg("failed to load Gmail history for user")
 			isBadToken := strings.Contains(err.Error(), "invalid_grant") ||
 				strings.Contains(err.Error(), "authError")
