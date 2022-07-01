8dce62cc226961f1055dfddbd708f71b62238119
Nolan Jimenez <nolan1299@gmail.com>
2022-07-01 15:39:58 -0700

    GetUser helper function

--- backend/api/helpers.go
+++ backend/api/helpers.go
@@ -1,12 +1,27 @@
 package api
 
 import (
+	"context"
+
+	"github.com/GeneralTask/task-manager/backend/database"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func getUserIDFromContext(c *gin.Context) primitive.ObjectID {
 	userIDRaw, _ := c.Get("user")
 	userID := userIDRaw.(primitive.ObjectID)
 	return userID
+}
+
+func getUserFromUserID(db *mongo.Database, userID primitive.ObjectID) (database.User, error) {
+	userCollection := database.GetUserCollection(db)
+	var user database.User
+	err := userCollection.FindOne(context.Background(), bson.M{"_id": userID}).Decode(&user)
+	if err != nil {
+		return database.User{}, err
+	}
+	return user, nil
 }
