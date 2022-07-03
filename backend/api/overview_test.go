e42aab3ee4b69d1a9b67ae11acd2a24fcc9f076c
Nolan Jimenez <nolan1299@gmail.com>
2022-07-03 16:29:28 -0700

    more test

--- backend/api/overview_test.go
+++ backend/api/overview_test.go
@@ -5,9 +5,9 @@ import (
 	"testing"
 
 	"github.com/GeneralTask/task-manager/backend/database"
+	"github.com/GeneralTask/task-manager/backend/external"
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"github.com/GeneralTask/task-manager/backend/external"
 )
 
 func TestGetTaskSectionOverviewResult(t *testing.T) {
@@ -19,42 +19,27 @@ func TestGetTaskSectionOverviewResult(t *testing.T) {
 	taskSectionName := "Test Task Section"
 	taskSectionCollection := database.GetTaskSectionCollection(db)
 	taskSectionResult, err := taskSectionCollection.InsertOne(parentCtx, database.TaskSection{
-		Name:      taskSectionName,
+		Name: taskSectionName,
 	})
 	assert.NoError(t, err)
 	taskSectionID := taskSectionResult.InsertedID.(primitive.ObjectID)
 
-	userID1 := primitive.NewObjectID()
-	taskCollection := database.GetTaskCollection(db)
-	taskResult, err := taskCollection.InsertOne(parentCtx, database.Item{
-		TaskBase: database.TaskBase{
-			UserID:    userID1,
-			IsCompleted: false,
-			IDTaskSection: taskSectionID,
-			SourceID: external.TASK_SOURCE_ID_GT_TASK,
-		},
-		TaskType: database.TaskType{
-			IsTask: true,
-		},
-	})
-	assert.NoError(t, err)
-	taskID := taskResult.InsertedID.(primitive.ObjectID)
-	
+	userID := primitive.NewObjectID()
 	view := database.View{
-		UserID: userID1,
-		IDOrdering: 1,
-		Type: "generaltask",
-		IsPaginated: false,
-		IsLinked: false,
+		UserID:        userID,
+		IDOrdering:    1,
+		Type:          "generaltask",
+		IsPaginated:   false,
+		IsLinked:      false,
 		TaskSectionID: taskSectionResult.InsertedID.(primitive.ObjectID),
 	}
 	viewCollection := database.GetViewCollection(db)
 	_, err = viewCollection.InsertOne(parentCtx, view)
 	assert.NoError(t, err)
 
-	t.Run("Success", func(t *testing.T) {
+	t.Run("NoTaskView", func(t *testing.T) {
 		api := GetAPI()
-		result, err := api.GetTaskSectionOverviewResult(parentCtx, view, userID1)
+		result, err := api.GetTaskSectionOverviewResult(parentCtx, view, userID)
 		assert.NoError(t, err)
 		assert.NotNil(t, result)
 		assert.Equal(t, taskSectionName, result.Name)
@@ -68,7 +53,40 @@ func TestGetTaskSectionOverviewResult(t *testing.T) {
 		assert.Equal(t, 1, result.IDOrdering)
 		items, ok := result.ViewItems.([]*TaskResult)
 		assert.True(t, ok)
-		assert.Equal(t, 1 , len(items))
+		assert.Equal(t, 0, len(items))
+	})
+	t.Run("SingleTaskView", func(t *testing.T) {
+		taskCollection := database.GetTaskCollection(db)
+		taskResult, err := taskCollection.InsertOne(parentCtx, database.Item{
+			TaskBase: database.TaskBase{
+				UserID:        userID,
+				IsCompleted:   false,
+				IDTaskSection: taskSectionID,
+				SourceID:      external.TASK_SOURCE_ID_GT_TASK,
+			},
+			TaskType: database.TaskType{
+				IsTask: true,
+			},
+		})
+		assert.NoError(t, err)
+		taskID := taskResult.InsertedID.(primitive.ObjectID)
+
+		api := GetAPI()
+		result, err := api.GetTaskSectionOverviewResult(parentCtx, view, userID)
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+		assert.Equal(t, taskSectionName, result.Name)
+		assert.Equal(t, ViewTaskSection, result.Type)
+		assert.Equal(t, "generaltask", result.Logo)
+		assert.Equal(t, false, result.IsLinked)
+		assert.Equal(t, []SourcesResult(nil), result.Sources)
+		assert.Equal(t, taskSectionID, *result.TaskSectionID)
+		assert.Equal(t, false, result.IsPaginated)
+		assert.Equal(t, false, result.IsReorderable)
+		assert.Equal(t, 1, result.IDOrdering)
+		items, ok := result.ViewItems.([]*TaskResult)
+		assert.True(t, ok)
+		assert.Equal(t, 1, len(items))
 		assert.Equal(t, taskID, items[0].ID)
 	})
 }
