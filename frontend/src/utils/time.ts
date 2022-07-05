a741aaf23d068188e76f0a2306e34cd3aacad964
Mitsuka Kiyohara <mitsuka@menlohacks.com>
2022-07-05 14:28:32 -0700

    fix bug for end date

--- frontend/src/utils/time.ts
+++ frontend/src/utils/time.ts
@@ -8,12 +8,15 @@ interface DateRange {
 // returns an array of the month of date, numMonths before, and numMonths after
 // for example given date = now, numMonths = 1, will return [now - 1 month, now, now + 1 month] 
 export function getMonthsAroundDate(date: DateTime, numMonths = 1): DateRange[] {
-    const startOfFirstMonth = date.startOf('month').minus({ months: numMonths })
-    const endOfFirstMonth = date.endOf('month').minus({ months: numMonths })
+    const startOfFirstMonth = date.startOf('month').plus({ months: numMonths })
+    const endOfFirstMonth = date.endOf('month').plus({ months: numMonths })
 
-    return [...Array(numMonths * 2 + 1).keys()].map((i: number): DateRange => ({
-        start: startOfFirstMonth.plus({ months: i }),
-        end: endOfFirstMonth.plus({ months: i }),
-    }))
+    return [...Array(numMonths * 2 + 1).keys()].map((i: number): DateRange => {
+        return {
+            start: startOfFirstMonth.plus({ months: i }).startOf('month'),
+            end: endOfFirstMonth.plus({ months: i }).endOf('month'),
+        }
+    })
+
 }
 
