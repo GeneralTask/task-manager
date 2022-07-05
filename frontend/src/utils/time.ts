2d5fea46fe6e79267adc743ab0b1e182c84197f5
Mitsuka Kiyohara <mitsuka@menlohacks.com>
2022-07-05 16:15:42 -0700

    fixed plus/minus error

--- frontend/src/utils/time.ts
+++ frontend/src/utils/time.ts
@@ -8,8 +8,8 @@ interface DateRange {
 // returns an array of the month of date, numMonths before, and numMonths after
 // for example given date = now, numMonths = 1, will return [now - 1 month, now, now + 1 month] 
 export function getMonthsAroundDate(date: DateTime, numMonths = 1): DateRange[] {
-    const startOfFirstMonth = date.startOf('month').plus({ months: numMonths })
-    const endOfFirstMonth = date.endOf('month').plus({ months: numMonths })
+    const startOfFirstMonth = date.startOf('month').minus({ months: numMonths })
+    const endOfFirstMonth = date.endOf('month').minus({ months: numMonths })
 
     return [...Array(numMonths * 2 + 1).keys()].map((i: number): DateRange => {
         return {
