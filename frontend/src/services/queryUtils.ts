343be14362eaf0c60c4f08f986e011b830e3b393
Scott Mai <scottmai702@gmail.com>
2022-07-06 23:42:41 -0700

    comment

--- frontend/src/services/queryUtils.ts
+++ frontend/src/services/queryUtils.ts
@@ -52,17 +52,15 @@ export const mutateUserInfo = async (userInfo: { agreed_to_terms: boolean; opted
     return res.json()
 }
 
+// Wrapper for useQueryClient that adds getImmutableQueryData method
+// getImmutableQueryData simply casts the result as Immutable so the editor disallows direct assignment
 interface GTQueryClient extends QueryClient {
     getImmutableQueryData: <TData = unknown>(queryKey: QueryKey, filters?: QueryFilters) => Immutable<TData> | undefined
 }
-
 export const useGTQueryClient = (): GTQueryClient => {
     const queryClient = useQueryClient() as GTQueryClient
 
     queryClient.getImmutableQueryData = <TData = unknown>(queryKey: QueryKey, filters?: QueryFilters): Immutable<TData> | undefined => queryClient.getQueryData(queryKey, filters)
-
-    // const newQueryClient = queryClient as GTQueryClient
-    // newQueryClient.getImmutableQueryData = getImmutableQueryData
 
     return queryClient
 }
