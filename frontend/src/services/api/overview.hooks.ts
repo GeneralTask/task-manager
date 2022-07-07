7142a0f19ca7145449232a5fcf4005da8cc39e96
Jack Hamilton <john.hamilton@wustl.edu>
2022-07-07 15:26:46 -0700

    allow linear tasks

--- frontend/src/services/api/overview.hooks.ts
+++ frontend/src/services/api/overview.hooks.ts
@@ -217,6 +217,154 @@ const dummyOverviewViews: TOverviewView[] = [
             },
         ],
     },
+    {
+        id: '4',
+        name: 'Linear',
+        type: 'linear',
+        is_paginated: false,
+        is_reorderable: true,
+        logo: 'linear',
+        view_items: [
+            {
+                id: '10',
+                id_ordering: 1,
+                title: 'Fix key error on AddViewModal',
+                body: 'no description, unless??',
+                deeplink: 'https://linear.app/general-task/issue/FRO-285/fix-key-error-on-addviewmodal',
+                sent_at: '',
+                time_allocated: 0,
+                due_date: '',
+                source: {
+                    name: 'Linear',
+                    logo: '',
+                    logo_v2: 'linear',
+                    is_completable: true,
+                    is_replyable: false,
+                },
+                external_status: {
+                    state: 'Todo',
+                    type: 'unstarted',
+                },
+                comments: [
+                    {
+                        body: 'possible resource: [https://stackoverflow.com/questions/26499759/how-to-display-an-html-email-in-a-web-application](https://stackoverflow.com/questions/26499759/how-to-display-an-html-email-in-a-web-application)',
+                        user: {
+                            ExternalID: '4c2421c6-9079-48ba-953a-b5faafe2b782',
+                            Name: 'Jack Hamilton',
+                            DisplayName: 'jack',
+                            Email: 'jack@generaltask.com',
+                        },
+                        created_at: '2022-07-06T20:49:41.409Z',
+                    },
+                    {
+                        body: '![image.png](https://uploads.linear.app/572f6728-59c0-4844-96b1-34b5e77b704e/25a0d73b-b120-4669-94f0-0dca972218a6/403cc0a9-c176-48bf-b521-c5193eefb9dc)\n\nthis is pretty embarrassingly bad for some of my emails',
+                        user: {
+                            ExternalID: 'b494ee99-47a4-4f29-abe4-c17a29308ee6',
+                            Name: 'John Reinstra',
+                            DisplayName: 'john',
+                            Email: 'john@generaltask.com',
+                        },
+                        created_at: '2022-07-05T00:27:00.945Z',
+                    }
+                ],
+                sender: '',
+                is_done: false,
+                recipients: { to: [], cc: [], bcc: [] },
+            },
+            {
+                id: '11',
+                id_ordering: 2,
+                title: 'Fix key error on AddViewModal 2',
+                body: 'no description, unless?? 2',
+                deeplink: 'https://linear.app/general-task/issue/FRO-285/fix-key-error-on-addviewmodal',
+                sent_at: '',
+                time_allocated: 0,
+                due_date: '',
+                source: {
+                    name: 'Linear',
+                    logo: '',
+                    logo_v2: 'linear',
+                    is_completable: true,
+                    is_replyable: false,
+                },
+                external_status: {
+                    state: 'Todo',
+                    type: 'unstarted',
+                },
+                comments: [
+                    {
+                        body: 'possible resource: [https://stackoverflow.com/questions/26499759/how-to-display-an-html-email-in-a-web-application](https://stackoverflow.com/questions/26499759/how-to-display-an-html-email-in-a-web-application)',
+                        user: {
+                            ExternalID: '4c2421c6-9079-48ba-953a-b5faafe2b782',
+                            Name: 'Jack Hamilton',
+                            DisplayName: 'jack',
+                            Email: 'jack@generaltask.com',
+                        },
+                        created_at: '2022-07-06T20:49:41.409Z',
+                    },
+                    {
+                        body: '![image.png](https://uploads.linear.app/572f6728-59c0-4844-96b1-34b5e77b704e/25a0d73b-b120-4669-94f0-0dca972218a6/403cc0a9-c176-48bf-b521-c5193eefb9dc)\n\nthis is pretty embarrassingly bad for some of my emails',
+                        user: {
+                            ExternalID: 'b494ee99-47a4-4f29-abe4-c17a29308ee6',
+                            Name: 'John Reinstra',
+                            DisplayName: 'john',
+                            Email: 'john@generaltask.com',
+                        },
+                        created_at: '2022-07-05T00:27:00.945Z',
+                    }
+                ],
+                sender: '',
+                is_done: false,
+                recipients: { to: [], cc: [], bcc: [] },
+            },
+            {
+                id: '12',
+                id_ordering: 3,
+                title: 'Fix key error on AddViewModal 3',
+                body: 'no description, unless?? 3',
+                deeplink: 'https://linear.app/general-task/issue/FRO-285/fix-key-error-on-addviewmodal',
+                sent_at: '',
+                time_allocated: 0,
+                due_date: '',
+                source: {
+                    name: 'Linear',
+                    logo: '',
+                    logo_v2: 'linear',
+                    is_completable: true,
+                    is_replyable: false,
+                },
+                external_status: {
+                    state: 'Todo',
+                    type: 'unstarted',
+                },
+                comments: [
+                    {
+                        body: 'possible resource: [https://stackoverflow.com/questions/26499759/how-to-display-an-html-email-in-a-web-application](https://stackoverflow.com/questions/26499759/how-to-display-an-html-email-in-a-web-application)',
+                        user: {
+                            ExternalID: '4c2421c6-9079-48ba-953a-b5faafe2b782',
+                            Name: 'Jack Hamilton',
+                            DisplayName: 'jack',
+                            Email: 'jack@generaltask.com',
+                        },
+                        created_at: '2022-07-06T20:49:41.409Z',
+                    },
+                    {
+                        body: '![image.png](https://uploads.linear.app/572f6728-59c0-4844-96b1-34b5e77b704e/25a0d73b-b120-4669-94f0-0dca972218a6/403cc0a9-c176-48bf-b521-c5193eefb9dc)\n\nthis is pretty embarrassingly bad for some of my emails',
+                        user: {
+                            ExternalID: 'b494ee99-47a4-4f29-abe4-c17a29308ee6',
+                            Name: 'John Reinstra',
+                            DisplayName: 'john',
+                            Email: 'john@generaltask.com',
+                        },
+                        created_at: '2022-07-05T00:27:00.945Z',
+                    }
+                ],
+                sender: '',
+                is_done: false,
+                recipients: { to: [], cc: [], bcc: [] },
+            },
+        ],
+    },
 ]
 
 const dummySupportedViews = [
@@ -254,7 +402,7 @@ const dummySupportedViews = [
         id: 'Linear',
         name: 'Linear',
         logo: 'linear',
-        is_added: false,
+        is_added: true,
     },
     {
         id: 'Slack',
