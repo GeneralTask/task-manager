8044a6b276d73c22a49c492fb6a8dd0e2f507b19
Mitsuka Kiyohara <mitsuka@menlohacks.com>
2022-07-12 12:17:54 -0500

    add min-width to scrollable list template

--- frontend/src/components/templates/ScrollableListTemplate.ts
+++ frontend/src/components/templates/ScrollableListTemplate.ts
@@ -1,10 +1,12 @@
 import styled from "styled-components"
 import { Colors } from "../../styles"
+import { DEFAULT_VIEW_WIDTH } from "../../styles/dimensions"
 
 const ScrollableListTemplate = styled.div<{ noTopPadding?: boolean }>`
     padding: ${({ noTopPadding }) => noTopPadding ? '0px' : '40px'} 10px 100px;
     overflow-y: auto;
     flex: 1 0;
+    min-width: ${DEFAULT_VIEW_WIDTH};
     background-color: ${Colors.gray._50};
 `
 
