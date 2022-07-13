9a9149a9f50c8c5966123334dadfff7cb0e64eed
Nolan Jimenez <nolan1299@gmail.com>
2022-07-13 11:38:56 -0500

    update tests

--- backend/api/overview_test.go
+++ backend/api/overview_test.go
@@ -261,32 +261,43 @@ func TestOverviewViewDelete(t *testing.T) {
 	api := GetAPI()
 	router := GetRouter(api)
 	authToken := login("testDeleteView@generaltask.com", "")
-	t.Run("DeleteUnauthorizedView", func(t *testing.T) {
-		request, _ := http.NewRequest("DELETE", "/overview/views/1/", nil)
-		response := httptest.NewRecorder()
-		router.ServeHTTP(response, request)
-		assert.Equal(t, http.StatusUnauthorized, response.Code)
+
+	db, dbCleanup, err := database.GetDBConnection()
+	assert.NoError(t, err)
+	defer dbCleanup()
+
+	viewCollection := database.GetViewCollection(db)
+	userID := getUserIDFromAuthToken(t, db, authToken)
+	view, err := viewCollection.InsertOne(parentCtx, database.View{
+		UserID: userID,
 	})
-	t.Run("DeleteInvalidURL", func(t *testing.T) {
+	assert.NoError(t, err)
+	viewID := view.InsertedID.(primitive.ObjectID)
+
+	t.Run("InvalidViewID", func(t *testing.T) {
 		request, _ := http.NewRequest("DELETE", "/overview/views/1/", nil)
 		request.Header.Set("Authorization", "Bearer "+authToken)
 		response := httptest.NewRecorder()
 		router.ServeHTTP(response, request)
 		assert.Equal(t, http.StatusNotFound, response.Code)
+
+		count, err := viewCollection.CountDocuments(parentCtx, bson.M{"_id": viewID})
+		assert.NoError(t, err)
+		assert.Equal(t, int64(1), count)
+	})
+	t.Run("InvalidUserID", func(t *testing.T) {
+		invalidToken := "invalidToken"
+		request, _ := http.NewRequest("DELETE", "/overview/views/"+viewID.Hex()+"/", nil)
+		request.Header.Set("Authorization", "Bearer "+invalidToken)
+		response := httptest.NewRecorder()
+		router.ServeHTTP(response, request)
+		assert.Equal(t, http.StatusUnauthorized, response.Code)
+
+		count, err := viewCollection.CountDocuments(parentCtx, bson.M{"_id": viewID})
+		assert.NoError(t, err)
+		assert.Equal(t, int64(1), count)
 	})
 	t.Run("Success", func(t *testing.T) {
-		db, dbCleanup, err := database.GetDBConnection()
-		assert.NoError(t, err)
-		defer dbCleanup()
-
-		viewCollection := database.GetViewCollection(db)
-		userID := getUserIDFromAuthToken(t, db, authToken)
-		view, err := viewCollection.InsertOne(parentCtx, database.View{
-			UserID: userID,
-		})
-		assert.NoError(t, err)
-		viewID := view.InsertedID.(primitive.ObjectID)
-
 		request, _ := http.NewRequest("DELETE", "/overview/views/"+viewID.Hex()+"/", nil)
 		request.Header.Set("Authorization", "Bearer "+authToken)
 		response := httptest.NewRecorder()
