d0a9268605a2ac3128119c77ab1497d7cfd3bef1
alfaindia <97246822+alfaindia@users.noreply.github.com>
2022-07-13 17:09:58 -0500

    Add last_updated_at to PR endpoint (#1377)
    * Add last_updated_at to PR endpoint
    
    * update
    
    * update

--- backend/testutils/github_constants.go
+++ backend/testutils/github_constants.go
@@ -4,7 +4,7 @@ const (
 	ClientResponsePayload            string = `{"id": 1, "plan": {}}`
 	UserResponsePayload              string = `{"id": 1, "login": "chad1616"}`
 	UserRepositoriesPayload          string = `[{"id": 1234, "name": "ExampleRepository", "owner": {"login": "gigaChad123"}}]`
-	UserPullRequestsPayload          string = `[{"id": 1, "number": 420, "title": "Fix big oopsie", "created_at": "2011-01-26T19:01:12Z", "html_url": "github.com", "user": {"login": "chad1616", "id": 1}, "requested_reviewers": [], "head": {"sha": "abc123", "ref": "ExampleBranch"}}]`
+	UserPullRequestsPayload          string = `[{"id": 1, "number": 420, "title": "Fix big oopsie", "created_at": "2011-01-26T19:01:12Z", "updated_at": "2011-01-26T19:01:12Z", "html_url": "github.com", "user": {"login": "chad1616", "id": 1}, "requested_reviewers": [], "head": {"sha": "abc123", "ref": "ExampleBranch"}}]`
 	PullRequestReviewersPayload      string = `{"users": [{"login": "goodTeamMember"}]}`
 	EmptyPullRequestReviewersPayload string = `{"users": []}`
 	EmptyCheckRunsForRefPayload      string = `{"total_count": 0, "check_runs": []}`
