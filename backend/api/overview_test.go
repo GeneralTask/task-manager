5e5bb6c1646093d1b74eb15d0567b22168b3408b
Nolan Jimenez <nolan1299@gmail.com>
2022-07-13 16:20:59 -0500

    add LinearTaskIsCompleted test

--- backend/api/overview_test.go
+++ backend/api/overview_test.go
@@ -327,6 +327,26 @@ func TestGetLinearOverviewResult(t *testing.T) {
 		expectedViewResult.ViewItems = []*TaskResult{}
 		assertOverviewViewResultEqual(t, expectedViewResult, *result)
 	})
+	t.Run("LinearTaskIsCompleted", func(t *testing.T) {
+		taskCollection := database.GetTaskCollection(db)
+		_, err := taskCollection.InsertOne(parentCtx, database.Item{
+			TaskBase: database.TaskBase{
+				UserID:        userID,
+				IsCompleted:   true,
+				IDTaskSection: primitive.NilObjectID,
+				SourceID:      external.TASK_SOURCE_ID_LINEAR,
+			},
+			TaskType: database.TaskType{
+				IsTask: true,
+			},
+		})
+		assert.NoError(t, err)
+		result, err := api.GetLinearOverviewResult(db, parentCtx, view, userID)
+		assert.NoError(t, err)
+		assert.NotNil(t, result)
+		expectedViewResult.ViewItems = []*TaskResult{}
+		assertOverviewViewResultEqual(t, expectedViewResult, *result)
+	})
 }
 
 func TestUpdateViewsLinkedStatus(t *testing.T) {
