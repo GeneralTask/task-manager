49fb5d908bf96b819faf83f7ab135057ebaf9747
Jack Hamilton <john.hamilton@wustl.edu>
2022-07-15 15:54:22 -0500

    sentry init

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,17 @@
 import App from '../App'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
+
+Sentry.init({
+    dsn: 'https://fac9999515c14cf7a15fbe9b4eddcd3d@o1302719.ingest.sentry.io/6577051',
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+})
 
 ReactDOM.render(<App />, document.getElementById('root'))
