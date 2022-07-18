8c2ed787ddb65bacb3a600a26fde1f726d30182a
Nolan Jimenez <nolan1299@gmail.com>
2022-07-18 10:37:48 -0700

    update cors for sentry

--- backend/api/utils.go
+++ backend/api/utils.go
@@ -131,7 +131,7 @@ func getToken(c *gin.Context) (string, error) {
 
 // CORSMiddleware sets CORS headers, abort if CORS preflight request is received
 func CORSMiddleware(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization,Access-Control-Allow-Origin,Access-Control-Allow-Headers,Access-Control-Allow-Methods,Content-Type,Timezone-Offset")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization,Access-Control-Allow-Origin,Access-Control-Allow-Headers,Access-Control-Allow-Methods,Content-Type,Timezone-Offset,sentry-trace,baggage")
 	c.Writer.Header().Set("Access-Control-Allow-Origin", config.GetConfigValue("ACCESS_CONTROL_ALLOW_ORIGIN"))
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, PATCH, DELETE")
 	if c.Request.Method == "OPTIONS" {
