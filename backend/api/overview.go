39b1480d3f861840a10957abcc4f835288c03ddc
Nolan Jimenez <nolan1299@gmail.com>
2022-07-18 14:42:07 -0700

    Revert "update supported views endpoint to only return views that are currently supported"
    This reverts commit 8d5c8210ce56135b4301df69430089a86decb1fa.

--- backend/api/overview.go
+++ backend/api/overview.go
@@ -487,7 +487,7 @@ func (api *API) OverviewSupportedViewsList(c *gin.Context) {
 			Views:    supportedTaskSectionViews,
 		},
 		{
-			Type:     ViewLinear,
+			Type:     "linear",
 			Name:     "Linear",
 			Logo:     "linear",
 			IsNested: false,
@@ -499,7 +499,7 @@ func (api *API) OverviewSupportedViewsList(c *gin.Context) {
 			},
 		},
 		{
-			Type:     ViewSlack,
+			Type:     "slack",
 			Name:     "Slack",
 			Logo:     "slack",
 			IsNested: false,
@@ -507,6 +507,18 @@ func (api *API) OverviewSupportedViewsList(c *gin.Context) {
 				{
 					Name:    "Slack View",
 					IsAdded: true,
+				},
+			},
+		},
+		{
+			Type:     "github",
+			Name:     "GitHub",
+			Logo:     "github",
+			IsNested: true,
+			Views: []SupportedViewItem{
+				{
+					Name:    "Github View",
+					IsAdded: false,
 				},
 			},
 		},
@@ -560,7 +572,7 @@ func (api *API) viewIsAdded(db *mongo.Database, userID primitive.ObjectID, viewT
 		return api.viewExists(db, userID, viewType, &[]bson.M{
 			{"task_section_id": view.TaskSectionID},
 		})
-	} else if viewType == ViewLinear || viewType == ViewSlack {
+	} else if viewType == ViewLinear {
 		return api.viewExists(db, userID, viewType, nil)
 	}
 	return false, errors.New("invalid view type")
