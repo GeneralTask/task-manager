bb9423499cfd04c3c8f9d54229c55969181f4b5b
Scott Mai <scottmai702@gmail.com>
2022-07-19 17:42:32 -0700

    Overview page mark tasks complete (#1421)
    * external
    
    * markComplete
    
    * flash fix
    
    * rm types
    
    * clear
    
    * rm unused func
    
    * cancel and invalidate tasks
    
    * lint

--- frontend/src/components/overview/viewItems/ExternalViewItems.tsx
+++ frontend/src/components/overview/viewItems/ExternalViewItems.tsx
@@ -1,14 +1,23 @@
-import React, { useRef } from 'react'
+import React, { useCallback, useRef } from 'react'
 import { useParams } from 'react-router-dom'
+import { useMarkTaskDone } from '../../../services/api/overview.hooks'
 import { TTask } from '../../../utils/types'
 import Task from '../../molecules/Task'
 import { ViewItemsProps } from './viewItems.types'
 
 const ExternalViewItems = ({ view, visibleItemsCount }: ViewItemsProps) => {
     const { overviewItem } = useParams()
+    const { mutate: markTaskDone } = useMarkTaskDone()
 
     // TODO: either change Task to make this optional or add better support for scrolling. Unused for now.
     const scrollingRef = useRef<HTMLDivElement>(null)
+
+    const handleMarkTaskComplete = useCallback(
+        (taskId: string, isComplete: boolean) => {
+            markTaskDone({ taskId, isCompleted: isComplete })
+        },
+        [markTaskDone]
+    )
 
     return (
         <div ref={scrollingRef}>
@@ -20,6 +29,7 @@ const ExternalViewItems = ({ view, visibleItemsCount }: ViewItemsProps) => {
                     sectionScrollingRef={scrollingRef}
                     isSelected={overviewItem === item.id}
                     link={`/overview/${item.id}`}
+                    onMarkComplete={handleMarkTaskComplete}
                 />
             ))}
         </div>
