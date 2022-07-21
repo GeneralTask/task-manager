d102873d49efbd42d0075655c0cd02307559ec15
Jack Hamilton <john.hamilton@wustl.edu>
2022-07-21 15:04:40 -0700

    remove comment fix constants

--- frontend/src/styles/colors.ts
+++ frontend/src/styles/colors.ts
@@ -33,30 +33,9 @@ const blue = {
 }
 const purple = {
     default: '#5634CF',
+    accent: '#452AA5',
     light: '#EEEBFA',
 }
-
-// export const purple = {
-//     _1: '#5C31D7',
-//     _2: '#3A15A0',
-//     _3: '#E1D7FD',
-// }
-// export const red = {
-//     _1: '#FF135A',
-//     _2: '#FFEFF2',
-// }
-// export const gray = {
-//     _900: '#18181B',
-//     _800: '#27272A',
-//     _700: '#3F3F46',
-//     _600: '#52525B',
-//     _500: '#71717A',
-//     _400: '#A1A1AA',
-//     _300: '#D4D3D8',
-//     _200: '#E4E3E7',
-//     _100: '#F4F4F5',
-//     _50: '#FAFAFA',
-// }
 
 
 export const background = {
@@ -74,15 +53,15 @@ export const text = {
     white: white,
 }
 
-export const button = { // TODO: move values to colors above
+export const button = {
     primary: {
-        default: '#5C31D7',
-        hover: '#452AA5',
-        active_text: '#DBD7EE',
+        default: purple.default,
+        hover: purple.accent,
+        active_text: gray.middark,
     },
     secondary: {
         default: white,
-        hover: '#F2F2F2',
+        hover: gray.midlight,
         active_text: black,
     },
 }
