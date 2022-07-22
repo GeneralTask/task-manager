38a2a76b9fd2e1302e84620aa69598016d5d6f1b
Jack Hamilton <john.hamilton@wustl.edu>
2022-07-22 14:22:11 -0700

    bit o renaming

--- frontend/src/styles/typography.ts
+++ frontend/src/styles/typography.ts
@@ -8,20 +8,20 @@ export const landingScreen = {
 }
 
 const fontSize = {
-    xxSmall: '10px',
-    xSmall: '12px',
-    small: '14px',
-    medium: '16px',
-    large: '20px',
-    xLarge: '32px',
-    xxLarge: '48px',
+    xxs: '10px',
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '20px',
+    xl: '32px',
+    xxl: '48px',
 }
 const lineHeight = {
-    xSmall: '14px',
-    small: '16px',
-    medium: '24px',
-    large: '40px',
-    xLarge: '56px',
+    xs: '14px',
+    s: '16px',
+    m: '24px',
+    l: '40px',
+    xl: '56px',
 }
 const weight2 = {
     regular: '400',
@@ -31,50 +31,50 @@ const weight2 = {
 }
 
 export const header = css`
-    font-size: ${fontSize.xxLarge};
-    line-height: ${lineHeight.xLarge};
+    font-size: ${fontSize.xxl};
+    line-height: ${lineHeight.xl};
     font-weight: ${weight2.bold};
     letter-spacing: -.02em; // -2%
 `
 export const title = css`
-    font-size: ${fontSize.xLarge};
-    line-height: ${lineHeight.large};
+    font-size: ${fontSize.xl};
+    line-height: ${lineHeight.l};
     font-weight: ${weight2.semibold};
     letter-spacing: -.01em; // -1%
 `
 export const subtitle = css`
-    font-size: ${fontSize.large};
-    line-height: ${lineHeight.medium};
+    font-size: ${fontSize.l};
+    line-height: ${lineHeight.m};
     font-weight: ${weight2.medium};
     letter-spacing: -.01em; // -1%
 `
 export const body = css`
-    font-size: ${fontSize.medium};
-    line-height: ${lineHeight.medium};
+    font-size: ${fontSize.m};
+    line-height: ${lineHeight.m};
     font-weight: ${weight2.regular};
     letter-spacing: -.01em; // -1%
 `
 export const bodySmall = css`
-    font-size: ${fontSize.small};
-    line-height: ${lineHeight.medium};
+    font-size: ${fontSize.s};
+    line-height: ${lineHeight.m};
     font-weight: ${weight2.regular};
     letter-spacing: -.01em; // -1%
 `
 export const label = css`
-    font-size: ${fontSize.xSmall};
-    line-height: ${lineHeight.small};
+    font-size: ${fontSize.xs};
+    line-height: ${lineHeight.s};
     font-weight: ${weight2.regular};
     letter-spacing: -.01em; // -1%
 `
 export const eyebrow = css`
-    font-size: ${fontSize.xSmall};
-    line-height: ${lineHeight.small};
+    font-size: ${fontSize.xs};
+    line-height: ${lineHeight.s};
     font-weight: ${weight2.regular};
     letter-spacing: .12em; // 12%
     text-transform: uppercase;
 `
 export const mini = css`
-    font-size: ${fontSize.xxSmall};
-    line-height: ${lineHeight.xSmall};
+    font-size: ${fontSize.xxs};
+    line-height: ${lineHeight.xs};
     font-weight: ${weight2.regular};
 `
