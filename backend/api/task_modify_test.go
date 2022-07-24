12f55aea56c307551811308ed44bdedf339f4758
wooglin <alfaindiagt@protonmail.com>
2022-07-24 18:49:40 -0500

    update

--- backend/api/task_modify_test.go
+++ backend/api/task_modify_test.go
@@ -96,7 +96,6 @@ func TestMarkAsComplete(t *testing.T) {
 	taskUpdateServer := testutils.GetMockAPIServer(t, 200, response)
 
 	api := GetAPI()
-
 	api.ExternalConfig.GoogleOverrideURLs.GmailModifyURL = &inboxGmailModifyServer.URL
 	api.ExternalConfig.Linear.ConfigValues.TaskUpdateURL = &taskUpdateServer.URL
 	router := GetRouter(api)
