7d79b3a144e832ca017716df7846d040336159b1
wooglin <alfaindiagt@protonmail.com>
2022-07-25 18:00:36 -0500

    update

--- backend/migrations/005_migration_test.go
+++ backend/migrations/005_migration_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/GeneralTask/task-manager/backend/database"
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"testing"
 )
 
@@ -16,13 +17,18 @@ func TestMigrate005(t *testing.T) {
 	defer dbCleanup()
 	migrate, err := getMigrate("")
 	assert.NoError(t, err)
+	err = migrate.Steps(1)
+	assert.NoError(t, err)
+
 	tasksCollection := database.GetTaskCollection(db)
 	dbCtx, cancel := context.WithTimeout(parentCtx, constants.DatabaseTimeout)
 	defer cancel()
+	userID := primitive.NewObjectID()
 
 	t.Run("MigrateUp", func(t *testing.T) {
 		tasksCollection.InsertOne(dbCtx, database.Item{
 			TaskBase: database.TaskBase{
+				UserID:   userID,
 				Title:    "task_1",
 				SourceID: "asana_task",
 			},
@@ -30,6 +36,7 @@ func TestMigrate005(t *testing.T) {
 		})
 		tasksCollection.InsertOne(dbCtx, database.Item{
 			TaskBase: database.TaskBase{
+				UserID:   userID,
 				Title:    "task_2",
 				SourceID: "asana_task",
 			},
@@ -37,6 +44,7 @@ func TestMigrate005(t *testing.T) {
 		})
 		tasksCollection.InsertOne(dbCtx, database.Item{
 			TaskBase: database.TaskBase{
+				UserID:   userID,
 				Title:    "task_3",
 				SourceID: "linear_task",
 			},
@@ -44,36 +52,47 @@ func TestMigrate005(t *testing.T) {
 		})
 		tasksCollection.InsertOne(dbCtx, database.Item{
 			TaskBase: database.TaskBase{
+				UserID:   userID,
 				Title:    "task_4",
 				SourceID: "gt",
 			},
 			TaskType: database.TaskType{IsTask: true},
 		})
 
-		err = migrate.Steps(5)
+		err = migrate.Steps(1)
 		assert.NoError(t, err)
 
 		var tasks []database.Item
-		cursor, err := tasksCollection.Find(dbCtx, bson.M{"task_type.is_task": true})
+		cursor, err := tasksCollection.Find(dbCtx, bson.M{
+			"$and": []bson.M{
+				{"task_type.is_task": true},
+				{"user_id": userID},
+			},
+		})
 		assert.NoError(t, err)
 		err = cursor.All(dbCtx, &tasks)
 		assert.NoError(t, err)
 		assert.Equal(t, int(2), len(tasks))
 
-		expectedTitles := []string{"task_3", "task_4"}
+		expectedTitles := []string{"task_4", "task_3"}
 		for i, item := range tasks {
 			assert.Equal(t, expectedTitles[i], item.Title)
 		}
 	})
 	t.Run("MigrateDown", func(t *testing.T) {
 		var tasks []database.Item
-		cursor, err := tasksCollection.Find(dbCtx, bson.M{"task_type.is_task": true})
+		cursor, err := tasksCollection.Find(dbCtx, bson.M{
+			"$and": []bson.M{
+				{"task_type.is_task": true},
+				{"user_id": userID},
+			},
+		})
 		assert.NoError(t, err)
 		err = cursor.All(dbCtx, &tasks)
 		assert.NoError(t, err)
 		assert.Equal(t, int(2), len(tasks))
 
-		expectedTitles := []string{"task_3", "task_4"}
+		expectedTitles := []string{"task_4", "task_3"}
 		for i, item := range tasks {
 			assert.Equal(t, expectedTitles[i], item.Title)
 		}
