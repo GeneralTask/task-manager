935d9ee5fa1c3cd28df23def73d2b6e8d627d1fa
Julian <julian@sweetrain.com>
2022-07-28 12:35:27 +0300

    add test

--- backend/api/linked_accounts_test.go
+++ backend/api/linked_accounts_test.go
@@ -29,6 +29,7 @@ func TestSupportedAccountTypesList(t *testing.T) {
 		body, err := ioutil.ReadAll(recorder.Body)
 		assert.NoError(t, err)
 		assert.True(t, strings.Contains(string(body), "{\"name\":\"Google\",\"logo\":\"/images/gmail.svg\",\"authorization_url\":\"http://localhost:8080/link/google/\"}"))
+		assert.Equal(t, 1, strings.Count(string(body), "{\"name\":\"Slack\",\"logo\":\"/images/slack.svg\",\"authorization_url\":\"http://localhost:8080/link/slack/\"}"))
 	})
 	t.Run("Unauthorized", func(t *testing.T) {
 		router := GetRouter(GetAPI())
