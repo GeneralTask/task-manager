36aa8aa8c2fa57ab631aad85eee8185b0fd1c840
Scott Mai <scottmai702@gmail.com>
2022-07-27 17:01:26 -1000

    ok no more tests for now

--- backend/api/event_modify_test.go
+++ backend/api/event_modify_test.go
@@ -2,27 +2,28 @@ package api
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"net/http"
 	"testing"
+
+	"github.com/GeneralTask/task-manager/backend/database"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEventModify(t *testing.T) {
-	// parentCtx := context.Background()
-	// db, dbCleanup, err := database.GetDBConnection()
-	// assert.NoError(t, err)
-	// defer dbCleanup()
+	parentCtx := context.Background()
+	db, dbCleanup, err := database.GetDBConnection()
+	assert.NoError(t, err)
+	defer dbCleanup()
 
 	authToken := login("approved@generaltask.com", "")
 	// userID := getUserIDFromAuthToken(t, db, authToken)
 
-	// taskCollection := database.GetTaskCollection(db)
+	taskCollection := database.GetTaskCollection(db)
+	taskCollection.InsertOne(parentCtx, database.Item{})
 
 	// dbCtx, cancel := context.WithTimeout(parentCtx, constants.DatabaseTimeout)
-	// defer cancel()
-	// authtoken := login("test_overview@generaltask.com", "")
-	// api := GetAPI()
-	// router := GetRouter(api)
 
 	eventID := "duck"
 	validUrl := fmt.Sprintf("/events/modify/%s/", eventID)
@@ -36,6 +37,11 @@ func TestEventModify(t *testing.T) {
 	t.Run("EmptyBody", func(t *testing.T) {
 		body := bytes.NewBuffer([]byte(`{}`))
 		ServeRequest(t, authToken, "PATCH", validUrl, body, http.StatusBadRequest)
+	})
+	t.Run("ModifyTitle", func(t *testing.T) {
+		body := bytes.NewBuffer([]byte(`{"title": "duck party"}`))
+		ServeRequest(t, authToken, "PATCH", validUrl, body, http.StatusOK)
+		// TODO: get gud
 	})
 
 }
