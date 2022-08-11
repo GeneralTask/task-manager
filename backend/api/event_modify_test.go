06dc659205bc8a555201df5112e6fb7a85dd9ffa
Scott Mai <scottmai702@gmail.com>
2022-08-10 19:37:25 -0700

    added test but it fails

--- backend/api/event_modify_test.go
+++ backend/api/event_modify_test.go
@@ -26,7 +26,7 @@ func TestInvalidEventModify(t *testing.T) {
 
 	authToken := login("approved@generaltask.com", "")
 	userID := getUserIDFromAuthToken(t, db, authToken)
-	accountID := "duccount_id"
+	accountID := "duck@duck.com"
 
 	taskCollection := database.GetTaskCollection(db)
 	event, err := taskCollection.InsertOne(parentCtx, database.Item{
@@ -36,6 +36,10 @@ func TestInvalidEventModify(t *testing.T) {
 			SourceID:   external.TASK_SOURCE_ID_GCAL,
 			Title:      "initial summary",
 			Body:       "initial description",
+		},
+		CalendarEvent: database.CalendarEvent{
+			DatetimeStart: primitive.DateTime(1609559200000),
+			DatetimeEnd:   primitive.DateTime(1609459200000),
 		},
 	})
 	assert.NoError(t, err)
@@ -48,9 +52,33 @@ func TestInvalidEventModify(t *testing.T) {
 	api.ExternalConfig.GoogleOverrideURLs.CalendarFetchURL = &calendarModifyServer.URL
 	router := GetRouter(api)
 
+	UnauthorizedTest(t, "PATCH", validUrl, bytes.NewBuffer([]byte(`{"account_id": "duck@duck.com", "summary": "duck"}`)))
+	t.Run("UpdateStartTimeSuccess", func(t *testing.T) {
+		body := bytes.NewBuffer([]byte(`{
+			"account_id": "duck@duck.com",
+			"datetime_start": "2021-01-01T00:00:00Z"
+		}`))
+		request, _ := http.NewRequest(
+			"PATCH",
+			"/events/modify/"+eventID+"/",
+			body)
+		request.Header.Add("Authorization", "Bearer "+authToken)
+		recorder := httptest.NewRecorder()
+		router.ServeHTTP(recorder, request)
+		assert.Equal(t, http.StatusOK, recorder.Code)
+
+		dbCtx, cancel := context.WithTimeout(parentCtx, constants.DatabaseTimeout)
+		defer cancel()
+		event, err := database.GetItem(dbCtx, eventObjectID, userID)
+		assert.NoError(t, err)
+		assert.Equal(t, "initial summary", event.Title)
+		assert.Equal(t, "initial description", event.Body)
+		assert.Equal(t, primitive.DateTime(1609459200000), event.DatetimeStart)
+		assert.Equal(t, primitive.DateTime(1609459200000), event.DatetimeEnd)
+	})
 	t.Run("Success", func(t *testing.T) {
 		body := bytes.NewBuffer([]byte(`{
-			"account_id": "duck@duck.com", 
+			"account_id": "duck@duck.com",
 			"summary": "new summary",
 			"description": "new description",
 			"datetime_start": "2020-01-01T00:00:00Z",
@@ -71,11 +99,9 @@ func TestInvalidEventModify(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, "new summary", event.Title)
 		assert.Equal(t, "new description", event.Body)
-		// assert that start and end times are correct
 		assert.Equal(t, primitive.DateTime(1577836800000), event.DatetimeStart)
-		assert.Equal(t, primitive.DateTime(1577836800000), event.DatetimeStart)
+		assert.Equal(t, primitive.DateTime(1580515200000), event.DatetimeEnd)
 	})
-	UnauthorizedTest(t, "PATCH", validUrl, bytes.NewBuffer([]byte(`{"account_id": "duck@duck.com", "summary": "duck"}`)))
 	t.Run("NoBody", func(t *testing.T) {
 		ServeRequest(t, authToken, "PATCH", validUrl, nil, http.StatusBadRequest)
 	})
