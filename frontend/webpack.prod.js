f7d85e3aef6b271a6e79e5b37061d8a89faf084e
Jack Hamilton <john.hamilton@wustl.edu>
2022-08-11 09:45:52 -0700

    Only upload sourcemaps and use sentry in prod

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -2,9 +2,18 @@
 /* eslint-disable no-undef */
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
+const SentryCliPlugin = require('@sentry/webpack-plugin')
 
 process.env.NODE_ENV = 'production'
 
 module.exports = merge(common, {
     mode: 'production',
+    devtool: 'source-map',
+    plugins: [
+        new SentryCliPlugin({
+            // Must be the last running plugin
+            include: '.',
+            ignore: ['node_modules', 'webpack.dev.js', 'webpack.prod.js', 'webpack.common.js'],
+        }),
+    ],
 })
