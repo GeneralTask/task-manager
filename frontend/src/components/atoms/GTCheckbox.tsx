163f8d18de532cdef6ba400121e1b7b31d34f699
Jack Hamilton <john.hamilton@wustl.edu>
2022-08-15 10:12:17 -0700

    lottie-react

--- frontend/src/components/atoms/GTCheckbox.tsx
+++ frontend/src/components/atoms/GTCheckbox.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { iconSize, TIconSize } from '../../styles/dimensions'
 import NoStyleButton from './buttons/NoStyleButton'
-import lottie, { AnimationItem } from 'lottie-web'
 import checkbox from '../../../public/animations/checkbox.json'
 import styled from 'styled-components'
+import Lottie, { LottieRef } from 'lottie-react'
 
 const AnimationContainer = styled.div`
     position: absolute;
@@ -28,46 +28,37 @@ interface GTCheckboxProps {
     disabled?: boolean
 }
 const GTCheckbox = ({ isChecked, onChange, size, disabled }: GTCheckboxProps) => {
-    const animationContainerRef = useRef<HTMLDivElement>(null)
-    const [animation, setAnimation] = useState<AnimationItem>()
+    const animRef: LottieRef = useRef(null)
 
     const onClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.stopPropagation()
         onChange(!isChecked)
         if (!isChecked) {
-            animation?.play()
+            animRef.current?.play()
         } else {
-            animation?.goToAndStop(0, true)
+            animRef.current?.goToAndStop(0, true)
         }
     }
-    useEffect(() => {
-        if (animationContainerRef.current) {
-            setAnimation(
-                lottie.loadAnimation({
-                    name: 'checkbox',
-                    container: animationContainerRef.current,
-                    renderer: 'svg',
-                    loop: false,
-                    autoplay: false,
-                    animationData: checkbox,
-                })
-            )
-        }
-        return () => {
-            animation?.destroy()
-        }
-    }, [])
 
     useEffect(() => {
-        if (!animation) return
-        animation.goToAndStop(isChecked ? animation.totalFrames : 0, true)
-        animation.setSpeed(3)
-    }, [animation])
+        if (!animRef.current) return
+        animRef.current.goToAndStop(isChecked ? 26 : 0, true)
+        animRef.current.setSpeed(1.5)
+    }, [animRef])
 
     size = size || 'small'
     return (
         <SlightlyStyledButton onClick={onClickHandler} disabled={disabled} size={size}>
-            <AnimationContainer ref={animationContainerRef} />
+            <AnimationContainer>
+                <Lottie
+                    name="checkbox"
+                    animationData={checkbox}
+                    loop={false}
+                    autoplay={false}
+                    lottieRef={animRef}
+                    initialSegment={[4, 29]}
+                />
+            </AnimationContainer>
         </SlightlyStyledButton>
     )
 }
