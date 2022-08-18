a3b528d8f40ba5bd349b9f93494a8c1b6ae9ec8e
wooglin <alfaindiagt@protonmail.com>
2022-08-18 13:28:10 -0500

    update

--- backend/api/test_utils.go
+++ backend/api/test_utils.go
@@ -189,7 +189,7 @@ func makeLoginCallbackRequest(
 		nil,
 	)
 	mockConfig.On("Client", mock.Anything, &mockToken).Return(&mockClient)
-	api, _ := GetAPI()
+	api := GetAPI()
 	api.ExternalConfig.GoogleLoginConfig = &mockConfig
 	api.SkipStateTokenCheck = skipStateTokenCheck
 	router := GetRouter(api)
