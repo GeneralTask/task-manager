011888a42bd686bc325b2b2e54d45394d7440c43
Jack Hamilton <31417618+johnhamilto@users.noreply.github.com>
2022-08-24 12:38:44 -0700

    Task completion fade out animation (#1570)
    * init
    
    * unused container
    
    * remove fadeoutcontainer (unused)
    
    * remove default_section_id (unused)
    
    * use const for fade-out length
    
    * nits

--- frontend/src/components/atoms/TaskTemplate.tsx
+++ frontend/src/components/atoms/TaskTemplate.tsx
@@ -1,21 +1,25 @@
 import React, { forwardRef } from 'react'
 import styled from 'styled-components'
 import { Dimensions } from '../../styles'
+import { TASK_MARK_AS_DONE_TIMEOUT } from '../../constants'
 
-const TemplateContainer = styled.div`
+const TemplateContainer = styled.div<{ isVisible?: boolean }>`
     width: 100%;
     position: relative;
     height: ${Dimensions.TASK_HEIGHT};
     padding: 1px 0;
     margin: 2px 0;
+    opacity: ${(props) => (props.isVisible ? 1 : 0)};
+    transition: opacity ${TASK_MARK_AS_DONE_TIMEOUT}s ease-out;
 `
 interface TaskTemplateProps extends React.HTMLAttributes<HTMLDivElement> {
     children: React.ReactNode
+    isVisible?: boolean
 }
 const TaskTemplate = forwardRef<HTMLDivElement, TaskTemplateProps>((props: TaskTemplateProps, ref) => {
-    const { children, ...attr } = props
+    const { children, isVisible, ...attr } = props
     return (
-        <TemplateContainer ref={ref} {...attr}>
+        <TemplateContainer ref={ref} isVisible={isVisible} {...attr}>
             {children}
         </TemplateContainer>
     )
