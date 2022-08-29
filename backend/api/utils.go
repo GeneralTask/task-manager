26e2e03cc8209c24e85cbf239a54e8d0410fa997
wooglin <alfaindiagt@protonmail.com>
2022-08-29 16:10:38 -0500

    update

--- backend/api/utils.go
+++ backend/api/utils.go
@@ -71,6 +71,37 @@ func (api *API) Ping(c *gin.Context) {
 	c.JSON(200, "success")
 }
 
+func TokenMiddleware(c *gin.Context) {
+	parentCtx := c.Request.Context()
+	handlerName := c.HandlerName()
+	if handlerName[len(handlerName)-9:] == "Handle404" {
+		// Do nothing if the route isn't recognized
+		return
+	}
+	token, err := getToken(c)
+	if err != nil {
+		// This means the auth token format was incorrect
+		return
+	}
+	db, dbCleanup, err := database.GetDBConnection()
+	if err != nil {
+		c.AbortWithStatusJSON(500, gin.H{"detail": "internal server error"})
+		return
+	}
+	defer dbCleanup()
+	internalAPITokenCollection := database.GetInternalTokenCollection(db)
+	var internalToken database.InternalAPIToken
+	dbCtx, cancel := context.WithTimeout(parentCtx, constants.DatabaseTimeout)
+	defer cancel()
+	err = internalAPITokenCollection.FindOne(dbCtx, bson.M{"token": token}).Decode(&internalToken)
+	if err != nil {
+		log.Error().Err(err).Msg("token auth failed")
+		c.AbortWithStatusJSON(401, gin.H{"detail": "unauthorized"})
+		return
+	}
+	c.Set("user", internalToken.UserID)
+}
+
 func LoggingMiddleware(c *gin.Context) {
 	if c.Request.URL.Path == "/log_events/" {
 		// no need to record API calls to the log event endpoint
