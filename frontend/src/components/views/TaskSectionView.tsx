acdcd0854a37849eece79e350db0953176bff2ce
Jack Hamilton <31417618+johnhamilto@users.noreply.github.com>
2022-09-06 17:20:18 -0700

    Make spinners always spin if anything is refetching (#1648)
    * spinnier spinners
    
    * change hook thing

--- frontend/src/components/views/TaskSectionView.tsx
+++ frontend/src/components/views/TaskSectionView.tsx
@@ -44,9 +44,9 @@ const TaskSectionView = () => {
     const sectionScrollingRef = useRef<HTMLDivElement | null>(null)
     const sectionViewRef = useRef<HTMLDivElement>(null)
 
-    const { data: taskSections, isLoading: isLoadingTasks, isFetching: isFetchingTasks } = useGetTasks()
+    const { data: taskSections, isLoading: isLoadingTasks } = useGetTasks()
     const { mutate: reorderTask } = useReorderTask()
-    const { isFetching: isFetchingExternal } = useFetchExternalTasks()
+    useFetchExternalTasks()
 
     const navigate = useNavigate()
     const params = useParams()
@@ -102,7 +102,6 @@ const TaskSectionView = () => {
                                 <SectionHeader
                                     sectionName={section.name}
                                     allowRefresh={true}
-                                    isRefreshing={isFetchingExternal || isFetchingTasks}
                                     taskSectionId={section.id}
                                 />
                                 {!section.is_done && <CreateNewTask sectionId={section.id} />}
