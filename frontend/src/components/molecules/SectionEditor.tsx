743ef792b525dc30681490741f250a3206efae06
Jack Hamilton <jack@generaltask.com>
2022-09-09 15:51:17 -0700

    update dropdown menu

--- frontend/src/components/molecules/SectionEditor.tsx
+++ frontend/src/components/molecules/SectionEditor.tsx
@@ -1,5 +1,5 @@
 import { useGetTasks, useReorderTask } from '../../services/api/tasks.hooks'
-import { Border, Colors, Dimensions, Shadows, Spacing } from '../../styles'
+import { Border, Colors, Dimensions, Shadows, Spacing, Typography } from '../../styles'
 import styled from 'styled-components'
 import { Icon } from '../atoms/Icon'
 import { icons } from '../../styles/images'
@@ -11,43 +11,31 @@ const SectionEditorContainer = styled.div`
     width: ${Dimensions.TASK_ACTION_WIDTH};
     position: absolute;
     background-color: ${Colors.background.white};
-    border-radius: ${Border.radius.medium};
+    border-radius: ${Border.radius.mini};
     box-shadow: ${Shadows.medium};
+    padding: ${Spacing._8} ${Spacing._4};
+    gap: ${Spacing._4};
     z-index: 1;
     top: 100%;
     right: 0;
-    cursor: default;
-`
-const OptionsContainer = styled.div`
     overflow: auto;
     max-height: 500px;
+    cursor: default;
+    ${Typography.bodySmall};
 `
-const TopNav = styled.div`
-    padding: ${Spacing._12} ${Spacing._16};
-    border-bottom: 1px solid ${Colors.background.medium};
-`
-const Header = styled.div`
-    color: ${Colors.text.light};
-`
-const ListItem = styled.div`
+const ListItem = styled.div<{ isSelected: boolean }>`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: ${Spacing._12} ${Spacing._16};
-    border-bottom: 1px solid ${Colors.background.medium};
+    padding: ${Spacing._4} ${Spacing._12};
+    gap: ${Spacing._8};
+    border-radius: ${Border.radius.mini};
+    color: ${(props) => (props.isSelected ? Colors.text.black : Colors.text.light)};
+    background-color: ${(props) => (props.isSelected ? Colors.background.medium : 'inherit')};
     &:hover {
-        background-color: ${Colors.background.medium};
+        background-color: ${Colors.background.dark};
     }
     cursor: pointer;
-`
-const SectionTitleBox = styled.div<{ isSelected: boolean }>`
-    display: flex;
-    flex: 1;
-    flex-direction: row;
-    align-items: center;
-    gap: ${Spacing._8};
-    color: ${(props) => (props.isSelected ? Colors.gtColor.primary : Colors.text.light)};
-    min-width: 0;
 `
 const SectionName = styled.span`
     flex: 1;
@@ -84,26 +72,16 @@ export default function SectionEditor({ task_id, closeSectionEditor }: SectionEd
         }
 
         return (
-            <ListItem key={section.id} onClick={handleOnClick}>
-                <SectionTitleBox isSelected={isCurrentSection}>
-                    <Icon
-                        size={'small'}
-                        icon={icons.folder}
-                        color={isCurrentSection ? Colors.icon.purple : Colors.icon.gray}
-                    />
-                    <SectionName>{section.name}</SectionName>
-                </SectionTitleBox>
-                {isCurrentSection && <Icon size={'xSmall'} icon={icons.checkbox_checked} color={Colors.icon.purple} />}
+            <ListItem key={section.id} onClick={handleOnClick} isSelected={isCurrentSection}>
+                <Icon
+                    size="xSmall"
+                    icon={icons.folder}
+                    color={isCurrentSection ? Colors.icon.black : Colors.icon.gray}
+                />
+                <SectionName>{section.name}</SectionName>
             </ListItem>
         )
     })
 
-    return (
-        <SectionEditorContainer onClick={(e) => e.stopPropagation()}>
-            <TopNav>
-                <Header>Set Section</Header>
-            </TopNav>
-            <OptionsContainer>{options}</OptionsContainer>
-        </SectionEditorContainer>
-    )
+    return <SectionEditorContainer onClick={(e) => e.stopPropagation()}>{options}</SectionEditorContainer>
 }
