79cc765b0d06cdb6c5b78f31dc17b5d4e761abe8
Nolan Jimenez <nolan1299@gmail.com>
2022-09-09 14:58:18 -0700

    Make event select border purple instead of gray (#1686)

--- frontend/src/components/calendar/CalendarEvents-styles.tsx
+++ frontend/src/components/calendar/CalendarEvents-styles.tsx
@@ -140,7 +140,8 @@ export const EventFill = styled.div<{ squareStart: boolean; squareEnd: boolean; 
     width: 100%;
     height: 100%;
     background: ${Colors.background.white};
-    border: ${Border.stroke.medium} solid ${(props) => (props.isSelected ? Colors.border.gray : EVENT_CONTAINER_COLOR)};
+    border: ${Border.stroke.medium} solid
+        ${(props) => (props.isSelected ? Colors.border.purple : EVENT_CONTAINER_COLOR)};
     box-sizing: border-box;
     box-shadow: ${Shadows.light};
     border-top-left-radius: ${(props) => (props.squareStart ? '0' : Border.radius.mini)};
