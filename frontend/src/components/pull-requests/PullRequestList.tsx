e1cf27543949c6195e143a849df7c897e865ea94
Scott Mai <scottmai702@gmail.com>
2022-09-12 05:24:26 -0700

    useSortAndFilter hook

--- frontend/src/components/pull-requests/PullRequestList.tsx
+++ frontend/src/components/pull-requests/PullRequestList.tsx
@@ -1,15 +1,23 @@
+import { useSortAndFilter } from '../../hooks'
+import { SORT_ORDER } from '../../utils/enums'
 import { TPullRequest } from '../../utils/types'
 import PullRequest from './PullRequest'
 import { useParams } from 'react-router-dom'
 
 interface PullRequestListProps {
     pullRequests: TPullRequest[]
+    sort?: {
+        field: keyof TPullRequest
+        direction: SORT_ORDER
+    }
+    filter?: (item: TPullRequest) => boolean // should return true if item should be included in the filtered list
 }
 const PullRequestList = ({ pullRequests }: PullRequestListProps) => {
     const { pullRequest: selectedPullRequestId } = useParams()
+    const sortedAndFilteredPullRequests = useSortAndFilter({ items: pullRequests, filter: () => false })
     return (
         <>
-            {pullRequests.map((pr) => (
+            {sortedAndFilteredPullRequests.map((pr) => (
                 <PullRequest
                     key={pr.id}
                     pullRequest={pr}
---
+++ frontend/src/hooks/useSortAndFilter.ts
@@ -0,0 +1,32 @@
+import { useMemo } from "react"
+import { SORT_ORDER } from "../utils/enums"
+
+interface SortAndFilterArgs<T> {
+    items: T[]
+    sort?: {
+        field: keyof T // the attribute in T to sort by (i.e. 'id')
+        direction: SORT_ORDER
+    }
+    filter?: (item: T) => boolean // should return true if item should be included in the filtered list
+}
+const useSortAndFilter = <T>({ items, sort, filter }: SortAndFilterArgs<T>) => {
+    return useMemo(() => {
+        let sortedAndFiltered = items
+        if (filter) {
+            sortedAndFiltered = sortedAndFiltered.filter(filter)
+        }
+        if (sort) {
+            sortedAndFiltered = sortedAndFiltered.sort((a, b) => {
+                if (sort.direction === SORT_ORDER.ASC) {
+                    return a[sort.field] > b[sort.field] ? 1 : -1
+                } else {
+                    return a[sort.field] < b[sort.field] ? 1 : -1
+                }
+            })
+        }
+        return sortedAndFiltered
+    }, [items, filter, sort])
+
+}
+
+export default useSortAndFilter
