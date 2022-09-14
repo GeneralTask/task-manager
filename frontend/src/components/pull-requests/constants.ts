fda0846fabeb1688a6414bcd279659be1626adda
Scott Mai <scottmai702@gmail.com>
2022-09-14 02:01:38 -0700

    sort by required action

--- frontend/src/components/pull-requests/constants.ts
+++ frontend/src/components/pull-requests/constants.ts
@@ -1,13 +1,31 @@
 import { TPullRequest } from '../../utils/types'
 import { SortSelectorItems } from '../molecules/SortSelector'
 
+const PULL_REQUEST_REQUIRED_ACTIONS = [
+    'Add Reviewers',
+    'Fix Merge Conflicts',
+    'Fix Failed CI',
+    'Address Comments',
+    'Waiting on CI',
+    'Merge PR',
+    'Waiting on Author',
+    'Waiting on Review',
+    'Review PR',
+    'Not Actionable',
+]
+
+const requiredActionToIndexMap = new Map<string, number>(PULL_REQUEST_REQUIRED_ACTIONS.map((action, index) => [action, index]))
+
 export const PR_SORT_SELECTOR_ITEMS: SortSelectorItems<TPullRequest> = {
     requiredAction: {
         label: 'Required action',
         sort: {
             id: 'requiredAction',
             customComparator: (a: TPullRequest, b: TPullRequest) => {
-                return 1
+                const aPriority = requiredActionToIndexMap.get(a.status.text)
+                const bPriority = requiredActionToIndexMap.get(b.status.text)
+                if (aPriority === undefined || bPriority === undefined) return 0
+                return aPriority - bPriority
             },
         },
     },
