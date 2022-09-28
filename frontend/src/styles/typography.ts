6ca8459c631f7cef273aa2e0260b4590cff3c2ab
Jack Hamilton <john.hamilton@wustl.edu>
2022-09-28 13:23:12 -0700

    global now

--- frontend/src/styles/typography.ts
+++ frontend/src/styles/typography.ts
@@ -29,56 +29,44 @@ const weight = {
     semibold: '590',
     bold: '700',
 }
-const GTFontFamily = css`
-    font-family: -apple-system, BlinkMacSystemFont, sans-serif, 'Segoe UI', Helvetica, Roboto, Oxygen, Ubuntu,
-            Cantarell, Arial, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', 'Apple Color Emoji', 'Segoe UI Emoji',
-            'Segoe UI Symbol' !important;
-`
 
 export const header = css`
-    ${GTFontFamily};
     font-size: ${fontSize.xxl}; // 48px
     line-height: ${lineHeight.xl}; // 56px
     font-weight: ${weight.bold}; // 700
     letter-spacing: -0.02em; // -2%
 `
 export const title = css`
-    ${GTFontFamily};
     font-size: ${fontSize.xl}; // 32px
     line-height: ${lineHeight.l}; // 40px
     font-weight: ${weight.semibold}; // 590
     letter-spacing: -0.01em; // -1%
 `
 export const subtitle = css`
-    ${GTFontFamily};
     font-size: ${fontSize.l}; // 20px
     line-height: ${lineHeight.m}; // 24px
     font-weight: ${weight.medium}; // 510
     letter-spacing: -0.01em; // -1%
 `
 export const body = css`
-    ${GTFontFamily};
     font-size: ${fontSize.m}; // 16px
     line-height: ${lineHeight.m}; // 24px
     font-weight: ${weight.regular}; // 400
     letter-spacing: -0.01em; // -1%
 `
 export const bodySmall = css`
-    ${GTFontFamily};
     font-size: ${fontSize.s}; // 14px
     line-height: ${lineHeight.m}; // 24px
     font-weight: ${weight.regular}; // 400
     letter-spacing: -0.01em; // -1%
 `
 export const label = css`
-    ${GTFontFamily};
     font-size: ${fontSize.xs}; // 12px
     line-height: ${lineHeight.s}; // 16px
     font-weight: ${weight.regular}; // 400
     letter-spacing: -0.01em; // -1%
 `
 export const eyebrow = css`
-    ${GTFontFamily};
     font-size: ${fontSize.xs}; // 12px
     line-height: ${lineHeight.s}; // 16px
     font-weight: ${weight.regular}; // 400
@@ -86,7 +74,6 @@ export const eyebrow = css`
     text-transform: uppercase; // UPPERCASE
 `
 export const mini = css`
-    ${GTFontFamily};
     font-size: ${fontSize.xxs}; // 10px
     line-height: ${lineHeight.xs}; // 14px
     font-weight: ${weight.regular}; // 400
