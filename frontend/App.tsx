ccb5bee65814bb1f3bb8be266ba59cc1340890c7
Scott Mai <scottmai702@gmail.com>
2022-09-29 11:02:35 -0700

    Separate authenticated and non-authenticated routes

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,80 +1,31 @@
 import { Suspense, lazy } from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
-import { BrowserRouter, Navigate, Outlet, Route, Routes } from 'react-router-dom'
-import { enableMapSet } from 'immer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Loading from './src/components/atoms/Loading'
-import StyledToastContainer from './src/components/atoms/toast/StyledToastContainer'
-import FocusModeScreen from './src/components/screens/FocusModeScreen'
 import LandingScreen from './src/components/screens/LandingScreen'
-import { FOCUS_MODE_ROUTE, PRIVACY_POLICY_ROUTE, TERMS_OF_SERVICE_ROUTE } from './src/constants'
-import AppContextProvider from './src/context/AppContextProvider'
-import { isDevelopmentMode } from './src/environment'
-import PrivateOutlet from './src/services/PrivateOutlet'
+import { PRIVACY_POLICY_ROUTE, TERMS_OF_SERVICE_ROUTE } from './src/constants'
 import { GlobalStyle } from './src/styles'
+import { CompanyPolicyPages } from './src/utils/enums'
 
-const CompanyPolicyScreen = lazy(() => import('./src/components/screens/CompanyPolicyScreen'))
-const MainScreen = lazy(() => import('./src/components/screens/MainScreen'))
-const TermsOfServiceSummaryScreen = lazy(() => import('./src/components/screens/TermsOfServiceSummaryScreen'))
-
-enableMapSet() // this allows immer to produce immutable maps and sets
+const CompanyPolicyView = lazy(() => import('./src/components/views/CompanyPolicyView'))
 
 const App = () => {
-    const queryClient = new QueryClient()
-
     return (
-        <QueryClientProvider client={queryClient}>
-            {isDevelopmentMode && <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />}
-            <AppContextProvider>
-                <GlobalStyle />
-                <BrowserRouter>
-                    <Suspense fallback={<Loading />}>
-                        <Routes>
-                            <Route path="*" element={<Navigate to="/" />} />
-                            <Route path="/" element={<Outlet />}>
-                                <Route index element={<LandingScreen />} />
-                                <Route path={TERMS_OF_SERVICE_ROUTE} element={<CompanyPolicyScreen />} />
-                                <Route path={PRIVACY_POLICY_ROUTE} element={<CompanyPolicyScreen />} />
-                                <Route path="tos-summary" element={<PrivateOutlet />}>
-                                    <Route index element={<TermsOfServiceSummaryScreen />} />
-                                </Route>
-                                <Route path="overview" element={<PrivateOutlet />}>
-                                    <Route index element={<MainScreen />} />
-                                    <Route path=":overviewViewId" element={<MainScreen />}>
-                                        <Route path=":overviewItemId" element={<MainScreen />} />
-                                    </Route>
-                                </Route>
-                                <Route path="tasks" element={<PrivateOutlet />}>
-                                    <Route index element={<MainScreen />} />
-                                    <Route path=":section" element={<MainScreen />}>
-                                        <Route path=":task" element={<MainScreen />} />
-                                    </Route>
-                                </Route>
-                                <Route path="pull-requests" element={<PrivateOutlet />}>
-                                    <Route index element={<MainScreen />} />
-                                    <Route path=":pullRequest" element={<MainScreen />} />
-                                </Route>
-                                <Route path="linear" element={<PrivateOutlet />}>
-                                    <Route index element={<MainScreen />} />
-                                    <Route path=":linearIssueId" element={<MainScreen />} />
-                                </Route>
-                                <Route path="slack" element={<PrivateOutlet />}>
-                                    <Route index element={<MainScreen />} />
-                                    <Route path=":slackTaskId" element={<MainScreen />} />
-                                </Route>
-                                <Route path={FOCUS_MODE_ROUTE} element={<PrivateOutlet />}>
-                                    <Route index element={<FocusModeScreen />} />
-                                </Route>
-                                <Route path="settings" element={<PrivateOutlet />}>
-                                    <Route index element={<MainScreen />} />
-                                </Route>
-                            </Route>
-                        </Routes>
-                    </Suspense>
-                </BrowserRouter>
-            </AppContextProvider>
-            <StyledToastContainer />
-        </QueryClientProvider>
+        <BrowserRouter>
+            <GlobalStyle />
+            <Suspense fallback={<Loading />}>
+                <Routes>
+                    <Route index element={<LandingScreen />} />
+                    <Route
+                        path={TERMS_OF_SERVICE_ROUTE}
+                        element={<CompanyPolicyView page={CompanyPolicyPages.TermsOfService} />}
+                    />
+                    <Route
+                        path={PRIVACY_POLICY_ROUTE}
+                        element={<CompanyPolicyView page={CompanyPolicyPages.PrivacyPolicy} />}
+                    />
+                </Routes>
+            </Suspense>
+        </BrowserRouter>
     )
 }
 
