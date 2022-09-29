ac19dc0c621ada2ffa9a27502c0ce249e7caad66
wooglin <alfaindiagt@protonmail.com>
2022-09-29 17:05:29 -0500

    update

--- backend/database/helpers_test.go
+++ backend/database/helpers_test.go
@@ -589,7 +589,7 @@ func TestTaskSectionName(t *testing.T) {
 	t.Run("DefaultTaskSection", func(t *testing.T) {
 		name, err := GetTaskSectionName(db, constants.IDTaskSectionDefault, userID)
 		assert.NoError(t, err)
-		assert.Equal(t, "Default", name)
+		assert.Equal(t, "Task Inbox", name)
 	})
 	t.Run("CustomTaskSection", func(t *testing.T) {
 		sectionName := "TestSection"
@@ -943,7 +943,7 @@ func TestGetDefaultSectionName(t *testing.T) {
 		)
 
 		sectionName := GetDefaultSectionName(db, otherUserID)
-		assert.Equal(t, "Default", sectionName)
+		assert.Equal(t, "Task Inbox", sectionName)
 
 	})
 }
