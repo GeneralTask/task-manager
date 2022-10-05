bea7cbfc835d268e6463d9b444c54741a70f0f0e
wooglin <alfaindiagt@protonmail.com>
2022-10-05 15:25:03 -0600

    Fix linter errors

--- frontend/src/components/radix/TaskContextMenuWrapper.tsx
+++ frontend/src/components/radix/TaskContextMenuWrapper.tsx
@@ -71,6 +71,7 @@ const TaskContextMenuWrapper = ({ task, sectionId, children }: TaskContextMenuPr
             ],
         },
         {
+            //
             label: sectionId !== TRASH_SECTION_ID ? 'Delete Task' : 'Restore Task',
             icon: icons.trash,
             iconColor: 'red',
