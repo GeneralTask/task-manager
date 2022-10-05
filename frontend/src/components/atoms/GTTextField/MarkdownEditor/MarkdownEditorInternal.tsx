4ba35bc393a9b62c732c8f05ce10388420ca32c2
Scott Mai <scottmai702@gmail.com>
2022-10-05 12:29:13 -0600

    fix spacing for lists in firefox

--- frontend/src/components/atoms/GTTextField/MarkdownEditor/MarkdownEditorInternal.tsx
+++ frontend/src/components/atoms/GTTextField/MarkdownEditor/MarkdownEditorInternal.tsx
@@ -25,6 +25,9 @@ const EditorContainer = styled.div<{ maxHeight?: number; isFullHeight?: boolean;
             margin-top: 0;
         }
     }
+    .remirror-list-item-marker-container {
+        display: none;
+    }
 `
 
 const MarkdownEditorInternal = (props: MarkdownEditorProps) => {
