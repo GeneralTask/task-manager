c6ea8ab414e92266f71af1a2f994246a567c1893
Nolan Jimenez <nolan1299@gmail.com>
2022-10-05 15:20:15 -0600

    Hide overfow text + show alt text on hover (#1911)

--- frontend/src/components/screens/FocusModeScreen.tsx
+++ frontend/src/components/screens/FocusModeScreen.tsx
@@ -266,7 +266,7 @@ const FocusModeScreen = () => {
                             )}
                             {chosenEvent && (
                                 <>
-                                    <GTHeader>{title}</GTHeader>
+                                    <GTHeader title={title}>{title}</GTHeader>
                                     <GTTitle>
                                         <TimeRange start={timeStart} end={timeEnd} />
                                     </GTTitle>
