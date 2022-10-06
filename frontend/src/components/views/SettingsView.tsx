3bc12ebd4041416de064275efac9658d0e9c6f78
Jack Hamilton <john.hamilton@wustl.edu>
2022-10-06 14:43:39 -0600

    add to slack

--- frontend/src/components/views/SettingsView.tsx
+++ frontend/src/components/views/SettingsView.tsx
@@ -133,6 +133,22 @@ const SettingsView = () => {
                             </Service>
                         ))}
                 </ServicesContainer>
+                <ServicesContainer>
+                    <Service>
+                        <ServiceDetails>
+                            Add General Task to your Slack workspace. This is only required once per workspace.
+                        </ServiceDetails>
+                        <a href="https://slack.com/oauth/v2/authorize?client_id=1734323190625.3674283101555&scope=commands,chat:write&user_scope=users:read">
+                            <img
+                                alt="Add to Slack"
+                                height="40"
+                                width="139"
+                                src="https://platform.slack-edge.com/img/add_to_slack.png"
+                                srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+                            />
+                        </a>
+                    </Service>
+                </ServicesContainer>
                 <Divider color={Colors.border.light} />
                 <SectionDescriptor>My services</SectionDescriptor>
                 {linkedAccounts?.map((account) => (
