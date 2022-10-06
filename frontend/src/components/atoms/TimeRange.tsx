aed8bc2f0316e519cc03a4e1eaf6533201806fa8
Nolan Jimenez <nolan1299@gmail.com>
2022-10-06 12:55:17 -0600

    Fix text formatting bugs around event title in multiple events view

--- frontend/src/components/atoms/TimeRange.tsx
+++ frontend/src/components/atoms/TimeRange.tsx
@@ -1,13 +1,19 @@
 import { DateTime } from 'luxon'
+import styled from 'styled-components'
+
+const TimeRangeContainer = styled.span<{ wrapText: boolean }>`
+    ${(props) => !props.wrapText && `white-space: nowrap;`}
+`
 
 interface TimeRangeProps {
     start: DateTime
     end: DateTime
+    wrapText?: boolean
 }
-const TimeRange = ({ start, end }: TimeRangeProps) => {
+const TimeRange = ({ start, end, wrapText = true }: TimeRangeProps) => {
     const formattedStart = start.toLocaleString(DateTime.TIME_SIMPLE)
     const formattedEnd = end.toLocaleString(DateTime.TIME_SIMPLE)
-    return <span>{`${formattedStart} – ${formattedEnd}`}</span>
+    return <TimeRangeContainer wrapText={wrapText}>{`${formattedStart} – ${formattedEnd}`}</TimeRangeContainer>
 }
 
 export default TimeRange
