00a48d9c8222a3eb8603fdbe72faf75c7bdfe9ba
Jack Hamilton <31417618+johnhamilto@users.noreply.github.com>
2022-10-06 21:11:24 -0600

    Tooltip description for required action (#2018)

--- frontend/src/components/pull-requests/PullRequest.tsx
+++ frontend/src/components/pull-requests/PullRequest.tsx
@@ -1,9 +1,12 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import ReactTooltip from 'react-tooltip'
 import { TPullRequest } from '../../utils/types'
 import CommentCount from '../atoms/CommentCount'
 import { PurpleEdge } from '../atoms/SelectableContainer'
+import TooltipWrapper from '../atoms/TooltipWrapper'
 import ExternalLinkButton from '../atoms/buttons/ExternalLinkButton'
+import { PULL_REQUEST_ACTIONS } from './constants'
 import { Column, LinkButtonContainer, PullRequestRow, Status, TitleContainer } from './styles'
 
 interface PullRequestProps {
@@ -21,12 +24,24 @@ const PullRequest = ({ pullRequest, link, isSelected }: PullRequestProps) => {
         navigate(link)
     }, [params, pullRequest])
 
+    const statusDescription = PULL_REQUEST_ACTIONS.find((action) => action.text === status.text)?.description
+
+    useEffect(() => {
+        ReactTooltip.rebuild()
+    }, [])
+
     return (
         <PullRequestRow onClick={onClickHandler} isSelected={isSelected}>
             {isSelected && <PurpleEdge />}
             <TitleContainer>{title}</TitleContainer>
             <Column>
-                <Status type={status.color}>{status.text}</Status>
+                {statusDescription ? (
+                    <TooltipWrapper dataTip={statusDescription} tooltipId="tooltip">
+                        <Status type={status.color}>{status.text}</Status>
+                    </TooltipWrapper>
+                ) : (
+                    <Status type={status.color}>{status.text}</Status>
+                )}
                 {num_comments > 0 && <CommentCount count={num_comments} />}
                 <LinkButtonContainer>
                     <ExternalLinkButton link={deeplink} />
