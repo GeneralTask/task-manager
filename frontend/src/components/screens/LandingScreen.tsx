be1c5efa190533b8dfc06570c1477e803fad54a4
Nolan Jimenez <nolan1299@gmail.com>
2022-10-07 12:56:20 -0600

    pls

--- frontend/src/components/screens/LandingScreen.tsx
+++ frontend/src/components/screens/LandingScreen.tsx
@@ -2,21 +2,18 @@ import { useEffect } from 'react'
 import { Navigate } from 'react-router-dom'
 import Cookies from 'js-cookie'
 import { AUTHORIZATION_COOKE } from '../../constants'
-import getEnvVars from '../../environment'
+import getEnvVars, { isDevelopmentMode } from '../../environment'
 import GoogleSignInButton from '../atoms/buttons/GoogleSignInButton'
 
 const LandingScreen = () => {
-    const isDevelopment = getEnvVars()
-
     if (Cookies.get(AUTHORIZATION_COOKE)) return <Navigate to="/overview" />
-
     useEffect(() => {
-        if (!isDevelopment) {
+        if (!isDevelopmentMode) {
             window.location.href = getEnvVars().REACT_APP_TRY_BASE_URL
         }
     }, [])
 
-    if (!isDevelopment) return <></>
+    if (!isDevelopmentMode) return <></>
     return (
         <div>
             <GoogleSignInButton />
