bcb3e5f6846fe3b8ec8016d9f1e5977bad5e3d23
Nolan Jimenez <nolan1299@gmail.com>
2022-10-07 00:35:51 -0600

    Prevnet selecting times in calendar (#2032)

--- frontend/src/components/calendar/CalendarEvents-styles.tsx
+++ frontend/src/components/calendar/CalendarEvents-styles.tsx
@@ -52,6 +52,7 @@ export const CalendarTableStyle = styled.table`
 `
 export const CalendarTimesTableStyle = styled.table`
     border-collapse: collapse;
+    user-select: none;
 `
 export const CalendarRow = styled.tr`
     display: block;
