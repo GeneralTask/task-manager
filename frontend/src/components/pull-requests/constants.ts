15c2aa54952afa721c9dac8456836354df2ee250
Jack Hamilton <john.hamilton@wustl.edu>
2022-10-06 20:59:55 -0600

    Tooltip description for required action

--- frontend/src/components/pull-requests/constants.ts
+++ frontend/src/components/pull-requests/constants.ts
@@ -7,18 +7,42 @@ import {
 import { TPullRequest } from '../../utils/types'
 import { emptyFunction } from '../../utils/utils'
 
-const ACTION_REVIEW_PR = 'Review PR'
-const ACTION_ADD_REVIEWERS = 'Add Reviewers'
-const ACTION_FIX_FAILED_CI = 'Fix Failed CI'
-const ACTION_ADDRESS_COMMENTS = 'Address Comments'
-const ACTION_FIX_MERGE_CONFLICTS = 'Fix Merge Conflicts'
-const ACTION_WAITING_ON_CI = 'Waiting on CI'
-const ACTION_MERGE_PR = 'Merge PR'
-const ACTION_WAITING_ON_REVIEW = 'Waiting on Review'
-const ACTION_WAITING_ON_AUTHOR = 'Waiting on Author'
-const ACTION_NOT_ACTIONABLE = 'Not Actionable'
+const ACTION_REVIEW_PR = { text: 'Review PR', description: 'You have been added as a requested reviewer for the PR' }
+const ACTION_ADD_REVIEWERS = {
+    text: 'Add Reviewers',
+    description: 'You have not yet requested any reviewers for the PR',
+}
+const ACTION_FIX_FAILED_CI = {
+    text: 'Fix Failed CI',
+    description: 'The CI build has failed and needs fixing before the PR should be merged',
+}
+const ACTION_ADDRESS_COMMENTS = {
+    text: 'Address Comments',
+    description: 'Your PR has requested changes which need to be addressed',
+}
+const ACTION_FIX_MERGE_CONFLICTS = {
+    text: 'Fix Merge Conflicts',
+    description: 'Your PR has merge conflicts that need fixing before it can be merged',
+}
+const ACTION_WAITING_ON_CI = { text: 'Waiting on CI', description: 'The CI is currently still running' }
+const ACTION_MERGE_PR = {
+    text: 'Merge PR',
+    description: 'The PR is approved and has a passing CI and is therefore ready to be merged',
+}
+const ACTION_WAITING_ON_REVIEW = {
+    text: 'Waiting on Review',
+    description: 'Your PR has not been reviewed by the requested reviewers',
+}
+const ACTION_WAITING_ON_AUTHOR = {
+    text: 'Waiting on Author',
+    description: 'You have already given your review for the PR and are now waiting on the author to update the PR',
+}
+const ACTION_NOT_ACTIONABLE = {
+    text: 'Not Actionable',
+    description: 'You are neither the owner nor a requested reviewer for the PR',
+}
 
-const PULL_REQUEST_REQUIRED_ACTIONS = [
+export const PULL_REQUEST_ACTIONS = [
     ACTION_REVIEW_PR,
     ACTION_ADD_REVIEWERS,
     ACTION_FIX_FAILED_CI,
@@ -31,11 +55,24 @@ const PULL_REQUEST_REQUIRED_ACTIONS = [
     ACTION_NOT_ACTIONABLE,
 ]
 
+const PULL_REQUEST_REQUIRED_ACTIONS = [
+    ACTION_REVIEW_PR.text,
+    ACTION_ADD_REVIEWERS.text,
+    ACTION_FIX_FAILED_CI.text,
+    ACTION_ADDRESS_COMMENTS.text,
+    ACTION_FIX_MERGE_CONFLICTS.text,
+    ACTION_WAITING_ON_CI.text,
+    ACTION_MERGE_PR.text,
+    ACTION_WAITING_ON_REVIEW.text,
+    ACTION_WAITING_ON_AUTHOR.text,
+    ACTION_NOT_ACTIONABLE.text,
+]
+
 const NON_ACTIONABLE_REQUIRED_ACTIONS = new Set([
-    ACTION_WAITING_ON_REVIEW,
-    ACTION_WAITING_ON_AUTHOR,
-    ACTION_NOT_ACTIONABLE,
-    ACTION_WAITING_ON_CI,
+    ACTION_WAITING_ON_REVIEW.text,
+    ACTION_WAITING_ON_AUTHOR.text,
+    ACTION_NOT_ACTIONABLE.text,
+    ACTION_WAITING_ON_CI.text,
 ])
 
 const requiredActionToIndexMap = new Map<string, number>(
