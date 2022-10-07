10f6beee0c518a9b7f8be4218b1bdda4fb479725
Nolan Jimenez <nolan1299@gmail.com>
2022-10-07 10:10:59 -0600

    bad

---
+++ frontend/src/landing/index.html.js
@@ -0,0 +1,122 @@
+const LandingPageHTML = `<!DOCTYPE html><!--  This site was created in Webflow. https://www.webflow.com  -->
+<!--  Last Published: Fri Oct 07 2022 14:29:34 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="633f43ba933322f813de4df0" data-wf-site="633f43b99333222c11de4dd1">
+<head>
+  <meta charset="utf-8">
+  <title>Dalma - Webflow HTML website template</title>
+  <meta content="Dalma - Webflow HTML website template" property="og:title">
+  <meta content="Dalma - Webflow HTML website template" property="twitter:title">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="css/general-task-landing-page.webflow.css" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+  <script type="text/javascript">WebFont.load({  google: {    families: ["DM Sans:regular,italic,500,500italic,700,700italic"]  }});</script>
+  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="images/favicon.svg" rel="shortcut icon" type="image/x-icon">
+  <link href="images/webclip.svg" rel="apple-touch-icon">
+  <style>
+* {
+  -webkit-font-smoothing: antialiased;
+}
+</style>
+</head>
+<body class="body">
+  <div data-animation="default" data-collapse="medium" data-duration="100" data-easing="ease" data-easing2="ease" role="banner" class="navbar w-nav">
+    <div class="container w-container">
+      <div class="navbar-conents"><img src="images/beta-logo.png" loading="lazy" id="w-node-b510f933-81b2-1e83-6fe8-33346518540d-96d45f0a" srcset="images/beta-logo-p-500.png 500w, images/beta-logo.png 734w" sizes="(max-width: 479px) 57vw, (max-width: 767px) 36vw, (max-width: 991px) 200px, 23vw" alt="" class="image-5">
+        <a id="w-node-_5ce72d31-a5f6-4353-b128-451396d45f5b-96d45f0a" href="#" class="blue-cta w-button">Sign up for free</a>
+        <nav role="navigation" id="w-node-_5ce72d31-a5f6-4353-b128-451396d45f10-96d45f0a" class="nav-menu w-nav-menu">
+          <a href="#" class="green-button nav _1 w-button">Purchase template</a>
+        </nav>
+        <div id="w-node-_5ce72d31-a5f6-4353-b128-451396d45f58-96d45f0a" class="regular-navbar-right">
+          <div class="menu-button w-nav-button"><img src="images/hamburger.svg" loading="lazy" alt=""></div>
+        </div>
+        <a id="w-node-fe386b41-673d-470e-1596-f3f8d663cbdc-96d45f0a" href="#" class="white-cta w-button">Sign in</a>
+      </div>
+    </div>
+  </div>
+  <div class="section no-padd-top wf-section">
+    <div class="container w-container">
+      <div data-w-id="62be0878-3386-4509-36b0-1de68013a14c" style="opacity:0;-webkit-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)" class="left-align-mobile section">
+        <h1 data-w-id="62be0878-3386-4509-36b0-1de68013a14d" style="opacity:0" class="space-top-tiny"><span class="text-span-3">Build</span> like never before.</h1>
+        <div class="large-text">Effective time management is a superpower. Unlock it with our free software.</div>
+        <div class="button-stack space-top-small">
+          <a href="#" class="blue-cta w-inline-block">
+            <div>Sign up for free</div>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="container w-container">
+      <div data-poster-url="https://uploads-ssl.webflow.com/633f43b99333222c11de4dd1/633fbcce563b533889bc3de3_T2C (Fullscreen) copy_2xqual-poster-00001.jpg" data-video-urls="https://uploads-ssl.webflow.com/633f43b99333222c11de4dd1/633fbcce563b533889bc3de3_T2C (Fullscreen) copy_2xqual-transcode.mp4,https://uploads-ssl.webflow.com/633f43b99333222c11de4dd1/633fbcce563b533889bc3de3_T2C (Fullscreen) copy_2xqual-transcode.webm" data-autoplay="true" data-loop="true" data-wf-ignore="true" class="background-video w-background-video w-background-video-atom"><video id="ef80a760-6679-3172-315d-d574836a9098-video" autoplay="" loop="" style="background-image:url(&quot;https://uploads-ssl.webflow.com/633f43b99333222c11de4dd1/633fbcce563b533889bc3de3_T2C (Fullscreen) copy_2xqual-poster-00001.jpg&quot;)" muted="" playsinline="" data-wf-ignore="true" data-object-fit="cover">
+          <source src="https://uploads-ssl.webflow.com/633f43b99333222c11de4dd1/633fbcce563b533889bc3de3_T2C (Fullscreen) copy_2xqual-transcode.mp4" data-wf-ignore="true">
+          <source src="https://uploads-ssl.webflow.com/633f43b99333222c11de4dd1/633fbcce563b533889bc3de3_T2C (Fullscreen) copy_2xqual-transcode.webm" data-wf-ignore="true">
+        </video></div>
+    </div><img src="images/Hero-right-gradient.png" loading="lazy" srcset="images/Hero-right-gradient-p-500.png 500w, images/Hero-right-gradient.png 572w" sizes="(max-width: 479px) 100vw, (max-width: 767px) 67vw, (max-width: 991px) 336.78125px, 300.90625px" alt="" class="top-right">
+  </div>
+  <div class="section wf-section">
+    <div class="container w-container">
+      <div class="split">
+        <div id="w-node-cc0b847b-270c-cc90-878f-a54da35013ec-13de4df0" data-w-id="cc0b847b-270c-cc90-878f-a54da35013ec" style="opacity:0;-webkit-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)" class="limit-mobile">
+          <h2 class="heading">Set aside time for what you need to do.</h2>
+          <div class="text-block">Drag any task or even an entire folder to your calendar, and an event will be created on your Google Calendar.</div>
+        </div><img src="images/task-to-cal-2.png" loading="lazy" width="816" srcset="images/task-to-cal-2-p-500.png 500w, images/task-to-cal-2-p-800.png 800w, images/task-to-cal-2-p-1080.png 1080w, images/task-to-cal-2.png 1092w" sizes="(max-width: 479px) 100vw, (max-width: 991px) 46vw, 45vw" alt="" class="image-2">
+      </div>
+    </div>
+    <div class="container w-container">
+      <div class="split"><img class="image-3" src="images/Group-569.png" width="419" alt="" style="opacity:0" sizes="(max-width: 479px) 300px, (max-width: 767px) 46vw, 45vw" data-w-id="34d1690a-7cdb-52c0-d306-e9b37545b58f" id="w-node-_34d1690a-7cdb-52c0-d306-e9b37545b58f-13de4df0" loading="lazy" srcset="images/Group-569-p-500.png 500w, images/Group-569-p-800.png 800w, images/Group-569-p-1080.png 1080w, images/Group-569.png 1571w">
+        <div id="w-node-_438134e0-0555-a2b2-e930-258322024d17-13de4df0" class="limit-mobile">
+          <h2 class="heading">All your most important tasks at a glance.</h2>
+          <div class="text-block">Our integrations stay in sync with all of your actionable items— PRs from GitHub, tasks from Linear, meetings from Google Calendar, and even Slack messages. Easily see a bird’s-eye view and plan your day on your Overview page.</div>
+        </div>
+      </div>
+    </div>
+    <div class="container w-container">
+      <div id="w-node-_0e0a7754-29b1-f7a1-8749-7d216f95f7b9-13de4df0" data-w-id="0e0a7754-29b1-f7a1-8749-7d216f95f7b9" style="opacity:0;-webkit-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)" class="limit-mobile"><img src="images/Focus-Mode-Fullscreen.png" loading="lazy" width="419" srcset="images/Focus-Mode-Fullscreen-p-500.png 500w, images/Focus-Mode-Fullscreen-p-800.png 800w, images/Focus-Mode-Fullscreen-p-1080.png 1080w, images/Focus-Mode-Fullscreen-p-1600.png 1600w, images/Focus-Mode-Fullscreen-p-2000.png 2000w, images/Focus-Mode-Fullscreen-p-2600.png 2600w, images/Focus-Mode-Fullscreen.png 3049w" sizes="(max-width: 479px) 300px, (max-width: 767px) 93vw, 700px" alt="" class="image-4">
+        <h2 class="heading">Forget multitasking — <br>this is <em>singletasking</em>.</h2>
+        <div class="text-block">A single distraction can take up to<strong> 27 minutes</strong> to fully recover from. With Focus Mode, you can devote all your attention on one task or topic at a time without distractions. <br><br>See info about your current meeting or any tasks you’ve dragged to the calendar. Stay focused on one thing at a time.</div>
+      </div>
+    </div>
+  </div>
+  <div class="section wf-section"><img src="images/Hero-right-gradient.png" loading="lazy" srcset="images/Hero-right-gradient-p-500.png 500w, images/Hero-right-gradient.png 572w" sizes="(max-width: 479px) 100vw, (max-width: 767px) 15.1015625px, (max-width: 991px) 2vw, 15.1015625px" alt="" class="top-right"></div>
+  <div class="section-2 wf-section">
+    <div id="w-node-aba7eb70-8bfc-c676-bd6d-48cab3f51495-13de4df0" data-w-id="aba7eb70-8bfc-c676-bd6d-48cab3f51495" style="opacity:0;-webkit-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)" class="center-content center-text">
+      <div class="three-column-grid space-top-medium">
+        <div id="w-node-_632d6801-5a25-62e3-6372-0c3d0e1eab19-13de4df0" class="center-content center-text">
+          <h4 class="space-top-tiny-2">An ergonomic tool for daily use.</h4>
+          <div class="opacity-text">Software should help you, not get in your way.<br><br>We strongly believe that the interfaces we use each day should be as efficient and comfortable as possible.<br><br>With our Quick Command+K tool, you can take many shortcuts with just a few keystrokes.</div>
+        </div>
+        <div id="w-node-b4934ed4-d365-9423-d1c5-b915a2f6b8e2-13de4df0" class="center-content center-text">
+          <h4 class="space-top-tiny-2">Built by builders.</h4>
+          <div class="opacity-text">We’re a scrappy startup, not a megacorporation.<br><br>We&#x27;re driven not by quarterly earnings but by our mission: <strong>to make knowledge workers more productive</strong>, starting with software engineers at small companies like ours. We use our own software every day, and we hope you&#x27;ll love it as much as we do.<br><br>We are always looking for ways to improve our app. You can reach us at any time at support@generaltask.com.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="section wf-section">
+    <div class="container w-container">
+      <div class="center-content center-text">
+        <div data-w-id="09e76cdb-6c1f-454d-e00e-92728d0127ff" style="opacity:0;-webkit-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 0, 0) scale3d(0.95, 0.95, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)" class="limit-large-copy center-content">
+          <h2 class="space-top-tiny large-section-heading">It&#x27;s free — get started below.</h2>
+        </div>
+        <div class="large-text-3">Did we mention it&#x27;s free? Sign in with your Google account and be ready to go within a minute.</div>
+        <a href="#" class="blue-cta w-inline-block">
+          <div>Sign up for free</div>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="section wf-section">
+    <div class="container w-container"></div><img src="images/Bottom-left-gradient.png" loading="lazy" width="785.5" srcset="images/Bottom-left-gradient-p-500.png 500w, images/Bottom-left-gradient-p-800.png 800w, images/Bottom-left-gradient-p-1080.png 1080w, images/Bottom-left-gradient.png 1571w" sizes="(max-width: 991px) 100vw, 785.5px" alt="" class="bottom-left">
+    <div class="text-block-4"><span class="text-span-5">ⓒ 2022 GENERAL TASK</span></div>
+  </div>
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=633f43b99333222c11de4dd1" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+</body>
+</html>`
+
+export default LandingPageHTML
