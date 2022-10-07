eebe5689c6468ecacc06ee344cd211f066fc31f3
Jack Hamilton <john.hamilton@wustl.edu>
2022-10-07 12:51:25 -0600

    Authentication fix

--- frontend/src/components/screens/LandingScreen.tsx
+++ frontend/src/components/screens/LandingScreen.tsx
@@ -1,9 +1,15 @@
 import { useEffect } from 'react'
+import { Navigate } from 'react-router-dom'
+import Cookies from 'js-cookie'
+import { AUTHORIZATION_COOKE } from '../../constants'
 import getEnvVars from '../../environment'
 import GoogleSignInButton from '../atoms/buttons/GoogleSignInButton'
 
 const LandingScreen = () => {
     const isDevelopment = getEnvVars()
+
+    if (Cookies.get(AUTHORIZATION_COOKE)) return <Navigate to="/overview" />
+
     useEffect(() => {
         if (!isDevelopment) {
             window.location.href = getEnvVars().REACT_APP_TRY_BASE_URL
