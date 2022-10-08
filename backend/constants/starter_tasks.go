e313818c100eed48be8453767f91321f12d6cd35
Scott Mai <scottmai702@gmail.com>
2022-10-08 11:48:39 -0600

    update starter tasks copy

--- backend/constants/starter_tasks.go
+++ backend/constants/starter_tasks.go
@@ -1,8 +1,8 @@
 package constants
 
 var StarterTasks = []string{
-	"Putting tasks on your calendar with click-and-drag",
-	"Focus mode",
-	"Integrations",
+	"Put tasks on your calendar with click-and-drag",
+	"Shut out distractions with Focus Mode",
+	"Connect other services to see things in one place",
 	"A sincere thank you from the team",
 }
