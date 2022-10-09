52a7d20cda67fd983bbf8dc730021600f5e0e821
Nolan Jimenez <nolan1299@gmail.com>
2022-10-08 19:20:04 -0700

    Fix text overflow bug in calendar. This is not a perfect solution since some of the text incorrectly truncates, but it is a good temporary solution.

--- frontend/src/components/calendar/CalendarEvents-styles.tsx
+++ frontend/src/components/calendar/CalendarEvents-styles.tsx
@@ -142,7 +142,7 @@ export const EventTime = styled.div`
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-    min-width: fit-content;
+    min-width: 0;
 `
 export const EventFill = styled.div<{ squareStart: boolean; squareEnd: boolean; isSelected: boolean }>`
     width: 100%;
