5a2de69ade941b3adcae3c969c3a5baf4ddf2c97
Julian <julian@sweetrain.com>
2022-10-12 14:13:14 -0400

    remove other PR stuff

--- backend/api/linear_webhook.go
+++ backend/api/linear_webhook.go
@@ -168,11 +168,6 @@ func (api *API) createOrModifyIssueFromPayload(userID primitive.ObjectID, accoun
 	task.AllStatuses = statuses
 	task.CompletedStatus = getCompletedLinearStatus(task.AllStatuses)
 
-	if task.Status.Type == external.LinearCompletedType || task.Status.Type == external.LinearCanceledType {
-		_true := true
-		task.IsCompleted = &_true
-	}
-
 	_, err = database.UpdateOrCreateTask(
 		api.DB,
 		userID,
@@ -369,7 +364,7 @@ func populateLinearTask(userID primitive.ObjectID, accountID string, webhookPayl
 		SourceID:           external.TASK_SOURCE_ID_LINEAR,
 		Title:              &issuePayload.Title,
 		Body:               &issuePayload.Description,
-		SourceAccountID:    accountID,
+		SourceAccountID:    issuePayload.AssigneeID,
 		CreatedAtExternal:  primitive.NewDateTimeFromTime(issueCreatedAt),
 		IsCompleted:        &_false,
 		IsDeleted:          &_false,
