f98898d0f436e12fd3839b11d17afee9242b22a2
Nolan Jimenez <nolan1299@gmail.com>
2022-10-12 17:51:11 -0700

    Remove unused imports

--- frontend/src/components/molecules/CreateNewSubtask.tsx
+++ frontend/src/components/molecules/CreateNewSubtask.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { v4 as uuidv4 } from 'uuid'
 import KEYBOARD_SHORTCUTS from '../../constants/shortcuts'
