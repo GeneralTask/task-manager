880c57d71b620e89aa4186416bb6c499a5a6b331
Scott Mai <scottmai702@gmail.com>
2022-10-14 15:02:14 -0500

    sorticon

--- frontend/src/utils/sortAndFilter/SortAndFilterSelectors.tsx
+++ frontend/src/utils/sortAndFilter/SortAndFilterSelectors.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from 'react'
 import styled from 'styled-components'
+import { TIconType } from '../../components/atoms/Icon'
 import GTButton from '../../components/atoms/buttons/GTButton'
 import GTDropdownMenu from '../../components/radix/GTDropdownMenu'
 import { GTMenuItem } from '../../components/radix/RadixUIConstants'
@@ -71,6 +72,10 @@ const SortAndFilterDropdowns = <T,>({
     if (!selectedSort.forceAndHideDirection) {
         sortSelectorItems.push(sortDirectionGroups)
     }
+    let sortIcon: TIconType | undefined
+    if (!selectedSort.forceAndHideDirection) {
+        sortIcon = selectedSortDirection === SORT_DIRECTION.ASC ? icons.arrow_up : icons.arrow_down
+    }
 
     return (
         <SortAndFilterContainer>
@@ -92,7 +97,7 @@ const SortAndFilterDropdowns = <T,>({
                 items={sortSelectorItems}
                 trigger={
                     <GTButton
-                        icon={selectedSortDirection === SORT_DIRECTION.ASC ? icons.arrow_up : icons.arrow_down}
+                        icon={sortIcon}
                         value={sortOptions[selectedSort.id].label}
                         styleType="secondary"
                         size="small"
