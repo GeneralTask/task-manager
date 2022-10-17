851b1885f7311162561e0ded6f715eb5781c3ade
Jack Hamilton <john.hamilton@wustl.edu>
2022-10-17 12:47:35 -0700

    min-height for feedback modal

--- frontend/src/components/molecules/FeedbackModal.tsx
+++ frontend/src/components/molecules/FeedbackModal.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import styled from 'styled-components'
 import { useToast } from '../../hooks'
 import { usePostFeedback } from '../../services/api/feedback.hooks'
-import { Spacing } from '../../styles'
 import Flex from '../atoms/Flex'
 import GTTextField from '../atoms/GTTextField'
 import GTButton from '../atoms/buttons/GTButton'
@@ -10,13 +9,10 @@ import { Subtitle } from '../atoms/subtitle/Subtitle'
 import { TitleMedium } from '../atoms/title/Title'
 import GTModal from '../mantine/GTModal'
 
+const FEEDBACK_MIN_HEIGHT = 100
+
 const FeedbackTextField = styled(GTTextField)`
-    flex: 1;
-    margin: ${Spacing._4} 0;
-`
-const FeedbackTextFieldContainer = styled.div`
-    display: flex;
-    height: 300px;
+    min-height: ${FEEDBACK_MIN_HEIGHT}px;
 `
 
 const FeedbackModal = () => {
@@ -54,17 +50,14 @@ const FeedbackModal = () => {
                     Feedback is a gift — thank you. Let us know what things you’d like to see us do more and what things
                     we can do better.
                 </Subtitle>
-                <FeedbackTextFieldContainer>
-                    <FeedbackTextField
-                        type="plaintext"
-                        value={feedback}
-                        onChange={(val) => setFeedback(val)}
-                        fontSize="small"
-                        placeholder="Let us know your thoughts"
-                        isFullHeight
-                        autoFocus
-                    />
-                </FeedbackTextFieldContainer>
+                <FeedbackTextField
+                    type="plaintext"
+                    value={feedback}
+                    onChange={(val) => setFeedback(val)}
+                    fontSize="small"
+                    placeholder="Let us know your thoughts"
+                    autoFocus
+                />
                 <Flex justifyContentSpaceBetween>
                     <GTButton onClick={() => setModalIsOpen(false)} value="Cancel" styleType="secondary" />
                     <GTButton onClick={submitFeedback} value="Send feedback" styleType="primary" />
