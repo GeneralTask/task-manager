b9d9adda91bf43a3ff98f5116c7bf9f174f69fdd
Scott Mai <scottmai702@gmail.com>
2022-10-18 12:47:25 -0700

    fix sorting

--- frontend/src/utils/sortAndFilter/sortAndFilterItems.ts
+++ frontend/src/utils/sortAndFilter/sortAndFilterItems.ts
@@ -4,6 +4,7 @@ const sortAndFilterItems = <T>({ items, sort, sortDirection, filter, tieBreakerF
     const sortedAndFiltered = filter ? items.filter(filter.lambda) : [...items]
     if (sort && sortDirection) {
         sortedAndFiltered.sort((a, b) => {
+            console.log('sup')
             const sortDirectionMultiplier =
                 (sort.forceAndHideDirection ?? sortDirection) === SORT_DIRECTION.ASC ? 1 : -1
             let result = 0
@@ -12,7 +13,7 @@ const sortAndFilterItems = <T>({ items, sort, sortDirection, filter, tieBreakerF
             } else if (sort.field && a[sort.field] === b[sort.field]) {
                 result = a[tieBreakerField] < b[tieBreakerField] ? -1 : 1
             } else if (sort.field) {
-                if (!a[sort.field] && !b[sort.field]) {
+                if (a[sort.field] && b[sort.field]) {
                     result = a[sort.field] > b[sort.field] ? 1 : -1
                 }
                 // ensure that empty fields are always sorted to the bottom regardless of order
