9833004e082a3c5ba70460b86dd2a12091620ff6
Scott Mai <scottmai702@gmail.com>
2022-10-24 10:26:33 -0700

    slakkin

--- frontend/src/components/views/SlackTasksView.tsx
+++ frontend/src/components/views/SlackTasksView.tsx
@@ -9,40 +9,17 @@ import { Colors, Spacing, Typography } from '../../styles'
 import { icons } from '../../styles/images'
 import { TTask } from '../../utils/types'
 import { isSlackLinked } from '../../utils/utils'
-import SelectableContainer, { PurpleEdge } from '../atoms/SelectableContainer'
-import TaskTemplate from '../atoms/TaskTemplate'
-import ExternalLinkButton from '../atoms/buttons/ExternalLinkButton'
-import MarkTaskDoneButton from '../atoms/buttons/MarkTaskDoneButton'
 import EmptyDetails from '../details/EmptyDetails'
 import TaskDetails from '../details/TaskDetails'
 import ConnectIntegration from '../molecules/ConnectIntegration'
 import { SectionHeader } from '../molecules/Header'
+import SlackTask from '../molecules/SlackTask'
 import ScrollableListTemplate from '../templates/ScrollableListTemplate'
 
-const SlackTemplateContainer = styled(TaskTemplate)`
-    height: fit-content;
-`
-
-const SlackSelectableContainer = styled(SelectableContainer)`
-    display: flex;
-    align-items: center;
-    gap: ${Spacing._8};
-    padding: ${Spacing._16} ${Spacing._24};
-    margin-bottom: ${Spacing._4};
-    ${Typography.bodySmall};
-`
-const LinearBodyHeader = styled.div`
+const BodyHeader = styled.div`
     ${Typography.body};
     color: ${Colors.text.light};
     margin-bottom: ${Spacing._16};
-`
-const LinearTitle = styled.span`
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-`
-const ExternalLinkContainer = styled.div`
-    margin-left: auto;
 `
 
 const SlackTasksView = () => {
@@ -51,8 +28,58 @@ const SlackTasksView = () => {
     const navigate = useNavigate()
 
     const slackTasks = useMemo(() => {
-        const tasks = taskSections?.flatMap((section) => section.tasks) ?? []
-        return tasks.filter((task) => task.source.name === 'Slack' && (!task.is_done || task.isOptimistic))
+        return [
+            {
+                id: '1',
+                id_ordering: 1,
+                title: 'slack 1',
+                body: 'hi',
+                deeplink: '',
+                sent_at: '',
+                priority_normalized: 0,
+                time_allocated: 0,
+                due_date: '',
+                source: {
+                    name: 'General Task',
+                    logo: '',
+                    logo_v2: 'slack',
+                    is_completable: false,
+                    is_replyable: false,
+                },
+                sender: '',
+                is_done: false,
+                is_deleted: false,
+                isOptimistic: true,
+                is_meeting_preparation_task: false,
+                nux_number_id: 0,
+            },
+            {
+                id: '2',
+                id_ordering: 2,
+                title: 'slack 2',
+                body: 'hi',
+                deeplink: '',
+                sent_at: '',
+                priority_normalized: 0,
+                time_allocated: 0,
+                due_date: '',
+                source: {
+                    name: 'General Task',
+                    logo: '',
+                    logo_v2: 'slack',
+                    is_completable: false,
+                    is_replyable: false,
+                },
+                sender: '',
+                is_done: false,
+                is_deleted: false,
+                isOptimistic: true,
+                is_meeting_preparation_task: false,
+                nux_number_id: 0,
+            },
+        ] as TTask[]
+        // const tasks = taskSections?.flatMap((section) => section.tasks) ?? []
+        // return tasks.filter((task) => task.source.name === 'Slack' && (!task.is_done || task.isOptimistic))
     }, [taskSections])
 
     const { data: linkedAccounts } = useGetLinkedAccounts()
@@ -86,29 +113,16 @@ const SlackTasksView = () => {
         <>
             <ScrollableListTemplate>
                 <SectionHeader sectionName="Slack Messages" />
-                {isSlackIntegrationLinked ? (
+                {true || isSlackIntegrationLinked ? (
                     <>
-                        <LinearBodyHeader>All messages you&apos;ve created tasks for</LinearBodyHeader>
+                        <BodyHeader>All messages you&apos;ve created tasks for</BodyHeader>
                         {slackTasks?.map((task) => (
-                            <SlackTemplateContainer key={task.id}>
-                                <SlackSelectableContainer
-                                    isSelected={task.id === slackTaskId}
-                                    key={task.id}
-                                    onClick={() => onClick(task.id)}
-                                >
-                                    {slackTaskId === task.id && <PurpleEdge />}
-                                    <MarkTaskDoneButton
-                                        isDone={task.is_done}
-                                        taskId={task.id}
-                                        isSelected={true}
-                                        isDisabled={task.isOptimistic}
-                                    />
-                                    <LinearTitle>{task.title}</LinearTitle>
-                                    <ExternalLinkContainer>
-                                        <ExternalLinkButton link={task.deeplink} />
-                                    </ExternalLinkContainer>
-                                </SlackSelectableContainer>
-                            </SlackTemplateContainer>
+                            <SlackTask
+                                key={task.id}
+                                task={task}
+                                isSelected={task.id === slackTaskId}
+                                onClick={onClick}
+                            />
                         ))}
                     </>
                 ) : (
