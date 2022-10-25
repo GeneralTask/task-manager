f838e16c223cc3d8111adae26f50a833a61252d0
Scott Mai <scottmai702@gmail.com>
2022-10-25 02:38:02 -0700

    mult

--- frontend/src/utils/sortAndFilter/sortAndFilterItems.ts
+++ frontend/src/utils/sortAndFilter/sortAndFilterItems.ts
@@ -7,7 +7,8 @@ const sortAndFilterItems = <T>({ items, sort, sortDirection, filter, tieBreakerF
             const sortDirectionMultiplier = (sort.forceDirection ?? sortDirection) === SORT_DIRECTION.ASC ? 1 : -1
             let result = 0
             if (a[sort.field] === b[sort.field]) {
-                result = a[tieBreakerField] > b[tieBreakerField] ? -1 : 1
+                result = a[tieBreakerField] < b[tieBreakerField] ? -1 : 1
+                result *= sortDirectionMultiplier
             } else {
                 if (a[sort.field] && b[sort.field]) {
                     if (sort.customComparator) {
