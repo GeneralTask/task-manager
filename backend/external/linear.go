2fe60f3a09b51b7a9a6c5b0ca082dfc8b0f5ab77
Julian <julian@sweetrain.com>
2022-10-27 12:57:59 -0700

    Fix Linear comment issue when left on GT

--- backend/external/linear.go
+++ backend/external/linear.go
@@ -411,11 +411,13 @@ const linearCommentCreateQueryStr = `
 	mutation CommentCreate (
 		$body: String
 		, $issueId: String!
+		, $id: String
 	) {
 		commentCreate(
 		input: {
 			body: $body
 			, issueId: $issueId
+			, id: $id
 		}
 		) {
 		success
@@ -425,7 +427,7 @@ const linearCommentCreateQueryStr = `
 type linearCommentCreateQuery struct {
 	CommentCreate struct {
 		Success graphql.Boolean
-	} `graphql:"commentCreate(input: {body: $body, issueId: $issueId})"`
+	} `graphql:"commentCreate(input: {body: $body, issueId: $issueId, id: $id})"`
 }
 
 func handleMutateLinearIssue(client *graphqlBasic.Client, issueID string, updateFields *database.Task, task *database.Task) (bool, error) {
@@ -486,6 +488,7 @@ func updateLinearIssue(client *graphqlBasic.Client, issueID string, updateFields
 func addLinearComment(client *graphqlBasic.Client, issueID string, comment database.Comment) error {
 	request := graphqlBasic.NewRequest(linearCommentCreateQueryStr)
 	request.Var("body", comment.Body)
+	request.Var("id", comment.ExternalID)
 	request.Var("issueId", issueID)
 
 	log.Debug().Msgf("sending request to Linear: %+v", request)
