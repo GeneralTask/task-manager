166b4393927d008d63b43ecfa12f97db7aefe06f
Jack Hamilton <31417618+johnhamilto@users.noreply.github.com>
2022-10-27 12:36:35 -0700

    Add PR status tooltip to details (#2157)
    * Add status tooltip to details
    
    * move status to a separate component/file

--- frontend/src/components/details/PullRequestDetails.tsx
+++ frontend/src/components/details/PullRequestDetails.tsx
@@ -1,9 +1,11 @@
+import { useEffect } from 'react'
+import ReactTooltip from 'react-tooltip'
 import { DateTime } from 'luxon'
 import styled from 'styled-components'
 import { useGetPullRequests } from '../../services/api/pull-request.hooks'
 import { Colors, Spacing, Typography } from '../../styles'
 import { icons, logos } from '../../styles/images'
-import { colorToIcon } from '../../utils/sortAndFilter/pull-requests.config'
+import { PULL_REQUEST_ACTIONS } from '../../utils/sortAndFilter/pull-requests.config'
 import { TPullRequest } from '../../utils/types'
 import { getHumanTimeSinceDateTime } from '../../utils/utils'
 import { Icon } from '../atoms/Icon'
@@ -11,7 +13,7 @@ import { Divider } from '../atoms/SectionDivider'
 import ExternalLinkButton from '../atoms/buttons/ExternalLinkButton'
 import { Eyebrow, Label } from '../atoms/typography/Typography'
 import BranchName from '../pull-requests/BranchName'
-import { Status } from '../pull-requests/styles'
+import Status from '../pull-requests/Status'
 import DetailsViewTemplate from '../templates/DetailsViewTemplate'
 import PullRequestComment from './pr/PullRequestComment'
 
@@ -85,6 +87,12 @@ const PullRequestDetails = ({ pullRequest }: PullRequestDetailsProps) => {
         deletions,
     } = pullRequest
     const formattedTimeSince = getHumanTimeSinceDateTime(DateTime.fromISO(last_updated_at))
+    const statusDescription = PULL_REQUEST_ACTIONS.find((action) => action.text === status.text)?.description
+
+    useEffect(() => {
+        ReactTooltip.rebuild()
+    }, [])
+
     return (
         <DetailsViewTemplate>
             <DetailsTopContainer>
@@ -96,10 +104,7 @@ const PullRequestDetails = ({ pullRequest }: PullRequestDetailsProps) => {
             </DetailsTopContainer>
             <TitleContainer>{title}</TitleContainer>
             <InfoContainer>
-                <Status type={status.color}>
-                    <Icon icon={colorToIcon[status.color]} color={status.color} />
-                    {status.text}
-                </Status>
+                <Status description={statusDescription} status={status.text} color={status.color} />
                 <Gap4>
                     <LinesModified color="green">{`+${additions}`}</LinesModified>
                     <LinesModified color="red">{`-${deletions}`}</LinesModified>
