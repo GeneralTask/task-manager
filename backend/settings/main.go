fbc34beadbe52e143d874f75627d1ad5802c6c80
wooglin <alfaindiagt@protonmail.com>
2022-11-08 20:14:27 -0600

    update

--- backend/settings/main.go
+++ backend/settings/main.go
@@ -55,8 +55,6 @@ const (
 	ChoiceKeyManual                   = "manual"
 	ChoiceKeyDueDate                  = "due_date"
 	ChoiceKeyPriority                 = "priority"
-	ChoiceKeyCreatedAt                = "created_at"
-	ChoiceKeyUpdatedAt                = "updated_at"
 	// Calendar choice
 	SettingFieldCalendarForNewTasks = "calendar_account_id_for_new_tasks"
 )
