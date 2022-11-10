ae173910456e8377f26ac666b81234f5fb6f3f32
Nolan Jimenez <nolan1299@gmail.com>
2022-11-10 15:53:53 -0500

    Remove place

--- frontend/src/components/atoms/TooltipWrapper.tsx
+++ frontend/src/components/atoms/TooltipWrapper.tsx
@@ -3,7 +3,6 @@ interface TooltipWrapperProps {
     dataTip: string
     tooltipId: string
     inline?: boolean
-    place?: string
 }
 const TooltipWrapper = ({ children, dataTip, tooltipId, inline }: TooltipWrapperProps) => {
     if (inline)
