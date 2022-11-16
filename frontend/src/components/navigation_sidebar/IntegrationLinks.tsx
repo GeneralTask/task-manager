ce538d3971992dc3a2c8ccc163205d15199fea46
Jack Hamilton <john.hamilton@wustl.edu>
2022-11-15 17:14:55 -0800

    Show count of filtered pull requests

--- frontend/src/components/navigation_sidebar/IntegrationLinks.tsx
+++ frontend/src/components/navigation_sidebar/IntegrationLinks.tsx
@@ -4,6 +4,9 @@ import { useGetPullRequests } from '../../services/api/pull-request.hooks'
 import { useGetLinkedAccounts } from '../../services/api/settings.hooks'
 import { useGetTasks } from '../../services/api/tasks.hooks'
 import { icons, logos } from '../../styles/images'
+import { PR_SORT_AND_FILTER_CONFIG } from '../../utils/sortAndFilter/pull-requests.config'
+import useSortAndFilterSettings from '../../utils/sortAndFilter/useSortAndFilterSettings'
+import { TPullRequest } from '../../utils/types'
 import { doesAccountNeedRelinking, isGithubLinked, isLinearLinked, isSlackLinked } from '../../utils/utils'
 import NavigationLink from './NavigationLink'
 
@@ -33,8 +36,12 @@ const IntegrationLinks = ({ isCollapsed }: IntegrationLinksProps) => {
     const isLinearIntegrationLinked = isLinearLinked(linkedAccounts || [])
     const isSlackIntegrationLinked = isSlackLinked(linkedAccounts || [])
 
+    const { selectedFilter: pullRequestFilter } = useSortAndFilterSettings<TPullRequest>(PR_SORT_AND_FILTER_CONFIG)
     const githubCount = isGithubIntegrationLinked
-        ? pullRequestRepositories?.reduce<number>((total, repo) => total + repo.pull_requests.length, 0)
+        ? pullRequestRepositories?.reduce<number>(
+              (total, repo) => total + repo.pull_requests.filter(pullRequestFilter.lambda).length,
+              0
+          )
         : undefined
     const linearCount = isLinearIntegrationLinked ? linearTasksCount : undefined
     const slackCount = isSlackIntegrationLinked ? slackTasksCount : undefined
