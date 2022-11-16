c8214319d1eb8c3a25cfabd39b765c1c2c61c9e7
Nolan Jimenez <nolan1299@gmail.com>
2022-11-16 17:25:05 -0500

    Update sidebar icon for collapsed navbar button

--- frontend/src/components/calendar/CalendarHeader.tsx
+++ frontend/src/components/calendar/CalendarHeader.tsx
@@ -137,7 +137,7 @@ export default function CalendarHeader({
                                     onClick={toggleCalendar}
                                     icon={calendarType === 'week' ? icons.arrows_in : icons.arrows_out}
                                 />
-                                <GTIconButton onClick={() => setIsCollapsed(true)} icon={icons.sidebar} />
+                                <GTIconButton onClick={() => setIsCollapsed(true)} icon={icons.sidebarFlipped} />
                             </HeaderIconsContainer>
                         </HeaderBodyContainer>
                     </PaddedContainer>
