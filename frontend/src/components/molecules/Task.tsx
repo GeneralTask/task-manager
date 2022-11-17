45a26ac45d168602d3c6bc90424a453b6a453556
Scott Mai <scottmai702@gmail.com>
2022-11-17 12:06:44 -0800

    fix meeting is now bug for done tasks

--- frontend/src/components/molecules/Task.tsx
+++ frontend/src/components/molecules/Task.tsx
@@ -89,16 +89,21 @@ const Task = ({
     const [isMeetingTextColored, setIsMeetingTextColor] = useState<boolean>(false)
     const { meeting_preparation_params } = task
     const dateTimeStart = DateTime.fromISO(task.meeting_preparation_params?.datetime_start || '')
+    const dateTimeEnd = DateTime.fromISO(meeting_preparation_params?.datetime_end || '')
 
     useInterval(() => {
         if (!meeting_preparation_params) return
-        const minutes = Math.ceil(dateTimeStart.diffNow('minutes').minutes)
-        if (minutes < 0) {
+        const minutesToStart = Math.ceil(dateTimeStart.diffNow('minutes').minutes)
+        const minutesToEnd = Math.ceil(dateTimeEnd.diffNow('minutes').minutes)
+
+        if (minutesToStart < 0 && minutesToEnd > 0) {
             setMeetingStartText('Meeting is now')
             setIsMeetingTextColor(true)
-        } else if (minutes <= 30) {
-            const minutesText = minutes === 1 ? 'minute' : 'minutes'
-            setMeetingStartText(`Starts in ${minutes} ${minutesText}`)
+        } else if (minutesToStart < 0 && minutesToEnd < 0) {
+            setMeetingStartText(null)
+        } else if (minutesToStart <= 30) {
+            const minutesToStartText = minutesToStart === 1 ? 'minute' : 'minutes'
+            setMeetingStartText(`Starts in ${minutesToStart} ${minutesToStartText}`)
             setIsMeetingTextColor(true)
         } else {
             setMeetingStartText(dateTimeStart.toLocaleString(DateTime.TIME_SIMPLE))
