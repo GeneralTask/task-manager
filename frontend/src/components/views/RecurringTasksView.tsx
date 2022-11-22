bc43b9dfe03fda06efbac88a3356ab6b78dc747b
Scott Mai <scottmai702@gmail.com>
2022-11-21 21:42:31 -0800

    revert tests

--- frontend/src/components/views/RecurringTasksView.tsx
+++ frontend/src/components/views/RecurringTasksView.tsx
@@ -1,10 +1,7 @@
-import { useRecurringTaskTemplates } from '../../services/api/recurring-tasks.hooks'
 import { SectionHeader } from '../molecules/Header'
 import ScrollableListTemplate from '../templates/ScrollableListTemplate'
 
 const RecurringTasksView = () => {
-    const { data: recurringTaskTemplates } = useRecurringTaskTemplates()
-    console.log(recurringTaskTemplates)
     return (
         <ScrollableListTemplate>
             <SectionHeader sectionName="Recurring tasks" />
