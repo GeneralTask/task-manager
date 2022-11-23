c77c8481d59c301c57938305dfc59f5934c0de92
Scott Mai <scottmai702@gmail.com>
2022-11-23 11:18:56 -0800

    Recurring tasks selection (#2322)
    * endpoints
    
    * some-more-changes
    
    * types
    
    * revert tests
    
    * error names
    
    * ??
    
    * recurring tasks initial page layout
    
    * truncate-test
    
    * click selection
    
    * kb

--- frontend/src/components/molecules/recurring-tasks/RecurringTask.tsx
+++ frontend/src/components/molecules/recurring-tasks/RecurringTask.tsx
@@ -2,7 +2,6 @@ import { TASK_PRIORITIES } from '../../../constants'
 import { Spacing } from '../../../styles'
 import { icons } from '../../../styles/images'
 import { TRecurringTaskTemplate } from '../../../utils/types'
-import { emptyFunction } from '../../../utils/utils'
 import Flex from '../../atoms/Flex'
 import { Icon } from '../../atoms/Icon'
 import TaskTemplate from '../../atoms/TaskTemplate'
@@ -11,11 +10,13 @@ import ItemContainer from '../ItemContainer'
 
 interface RecurringTaskProps {
     recurringTask: TRecurringTaskTemplate
+    isSelected: boolean
+    onSelect: (recurringTask: TRecurringTaskTemplate) => void
 }
-const RecurringTask = ({ recurringTask }: RecurringTaskProps) => {
+const RecurringTask = ({ recurringTask, isSelected, onSelect }: RecurringTaskProps) => {
     return (
         <TaskTemplate>
-            <ItemContainer isSelected={false} onClick={emptyFunction}>
+            <ItemContainer isSelected={isSelected} onClick={() => onSelect(recurringTask)}>
                 <Truncated>{recurringTask.title}</Truncated>
                 <Flex gap={Spacing._12}>
                     <Icon icon={icons.arrows_repeat} />
--- frontend/src/components/views/RecurringTasksView.tsx
+++ frontend/src/components/views/RecurringTasksView.tsx
@@ -1,5 +1,10 @@
+import { useCallback, useEffect, useMemo } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useItemSelectionController } from '../../hooks'
+import Log from '../../services/api/log'
 import { useRecurringTaskTemplates } from '../../services/api/recurring-tasks.hooks'
 import { icons } from '../../styles/images'
+import { TRecurringTaskTemplate } from '../../utils/types'
 import Spinner from '../atoms/Spinner'
 import EmptyDetails from '../details/EmptyDetails'
 import { SectionHeader } from '../molecules/Header'
@@ -8,6 +13,25 @@ import ScrollableListTemplate from '../templates/ScrollableListTemplate'
 
 const RecurringTasksView = () => {
     const { data: recurringTaskTemplates } = useRecurringTaskTemplates()
+    const { recurringTaskId } = useParams()
+    const navigate = useNavigate()
+
+    const selectedRecurringTask = useMemo(() => {
+        if (recurringTaskTemplates == null || recurringTaskTemplates.length === 0) return null
+        return recurringTaskTemplates.find((pr) => pr.id === recurringTaskId) ?? recurringTaskTemplates[0]
+    }, [recurringTaskId, recurringTaskTemplates])
+
+    useEffect(() => {
+        if (selectedRecurringTask == null) return
+        navigate(`/recurring-tasks/${selectedRecurringTask.id}`, { replace: true })
+    }, [selectedRecurringTask])
+
+    const selectRecurringTask = useCallback((recurringTask: TRecurringTaskTemplate) => {
+        navigate(`/recurring-tasks/${recurringTask.id}`)
+        Log(`recurring_task_select_${recurringTask.id}`)
+    }, [])
+
+    useItemSelectionController(recurringTaskTemplates ?? [], selectRecurringTask)
 
     return (
         <>
@@ -17,11 +41,19 @@ const RecurringTasksView = () => {
                     <Spinner />
                 ) : (
                     recurringTaskTemplates.map((recurringTask) => (
-                        <RecurringTask key={recurringTask.id} recurringTask={recurringTask} />
+                        <RecurringTask
+                            key={recurringTask.id}
+                            recurringTask={recurringTask}
+                            isSelected={recurringTask.id === recurringTaskId}
+                            onSelect={selectRecurringTask}
+                        />
                     ))
                 )}
             </ScrollableListTemplate>
-            <EmptyDetails icon={icons.arrows_repeat} text="Details view coming soon" />
+            <EmptyDetails
+                icon={icons.arrows_repeat}
+                text={`Details view coming soon for ${selectedRecurringTask?.title}`}
+            />
         </>
     )
 }
