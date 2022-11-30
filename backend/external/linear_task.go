cae92b1b19b25a6ffd2b1dfc496d1bdcabf1f07a
alfaindia <97246822+alfaindia@users.noreply.github.com>
2022-11-30 10:33:01 -0600

    Remove debug logging from linear_task (#2365)

--- backend/external/linear_task.go
+++ backend/external/linear_task.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"time"
 
-	"github.com/rs/zerolog/log"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
@@ -26,7 +25,6 @@ func (linearTask LinearTaskSource) GetEvents(db *mongo.Database, userID primitiv
 }
 
 func (linearTask LinearTaskSource) GetTasks(db *mongo.Database, userID primitive.ObjectID, accountID string, result chan<- TaskResult) {
-	log.Error().Msg("jerdjerd linear get")
 	client, err := GetLinearClient(linearTask.Linear.Config.ConfigValues.UserInfoURL, db, userID, accountID)
 	logger := logging.GetSentryLogger()
 	if err != nil {
@@ -72,7 +70,6 @@ func (linearTask LinearTaskSource) GetTasks(db *mongo.Database, userID primitive
 
 	var tasks []*database.Task
 	for _, linearIssue := range issuesQuery.Issues.Nodes {
-		log.Error().Msgf("jerd %+v", linearIssue)
 		createdAt, _ := time.Parse("2006-01-02T15:04:05.000Z", string(linearIssue.CreatedAt))
 		updatedAt, _ := time.Parse("2006-01-02T15:04:05.000Z", string(linearIssue.UpdatedAt))
 		stringTitle := string(linearIssue.Title)
