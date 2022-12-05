32c1f3c91f0d4e2ff2e9d9720b5cd78f17be276d
Jack Hamilton <john.hamilton@wustl.edu>
2022-12-05 15:01:15 -0800

    lint

--- frontend/src/components/molecules/CommandPalette.tsx
+++ frontend/src/components/molecules/CommandPalette.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Command } from 'cmdk'
 import styled from 'styled-components'
