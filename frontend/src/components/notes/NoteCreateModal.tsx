58a3e9d2e4e31af1829c740010d163e1e17f04bf
Jack Hamilton <john.hamilton@wustl.edu>
2022-12-07 14:54:54 -0800

    create modal update

--- frontend/src/components/notes/NoteCreateModal.tsx
+++ frontend/src/components/notes/NoteCreateModal.tsx
@@ -5,15 +5,14 @@ import KEYBOARD_SHORTCUTS from '../../constants/shortcuts'
 import { useGTLocalStorage, useKeyboardShortcut } from '../../hooks'
 import { useCreateNote } from '../../services/api/notes.hooks'
 import { useGetUserInfo } from '../../services/api/user-info.hooks'
+import { Spacing } from '../../styles'
 import { icons } from '../../styles/images'
 import Flex from '../atoms/Flex'
 import GTTextField from '../atoms/GTTextField'
 import GTButton from '../atoms/buttons/GTButton'
 import GTModal from '../mantine/GTModal'
 
-const TitleField = styled(GTTextField)`
-    width: 890px;
-`
+const TitleField = styled(GTTextField)``
 const ShareButton = styled(GTButton)`
     margin-left: auto;
 `
@@ -61,39 +60,42 @@ const NoteCreateModal = ({ isOpen, setIsOpen }: NoteCreateModalProps) => {
             setIsModalOpen={setIsOpen}
             size="lg"
             tabs={{
-                title: (
-                    <TitleField
-                        type="plaintext"
-                        value={title}
-                        onChange={(val) => {
-                            setTitle(val)
-                            setCoupledTitle(false)
-                        }}
-                        fontSize="medium"
-                    />
-                ),
+                title: 'Note',
                 body: (
-                    <Flex data-autofocus>
-                        <GTTextField
-                            type="markdown"
-                            value={note}
-                            onChange={(val) => setNote(val)}
-                            fontSize="small"
-                            placeholder="Type your note here."
-                            keyDownExceptions={[KEYBOARD_SHORTCUTS.submitText.key]}
-                            minHeight={300}
-                            actions={
-                                <ShareButton
-                                    onClick={finishNote}
-                                    value="Save note"
-                                    icon={icons.save}
-                                    styleType="secondary"
-                                    size="small"
-                                    fitContent
-                                />
-                            }
-                            autoFocus
-                        />
+                    <Flex column gap={Spacing._12}>
+                        <Flex>
+                            <TitleField
+                                type="plaintext"
+                                value={title}
+                                onChange={(val) => {
+                                    setTitle(val)
+                                    setCoupledTitle(false)
+                                }}
+                                fontSize="medium"
+                            />
+                        </Flex>
+                        <Flex data-autofocus>
+                            <GTTextField
+                                type="markdown"
+                                value={note}
+                                onChange={(val) => setNote(val)}
+                                fontSize="small"
+                                placeholder="Type your note here."
+                                keyDownExceptions={[KEYBOARD_SHORTCUTS.submitText.key, KEYBOARD_SHORTCUTS.close.key]}
+                                minHeight={300}
+                                actions={
+                                    <ShareButton
+                                        onClick={finishNote}
+                                        value="Save note"
+                                        icon={icons.save}
+                                        styleType="secondary"
+                                        size="small"
+                                        fitContent
+                                    />
+                                }
+                                autoFocus
+                            />
+                        </Flex>
                     </Flex>
                 ),
             }}
