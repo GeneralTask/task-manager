715f2d64dfa9b153143c75b3c459cbfb65c31563
Jack Hamilton <john.hamilton@wustl.edu>
2022-12-06 18:30:15 -0800

    note view changes

--- frontend/src/components/notes/NoteView.tsx
+++ frontend/src/components/notes/NoteView.tsx
@@ -1,14 +1,18 @@
 import { useNavigate, useParams } from 'react-router-dom'
+import { DateTime } from 'luxon'
 import styled from 'styled-components'
 import { useGetNote } from '../../services/api/notes.hooks'
-import { Colors, Spacing } from '../../styles'
-import { noteBackground } from '../../styles/images'
-import { emptyFunction } from '../../utils/utils'
+import { Border, Colors, Shadows, Spacing } from '../../styles'
+import { icons, noteBackground } from '../../styles/images'
+import { emptyFunction, getHumanTimeSinceDateTime } from '../../utils/utils'
+import Flex from '../atoms/Flex'
 import GTTextField from '../atoms/GTTextField'
+import { Icon } from '../atoms/Icon'
+import { Divider } from '../atoms/SectionDivider'
 import Spinner from '../atoms/Spinner'
 import GoogleSignInButton from '../atoms/buttons/GoogleSignInButton'
 import NoStyleButton from '../atoms/buttons/NoStyleButton'
-import { Header, Subtitle } from '../atoms/typography/Typography'
+import { Body, Label, Subtitle } from '../atoms/typography/Typography'
 import { GTBetaLogo } from '../views/NavigationView'
 
 const MainContainer = styled.div`
@@ -18,27 +22,29 @@ const MainContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-`
-const Body = styled.div`
-    width: 700px;
-    height: 100vh;
-    background: ${Colors.background.white};
-    display: flex;
-    flex-direction: column;
-    padding: ${Spacing._64};
-    gap: ${Spacing._32};
     overflow-y: auto;
 `
-const SignInButton = styled.div`
-    position: absolute;
-    top: ${Spacing._16};
-    right: ${Spacing._16};
-    width: 200px;
+const ColumnContainer = styled.div`
+    width: 700px;
 `
-const GTLogo = styled.div`
-    position: absolute;
-    top: ${Spacing._24};
-    left: ${Spacing._16};
+const TopContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: ${Spacing._32} 0;
+`
+const NoteBody = styled.div`
+    background: ${Colors.background.white};
+    border-radius: ${Border.radius.small};
+    box-shadow: ${Shadows.medium};
+    display: flex;
+    flex-direction: column;
+    padding: ${Spacing._24};
+    gap: ${Spacing._32};
+    margin-bottom: ${Spacing._32};
+`
+const SignInButton = styled.div`
+    width: 200px;
 `
 
 const NoteView = () => {
@@ -50,23 +56,45 @@ const NoteView = () => {
     if (!isLoading && !note) navigate('/')
     if (!note) return <Spinner />
 
-    const { title, author, body } = note
+    const { title, author, body, updated_at, shared_until } = note
 
     return (
         <MainContainer>
-            <GTLogo>
-                <NoStyleButton onClick={() => navigate('/')}>
-                    <GTBetaLogo src="/images/GT-beta-logo.png" />
-                </NoStyleButton>
-            </GTLogo>
-            <SignInButton>
-                <GoogleSignInButton />
-            </SignInButton>
-            <Body>
-                <Header>{title}</Header>
-                <Subtitle>{`From ${author}`}</Subtitle>
-                <GTTextField type="markdown" value={body} onChange={emptyFunction} fontSize="small" disabled readOnly />
-            </Body>
+            <ColumnContainer>
+                <TopContainer>
+                    <NoStyleButton onClick={() => navigate('/')}>
+                        <GTBetaLogo src="/images/GT-beta-logo.png" />
+                    </NoStyleButton>
+                    <SignInButton>
+                        <GoogleSignInButton />
+                    </SignInButton>
+                </TopContainer>
+                <NoteBody>
+                    <Subtitle>{title}</Subtitle>
+                    <GTTextField
+                        type="markdown"
+                        value={body}
+                        onChange={emptyFunction}
+                        fontSize="small"
+                        disabled
+                        readOnly
+                    />
+                    <Divider color={Colors.border.light} />
+                    <Flex justifyContent="space-between" alignItems="center">
+                        <Flex gap={Spacing._4}>
+                            <Label>{author}</Label>
+                            <Label color="light">shared this note with you</Label>
+                            <Label>{getHumanTimeSinceDateTime(DateTime.fromISO(updated_at))}</Label>
+                        </Flex>
+                        <Flex gap={Spacing._4}>
+                            <Icon color="gray" icon={icons.link} />
+                            <Label color="light">{`Link expires in ${DateTime.fromISO(shared_until)
+                                .diffNow(['days', 'hours'])
+                                .toHuman({ maximumFractionDigits: 0 })}`}</Label>
+                        </Flex>
+                    </Flex>
+                </NoteBody>
+            </ColumnContainer>
         </MainContainer>
     )
 }
