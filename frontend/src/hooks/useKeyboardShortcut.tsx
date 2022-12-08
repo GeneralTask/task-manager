3692ce00671cc9e4c5bf351f97cd30437b7512d3
Scott Mai <scottmai702@gmail.com>
2022-12-08 12:19:41 -0800

    Add logging for quick commands/keyboard shortcuts (#2438)
    * log keyboard shortcuts
    
    * commandpalette
    
    * open

--- frontend/src/hooks/useKeyboardShortcut.tsx
+++ frontend/src/hooks/useKeyboardShortcut.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react'
 import produce from 'immer'
 import KEYBOARD_SHORTCUTS, { TShortcutName } from '../constants/shortcuts'
 import useShortcutContext from '../context/ShortcutContext'
+import Log from '../services/api/log'
 
 // action should be a useCallback function to avoid unnecessary rerenders
 export default function useKeyboardShortcut(shortcutName: TShortcutName, action: () => void, disabled = false) {
@@ -13,7 +14,10 @@ export default function useKeyboardShortcut(shortcutName: TShortcutName, action:
                 produce(activeShortcuts, (draft) => {
                     draft.set(shortcut.key, {
                         ...shortcut,
-                        action,
+                        action: () => {
+                            action()
+                            Log(`keyboard_shortcut_${shortcut.label.replaceAll(' ', '_').toLowerCase()}`)
+                        },
                     })
                 })
             )
