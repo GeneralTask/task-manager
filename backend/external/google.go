ab10f5987765edcde00d82d5d184a1ee14ad28ce
wooglin <alfaindiagt@protonmail.com>
2022-12-08 11:49:04 -0600

    update

--- backend/external/google.go
+++ backend/external/google.go
@@ -139,7 +139,7 @@ func (Google GoogleService) HandleLinkCallback(db *mongo.Database, params Callba
 			DisplayID:      userInfo.EMAIL,
 			IsUnlinkable:   true,
 			IsPrimaryLogin: false,
-			Scopes:         strings.Join(getGoogleGrantedScopes(&client, token), " "),
+			Scopes:         getGoogleGrantedScopes(&client, token),
 		}},
 		options.Update().SetUpsert(true),
 	)
@@ -267,7 +267,7 @@ func (Google GoogleService) HandleSignupCallback(db *mongo.Database, params Call
 				DisplayID:      userInfo.EMAIL,
 				IsUnlinkable:   false,
 				IsPrimaryLogin: true,
-				Scopes:         strings.Join(getGoogleGrantedScopes(&client, token), " "),
+				Scopes:         getGoogleGrantedScopes(&client, token),
 			}},
 			options.Update().SetUpsert(true),
 		)
