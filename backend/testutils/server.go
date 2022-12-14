1b2470b8c54354c39dc9b36f62a12b81f9207b16
wooglin <alfaindiagt@protonmail.com>
2022-12-14 12:36:59 -0600

    update

--- backend/testutils/server.go
+++ backend/testutils/server.go
@@ -1,31 +1,12 @@
 package testutils
 
 import (
-	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/api/calendar/v3"
 	"google.golang.org/api/googleapi"
-	"net/http"
 	"net/http/httptest"
 )
 
-func GetServerForEvents(events []*calendar.Event) *httptest.Server {
-	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		resp := &calendar.Events{
-			Items:          events,
-			ServerResponse: googleapi.ServerResponse{HTTPStatusCode: 200},
-		}
-
-		b, err := json.Marshal(resp)
-		if err != nil {
-			http.Error(w, "unable to marshal request: "+err.Error(), http.StatusBadRequest)
-			return
-		}
-		w.Write(b)
-	}))
-}
-
-//func GetGcalFetchServer(t *testing.T, threadsMap map[string]*gmail.Thread, messages []*gmail.Message) *httptest.Server {
 func GetGcalFetchServer(events []*calendar.Event) *httptest.Server {
 	return httptest.NewServer(func() *gin.Engine {
 		w := httptest.NewRecorder()
@@ -48,23 +29,6 @@ func GetGcalFetchServer(events []*calendar.Event) *httptest.Server {
 			}
 			c.JSON(200, response)
 		})
-
-		////r.Any("/*proxyPath", func(c *gin.Context) {
-		//r.NoRoute(func(c *gin.Context) {
-		//	//threads := make([]*gmail.Thread, 0, len(threadsMap))
-		//	//for _, value := range threadsMap {
-		//	//	threads = append(threads, value)
-		//	//}
-		//	//response := &gmail.ListThreadsResponse{Threads: threads}
-		//	//c.JSON(200, response)
-		//	log.Error().Msgf("jerddy %s", c.Request.RequestURI)
-		//	log.Error().Msgf("jerddy %s", c.Request.URL)
-		//	response := &calendar.Events{
-		//		Items:          events,
-		//		ServerResponse: googleapi.ServerResponse{HTTPStatusCode: 200},
-		//	}
-		//	c.JSON(200, response)
-		//})
 
 		return r
 	}())
