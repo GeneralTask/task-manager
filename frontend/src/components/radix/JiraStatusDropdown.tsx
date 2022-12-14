c90d6594b41f1b74a61b6076f8408da8fc9fd181
Nolan Jimenez <nolan1299@gmail.com>
2022-12-14 12:52:18 -0800

    Add Jira status dropdown to details view (#2509)

---
+++ frontend/src/components/radix/JiraStatusDropdown.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { useModifyTask } from '../../services/api/tasks.hooks'
+import { TTask } from '../../utils/types'
+import GTButton from '../atoms/buttons/GTButton'
+import GTDropdownMenu from './GTDropdownMenu'
+
+interface JiraStatusDropdownProps {
+    task: TTask
+    disabled?: boolean
+}
+
+const JiraStatusDropdown = ({ task, disabled }: JiraStatusDropdownProps) => {
+    const [isOpen, setIsOpen] = useState(false)
+    const { mutate: modifyTask } = useModifyTask()
+
+    const externalStatus = task.external_status ? task.external_status : null
+    const allStatuses = task.all_statuses ? task.all_statuses : null
+    if (!externalStatus || !allStatuses) return <></>
+
+    const dropdownItems = allStatuses.map((status) => ({
+        label: status.state,
+        onClick: () => modifyTask({ id: task.id, status: status }, task.optimisticId),
+        selected: status.state === externalStatus.state,
+    }))
+
+    return (
+        <GTDropdownMenu
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            items={dropdownItems}
+            unstyledTrigger
+            trigger={
+                <GTButton
+                    value={externalStatus.state}
+                    size="small"
+                    styleType="simple"
+                    isDropdown
+                    active={isOpen}
+                    disabled={disabled}
+                    asDiv
+                />
+            }
+        />
+    )
+}
+
+export default JiraStatusDropdown
