925cbdf9d464cf5ab1556b768c9bf674330de736
Jack Hamilton <jack@generaltask.com>
2022-12-15 12:07:10 -0800

    rem comment

--- frontend/src/components/views/NoteListView.tsx
+++ frontend/src/components/views/NoteListView.tsx
@@ -61,7 +61,6 @@ const NoteListView = () => {
                     <Spinner />
                 ) : (
                     <>
-                        {/* temporarily reversing notes so that the most recent note is at the top (backend will change to be correct soon) */}
                         {sortedNotes.map((note) => (
                             <Note key={note.id} note={note} isSelected={note.id === noteId} onSelect={selectNote} />
                         ))}
