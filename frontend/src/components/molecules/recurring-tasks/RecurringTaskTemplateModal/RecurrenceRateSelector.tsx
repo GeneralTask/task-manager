1c8983c5d3757471853716fb0f8e2c42d985e6e6
Scott Mai <scottmai702@gmail.com>
2022-12-16 13:04:21 -0800

    move to util

--- frontend/src/components/molecules/recurring-tasks/RecurringTaskTemplateModal/RecurrenceRateSelector.tsx
+++ frontend/src/components/molecules/recurring-tasks/RecurringTaskTemplateModal/RecurrenceRateSelector.tsx
@@ -1,10 +1,10 @@
 import { DateTime } from 'luxon'
 import { Spacing } from '../../../../styles'
 import { RecurrenceRate } from '../../../../utils/enums'
-import { getOrdinal } from '../../../../utils/time'
 import Flex from '../../../atoms/Flex'
 import { BodySmall } from '../../../atoms/typography/Typography'
 import GTSelect from '../../../radix/GTSelect'
+import { getRecurrenceRateSelectorOptions } from '../recurringTasks.utils'
 
 /*
     The core radix Select component only selects strings, so the RECURRENCE_RATE enum is converted to a string in order to be used here.
@@ -20,19 +20,7 @@ const RecurrenceRateSelector = ({ value, onChange, selectedDate }: RecurrenceRat
         <Flex column gap={Spacing._12}>
             <BodySmall>How often would you like this task to repeat?</BodySmall>
             <GTSelect
-                items={[
-                    { value: RecurrenceRate.DAILY.toString(), label: 'Daily' },
-                    { value: RecurrenceRate.WEEKLY.toString(), label: `Weekly on ${selectedDate.weekdayLong}` },
-                    {
-                        value: RecurrenceRate.MONTHLY.toString(),
-                        label: `Monthly on the ${getOrdinal(selectedDate.day)}`,
-                    },
-                    {
-                        value: RecurrenceRate.YEARLY.toString(),
-                        label: `Annually on ${selectedDate.monthShort} ${getOrdinal(selectedDate.day)}`,
-                    },
-                    { value: RecurrenceRate.WEEK_DAILY.toString(), label: 'Every weekday (Monday to Friday)' },
-                ]}
+                items={getRecurrenceRateSelectorOptions(selectedDate)}
                 value={value.toString()}
                 onChange={(newValue) => onChange(parseInt(newValue))}
             />
