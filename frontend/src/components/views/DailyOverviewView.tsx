24b2ed8f524dc0c312c54249e31b3e0555d89131
Nolan Jimenez <nolan1299@gmail.com>
2022-12-20 16:29:57 -0800

    Create accordion containers for overview lists (#2594)

--- frontend/src/components/views/DailyOverviewView.tsx
+++ frontend/src/components/views/DailyOverviewView.tsx
@@ -1,5 +1,32 @@
+import * as Accordion from '@radix-ui/react-accordion'
+import styled from 'styled-components'
+import { Spacing } from '../../styles'
+import { SectionHeader } from '../molecules/Header'
+import useOverviewLists from '../overview/useOverviewLists'
+import OverviewAccordionItem from '../radix/OverviewAccordionItem'
+import ScrollableListTemplate from '../templates/ScrollableListTemplate'
+
+const AccordionRoot = styled(Accordion.Root)`
+    > * > h3 {
+        all: unset;
+    }
+    > div {
+        margin-bottom: ${Spacing._4};
+    }
+`
+
 const DailyOverviewView = () => {
-    return <div>welcome to the new daily overview page</div>
+    const { lists } = useOverviewLists()
+    return (
+        <ScrollableListTemplate>
+            <SectionHeader sectionName="Daily Overview" />
+            <AccordionRoot type="multiple">
+                {lists.map((list) => (
+                    <OverviewAccordionItem key={list.id} list={list} />
+                ))}
+            </AccordionRoot>
+        </ScrollableListTemplate>
+    )
 }
 
 export default DailyOverviewView
