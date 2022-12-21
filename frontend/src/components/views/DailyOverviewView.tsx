2213bf50ff4ea703e53168eeec044d4541a5509f
Nolan Jimenez <nolan1299@gmail.com>
2022-12-20 17:36:53 -0800

    Add styling

--- frontend/src/components/views/DailyOverviewView.tsx
+++ frontend/src/components/views/DailyOverviewView.tsx
@@ -1,12 +1,22 @@
 import { useCallback, useState } from 'react'
 import * as Accordion from '@radix-ui/react-accordion'
 import styled from 'styled-components'
-import { Spacing } from '../../styles'
+import { Border, Colors, Spacing } from '../../styles'
+import { icons } from '../../styles/images'
+import GTButton from '../atoms/buttons/GTButton'
 import { SectionHeader } from '../molecules/Header'
 import useOverviewLists from '../overview/useOverviewLists'
 import OverviewAccordionItem from '../radix/OverviewAccordionItem'
 import ScrollableListTemplate from '../templates/ScrollableListTemplate'
 
+const ActionsContainer = styled.div`
+    background-color: ${Colors.background.medium};
+    padding: ${Spacing._8} ${Spacing._12};
+    border-radius: ${Border.radius.mini};
+    display: flex;
+    gap: ${Spacing._24};
+    margin-bottom: ${Spacing._16};
+`
 const AccordionRoot = styled(Accordion.Root)`
     > * > h3 {
         all: unset;
@@ -25,8 +35,24 @@ const DailyOverviewView = () => {
     return (
         <ScrollableListTemplate>
             <SectionHeader sectionName="Daily Overview" />
-            <button onClick={collapseAll}>collapse all</button>
-            <button onClick={expandAll}>expand all</button>
+            <ActionsContainer>
+                <GTButton
+                    styleType="simple"
+                    size="small"
+                    onClick={collapseAll}
+                    icon={icons.squareMinus}
+                    iconColor="gray"
+                    value="Collapse All"
+                />
+                <GTButton
+                    styleType="simple"
+                    size="small"
+                    onClick={expandAll}
+                    icon={icons.squarePlus}
+                    iconColor="gray"
+                    value="Expand All"
+                />
+            </ActionsContainer>
             <AccordionRoot type="multiple" value={values} onValueChange={setValues}>
                 {lists.map((list) => (
                     <OverviewAccordionItem key={list.id} list={list} />
