47f77055961fe67c16a1958f2b78dcd289384af9
Nolan Jimenez <nolan1299@gmail.com>
2022-12-20 16:22:44 -0800

    Better support long titles

--- frontend/src/components/radix/OverviewAccordionItem.tsx
+++ frontend/src/components/radix/OverviewAccordionItem.tsx
@@ -31,11 +31,23 @@ const TriggerTitle = styled.div`
     display: flex;
     align-items: center;
     gap: ${Spacing._16};
+    overflow: hidden;
+    text-overflow: ellipsis;
+    margin-right: ${Spacing._8};
+    white-space: nowrap;
+`
+const ListTitle = styled(Body)`
+    overflow: hidden;
+    text-overflow: ellipsis;
+    min-width: 0;
 `
 const TriggerRightContainer = styled.div`
     display: flex;
     align-items: center;
     gap: ${Spacing._16};
+`
+const ItemsRemainingText = styled(Label)`
+    white-space: nowrap;
 `
 const ListContent = styled.div`
     padding: ${Spacing._16};
@@ -53,10 +65,12 @@ const OverviewAccordionItem = ({ list }: OverviewAccordionItemProps) => {
                 <AccordionTrigger>
                     <TriggerTitle>
                         <Icon icon={logos[list.logo]} />
-                        <Body>{list.name}</Body>
+                        <ListTitle>{list.name}</ListTitle>
                     </TriggerTitle>
                     <TriggerRightContainer>
-                        {list.view_items.length > 0 && <Label>{list.view_items.length} remaining</Label>}
+                        {list.view_items.length > 0 && (
+                            <ItemsRemainingText>{list.view_items.length} remaining</ItemsRemainingText>
+                        )}
                         <Icon icon={icons.caret_down} className="AccordionChevron" />
                     </TriggerRightContainer>
                 </AccordionTrigger>
