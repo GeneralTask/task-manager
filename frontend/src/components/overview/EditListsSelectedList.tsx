626a07bc6cfccb7c69c2b530738bf5e2cc990b1a
Nolan Jimenez <nolan1299@gmail.com>
2022-12-22 11:23:24 -0800

    Change overview modal list styling

--- frontend/src/components/overview/EditListsSelectedList.tsx
+++ frontend/src/components/overview/EditListsSelectedList.tsx
@@ -1,12 +1,29 @@
 import { useDrag } from 'react-dnd'
+import styled from 'styled-components'
 import { useRemoveView } from '../../services/api/overview.hooks'
+import { Spacing } from '../../styles'
 import { icons } from '../../styles/images'
 import { DropItem, DropType, TOverviewView } from '../../utils/types'
 import Domino from '../atoms/Domino'
 import { Icon } from '../atoms/Icon'
 import ReorderDropContainer from '../atoms/ReorderDropContainer'
+import { TemplateContainer } from '../atoms/TaskTemplate'
+import ItemContainer from '../molecules/ItemContainer'
 import { getOverviewAccordionHeaderIcon } from '../radix/OverviewAccordionItem'
-import { EditViewsDeleteButton, SelectedList } from './styles'
+import { EditViewsDeleteButton } from './styles'
+
+const StyledItemContainer = styled(ItemContainer)`
+    display: flex;
+    width: 100%;
+    padding: ${Spacing._12};
+    box-sizing: border-box;
+`
+const TextContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-left: ${Spacing._12};
+    gap: ${Spacing._12};
+`
 
 interface EditViewsSelectedViewProps {
     view: TOverviewView
@@ -32,14 +49,18 @@ const EditViewsSelectedView = ({ view, viewIndex, onReorder }: EditViewsSelected
             onReorder={onReorder}
             ref={drag}
         >
-            <SelectedList key={view.id} ref={dragPreview}>
-                <Domino />
-                <Icon icon={getOverviewAccordionHeaderIcon(view.logo, view.task_section_id)} />
-                {view.name}
-                <EditViewsDeleteButton onClick={() => removeView({ id: view.id }, view.optimisticId)}>
-                    <Icon icon={icons.trash} />
-                </EditViewsDeleteButton>
-            </SelectedList>
+            <TemplateContainer isVisible>
+                <StyledItemContainer key={view.id} ref={dragPreview}>
+                    <Domino />
+                    <TextContainer>
+                        <Icon icon={getOverviewAccordionHeaderIcon(view.logo, view.task_section_id)} />
+                        {view.name}
+                    </TextContainer>
+                    <EditViewsDeleteButton onClick={() => removeView({ id: view.id }, view.optimisticId)}>
+                        <Icon icon={icons.trash} />
+                    </EditViewsDeleteButton>
+                </StyledItemContainer>
+            </TemplateContainer>
         </ReorderDropContainer>
     )
 }
