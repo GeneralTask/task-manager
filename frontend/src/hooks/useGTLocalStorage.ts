2151e959f35c6fda22d5a80cd1388ccb29b738cd
Scott Mai <scottmai702@gmail.com>
2022-12-22 16:42:07 -0800

    Read from localstorage only once on mount (except for previewMode) (#2625)
    * revert
    
    * sync

--- frontend/src/hooks/useGTLocalStorage.ts
+++ frontend/src/hooks/useGTLocalStorage.ts
@@ -1,5 +1,4 @@
-import { Dispatch, SetStateAction } from 'react'
-import { useLocalStorage } from 'usehooks-ts'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
 type TLocalStorageKeys =
     | 'noteCreation'
@@ -11,9 +10,52 @@ type TLocalStorageKeys =
     | 'dueTodayCollapsed'
     | 'taskToCalendarSidebar'
 
-declare type SetValue<T> = Dispatch<SetStateAction<T>>
-const useGTLocalStorage = <T>(key: TLocalStorageKeys, initialValue: T): [T, SetValue<T>] => {
-    return useLocalStorage(key, initialValue)
+// based on https://usehooks.com/useLocalStorage/
+// if updateOnStoreChange is true, the hook will update the state when the value is changed on this tab or another tab
+const useGTLocalStorage = <T>(
+    key: TLocalStorageKeys,
+    initialValue: T,
+    updateOnStoreChange = false
+): [T, Dispatch<SetStateAction<T>>] => {
+    const [storedValue, setStoredValue] = useState(() => {
+        // Get from local storage by key
+        const item = window.localStorage.getItem(key)
+        // Parse stored json or if none return initialValue
+        return item ? JSON.parse(item) : initialValue
+    })
+
+    const setValue = (value: T | ((val: T) => T)) => {
+        // Allow value to be a function so we have same API as useState
+        const valueToStore = value instanceof Function ? value(storedValue) : value
+        // Save state
+        setStoredValue(valueToStore)
+        const serialized = JSON.stringify(valueToStore)
+        // Save to local storage
+        window.localStorage.setItem(key, serialized)
+        // Dispatch custom event so other instances of this hook can re-render
+        window.dispatchEvent(
+            new StorageEvent('storage', {
+                key,
+                newValue: serialized,
+            })
+        )
+    }
+
+    useEffect(() => {
+        if (!updateOnStoreChange) return
+
+        const handleStorageChange = (e: StorageEvent) => {
+            if (e.key === key && e.newValue) {
+                setStoredValue(JSON.parse(e.newValue))
+            }
+        }
+        addEventListener('storage', handleStorageChange)
+        return () => {
+            removeEventListener('storage', handleStorageChange)
+        }
+    }, [key])
+
+    return [storedValue, setValue]
 }
 
 export default useGTLocalStorage
