ae78583de059b97cea5acd97d4947de64f719577
Scott Mai <scottmai702@gmail.com>
2022-12-28 23:31:20 -0800

    looks really good :0

--- frontend/src/components/atoms/GTTextField/AtlassianEditor/AtlassianEditor.tsx
+++ frontend/src/components/atoms/GTTextField/AtlassianEditor/AtlassianEditor.tsx
@@ -1,24 +1,33 @@
 import '@atlaskit/css-reset'
 import { Editor } from '@atlaskit/editor-core'
 import styled from 'styled-components'
-import { Typography } from '../../../../styles'
+import { Border, Spacing, Typography } from '../../../../styles'
 import { MarkdownEditorProps } from '../types'
 
 const Container = styled.div`
     height: 100%;
-    .ProseMirror {
+    .akEditor {
+        border: none;
+        border-radius: ${Border.radius.small};
+        height: 100%;
+    }
+    /* && allows us to override existing styles - basically same as !important for entire selectors */
+    && .ProseMirror {
         ${Typography.bodySmall};
     }
     /* align text to top of editor */
-    [aria-label='Editable content'] {
-        padding: 0 !important;
+    .ak-editor-content-area {
+        padding: 0 ${Spacing._8};
     }
     /* stop from intersecting with parent border */
     [data-testid='ak-editor-main-toolbar'] {
-        border-radius: 8px !important;
+        border-radius: ${Border.radius.small};
+        height: fit-content;
+        padding: 0 ${Spacing._8};
     }
-    #editor-scroll-gutter {
-        display: none;
+    /* needed to make editor match container height */
+    > div > :nth-child(2) {
+        height: 100%;
     }
 `
 
@@ -26,7 +35,7 @@ const AtlassianEditor = (props: MarkdownEditorProps) => {
     console.log(props)
     return (
         <Container id="duck">
-            <Editor appearance="full-width" onChange={(e) => console.log(e)} />
+            <Editor appearance="comment" onChange={(e) => console.log(e)} placeholder="Add details" />
         </Container>
     )
 }
