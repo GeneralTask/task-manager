8ffb42798d49a9f8bacffd35524a583d4eef67b1
Scott Mai <scottmai702@gmail.com>
2022-12-28 22:17:29 -0800

    actually kind of looks good

--- frontend/src/components/atoms/GTTextField/AtlassianEditor/AtlassianEditor.tsx
+++ frontend/src/components/atoms/GTTextField/AtlassianEditor/AtlassianEditor.tsx
@@ -1,9 +1,34 @@
+import '@atlaskit/css-reset'
 import { Editor } from '@atlaskit/editor-core'
+import styled from 'styled-components'
+import { Typography } from '../../../../styles'
 import { MarkdownEditorProps } from '../types'
+
+const Container = styled.div`
+    height: 100%;
+    .ProseMirror {
+        ${Typography.bodySmall};
+    }
+    /* align text to top of editor */
+    [aria-label='Editable content'] {
+        padding: 0 !important;
+    }
+    /* stop from intersecting with parent border */
+    [data-testid='ak-editor-main-toolbar'] {
+        border-radius: 8px !important;
+    }
+    #editor-scroll-gutter {
+        display: none;
+    }
+`
 
 const AtlassianEditor = (props: MarkdownEditorProps) => {
     console.log(props)
-    return <Editor appearance="comment" onChange={(e) => console.log(e)} />
+    return (
+        <Container id="duck">
+            <Editor appearance="full-width" onChange={(e) => console.log(e)} />
+        </Container>
+    )
 }
 
 export default AtlassianEditor
