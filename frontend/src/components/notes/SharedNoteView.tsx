bce80d1d82ec78a1b66a6286a1f984372684933f
Scott Mai <scottmai702@gmail.com>
2023-01-03 11:11:53 -0800

    meta

--- frontend/src/components/notes/SharedNoteView.tsx
+++ frontend/src/components/notes/SharedNoteView.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react'
+import { Helmet } from 'react-helmet'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import Cookies from 'js-cookie'
 import { DateTime } from 'luxon'
@@ -102,129 +103,141 @@ const SharedNoteView = () => {
 
     if (isLoading || isLoadingNotes) return <Spinner />
     return (
-        <MainContainer>
-            <ColumnContainer>
-                <TopContainer>
-                    <NoStyleButton
-                        onClick={() => {
-                            GALog('Button click', 'Logo')
-                            navigate('/')
-                        }}
-                    >
-                        <Logo src="/images/gt-logo-black-on-white.svg" />
-                    </NoStyleButton>
-                    {isLoggedIn ? (
-                        <GTButton
-                            styleType="secondary"
-                            value="Back to General Task"
+        <>
+            {note && (
+                <Helmet>
+                    <title>{note.title}</title>
+                    <meta content={note.title} property="og:title" />
+                    <meta content={note.body} property="og:description" />
+                </Helmet>
+            )}
+            <MainContainer>
+                <ColumnContainer>
+                    <TopContainer>
+                        <NoStyleButton
                             onClick={() => {
-                                GALog('Button click', 'Back to General Task')
+                                GALog('Button click', 'Logo')
                                 navigate('/')
                             }}
-                        />
-                    ) : (
-                        <SignInButton
-                            onClick={() => {
-                                GALog('Button click', 'Sign in with Google')
-                                openPopupWindow(LOGIN_URL, emptyFunction, false, true)
-                            }}
                         >
-                            <GoogleImage src={buttons.google_sign_in} />
-                        </SignInButton>
-                    )}
-                </TopContainer>
-                <BottomContainer>
-                    <NoteBody>
-                        {note && note.shared_until ? (
-                            <>
-                                <Flex alignItems="flex-start">
+                            <Logo src="/images/gt-logo-black-on-white.svg" />
+                        </NoStyleButton>
+                        {isLoggedIn ? (
+                            <GTButton
+                                styleType="secondary"
+                                value="Back to General Task"
+                                onClick={() => {
+                                    GALog('Button click', 'Back to General Task')
+                                    navigate('/')
+                                }}
+                            />
+                        ) : (
+                            <SignInButton
+                                onClick={() => {
+                                    GALog('Button click', 'Sign in with Google')
+                                    openPopupWindow(LOGIN_URL, emptyFunction, false, true)
+                                }}
+                            >
+                                <GoogleImage src={buttons.google_sign_in} />
+                            </SignInButton>
+                        )}
+                    </TopContainer>
+                    <BottomContainer>
+                        <NoteBody>
+                            {note && note.shared_until ? (
+                                <>
+                                    <Flex alignItems="flex-start">
+                                        <GTTextField
+                                            type="plaintext"
+                                            itemId={note.title}
+                                            value={note.title}
+                                            onChange={emptyFunction}
+                                            fontSize="large"
+                                            disabled
+                                            readOnly
+                                        />
+                                        <NoteActionsDropdown note={note} />
+                                    </Flex>
                                     <GTTextField
-                                        type="plaintext"
-                                        itemId={note.title}
-                                        value={note.title}
+                                        type="markdown"
+                                        itemId={note.body}
+                                        value={note.body}
                                         onChange={emptyFunction}
-                                        fontSize="large"
+                                        fontSize="small"
                                         disabled
                                         readOnly
                                     />
-                                    <NoteActionsDropdown note={note} />
-                                </Flex>
-                                <GTTextField
-                                    type="markdown"
-                                    itemId={note.body}
-                                    value={note.body}
-                                    onChange={emptyFunction}
-                                    fontSize="small"
-                                    disabled
-                                    readOnly
-                                />
-                                <Divider color={Colors.border.light} />
-                                <FlexPadding8Horizontal justifyContent="space-between" alignItems="center">
-                                    <Flex gap={Spacing._4}>
-                                        {isLoggedIn && notes?.findIndex((n) => n.id === note.id) !== -1 ? (
-                                            <>
-                                                <Label color="light">{`You shared this note ${getHumanTimeSinceDateTime(
+                                    <Divider color={Colors.border.light} />
+                                    <FlexPadding8Horizontal justifyContent="space-between" alignItems="center">
+                                        <Flex gap={Spacing._4}>
+                                            {isLoggedIn && notes?.findIndex((n) => n.id === note.id) !== -1 ? (
+                                                <>
+                                                    <Label color="light">{`You shared this note ${getHumanTimeSinceDateTime(
+                                                        DateTime.fromISO(note.updated_at)
+                                                    )}`}</Label>
+                                                    <Label>
+                                                        {'('}
+                                                        <Link to={`/notes/${noteId}`}>edit note</Link>
+                                                        {')'}
+                                                    </Label>
+                                                </>
+                                            ) : (
+                                                <Label color="light">{`${
+                                                    note.author
+                                                } shared this note ${getHumanTimeSinceDateTime(
                                                     DateTime.fromISO(note.updated_at)
                                                 )}`}</Label>
-                                                <Label>
-                                                    {'('}
-                                                    <Link to={`/notes/${noteId}`}>edit note</Link>
-                                                    {')'}
-                                                </Label>
-                                            </>
+                                            )}
+                                        </Flex>
+                                        <Label color="light">
+                                            {note.shared_until === SHARED_NOTE_INDEFINITE_DATE
+                                                ? ''
+                                                : `Link expires in ${getFormattedDuration(
+                                                      DateTime.fromISO(note.shared_until).diffNow('milliseconds', {
+                                                          conversionAccuracy: 'longterm',
+                                                      }),
+                                                      2
+                                                  )}`}
+                                        </Label>
+                                    </FlexPadding8Horizontal>
+                                </>
+                            ) : (
+                                <>
+                                    <Title>This note is not available</Title>
+                                    <Body>
+                                        If you need access to this note, please reach out to the person who sent it.
+                                    </Body>
+                                    <FlexMargin8Top gap={Spacing._8}>
+                                        {isLoggedIn ? (
+                                            <GTButton
+                                                styleType="primary"
+                                                value="Back to General Task"
+                                                onClick={() => {
+                                                    GALog('Button click', 'Back to General Task')
+                                                    navigate('/')
+                                                }}
+                                            />
                                         ) : (
-                                            <Label color="light">{`${
-                                                note.author
-                                            } shared this note ${getHumanTimeSinceDateTime(
-                                                DateTime.fromISO(note.updated_at)
-                                            )}`}</Label>
+                                            <>
+                                                <NoStyleAnchor href={getEnvVars().REACT_APP_TRY_SIGN_UP_URL}>
+                                                    <GTButton styleType="primary" value="Sign In to General Task" />
+                                                </NoStyleAnchor>
+                                                <NoStyleAnchor href={getEnvVars().REACT_APP_TRY_BASE_URL}>
+                                                    <GTButton
+                                                        styleType="secondary"
+                                                        value="Learn more about General Task"
+                                                    />
+                                                </NoStyleAnchor>
+                                            </>
                                         )}
-                                    </Flex>
-                                    <Label color="light">
-                                        {note.shared_until === SHARED_NOTE_INDEFINITE_DATE
-                                            ? ''
-                                            : `Link expires in ${getFormattedDuration(
-                                                  DateTime.fromISO(note.shared_until).diffNow('milliseconds', {
-                                                      conversionAccuracy: 'longterm',
-                                                  }),
-                                                  2
-                                              )}`}
-                                    </Label>
-                                </FlexPadding8Horizontal>
-                            </>
-                        ) : (
-                            <>
-                                <Title>This note is not available</Title>
-                                <Body>
-                                    If you need access to this note, please reach out to the person who sent it.
-                                </Body>
-                                <FlexMargin8Top gap={Spacing._8}>
-                                    {isLoggedIn ? (
-                                        <GTButton
-                                            styleType="primary"
-                                            value="Back to General Task"
-                                            onClick={() => {
-                                                GALog('Button click', 'Back to General Task')
-                                                navigate('/')
-                                            }}
-                                        />
-                                    ) : (
-                                        <>
-                                            <NoStyleAnchor href={getEnvVars().REACT_APP_TRY_SIGN_UP_URL}>
-                                                <GTButton styleType="primary" value="Sign In to General Task" />
-                                            </NoStyleAnchor>
-                                            <NoStyleAnchor href={getEnvVars().REACT_APP_TRY_BASE_URL}>
-                                                <GTButton styleType="secondary" value="Learn more about General Task" />
-                                            </NoStyleAnchor>
-                                        </>
-                                    )}
-                                </FlexMargin8Top>
-                            </>
-                        )}
-                    </NoteBody>
-                </BottomContainer>
-            </ColumnContainer>
-        </MainContainer>
+                                    </FlexMargin8Top>
+                                </>
+                            )}
+                        </NoteBody>
+                    </BottomContainer>
+                </ColumnContainer>
+            </MainContainer>
+        </>
     )
 }
 
