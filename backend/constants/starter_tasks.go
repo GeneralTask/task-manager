09c267b6e257b3b67a2836c45fcbbc7b5e276518
wooglin <alfaindiagt@protonmail.com>
2023-01-13 16:42:07 -0600

    Add slack nux task

--- backend/constants/starter_tasks.go
+++ backend/constants/starter_tasks.go
@@ -4,5 +4,6 @@ var StarterTasks = []string{
 	"Put tasks on your calendar with click-and-drag",
 	"Shut out distractions with Focus Mode",
 	"Connect other services to see things in one place",
+	"Create tasks out of Slack Messages",
 	"A sincere thank you from the team",
 }
