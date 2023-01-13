fb7f4d36a2a4c770f0b5d7e7be055a6839a65783
wooglin <alfaindiagt@protonmail.com>
2023-01-13 16:51:34 -0600

    update

--- backend/constants/starter_tasks.go
+++ backend/constants/starter_tasks.go
@@ -1,9 +1,9 @@
 package constants
 
 var StarterTasks = map[string]int{
-	"Put tasks on your calendar with click-and-drag":    1,
-	"Shut out distractions with Focus Mode":             2,
-	"Connect other services to see things in one place": 3,
-	"Create tasks out of Slack Messages":                5,
-	"A sincere thank you from the team":                 4,
+	"🗓 Drag tasks onto your calendar":                     1,
+	"🔎 Shut out distractions with Focus Mode":             2,
+	"⚙️ Connect your services to see things in one place": 3,
+	"💬 Create tasks out of Slack Messages":                5,
+	"A sincere thank you from the team":                   4,
 }
