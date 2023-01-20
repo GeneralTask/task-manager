26fd2787b1df1549ccd581eea51facb8907b9487
scottmai <scottmai702@gmail.com>
2023-01-20 11:38:44 -0800

    make shared note work in preview mode

--- frontend/src/hooks/usePreviewMode.ts
+++ frontend/src/hooks/usePreviewMode.ts
@@ -3,17 +3,19 @@ import { useGetUserInfo } from '../services/api/user-info.hooks'
 
 interface UsePreviewModeOutput {
     isPreviewMode: boolean
+    isLoading: boolean
     toggle: () => void
     enable: () => void
     disable: () => void
 }
 
 function usePreviewMode(defaultValue?: boolean): UsePreviewModeOutput {
-    const { data: userInfo } = useGetUserInfo()
+    const { data: userInfo, isLoading } = useGetUserInfo()
     const [isPreviewMode, setPreviewMode] = useGTLocalStorage<boolean>('previewMode', defaultValue ?? false, true)
 
     return {
         isPreviewMode: isPreviewMode && (userInfo?.is_employee ?? false),
+        isLoading,
         toggle: () => setPreviewMode((prev) => !prev),
         enable: () => setPreviewMode(true),
         disable: () => setPreviewMode(false),
