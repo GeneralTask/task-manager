88a14b0686de3e981350e0a3be4796b6fe4283eb
Scott Mai <scottmai702@gmail.com>
2023-01-24 11:11:33 -0800

    stop using jira editor for markdown

--- frontend/src/components/atoms/GTTextField/GTTextField.tsx
+++ frontend/src/components/atoms/GTTextField/GTTextField.tsx
@@ -1,6 +1,5 @@
 import { Suspense, forwardRef, lazy, useRef } from 'react'
 import styled from 'styled-components'
-import { usePreviewMode } from '../../../hooks'
 import { Border, Colors, Shadows } from '../../../styles'
 import { stopKeydownPropogation } from '../../../utils/utils'
 import Spinner from '../Spinner'
@@ -56,15 +55,11 @@ const Container = styled.div<{
 
 const GTTextField = forwardRef((props: GTTextFieldProps, ref) => {
     const containerRef = useRef<HTMLDivElement>(null)
-    const { isPreviewMode } = usePreviewMode()
 
     const getEditor = () => {
         if (props.type === 'plaintext') {
             return <PlainTextEditor ref={ref} {...props} />
         } else if (props.type === 'markdown') {
-            if (isPreviewMode) {
-                return <AtlassianEditor {...props} />
-            }
             return <MarkdownEditor {...props} />
         } else if (props.type === 'atlassian') {
             return <AtlassianEditor {...props} />
