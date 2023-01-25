a8033dc25d01ae0e21f97c3070cb73eabe4cc86c
Julian <julian@sweetrain.com>
2023-01-24 17:35:12 -0800

    Initial reordering work

--- backend/api/task_modify.go
+++ backend/api/task_modify.go
@@ -10,6 +10,7 @@ import (
 	"github.com/GeneralTask/task-manager/backend/constants"
 	"github.com/GeneralTask/task-manager/backend/database"
 	"github.com/GeneralTask/task-manager/backend/external"
+	"github.com/GeneralTask/task-manager/backend/logging"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -260,13 +261,20 @@ func (api *API) ReOrderTask(c *gin.Context, taskID primitive.ObjectID, userID pr
 		{"id_ordering": bson.M{"$gte": *IDOrdering}},
 		{"user_id": userID},
 	}
+	taskQuery := []bson.M{
+		{"user_id": userID},
+		{"is_deleted": bson.M{"$ne": true}},
+	}
 	if task.ParentTaskID != primitive.NilObjectID {
 		dbQuery = append(dbQuery, bson.M{"parent_task_id": task.ParentTaskID})
+		taskQuery = append(taskQuery, bson.M{"parent_task_id": task.ParentTaskID})
 	} else {
 		dbQuery = append(dbQuery, bson.M{"id_task_section": IDTaskSection})
+		taskQuery = append(taskQuery, bson.M{"id_task_section": IDTaskSection})
+		taskQuery = append(taskQuery, bson.M{"is_completed": false})
 	}
 
-	// Move back other tasks to ensure ordering is preserved (gaps are removed in GET task list)
+	// Move back other tasks to ensure ordering is preserved
 	_, err = taskCollection.UpdateMany(
 		context.Background(),
 		bson.M{"$and": dbQuery},
@@ -277,7 +285,35 @@ func (api *API) ReOrderTask(c *gin.Context, taskID primitive.ObjectID, userID pr
 		Handle500(c)
 		return err
 	}
+
+	// Remove gaps in ordering IDs
+	taskResults, err := api.getTaskResultsFromQuery(taskQuery, userID)
+	err = api.updateOrderingIDsV2(api.DB, &taskResults)
+
 	return nil
+}
+
+func (api *API) getTaskResultsFromQuery(taskQuery []bson.M, userID primitive.ObjectID) ([]*TaskResult, error) {
+	taskCollection := database.GetTaskCollection(api.DB)
+	cursor, err := taskCollection.Find(
+		context.Background(),
+		bson.M{"$and": taskQuery},
+	)
+	if err != nil {
+		api.Logger.Error().Err(err).Msg("failed to fetch tasks")
+		return nil, err
+	}
+
+	var tasks []database.Task
+	err = cursor.All(context.Background(), &tasks)
+	if err != nil {
+		logger := logging.GetSentryLogger()
+		logger.Error().Err(err).Msg("failed to fetch tasks for user")
+		return nil, err
+	}
+
+	taskList := api.taskListToTaskResultList(&tasks, userID)
+	return taskList, nil
 }
 
 func (api *API) UpdateTaskInDB(c *gin.Context, task *database.Task, userID primitive.ObjectID, updateFields *database.Task) {
