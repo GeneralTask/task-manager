897ac6a71c3b135a04f8b64eadd9b5054214a665
Jack Hamilton <31417618+johnhamilto@users.noreply.github.com>
2023-02-01 16:07:25 -0500

    Update "due today" empty state (#2901)

--- frontend/src/components/overview/viewItems/DueTodayViewItems.tsx
+++ frontend/src/components/overview/viewItems/DueTodayViewItems.tsx
@@ -1,14 +1,17 @@
 import { Ref, forwardRef } from 'react'
 import { useParams } from 'react-router-dom'
+import { usePreviewMode } from '../../../hooks'
 import { TTask } from '../../../utils/types'
 import Task from '../../molecules/Task'
 import { ViewHeader, ViewName } from '../styles'
+import EmptyListMessage from './EmptyListMessage'
 import EmptyViewItem from './EmptyViewItem'
 import { ViewItemsProps } from './viewItems.types'
 
 const DueTodayViewItems = forwardRef(
     ({ view, visibleItemsCount, scrollRef, hideHeader }: ViewItemsProps, ref: Ref<HTMLDivElement>) => {
         const { overviewViewId, overviewItemId } = useParams()
+        const { isPreviewMode } = usePreviewMode()
         return (
             <>
                 {!hideHeader && (
@@ -30,6 +33,8 @@ const DueTodayViewItems = forwardRef(
                                 link={`/overview/${view.id}/${item.id}`}
                             />
                         ))
+                ) : isPreviewMode ? (
+                    <EmptyListMessage list={view} />
                 ) : (
                     <EmptyViewItem
                         header="You have no more tasks due today."
