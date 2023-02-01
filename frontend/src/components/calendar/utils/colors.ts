3f6d4394ee1ea7bb934aa12e53c871c5eec2957b
scottmai <scottmai702@gmail.com>
2023-02-01 14:24:21 -0800

    dont use event color

--- frontend/src/components/calendar/utils/colors.ts
+++ frontend/src/components/calendar/utils/colors.ts
@@ -147,10 +147,7 @@ export const eventColors = {
     },
 }
 
-// gcal tends to use the event color, but we don't have a complete list of event colors so fall back to calendar colors
 const getCalendarColor = (colorId: string): string =>
-    eventColors[colorId as keyof typeof eventColors]?.background ??
-    calendarColors[colorId as keyof typeof calendarColors]?.background ??
-    DEFAULT_CALENDAR_COLOR
+    calendarColors[colorId as keyof typeof calendarColors]?.background ?? DEFAULT_CALENDAR_COLOR
 
 export default getCalendarColor
