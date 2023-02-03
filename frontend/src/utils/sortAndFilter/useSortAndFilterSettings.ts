1fae5beb658ce27c5927ef4c8b2a435bc267e7c5
Jack Hamilton <john.hamilton@wustl.edu>
2023-02-03 11:44:30 -0500

    add recurring task filtering setting to frontend

--- frontend/src/utils/sortAndFilter/useSortAndFilterSettings.ts
+++ frontend/src/utils/sortAndFilter/useSortAndFilterSettings.ts
@@ -13,8 +13,8 @@ const useSortAndFilterSettings = <T>(
     const sortDirection = useSetting(`${settingPrefix}${config.sortDirectionId}${settingSuffix}`)
     const filterPreference = useSetting(`${settingPrefix}${config.filterPreferenceId}`)
 
-    // all settings come from one endpoint so we can just check if one is loading
-    if (sortingPreference.isLoading) {
+    // all settings come from one endpoint so if any are finished loading, they all are
+    if (!sortingPreference.isLoading && !sortDirection.isLoading && !filterPreference.isLoading) {
         return config.defaultSortsAndFilters
     }
 
