3fba6ff05bf09e4645ec14109f6d4d98264a6c9a
Jack Hamilton <31417618+johnhamilto@users.noreply.github.com>
2023-02-03 12:54:13 -0500

    unlock empty list message (#2917)

--- frontend/src/components/overview/viewItems/DueTodayViewItems.tsx
+++ frontend/src/components/overview/viewItems/DueTodayViewItems.tsx
@@ -1,17 +1,14 @@
 import { Ref, forwardRef } from 'react'
 import { useParams } from 'react-router-dom'
-import { usePreviewMode } from '../../../hooks'
 import { TTask } from '../../../utils/types'
 import Task from '../../molecules/Task'
 import { ViewHeader, ViewName } from '../styles'
 import EmptyListMessage from './EmptyListMessage'
-import EmptyViewItem from './EmptyViewItem'
 import { ViewItemsProps } from './viewItems.types'
 
 const DueTodayViewItems = forwardRef(
     ({ view, visibleItemsCount, scrollRef, hideHeader }: ViewItemsProps, ref: Ref<HTMLDivElement>) => {
         const { overviewViewId, overviewItemId } = useParams()
-        const { isPreviewMode } = usePreviewMode()
         return (
             <>
                 {!hideHeader && (
@@ -33,13 +30,8 @@ const DueTodayViewItems = forwardRef(
                                 link={`/overview/${view.id}/${item.id}`}
                             />
                         ))
-                ) : isPreviewMode ? (
-                    <EmptyListMessage list={view} />
                 ) : (
-                    <EmptyViewItem
-                        header="You have no more tasks due today."
-                        body="When you have tasks left which are due today, they will appear here."
-                    />
+                    <EmptyListMessage list={view} />
                 )}
             </>
         )
--- frontend/src/components/overview/viewItems/ExternalViewItems.tsx
+++ frontend/src/components/overview/viewItems/ExternalViewItems.tsx
@@ -1,36 +1,14 @@
 import { Ref, forwardRef } from 'react'
 import { useParams } from 'react-router-dom'
-import { usePreviewMode } from '../../../hooks'
 import { DropType, TTask } from '../../../utils/types'
 import Task from '../../molecules/Task'
 import { ViewHeader, ViewName } from '../styles'
 import EmptyListMessage from './EmptyListMessage'
-import EmptyViewItem from './EmptyViewItem'
 import { ViewItemsProps } from './viewItems.types'
 
 const ExternalViewItems = forwardRef(
     ({ view, visibleItemsCount, scrollRef, hideHeader }: ViewItemsProps, ref: Ref<HTMLDivElement>) => {
         const { overviewViewId, overviewItemId } = useParams()
-        const { isPreviewMode } = usePreviewMode()
-
-        const getEmptyViewItem = () => {
-            if (isPreviewMode) return <EmptyListMessage list={view} />
-            if (view.type === 'slack') {
-                return (
-                    <EmptyViewItem
-                        header="You have no more slack messages!"
-                        body="When you create a task from a slack message, it will appear here."
-                    />
-                )
-            } else if (view.type === 'linear') {
-                return (
-                    <EmptyViewItem
-                        header="You have no more linear tasks!"
-                        body="When new linear tasks get assigned to you, they will appear here."
-                    />
-                )
-            }
-        }
 
         return (
             <>
@@ -39,7 +17,7 @@ const ExternalViewItems = forwardRef(
                         <ViewName>{view.name}</ViewName>
                     </ViewHeader>
                 )}
-                {view.view_items.length === 0 && view.is_linked && getEmptyViewItem()}
+                {view.view_items.length === 0 && view.is_linked && <EmptyListMessage list={view} />}
                 {view.view_items.slice(0, visibleItemsCount).map((item) => (
                     <Task
                         key={item.id}
