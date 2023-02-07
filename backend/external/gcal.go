00291cae713cdebc0a0b926c34efe0ccfe974179
alfaindia <97246822+alfaindia@users.noreply.github.com>
2023-02-06 18:05:57 -0600

    Show prompt on calendar when we mark a token as bad (#2937)

--- backend/external/gcal.go
+++ backend/external/gcal.go
@@ -299,6 +299,11 @@ func CheckAndHandleBadToken(err error, db *mongo.Database, userID primitive.Obje
 	if err != nil {
 		logger.Error().Str("tokenID", token.ID.Hex()).Err(err).Msg("unable to update external token")
 	}
+
+	err = database.UpdateUserSetting(db, userID, constants.HasDismissedMulticalPrompt, constants.SettingFalse)
+	if err != nil {
+		logger.Error().Err(err).Msg("failed to set HasDismissedMulticalPrompt as false")
+	}
 	return true
 }
 
