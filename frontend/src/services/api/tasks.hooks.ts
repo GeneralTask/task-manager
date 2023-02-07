3e94f49708d1b96640145540af0266a3befdff8d
Nolan Jimenez <nolan1299@gmail.com>
2023-02-07 15:06:05 -0700

    Move function

--- frontend/src/services/api/tasks.hooks.ts
+++ frontend/src/services/api/tasks.hooks.ts
@@ -1,6 +1,5 @@
-import { Dispatch, SetStateAction } from 'react'
 import { QueryFunctionContext, useQuery } from 'react-query'
-import { NavigateFunction, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import produce, { castImmutable } from 'immer'
 import { DateTime } from 'luxon'
 import { DONE_SECTION_ID, TASK_MARK_AS_DONE_TIMEOUT, TRASH_SECTION_ID } from '../../constants'
@@ -8,6 +7,7 @@ import useOverviewContext from '../../context/OverviewContextProvider'
 import useQueryContext from '../../context/QueryContext'
 import { useGTLocalStorage } from '../../hooks'
 import apiClient from '../../utils/api'
+import navigateToNextItemAfterOverviewCommpletion from '../../utils/navigateToNextItemAfterOverviewCommpletion'
 import {
     TExternalStatus,
     TOverviewItem,
@@ -417,45 +417,6 @@ const modifyTask = async (data: TModifyTaskData) => {
         return castImmutable(res.data)
     } catch {
         throw new Error('modifyTask failed')
-    }
-}
-
-const navigateToNextItemAfterOverviewCommpletion = (
-    oldLists: TOverviewView[],
-    newLists: TOverviewView[],
-    deletedTaskId: string,
-    navigate: NavigateFunction,
-    setOpenListIds: Dispatch<SetStateAction<string[]>>
-) => {
-    const location = window.location.pathname.split('/')
-
-    // Get list with deleted task
-    const listWithDeletedTask = oldLists.find((list) => {
-        return list.view_items.find((item) => item.id === deletedTaskId)
-    })
-
-    // If list has more than one task, navigate to next task
-    if (listWithDeletedTask && listWithDeletedTask.view_items.length !== 1) {
-        if (location[3] !== deletedTaskId) return
-        const taskIndex = listWithDeletedTask.view_items.findIndex((item) => item.id === deletedTaskId)
-        const nextTask = listWithDeletedTask.view_items[taskIndex + 1]
-        if (nextTask) {
-            navigate(`/overview/${listWithDeletedTask.id}/${nextTask.id}`)
-        }
-    } else {
-        // If list has only one task, navigate to first non-empty list
-        const firstNonEmptyList = newLists.find((list) => list.view_items.length > 0)
-        if (firstNonEmptyList) {
-            setOpenListIds((openListIds) => {
-                if (!openListIds.includes(firstNonEmptyList.id)) {
-                    return [...openListIds, firstNonEmptyList.id]
-                }
-                return openListIds
-            })
-            navigate(`/overview/${firstNonEmptyList.id}/${firstNonEmptyList.view_items[0].id}`)
-        } else {
-            navigate(`/overview/`)
-        }
     }
 }
 
