7ee94edc89e71cca2eb974dedc76a3dc6db82058
Scott Mai <scottmai702@gmail.com>
2023-02-07 17:11:39 -0700

    conflicts haha

--- frontend/src/components/overview/OverviewDetails.tsx
+++ frontend/src/components/overview/OverviewDetails.tsx
@@ -1,9 +1,4 @@
 import { useParams } from 'react-router-dom'
-import { icons } from '../../styles/images'
-import { TPullRequest, TTask } from '../../utils/types'
-import EmptyDetails from '../details/EmptyDetails'
-import PullRequestDetails from '../details/PullRequestDetails'
-import TaskDetails from '../details/TaskDetails'
 import useOverviewLists from './useOverviewLists'
 
 const OverviewDetails = () => {
@@ -12,15 +7,7 @@ const OverviewDetails = () => {
     const selectedList = lists?.find((list) => list.id === overviewViewId)
     const selectedItem = selectedList?.view_items.find((item) => item.id === overviewItemId)
 
-    if (isLoading) return null
-    else if (!selectedList || !selectedItem) {
-        return null
-    } else if (!lists?.length) return <EmptyDetails icon={icons.list} text="You have no views" />
-    else if (selectedList.type === 'github') return <PullRequestDetails pullRequest={selectedItem as TPullRequest} />
-    else {
-        const subtask = selectedItem?.sub_tasks?.find((subtask) => subtask.id === subtaskId)
-        return <TaskDetails task={selectedItem as TTask} subtask={subtask} />
-    }
+    return <div>hello</div>
 }
 
 export default OverviewDetails
