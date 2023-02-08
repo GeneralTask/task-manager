3194797ae2486a8679c75635735eae882ae5cac2
Nolan Jimenez <nolan1299@gmail.com>
2023-02-08 15:22:36 -0700

    Replace link icon in atlassian editor

--- frontend/src/components/atoms/GTTextField/AtlassianEditor/Editor.tsx
+++ frontend/src/components/atoms/GTTextField/AtlassianEditor/Editor.tsx
@@ -58,6 +58,7 @@ const Editor = ({
     editorActions,
 }: EditorProps) => {
     useReplaceEditorButtonIcon(icons.trash, 'button[aria-label="Remove"]')
+    useReplaceEditorButtonIcon(icons.link, 'span[aria-label="Open link in a new tab"]')
 
     const handleKeyDown: React.KeyboardEventHandler<HTMLDivElement> = (e) => {
         if (e.key === 'Escape' || (enterBehavior === 'blur' && e.key === 'Enter')) {
