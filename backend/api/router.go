18f0bb2c185664b13c6744a3fac9de28709e1edc
yohanstra <jreinstra@gmail.com>
2023-02-09 15:00:12 -0700

    Add fixed title and shorten url for notes preview

--- backend/api/router.go
+++ backend/api/router.go
@@ -57,7 +57,7 @@ func GetRouter(handlers *API) *gin.Engine {
 	// The note detail endpoint is specifically unauthenticated for shareable notes
 	// only notes with is_shared=true can be shared
 	router.GET("/notes/detail/:note_id/", handlers.NoteDetails)
-	router.GET("/notes/preview/:note_id/", handlers.NotePreview)
+	router.GET("/note/", handlers.NotePreview)
 
 	// Unauthenticated endpoints only for dev environment
 	router.POST("/create_test_user/", handlers.CreateTestUser)
