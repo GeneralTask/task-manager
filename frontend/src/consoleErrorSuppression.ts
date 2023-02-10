d3313b18eeef290ed9231192fe671548d3562012
Nolan Jimenez <nolan1299@gmail.com>
2023-02-09 18:12:09 -0700

    Suppress error messages for first-child warning (#3007)

---
+++ frontend/src/consoleErrorSuppression.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/**
+ * This file is used to suppress console.error messages that are not relevant to the application.
+ * This is a temporary solution until the issue is resolved in the library.
+ */
+
+const messagesToSuppress = [
+    'The pseudo class ":first-child" is potentially unsafe when doing server-side rendering. Try changing it to ":first-of-type".',
+] as const
+
+const backupConsoleError = console.error.bind(console)
+console.error = (message?: any, ...optionalParams: any[]) => {
+    if (messagesToSuppress.includes(message)) {
+        return
+    } else {
+        backupConsoleError(message, ...optionalParams)
+    }
+}
