b31938bf6c428abccac44673454fcbd04e06d7d9
Nolan Jimenez <nolan1299@gmail.com>
2023-02-09 17:54:57 -0700

    pls

--- frontend/src/consoleErrorSuppression.ts
+++ frontend/src/consoleErrorSuppression.ts
@@ -1,4 +1,5 @@
-//eslint-disable no-console @typescript-eslint/no-explicit-any
+//eslint-disable no-console
+//eslint-disable @typescript-eslint/no-explicit-any
 /**
  * This file is used to suppress console.error messages that are not relevant to the application.
  * This is a temporary solution until the issue is resolved in the library.
