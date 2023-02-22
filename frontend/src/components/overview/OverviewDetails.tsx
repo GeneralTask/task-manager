99b293f142b97dc06acafe9bbfdd0ae5befcc1a2
Nolan Jimenez <nolan1299@gmail.com>
2023-02-22 18:29:30 -0500

    Better solution

--- frontend/src/components/overview/OverviewDetails.tsx
+++ frontend/src/components/overview/OverviewDetails.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom'
+import { useGetPullRequests } from '../../services/api/pull-request.hooks'
 import { useGetTasksV4 } from '../../services/api/tasksv4.hooks'
 import { icons } from '../../styles/images'
-import { TPullRequest } from '../../utils/types'
 import EmptyDetails from '../details/EmptyDetails'
 import PullRequestDetails from '../details/PullRequestDetails'
 import TaskDetails from '../details/TaskDetails'
@@ -11,21 +11,26 @@ const OverviewDetails = () => {
     const { lists, isLoading, flattenedLists } = useOverviewLists()
     const { overviewViewId, overviewItemId, subtaskId } = useParams()
     const selectedList = lists?.find((list) => list.id === overviewViewId)
-    const selectedItem = selectedList?.view_items.find((item) => item.id === overviewItemId)
-    const { data: allTasks } = useGetTasksV4()
+    const { data: repositories, isLoading: isGetPullRequestLoading } = useGetPullRequests()
+    const { data: allTasks, isLoading: isGetAllTasksLoading } = useGetTasksV4()
 
-    if (isLoading) return null
+    if (isLoading || isGetPullRequestLoading || isGetAllTasksLoading) return null
     else if (lists.length > 0 && flattenedLists.length === 0)
         return <EmptyDetails icon={icons.check} text="Your lists are all empty" />
     else if (lists.length === 0) return <EmptyDetails icon={icons.list} text="You have no lists" />
-    else if (!selectedList || !selectedItem) {
+    else if (!selectedList) {
         return null
-    } else if (selectedList.type === 'github') return <PullRequestDetails pullRequest={selectedItem as TPullRequest} />
-    else {
+    } else if (selectedList.type === 'github') {
+        const selectedPullRequest = repositories
+            ?.flatMap((repo) => repo.pull_requests)
+            .find((item) => item.id === overviewItemId)
+        if (!selectedPullRequest) return null
+        return <PullRequestDetails pullRequest={selectedPullRequest} />
+    } else {
         const selectedTaskId = subtaskId || overviewItemId
-        const task = allTasks?.find((task) => task.id === selectedTaskId)
-        if (!task) return null
-        return <TaskDetails task={task} />
+        const selectedTask = allTasks?.find((task) => task.id === selectedTaskId)
+        if (!selectedTask) return null
+        return <TaskDetails task={selectedTask} />
     }
 }
 
