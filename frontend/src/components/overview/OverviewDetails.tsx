49954162837952a0e3281dd3ab9b768a4beb892c
Nolan Jimenez <nolan1299@gmail.com>
2023-02-22 18:16:36 -0500

    Switch to using v4 for task section view item details

--- frontend/src/components/overview/OverviewDetails.tsx
+++ frontend/src/components/overview/OverviewDetails.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
+import { useGetTasksV4 } from '../../services/api/tasksv4.hooks'
 import { icons } from '../../styles/images'
-import { TPullRequest, TTaskV4 } from '../../utils/types'
+import { TPullRequest } from '../../utils/types'
 import EmptyDetails from '../details/EmptyDetails'
 import PullRequestDetails from '../details/PullRequestDetails'
 import TaskDetails from '../details/TaskDetails'
@@ -11,6 +12,7 @@ const OverviewDetails = () => {
     const { overviewViewId, overviewItemId, subtaskId } = useParams()
     const selectedList = lists?.find((list) => list.id === overviewViewId)
     const selectedItem = selectedList?.view_items.find((item) => item.id === overviewItemId)
+    const { data: allTasks } = useGetTasksV4()
 
     if (isLoading) return null
     else if (lists.length > 0 && flattenedLists.length === 0)
@@ -20,8 +22,10 @@ const OverviewDetails = () => {
         return null
     } else if (selectedList.type === 'github') return <PullRequestDetails pullRequest={selectedItem as TPullRequest} />
     else {
-        const subtask = selectedItem?.sub_tasks?.find((subtask) => subtask.id === subtaskId)
-        return <TaskDetails task={(subtask || selectedItem) as TTaskV4} />
+        const selectedTaskId = subtaskId || overviewItemId
+        const task = allTasks?.find((task) => task.id === selectedTaskId)
+        if (!task) return null
+        return <TaskDetails task={task} />
     }
 }
 
