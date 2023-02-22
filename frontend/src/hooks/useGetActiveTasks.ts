556e71e1186efe21979b5cbd2500ff0e232b398d
Nolan Jimenez <nolan1299@gmail.com>
2023-02-22 10:36:23 -0500

    Fix max depth bug on linear page

--- frontend/src/hooks/useGetActiveTasks.ts
+++ frontend/src/hooks/useGetActiveTasks.ts
@@ -1,10 +1,14 @@
+import { useMemo } from 'react'
 import { useGetTasksV4 } from '../services/api/tasksv4.hooks'
 
 const useGetActiveTasks = (isEnabled = true) => {
     const { data, ...rest } = useGetTasksV4(isEnabled)
+    const activeTasks = useMemo(() => {
+        return data?.filter((task) => !task.is_done && !task.is_deleted)
+    }, [data])
 
     return {
-        data: data?.filter((task) => !task.is_done && !task.is_deleted),
+        data: activeTasks,
         ...rest,
     }
 }
