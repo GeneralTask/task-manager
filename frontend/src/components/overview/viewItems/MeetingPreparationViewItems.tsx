49b559e10f00f94202a301d2d99d205fbad312d2
Nolan Jimenez <nolan1299@gmail.com>
2023-02-23 15:42:35 -0500

    Switch to using new meeting prep endpoint (#3127)

--- frontend/src/components/overview/viewItems/MeetingPreparationViewItems.tsx
+++ frontend/src/components/overview/viewItems/MeetingPreparationViewItems.tsx
@@ -1,8 +1,9 @@
 import { Ref, forwardRef } from 'react'
 import { useParams } from 'react-router-dom'
+import { useGetMeetingPreparationTasks } from '../../../services/api/meeting-preparation-tasks.hooks'
 import { useGetLinkedAccounts } from '../../../services/api/settings.hooks'
-import { TTaskV4 } from '../../../utils/types'
 import { isGoogleCalendarLinked } from '../../../utils/utils'
+import Skeleton from '../../atoms/Skeleton'
 import ConnectIntegration from '../../molecules/ConnectIntegration'
 import Task from '../../molecules/Task'
 import { ViewHeader, ViewName } from '../styles'
@@ -13,6 +14,7 @@ const MeetingPreparationViewItems = forwardRef(({ view, hideHeader }: ViewItemsP
     const { overviewViewId, overviewItemId } = useParams()
     const { data: linkedAccounts } = useGetLinkedAccounts()
     const isGoogleLinked = isGoogleCalendarLinked(linkedAccounts || [])
+    const { data: meetingTasks, isLoading: isMeetingTasksLoading } = useGetMeetingPreparationTasks()
 
     return (
         <>
@@ -21,23 +23,29 @@ const MeetingPreparationViewItems = forwardRef(({ view, hideHeader }: ViewItemsP
                     <ViewName>{view.name}</ViewName>
                 </ViewHeader>
             )}
-            {isGoogleLinked ? (
-                view.view_items.length > 0 ? (
-                    view.view_items.map((item, index) => (
-                        <Task
-                            key={item.id}
-                            task={item as TTaskV4}
-                            dragDisabled={true}
-                            index={index}
-                            isSelected={overviewViewId === view.id && overviewItemId === item.id}
-                            link={`/overview/${view.id}/${item.id}`}
-                        />
-                    ))
-                ) : (
-                    <EmptyListMessage list={view} />
-                )
+            {isMeetingTasksLoading ? (
+                <Skeleton />
             ) : (
-                <ConnectIntegration type="google_calendar" />
+                <>
+                    {isGoogleLinked ? (
+                        !isMeetingTasksLoading && meetingTasks && meetingTasks?.length > 0 ? (
+                            meetingTasks?.map((item, index) => (
+                                <Task
+                                    key={item.id}
+                                    task={item}
+                                    dragDisabled={true}
+                                    index={index}
+                                    isSelected={overviewViewId === view.id && overviewItemId === item.id}
+                                    link={`/overview/${view.id}/${item.id}`}
+                                />
+                            ))
+                        ) : (
+                            <EmptyListMessage list={view} />
+                        )
+                    ) : (
+                        <ConnectIntegration type="google_calendar" />
+                    )}
+                </>
             )}
         </>
     )
