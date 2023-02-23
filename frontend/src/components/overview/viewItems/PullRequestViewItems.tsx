43860f367d1738d69fde3d93fc375c2d9e936841
Nolan Jimenez <nolan1299@gmail.com>
2023-02-22 19:06:23 -0500

    Switch to using v4 for pull request view items

--- frontend/src/components/overview/viewItems/PullRequestViewItems.tsx
+++ frontend/src/components/overview/viewItems/PullRequestViewItems.tsx
@@ -1,7 +1,9 @@
-import { Ref, forwardRef } from 'react'
+import { Ref, forwardRef, useMemo } from 'react'
 import { useParams } from 'react-router-dom'
+import { useGetPullRequests } from '../../../services/api/pull-request.hooks'
 import SortAndFilterSelectors from '../../../utils/sortAndFilter/SortAndFilterSelectors'
 import { PR_SORT_AND_FILTER_CONFIG } from '../../../utils/sortAndFilter/pull-requests.config'
+import sortAndFilterItems from '../../../utils/sortAndFilter/sortAndFilterItems'
 import useSortAndFilterSettings from '../../../utils/sortAndFilter/useSortAndFilterSettings'
 import { TPullRequest } from '../../../utils/types'
 import PullRequest from '../../pull-requests/PullRequest'
@@ -14,6 +16,21 @@ const PullRequestViewItems = forwardRef(
     ({ view, visibleItemsCount, hideHeader }: ViewItemsProps, ref: Ref<HTMLDivElement>) => {
         const { overviewItemId } = useParams()
         const sortAndFilterSettings = useSortAndFilterSettings<TPullRequest>(PR_SORT_AND_FILTER_CONFIG, view.id)
+        const { data: repositories } = useGetPullRequests()
+        const sortedViewItems = useMemo(() => {
+            const viewItems =
+                repositories
+                    ?.flatMap((repo) => repo.pull_requests)
+                    ?.filter((pr) => view.view_item_ids.includes(pr.id)) ?? []
+            return sortAndFilterItems<TPullRequest>({
+                items: viewItems,
+                sort: sortAndFilterSettings.selectedSort,
+                sortDirection: sortAndFilterSettings.selectedSortDirection,
+                tieBreakerField: PR_SORT_AND_FILTER_CONFIG.tieBreakerField,
+            })
+        }, [repositories, view.view_item_ids, sortAndFilterSettings])
+
+        repositories?.flatMap((repo) => repo.pull_requests)?.filter((pr) => view.view_item_ids.includes(pr.id))
 
         return (
             <>
@@ -23,9 +40,9 @@ const PullRequestViewItems = forwardRef(
                     </ViewHeader>
                 )}
                 {view.total_view_items !== 0 && <SortAndFilterSelectors settings={sortAndFilterSettings} />}
-                {view.view_items.length === 0 && view.is_linked && <EmptyListMessage list={view} />}
+                {view.view_item_ids.length === 0 && view.is_linked && <EmptyListMessage list={view} />}
                 <Repository>
-                    {view.view_items.slice(0, visibleItemsCount).map((pr) => (
+                    {sortedViewItems?.slice(0, visibleItemsCount).map((pr) => (
                         <PullRequest
                             key={pr.id}
                             pullRequest={pr}
