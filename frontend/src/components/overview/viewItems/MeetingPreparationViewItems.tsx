d6ac2781a470f81dd889bf20a82f46b3b6090813
Nolan Jimenez <nolan1299@gmail.com>
2023-02-28 17:23:48 -0500

    Memoize

--- frontend/src/components/overview/viewItems/MeetingPreparationViewItems.tsx
+++ frontend/src/components/overview/viewItems/MeetingPreparationViewItems.tsx
@@ -1,4 +1,4 @@
-import { Ref, forwardRef } from 'react'
+import { Ref, forwardRef, useMemo } from 'react'
 import { useParams } from 'react-router-dom'
 import { DONE_FOLDER_ID, TRASH_FOLDER_ID } from '../../../constants'
 import { useGetMeetingPreparationTasks } from '../../../services/api/meeting-preparation-tasks.hooks'
@@ -16,8 +16,9 @@ const MeetingPreparationViewItems = forwardRef(({ view, hideHeader }: ViewItemsP
     const { data: linkedAccounts } = useGetLinkedAccounts()
     const isGoogleLinked = isGoogleCalendarLinked(linkedAccounts || [])
     const { data: meetingTasks, isLoading: isMeetingTasksLoading } = useGetMeetingPreparationTasks()
-    const activeMeetingTasks = meetingTasks?.filter(
-        (task) => task.id_folder !== DONE_FOLDER_ID && task.id_folder !== TRASH_FOLDER_ID
+    const activeMeetingTasks = useMemo(
+        () => meetingTasks?.filter((task) => task.id_folder !== DONE_FOLDER_ID && task.id_folder !== TRASH_FOLDER_ID),
+        [meetingTasks]
     )
 
     return (
