2fafa12949ce174f9b0e163feeda878c95e4c0d6
alfaindia <97246822+alfaindia@users.noreply.github.com>
2023-03-02 16:52:10 -0600

    Load calendar colors from gcal api (#3161)
    * Load calendar colors from gcal api
    
    * update
    
    * use calendar.event
    
    * Use user-specific colors instead of hard-coded color map (#3192)
    
    * colors
    
    * event
    
    * event-color
    
    * Revert "Use user-specific colors instead of hard-coded color map (#3192)" (#3201)
    
    This reverts commit 439f26776faa22de3fef64b4ed82eee0469c4917.
    
    * resolve merge conflicts
    
    ---------
    
    Co-authored-by: Scott Mai <scottmai702@gmail.com>

--- backend/database/models.go
+++ backend/database/models.go
@@ -190,10 +190,12 @@ type PullRequestComment struct {
 }
 
 type Calendar struct {
-	AccessRole string `bson:"access_role,omitempty"`
-	CalendarID string `bson:"calendar_id,omitempty"`
-	ColorID    string `bson:"color_id,omitempty"`
-	Title      string `bson:"title,omitempty"`
+	AccessRole      string `bson:"access_role,omitempty"`
+	CalendarID      string `bson:"calendar_id,omitempty"`
+	ColorID         string `bson:"color_id,omitempty"`
+	Title           string `bson:"title,omitempty"`
+	ColorBackground string `bson:"color_background,omitempty"`
+	ColorForeground string `bson:"color_foreground,omitempty"`
 }
 
 type CalendarAccount struct {
@@ -230,6 +232,8 @@ type CalendarEvent struct {
 	LinkedViewID        primitive.ObjectID `bson:"linked_view_id,omitempty"`
 	LinkedPullRequestID primitive.ObjectID `bson:"linked_pull_request_id,omitempty"`
 	LinkedSourceID      string             `bson:"linked_task_source_id,omitempty"`
+	ColorBackground     string             `bson:"color_background,omitempty"`
+	ColorForeground     string             `bson:"color_foreground,omitempty"`
 }
 
 type MeetingPreparationParams struct {
