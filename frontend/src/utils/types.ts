0267f4afd569ec70984a30d0e83c71633406905d
Scott Mai <scottmai702@gmail.com>
2023-03-02 14:46:11 -0800

    Use user-specific colors instead of hard-coded color map (again) (#3203)
    * colors
    
    * event
    
    * event_color

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -156,6 +156,8 @@ export interface TEvent {
     account_id: string
     calendar_id: string
     color_id: string
+    color_background?: string
+    color_foreground?: string
     logo: TLogoImage
     deeplink: string
     datetime_start: string
@@ -414,6 +416,8 @@ export interface TCalendar {
     title: string
     can_write: boolean
     access_role: string
+    color_background: string
+    color_foreground: string
 }
 
 export interface TCalendarAccount {
