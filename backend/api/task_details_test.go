a222d36e1abfb6aed84461e300383270c01c0a2a
Nolan Jimenez <nolan1299@gmail.com>
2023-03-09 10:55:07 -0500

    Cleanup

--- backend/api/task_details_test.go
+++ backend/api/task_details_test.go
@@ -8,17 +8,16 @@ import (
 	"testing"
 
 	"github.com/GeneralTask/task-manager/backend/database"
-	"github.com/GeneralTask/task-manager/backend/testutils"
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func TestTaskDetails(t *testing.T) {
 	authToken := login("test_task_details@generaltask.com", "")
-	title := "title"
-
 	db, dbCleanup, err := database.GetDBConnection()
 	assert.NoError(t, err)
+	defer dbCleanup()
+	api, dbCleanup := GetAPIWithDBCleanup()
 	defer dbCleanup()
 
 	taskCollection := database.GetTaskCollection(db)
@@ -27,14 +26,12 @@ func TestTaskDetails(t *testing.T) {
 	publicSharedAccess := database.SharedAccessPublic
 	domainSharedAccess := database.SharedAccessDomain
 
-	// futureTime := primitive.NewDateTimeFromTime(time.Now().Add(1 * time.Hour))
+	futureTime := primitive.NewDateTimeFromTime(time.Now().Add(1 * time.Hour))
 	expiredTime := primitive.NewDateTimeFromTime(time.Now().Add(-1 * time.Hour))
 
 	// Create a task that is not shared
 	mongoResult, err := taskCollection.InsertOne(context.Background(), &database.Task{
-		ID:     primitive.NewObjectID(),
 		UserID: userID,
-		Title:  &title,
 	})
 	assert.NoError(t, err)
 	notSharedTaskID := mongoResult.InsertedID.(primitive.ObjectID).Hex()
@@ -42,42 +39,31 @@ func TestTaskDetails(t *testing.T) {
 	// Create a task that is shared with everyone
 	mongoResult, err = taskCollection.InsertOne(context.Background(), &database.Task{
 		UserID:       userID,
-		Title:        &title,
-		SharedUntil:  *testutils.CreateDateTime("9999-01-01"),
+		SharedUntil:  futureTime,
 		SharedAccess: &publicSharedAccess,
 	})
 	assert.NoError(t, err)
 	publicSharedTaskID := mongoResult.InsertedID.(primitive.ObjectID).Hex()
 
 	// Create a task that is shared with everyone with same domain
-	domainSharedTask, err := database.GetOrCreateTask(
-		db,
-		userID,
-		"123abcdefg",
-		"foobar_source",
-		&database.Task{
-			UserID:       userID,
-			Title:        &title,
-			SharedUntil:  *testutils.CreateDateTime("9999-01-01"),
-			SharedAccess: &domainSharedAccess,
-		},
-	)
+	mongoResult, err = taskCollection.InsertOne(context.Background(), &database.Task{
+		UserID:       userID,
+		SharedUntil:  futureTime,
+		SharedAccess: &domainSharedAccess,
+	})
 	assert.NoError(t, err)
-	domainSharedTaskID := domainSharedTask.ID.Hex()
+	domainSharedTaskID := mongoResult.InsertedID.(primitive.ObjectID).Hex()
 
 	// Create task with expired sharedUntil
 	mongoResult, err = taskCollection.InsertOne(context.Background(), &database.Task{
 		UserID:       userID,
-		Title:        &title,
 		SharedUntil:  expiredTime,
 		SharedAccess: &publicSharedAccess,
 	})
 	assert.NoError(t, err)
 	expiredTaskID := mongoResult.InsertedID.(primitive.ObjectID).Hex()
 
-	api, dbCleanup := GetAPIWithDBCleanup()
-	defer dbCleanup()
-
+	UnauthorizedTest(t, "GET", fmt.Sprintf("/tasks/detail/%s/", publicSharedTaskID), nil)
 	t.Run("InvalidTaskID", func(t *testing.T) {
 		ServeRequest(t, authToken, "GET", fmt.Sprintf("/tasks/detail/%s/", primitive.NewObjectID()), nil, 404, api)
 	})
