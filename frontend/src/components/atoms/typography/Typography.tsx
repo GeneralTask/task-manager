4c8128b03c145062eeea9b7406b8c02a7fb1034e
Jack Hamilton <john.hamilton@wustl.edu>
2023-03-09 15:05:11 -0500

    more

--- frontend/src/components/atoms/typography/Typography.tsx
+++ frontend/src/components/atoms/typography/Typography.tsx
@@ -40,11 +40,73 @@ export const DeprecatedMini = styled.span<{ color?: TTextColor }>`
     ${SharedStyles};
     ${Typography.deprecated_mini};
 `
+export const DeprecatedBold = styled.span`
+    ${Typography.deprecated_bold};
+`
+
+export const DisplayLarge = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.display.large};
+`
+export const DisplayMedium = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.display.medium};
+`
+export const DisplaySmall = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.display.small};
+`
+export const HeadlineLarge = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.headline.large};
+`
+export const HeadlineMedium = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.headline.medium};
+`
+export const HeadlineSmall = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.headline.small};
+`
+export const TitleLarge = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.title.large};
+`
+export const TitleMedium = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.title.medium};
+`
+export const TitleSmall = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.title.small};
+`
+export const BodyLarge = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.body.large};
+`
+export const BodyMedium = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.body.medium};
+`
+export const BodySmall = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.body.small};
+`
+export const LabelLarge = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.label.large};
+`
+export const LabelMedium = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.label.medium};
+`
+export const LabelSmall = styled.span<{ color?: TTextColor }>`
+    ${SharedStyles};
+    ${Typography.label.small};
+`
+
 export const Truncated = styled.span`
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-`
-export const DeprecatedBold = styled.span`
-    ${Typography.deprecated_bold};
 `
