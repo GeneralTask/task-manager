383ab5d308700a8b7668052a9e2b638546cb31a2
Nolan Jimenez <nolan1299@gmail.com>
2023-03-13 17:42:05 -0400

    Cleanup

--- frontend/src/components/views/SharedTaskView.tsx
+++ frontend/src/components/views/SharedTaskView.tsx
@@ -1,18 +1,13 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 import { usePreviewMode } from '../../hooks'
 import Spinner from '../atoms/Spinner'
 
 const SharedTask = () => {
     const { isPreviewMode, isLoading } = usePreviewMode()
-    const navigate = useNavigate()
 
-    useEffect(() => {
-        if (!isLoading && !isPreviewMode) {
-            navigate('/overview', { replace: true })
-        }
-    }, [isPreviewMode, isLoading])
-
+    if (!isPreviewMode && !isLoading) {
+        return <Navigate to="/" replace />
+    }
     if (isLoading) {
         return <Spinner />
     }
