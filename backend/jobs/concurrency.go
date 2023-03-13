6c12abc33e90e94a1702586e5347567b16ed001e
yohanstra <jreinstra@gmail.com>
2023-03-13 11:25:02 -0400

    update

--- backend/jobs/concurrency.go
+++ backend/jobs/concurrency.go
@@ -2,11 +2,11 @@ package jobs
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	"github.com/GeneralTask/task-manager/backend/database"
 	lock "github.com/square/mongo-lock"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func EnsureJobOnlyRunsOnceToday(jobName string) error {
@@ -23,7 +23,6 @@ func EnsureJobOnlyRunsOnceToday(jobName string) error {
 	}
 
 	resourceName := jobName + "_" + time.Now().Format("01-02-2006")
-	fmt.Println("resource name:", resourceName)
 	// leave resource locked forever so all future job attempts on this day will fail (err returned if can't instantly get lock)
-	return lockClient.XLock(context.Background(), resourceName /*primitive.NewObjectID().Hex()*/, "", lock.LockDetails{})
+	return lockClient.XLock(context.Background(), resourceName, primitive.NewObjectID().Hex(), lock.LockDetails{})
 }
