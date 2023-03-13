7b21ab4d70e9f21f49977ff4454be0bfef20e6f4
Jack Hamilton <john.hamilton@wustl.edu>
2023-03-13 17:31:48 -0400

    add border

--- frontend/src/components/templates/ScrollableListTemplate.ts
+++ frontend/src/components/templates/ScrollableListTemplate.ts
@@ -2,11 +2,12 @@ import styled from 'styled-components'
 import { Colors, Spacing } from '../../styles'
 import { DEFAULT_VIEW_WIDTH } from '../../styles/dimensions'
 
-const ScrollableListTemplate = styled.div<{ noTopPadding?: boolean; width?: string }>`
-    padding: ${({ noTopPadding }) => (noTopPadding ? '0px' : Spacing._32)} ${Spacing._16} 100px;
+const ScrollableListTemplate = styled.div<{ width?: string }>`
+    padding: ${Spacing._32} ${Spacing._16} 100px;
     overflow-y: auto;
     width: ${({ width }) => width ?? DEFAULT_VIEW_WIDTH};
     background-color: ${Colors.background.base};
+    border-right: 1px solid ${Colors.background.border};
 `
 
 export default ScrollableListTemplate
