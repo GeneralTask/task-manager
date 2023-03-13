9e6499beee2698600e662fe9a66b9428243c9812
Jack Hamilton <john.hamilton@wustl.edu>
2023-03-13 17:40:46 -0400

    super dash fix

--- frontend/src/components/templates/ScrollableListTemplate.ts
+++ frontend/src/components/templates/ScrollableListTemplate.ts
@@ -2,10 +2,10 @@ import styled from 'styled-components'
 import { Colors, Spacing } from '../../styles'
 import { DEFAULT_VIEW_WIDTH } from '../../styles/dimensions'
 
-const ScrollableListTemplate = styled.div<{ width?: string }>`
+const ScrollableListTemplate = styled.div`
     padding: ${Spacing._32} ${Spacing._16} 100px;
     overflow-y: auto;
-    width: ${({ width }) => width ?? DEFAULT_VIEW_WIDTH};
+    width: ${DEFAULT_VIEW_WIDTH};
     background-color: ${Colors.background.base};
     border-right: 1px solid ${Colors.background.border};
 `
