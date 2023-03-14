81a7b7dac2e7bb2bea32e363867e5b6551040ae4
Scott Mai <scottmai702@gmail.com>
2023-03-14 09:03:34 -0700

    Display averages on super dashboard graph (#3304)
    * smooth
    
    * constants
    
    * circle
    
    * avg

--- frontend/src/components/superDashboard/constants.ts
+++ frontend/src/components/superDashboard/constants.ts
@@ -4,3 +4,4 @@ export const GRAPH_TOP_MARGIN = 10 // needed so top point doesn't get cut off
 export const GRAPH_RIGHT_MARGIN = 30 // needed so last label doesn't get cut off
 export const LINE_STROKE_WIDTH = 2 // px
 export const LINE_ANIMATION_DURATION = 500 // ms
+export const STROKE_DASH_ARRAY = '4' // px
