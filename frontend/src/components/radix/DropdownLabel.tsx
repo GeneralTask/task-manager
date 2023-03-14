53626a39e95585f7d5d3f5b5ecc281e7c43b8a83
Nolan Jimenez <nolan1299@gmail.com>
2023-03-14 10:41:50 -0400

    Switch sharing dropdown options to match design (#3306)

---
+++ frontend/src/components/radix/DropdownLabel.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+import { DeprecatedLabel } from '../atoms/typography/Typography'
+import { MENU_WIDTH } from './RadixUIConstants'
+
+export const LabelWrap = styled(DeprecatedLabel)`
+    width: ${MENU_WIDTH};
+`
+
+export default LabelWrap
