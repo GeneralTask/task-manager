6eff823852715601b2620e2e8f1bb3c707f26ed0
Scott Mai <scottmai702@gmail.com>
2023-03-17 08:14:46 -0700

    List dashboard team members endpoint (#3360)
    * nice
    
    * nice
    
    * save
    
    * save
    
    * new spec
    
    * add intervals and subjects to graph API
    
    * progress
    
    * oof
    
    * create-woo
    
    * compress into one commit for easy rebase
    
    * address comments
    
    * unauthorized
    
    * test
    
    * list
    
    * cringe department
    
    * merge
    
    ---------
    
    Co-authored-by: yohanstra <jreinstra@gmail.com>

--- backend/api/dashboard_team_members_test.go
+++ backend/api/dashboard_team_members_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/GeneralTask/task-manager/backend/database"
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type DashboardTeamMemberCreateResponse struct {
@@ -122,9 +123,103 @@ func TestDashboardTeamMemberCreate(t *testing.T) {
 	})
 }
 
+func TestDashboardTeamMemberList(t *testing.T) {
+	authToken := login("test_dashboard_team_members_list@generaltask.com", "")
+	UnauthorizedTest(t, "GET", "/dashboard/team_members/", nil)
+	api, dbCleanup := GetAPIWithDBCleanup()
+	defer dbCleanup()
+	userID := getUserIDFromAuthToken(t, api.DB, authToken)
+	teamMemberCollection := database.GetDashboardTeamMemberCollection(api.DB)
+
+	t.Run("NoBusinessAccess", func(t *testing.T) {
+		ServeRequest(t, authToken, "GET", "/dashboard/team_members/", nil, http.StatusForbidden, api)
+	})
+	_, err := database.GetUserCollection(api.DB).UpdateOne(context.Background(), bson.M{"_id": userID}, bson.M{"$set": bson.M{"business_mode_enabled": true}})
+	assert.NoError(t, err)
+	t.Run("SuccessNoResults", func(t *testing.T) {
+		teamMemberCollection.DeleteMany(context.Background(), bson.M{})
+		response := ServeRequest(t, authToken, "GET", "/dashboard/team_members/", nil, http.StatusOK, api)
+		var result []DashboardTeamMemberResult
+		err = json.Unmarshal(response, &result)
+		assert.NoError(t, err)
+		assert.Equal(t, 0, len(result))
+	})
+	t.Run("Success", func(t *testing.T) {
+		dashboardTeam, err := database.GetOrCreateDashboardTeam(api.DB, userID)
+		assert.NoError(t, err)
+
+		teamMember1 := database.DashboardTeamMember{
+			TeamID:   dashboardTeam.ID,
+			Name:     "scott",
+			Email:    "scott@gt.com",
+			GithubID: "scottmai",
+		}
+		teamMember2 := database.DashboardTeamMember{
+			TeamID: dashboardTeam.ID,
+			Name:   "john",
+			Email:  "john@gt.com",
+		}
+		teamMember3 := database.DashboardTeamMember{
+			TeamID:   dashboardTeam.ID,
+			Name:     "nolan",
+			GithubID: "nolancito",
+		}
+		teamMember4 := database.DashboardTeamMember{
+			TeamID: dashboardTeam.ID,
+			Name:   "jack",
+		}
+		differentTeamMember := database.DashboardTeamMember{
+			TeamID: primitive.NewObjectID(),
+			Name:   "snah",
+		}
+		insertResult, err := teamMemberCollection.InsertMany(context.Background(), []interface{}{
+			teamMember1,
+			teamMember2,
+			teamMember3,
+			teamMember4,
+			differentTeamMember,
+		})
+		assert.NoError(t, err)
+
+		response := ServeRequest(t, authToken, "GET", "/dashboard/team_members/", nil, http.StatusOK, api)
+		var result []DashboardTeamMemberResult
+		err = json.Unmarshal(response, &result)
+
+		assert.NoError(t, err)
+		assert.Equal(t, 4, len(result))
+		assertDashboardTeamMemberResultsAreEqual(t, DashboardTeamMemberResult{
+			ID:       insertResult.InsertedIDs[0].(primitive.ObjectID).Hex(),
+			Name:     "scott",
+			Email:    "scott@gt.com",
+			GithubID: "scottmai",
+		}, result[0])
+		assertDashboardTeamMemberResultsAreEqual(t, DashboardTeamMemberResult{
+			ID:    insertResult.InsertedIDs[1].(primitive.ObjectID).Hex(),
+			Name:  "john",
+			Email: "john@gt.com",
+		}, result[1])
+		assertDashboardTeamMemberResultsAreEqual(t, DashboardTeamMemberResult{
+			ID:       insertResult.InsertedIDs[2].(primitive.ObjectID).Hex(),
+			Name:     "nolan",
+			GithubID: "nolancito",
+		}, result[2])
+		assertDashboardTeamMemberResultsAreEqual(t, DashboardTeamMemberResult{
+			ID:   insertResult.InsertedIDs[3].(primitive.ObjectID).Hex(),
+			Name: "jack",
+		}, result[3])
+	})
+}
+
 func assertDashboardTeamMembersAreEqual(t *testing.T, expected, actual database.DashboardTeamMember) {
 	assert.Equal(t, expected.ID, actual.ID)
 	assert.Equal(t, expected.TeamID, actual.TeamID)
+	assert.Equal(t, expected.Name, actual.Name)
+	assert.Equal(t, expected.Email, actual.Email)
+	assert.Equal(t, expected.GithubID, actual.GithubID)
+}
+
+func assertDashboardTeamMemberResultsAreEqual(t *testing.T, expected, actual DashboardTeamMemberResult) {
+	assert.Equal(t, expected.ID, actual.ID)
 	assert.Equal(t, expected.Name, actual.Name)
 	assert.Equal(t, expected.Email, actual.Email)
 	assert.Equal(t, expected.GithubID, actual.GithubID)
