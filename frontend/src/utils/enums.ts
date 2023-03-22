64a4ff84b7e3bc785bf13f3cbc38da000646680b
Scott Mai <scottmai702@gmail.com>
2023-03-22 13:07:31 -0700

    remove old enums

--- frontend/src/utils/enums.ts
+++ frontend/src/utils/enums.ts
@@ -1,29 +1,3 @@
-export enum FetchStatusEnum {
-    LOADING,
-    SUCCESS,
-    ERROR,
-}
-
-export enum AbortID {
-    TASKS,
-    EVENTS,
-    LINKED_ACCOUNTS,
-    SETTINGS,
-}
-
-export enum LogEvents {
-    TASK_CREATED = 'task_created',
-    TASK_EXPANDED = 'task_expanded',
-    TASK_COLLAPSED = 'task_collapased',
-    TASK_REORDERED = 'task_reordered',
-    TASK_MARK_AS_DONE = 'task_mark_as_done',
-    SHOW_TASK_CREATE_FORM = 'show_task_create_form',
-    HIDE_TASK_CREATE_FORM = 'hide_task_create_form',
-    TASK_DEEPLINK_CLICKED = 'deeplink_clicked',
-    // for when the refresh spinner is clicked by the user
-    MANUAL_TASKS_REFRESH_CLICK = 'manual_tasks_refresh_click',
-}
-
 export enum CompanyPolicyPages {
     TermsOfService,
     PrivacyPolicy,
