604f3ddb817096693e98cb2334d8cbc470143288
scottmai <scottmai702@gmail.com>
2023-03-23 01:03:28 -0700

    add-team

---
+++ frontend/src/components/superDashboard/teamRoster/AddTeamMemberForm.tsx
@@ -0,0 +1,58 @@
+import { FormEvent, useState } from 'react'
+import styled from 'styled-components'
+import { v4 as uuidv4 } from 'uuid'
+import { useAddDashboardTeamMember } from '../../../services/api/super-dashboard.hooks'
+import { Spacing } from '../../../styles'
+import { icons } from '../../../styles/images'
+import Flex from '../../atoms/Flex'
+import GTInput from '../../atoms/GTInput'
+import GTButton from '../../atoms/buttons/GTButton'
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: ${Spacing._16};
+`
+
+const AddTeamMemberForm = () => {
+    const [showForm, setShowForm] = useState(false)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [githubId, setGithubId] = useState('')
+
+    const { mutate: addTeamMember } = useAddDashboardTeamMember()
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const id = uuidv4()
+        addTeamMember({ optimisticId: id, id, name, email, github_id: githubId })
+        setName('')
+        setEmail('')
+        setGithubId('')
+        setShowForm(false)
+    }
+
+    if (!showForm) {
+        return (
+            <GTButton
+                styleType="secondary"
+                icon={icons.plus}
+                value="Add team member"
+                onClick={() => setShowForm(true)}
+            />
+        )
+    }
+    return (
+        <Form onSubmit={handleSubmit}>
+            <GTInput value={name} onChange={setName} placeholder="Name*" autoFocus />
+            <GTInput value={email} onChange={setEmail} placeholder="Email" />
+            <GTInput value={githubId} onChange={setGithubId} placeholder="GitHub username" />
+            <Flex gap={Spacing._8}>
+                <GTButton styleType="primary" value="Add team member" disabled={!name} />
+                <GTButton styleType="secondary" value="Cancel" onClick={() => setShowForm(false)} />
+            </Flex>
+        </Form>
+    )
+}
+
+export default AddTeamMemberForm
--- frontend/src/components/superDashboard/teamRoster/TeamRosterModalContent.tsx
+++ frontend/src/components/superDashboard/teamRoster/TeamRosterModalContent.tsx
@@ -4,9 +4,11 @@ import { Spacing } from '../../../styles'
 import { icons } from '../../../styles/images'
 import Flex from '../../atoms/Flex'
 import { Icon } from '../../atoms/Icon'
+import { Divider } from '../../atoms/SectionDivider'
 import Spinner from '../../atoms/Spinner'
 import GTButton from '../../atoms/buttons/GTButton'
 import { BodyMedium, LabelSmall } from '../../atoms/typography/Typography'
+import AddTeamMemberForm from './AddTeamMemberForm'
 
 const TeamMemberList = styled.div`
     display: flex;
@@ -22,32 +24,35 @@ const TeamRosterModalContent = () => {
     if (isLoading) {
         return <Spinner />
     }
-    if (!teamMembers || teamMembers.length === 0) {
-        return <BodyMedium color="muted">No team members</BodyMedium>
-    }
     return (
-        <>
+        <Flex column gap={Spacing._16}>
             <TeamMemberList>
-                {teamMembers.map((teamMember) => (
-                    <Flex key={teamMember.id} justifyContent="space-between" alignItems="center">
-                        <Flex alignItems="center" gap={Spacing._24}>
-                            <Icon icon={icons.user} />
-                            <Flex column>
-                                <BodyMedium>{teamMember.name}</BodyMedium>
-                                {teamMember.email && <LabelSmall>{teamMember.email}</LabelSmall>}
-                                {teamMember.github_id && <LabelSmall>{teamMember.github_id}</LabelSmall>}
+                {!teamMembers || teamMembers.length === 0 ? (
+                    <BodyMedium color="muted">No team members</BodyMedium>
+                ) : (
+                    teamMembers.map((teamMember) => (
+                        <Flex key={teamMember.id} justifyContent="space-between" alignItems="center">
+                            <Flex alignItems="center" gap={Spacing._24}>
+                                <Icon icon={icons.user} />
+                                <Flex column>
+                                    <BodyMedium>{teamMember.name}</BodyMedium>
+                                    {teamMember.email && <LabelSmall>{teamMember.email}</LabelSmall>}
+                                    {teamMember.github_id && <LabelSmall>{teamMember.github_id}</LabelSmall>}
+                                </Flex>
                             </Flex>
+                            <GTButton
+                                styleType="icon"
+                                icon={icons.trash}
+                                iconColor="red"
+                                tooltipText="Remove team member"
+                            />
                         </Flex>
-                        <GTButton
-                            styleType="icon"
-                            icon={icons.trash}
-                            iconColor="red"
-                            tooltipText="Remove team member"
-                        />
-                    </Flex>
-                ))}
+                    ))
+                )}
             </TeamMemberList>
-        </>
+            <Divider />
+            <AddTeamMemberForm />
+        </Flex>
     )
 }
 
