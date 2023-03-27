34ae0066ad4e33e3f2faa50a4319216c78a655a2
scottmai <scottmai702@gmail.com>
2023-03-27 10:22:11 -0700

    lazy

--- frontend/src/components/views/LeaderboardView.tsx
+++ frontend/src/components/views/LeaderboardView.tsx
@@ -1,13 +1,12 @@
-import { Suspense } from 'react'
+import { Suspense, lazy } from 'react'
 import { Navigate } from 'react-router-dom'
 import styled from 'styled-components'
 import { useGetUserInfo } from '../../services/api/user-info.hooks'
 import { Colors, Spacing } from '../../styles'
 import Spinner from '../atoms/Spinner'
-import OnboardingSplash from '../b2b/leaderboard/OnboardingSplash'
 import { Header } from '../molecules/Header'
 
-// const OnboardingSplash = lazy(() => import('../b2b/leaderboard/OnboardingSplash'))
+const OnboardingSplash = lazy(() => import('../b2b/leaderboard/OnboardingSplash'))
 
 const FullWidthScroller = styled.div`
     padding: ${Spacing._32} ${Spacing._16} 100px;
