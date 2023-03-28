c2c29adb64072fefad2da0ff4eb2f074f40d9479
Scott Mai <scottmai702@gmail.com>
2023-03-28 13:23:40 -0700

    Add leaderboard repo selection modal (#3435)
    * repo selector
    
    * 2
    
    * grumble

---
+++ frontend/src/components/b2b/leaderboard/RepositorySelectionModal.tsx
@@ -0,0 +1,76 @@
+import styled from 'styled-components'
+import { useSetting } from '../../../hooks'
+import { useGetLeaderboardRepositories } from '../../../services/api/leaderboard.hooks'
+import { Border, Colors, Spacing } from '../../../styles'
+import { icons } from '../../../styles/images'
+import Flex from '../../atoms/Flex'
+import { Icon } from '../../atoms/Icon'
+import GTButton from '../../atoms/buttons/GTButton'
+import { BodyMedium } from '../../atoms/typography/Typography'
+import GTModal from '../../mantine/GTModal'
+
+const RepositoryList = styled.div`
+    display: flex;
+    flex-direction: column;
+    max-height: 70vh;
+    overflow-y: auto;
+    margin-bottom: ${Spacing._24};
+`
+const Repository = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: ${Spacing._12} ${Spacing._8};
+    border-radius: ${Border.radius.small};
+    :hover {
+        cursor: pointer;
+        background-color: ${Colors.background.hover};
+    }
+`
+
+interface RepositorySelectionModalProps {
+    isOpen: boolean
+    setIsOpen: (isOpen: boolean) => void
+}
+
+const RepositorySelectionModal = ({ isOpen, setIsOpen }: RepositorySelectionModalProps) => {
+    const { data: repositories } = useGetLeaderboardRepositories()
+    const { field_value: selectedRepositoryId, updateSetting: updateSelectedRepositoryId } = useSetting(
+        'leaderboard_github_repository_id'
+    )
+
+    return (
+        <GTModal
+            open={isOpen}
+            setIsModalOpen={setIsOpen}
+            tabs={{
+                title: 'Select a Repository',
+                subtitle: 'Choose which Repository to feature on the leaderboard',
+                body: (
+                    <>
+                        <RepositoryList>
+                            {repositories?.map((repository) => (
+                                <Repository
+                                    key={repository.id}
+                                    onClick={() => updateSelectedRepositoryId(repository.id)}
+                                >
+                                    <Flex alignItems="center" gap={Spacing._8}>
+                                        <Icon icon={repository.image_url} />
+                                        <BodyMedium>{repository.name}</BodyMedium>
+                                    </Flex>
+                                    {selectedRepositoryId === repository.id && <Icon icon={icons.check} />}
+                                </Repository>
+                            ))}
+                        </RepositoryList>
+                        <Flex alignItems="center" gap={Spacing._16}>
+                            <GTButton styleType="primary" value="Continue" onClick={() => setIsOpen(false)} />
+                            <GTButton styleType="secondary" value="Cancel" onClick={() => setIsOpen(false)} />
+                        </Flex>
+                    </>
+                ),
+            }}
+        />
+    )
+}
+
+export default RepositorySelectionModal
---
+++ frontend/src/services/api/leaderboard.hooks.ts
@@ -0,0 +1,16 @@
+import { useQuery } from 'react-query'
+import { logos } from '../../styles/images'
+import { TLeaderboardRepository } from '../../utils/types'
+
+export const useGetLeaderboardRepositories = () => {
+    return useQuery<TLeaderboardRepository[]>('leaderboard-repositories', getLeaderboardRepositories)
+}
+
+const getLeaderboardRepositories = async () => {
+    // returns dummy data until backend is ready
+    return Array.from({ length: 20 }).map((_, i) => ({
+        id: i.toString(),
+        name: `Repository ${i}`,
+        image_url: logos.generaltask_yellow_circle,
+    }))
+}
